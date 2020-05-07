409ccf4d8b35b749788f8be01109b281715d8cca
Quentin Lhoest <lhoest.q@gmail.com>
2020-05-07 10:35:57 +0200

    fix wikipedia url

--- datasets/nlp/wikipedia/wikipedia.py
+++ datasets/nlp/wikipedia/wikipedia.py
@@ -363,7 +363,7 @@ WIKIPEDIA_LANGUAGES = [
     "zu",
 ]
 
-_BASE_URL_TMPL = "https://dumps.wikimedia.your.org/{lang}wiki/{date}/"
+_BASE_URL_TMPL = "https://dumps.wikimedia.org/{lang}wiki/{date}/"
 _INFO_FILE = "dumpstatus.json"
 
 
