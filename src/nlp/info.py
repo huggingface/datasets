cef01d9756cdf5ba45ccda15c149e0f84c987154
Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>
2020-05-13 09:52:32 +0200

    Add nbytes + nexamples check (#80)
    * add num_bytes and num_examples check
    
    * make style
    
    * fix tests
    
    * prefill datasetinfo
    
    * make style
    
    * fix merge
    
    Co-authored-by: Thomas Wolf <thomwolf@users.noreply.github.com>

--- src/nlp/info.py
+++ src/nlp/info.py
@@ -33,10 +33,19 @@ import json
 import logging
 import os
 from dataclasses import asdict, dataclass, field
-from typing import List, Optional
+from typing import List, Optional, Union
+
+from nlp.utils.checksums_utils import (
+    CACHED_SIZES_FILE_NAME,
+    CHECKSUMS_FILE_NAME,
+    load_cached_sizes,
+    load_sizes_checksums,
+    store_cached_sizes,
+)
+from nlp.utils.version import Version
 
 from .features import Features, Value
-from .splits import SplitDict, check_splits_equals
+from .splits import SplitDict
 
 
 logger = logging.getLogger(__name__)
@@ -57,6 +66,14 @@ class SupervisedKeysData:
 class DownloadChecksumsEntryData:
     key: str = ""
     value: str = ""
+
+
+class MissingCachedSizesConfigError(Exception):
+    """The expected cached sizes of the download file are missing."""
+
+
+class NonMatchingCachedSizesError(Exception):
+    """The prepared split doesn't have expected sizes."""
 
 
 @dataclass
@@ -80,16 +97,22 @@ class DatasetInfo:
     # Set later by the builder
     builder_name: Optional[str] = None
     config_name: Optional[str] = None
-    version: Optional[str] = None
+    version: Optional[Union[str, Version]] = None
     splits: Optional[dict] = field(default_factory=SplitDict)
     size_in_bytes: int = 0
     download_size: int = 0
-    download_checksums: List[DownloadChecksumsEntryData] = field(default_factory=list)
+    dataset_size: int = 0
+    download_checksums: dict = field(default_factory=dict)
 
     def __post_init__(self):
         # Convert back to the correct classes when we reload from dict
         if self.features is not None and not isinstance(self.features, Features):
             self.features = Features.from_dict(self.features)
+        if self.version is not None and not isinstance(self.version, Version):
+            if isinstance(self.version, str):
+                self.version = Version(self.version)
+            else:
+                self.version = Version.from_dict(self.version)
         if self.splits is not None and not isinstance(self.splits, SplitDict):
             self.splits = SplitDict.from_split_dict(self.splits)
         if self.supervised_keys is not None and not isinstance(self.supervised_keys, SupervisedKeysData):
@@ -97,38 +120,6 @@ class DatasetInfo:
                 self.supervised_keys = SupervisedKeysData(*self.supervised_keys)
             else:
                 self.supervised_keys = SupervisedKeysData(**self.supervised_keys)
-        if self.download_checksums and not isinstance(self.download_checksums[0], DownloadChecksumsEntryData):
-            if isinstance(self.download_checksums[0], (tuple, list)):
-                self.download_checksums = [DownloadChecksumsEntryData(*args) for args in self.download_checksums]
-            else:
-                self.download_checksums = [DownloadChecksumsEntryData(**args) for args in self.download_checksums]
-
-    @property
-    def dataset_size(self):
-        """Generated dataset files size, in bytes."""
-        # For old datasets, maybe empty.
-        return sum(split.num_bytes for split in self.splits.values())
-
-    def update_splits_if_different(self, split_dict):
-        """Overwrite the splits if they are different from the current ones.
-
-        * If splits aren't already defined or different (ex: different number of
-            shards), then the new split dict is used. This will trigger stats
-            computation during download_and_prepare.
-        * If splits are already defined in DatasetInfo and similar (same names and
-            shards): keep the restored split which contains the statistics (restored
-            from GCS or file)
-
-        Args:
-            split_dict: `nlp.SplitDict`, the new split
-        """
-        assert isinstance(split_dict, SplitDict)
-
-        # If splits are already defined and identical, then we do not update
-        if self.splits and check_splits_equals(self.splits, split_dict):
-            return
-
-        self.splits = split_dict.copy()
 
     def _license_path(self, dataset_info_dir):
         return os.path.join(dataset_info_dir, LICENSE_FILENAME)
@@ -163,6 +154,45 @@ class DatasetInfo:
         with open(os.path.join(dataset_info_dir, DATASET_INFO_FILENAME), "r") as f:
             dataset_info_dict = json.load(f)
         return cls(**dataset_info_dict)
+
+    def prefill_dataset_size_attributes_from_urls_checksums_dir(self, urls_checksums_dir: str):
+        """Store upper bounds of dataset size if available"""
+        checksums_file_path = os.path.join(urls_checksums_dir, CHECKSUMS_FILE_NAME)
+        cached_sizes_file_path = os.path.join(urls_checksums_dir, CACHED_SIZES_FILE_NAME)
+        if os.path.exists(checksums_file_path):
+            self.download_checksums = load_sizes_checksums(checksums_file_path)
+        if os.path.exists(cached_sizes_file_path):
+            cached_sizes = load_cached_sizes(cached_sizes_file_path)
+            self.dataset_size = sum(size_nexamples[0] for size_nexamples in cached_sizes.values())
+
+    def cached_sizes(self):
+        return {
+            "/".join([str(self.builder_name), str(self.config_name), str(self.version), split_name]): (
+                split_info.num_bytes,
+                split_info.num_examples,
+            )
+            for split_name, split_info in self.splits.items()
+        }
+
+    def check_or_save_cached_sizes(self, urls_checksums_dir: str, ignore_cached_sizes: bool, save_cached_sizes):
+        if not ignore_cached_sizes:
+            cached_sizes_file_path = os.path.join(urls_checksums_dir, CACHED_SIZES_FILE_NAME)
+            if save_cached_sizes:
+                store_cached_sizes(self.cached_sizes(), cached_sizes_file_path)
+                logger.info("Stored the recorded cached sizes in {}.".format(urls_checksums_dir))
+            elif os.path.exists(cached_sizes_file_path):
+                expected_cached_sizes = load_cached_sizes(cached_sizes_file_path)
+                for full_config_name, rec_size_numexamples in self.cached_sizes().items():
+                    exp_size_numexamples = expected_cached_sizes.get(full_config_name)
+                    if exp_size_numexamples is None:
+                        raise MissingCachedSizesConfigError(full_config_name)
+                    if exp_size_numexamples != rec_size_numexamples:
+                        raise NonMatchingCachedSizesError(full_config_name)
+                logger.info("All cached sizes matched successfully.")
+            else:
+                logger.info("Cached sizes file not found.")
+        else:
+            logger.info("Cached sizes tests were ignored.")
 
 
 @dataclass
