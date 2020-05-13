e941eae67f58b1e3255227fb8624d27156372b88
Thomas Wolf <thomwolf@users.noreply.github.com>
2020-05-13 22:38:09 +0200

    update to 0.0.3

--- src/nlp/__init__.py
+++ src/nlp/__init__.py
@@ -18,7 +18,7 @@
 # pylint: enable=line-too-long
 # pylint: disable=g-import-not-at-top,g-bad-import-order,wrong-import-position
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 from pyarrow import total_allocated_bytes
 
