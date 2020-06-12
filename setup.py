8d7db6cad4f0a42165cf9b1c6124d024e01571a9
Quentin Lhoest <lhoest.q@gmail.com>
2020-06-12 17:57:38 +0200

    release 0.2.1

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ EXTRAS_REQUIRE = {
 
 setup(
     name='nlp',
-    version="0.2.0",
+    version="0.2.1",
     description=DOCLINES[0],
     long_description='\n'.join(DOCLINES[2:]),
     author='HuggingFace Inc.',
