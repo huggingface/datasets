a7125fb4c03df4a6907353c15eed53aaed42bcaf
Thomas Wolf <thomwolf@users.noreply.github.com>
2020-06-18 18:23:23 +0200

    Add sort, shuffle, test_train_split and select methods (#266)
    * Add a sorting method
    
    * fix sort + style
    
    * index, sort, shuffle, train_test_split
    
    * renaming index in select, finishing train_test_split
    
    * Adding tests, fixing cache_files
    
    * style quality and a few more tests
    
    * sort reverse + docs + remove unused var
    
    * default test size
    
    Co-authored-by: Quentin Lhoest <lhoest.q@gmail.com>

--- tests/test_arrow_dataset.py
+++ tests/test_arrow_dataset.py
@@ -112,3 +112,121 @@ class BaseDatasetTest(TestCase):
             tmp_file = os.path.join(tmp_dir, "test.arrow")
             dset_filter_even_num = dset.filter(lambda x: (int(x["filename"][-1]) % 2 == 0), cache_file_name=tmp_file)
             self.assertEqual(len(dset_filter_even_num), 15)
+
+    def test_select(self):
+        dset = self._create_dummy_dataset()
+        # select every two example
+        indices = list(range(0, len(dset), 2))
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            tmp_file = os.path.join(tmp_dir, "test.arrow")
+            dset_select_even = dset.select(indices, cache_file_name=tmp_file)
+            self.assertEqual(len(dset_select_even), 15)
+            for row in dset_select_even:
+                self.assertEqual(int(row["filename"][-1]) % 2, 0)
+
+    def test_shuffle(self):
+        dset = self._create_dummy_dataset()
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            tmp_file = os.path.join(tmp_dir, "test.arrow")
+            dset_shuffled = dset.shuffle(seed=1234, cache_file_name=tmp_file)
+            self.assertEqual(len(dset_shuffled), 30)
+            self.assertEqual(dset_shuffled[0]["filename"], "my_name-train_28")
+            self.assertEqual(dset_shuffled[2]["filename"], "my_name-train_10")
+
+            # Reproductibility
+            tmp_file = os.path.join(tmp_dir, "test_2.arrow")
+            dset_shuffled_2 = dset.shuffle(seed=1234, cache_file_name=tmp_file)
+            self.assertListEqual(dset_shuffled["filename"], dset_shuffled_2["filename"])
+
+    def test_sort(self):
+        dset = self._create_dummy_dataset()
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            # Keep only 10 examples
+            tmp_file = os.path.join(tmp_dir, "test.arrow")
+            dset = dset.select(range(10), cache_file_name=tmp_file)
+            tmp_file = os.path.join(tmp_dir, "test_2.arrow")
+            dset = dset.shuffle(seed=1234, cache_file_name=tmp_file)
+            self.assertEqual(len(dset), 10)
+            self.assertEqual(dset[0]["filename"], "my_name-train_8")
+            self.assertEqual(dset[1]["filename"], "my_name-train_9")
+            # Sort
+            tmp_file = os.path.join(tmp_dir, "test_3.arrow")
+            dset_sorted = dset.sort("filename", cache_file_name=tmp_file)
+            for i, row in enumerate(dset_sorted):
+                self.assertEqual(int(row["filename"][-1]), i)
+            # Sort reversed
+            tmp_file = os.path.join(tmp_dir, "test_4.arrow")
+            dset_sorted = dset.sort("filename", cache_file_name=tmp_file, reverse=True)
+            for i, row in enumerate(dset_sorted):
+                self.assertEqual(int(row["filename"][-1]), len(dset_sorted) - 1 - i)
+
+    def test_train_test_split(self):
+        dset = self._create_dummy_dataset()
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            tmp_file = os.path.join(tmp_dir, "test.arrow")
+            tmp_file_2 = os.path.join(tmp_dir, "test_2.arrow")
+            dset_dict = dset.train_test_split(
+                test_size=10, shuffle=False, train_cache_file_name=tmp_file, test_cache_file_name=tmp_file_2
+            )
+            self.assertListEqual(list(dset_dict.keys()), ["train", "test"])
+            dset_train = dset_dict["train"]
+            dset_test = dset_dict["test"]
+
+            self.assertEqual(len(dset_train), 20)
+            self.assertEqual(len(dset_test), 10)
+            self.assertEqual(dset_train[0]["filename"], "my_name-train_0")
+            self.assertEqual(dset_train[-1]["filename"], "my_name-train_19")
+            self.assertEqual(dset_test[0]["filename"], "my_name-train_20")
+            self.assertEqual(dset_test[-1]["filename"], "my_name-train_29")
+
+            tmp_file = os.path.join(tmp_dir, "test_3.arrow")
+            tmp_file_2 = os.path.join(tmp_dir, "test_4.arrow")
+            dset_dict = dset.train_test_split(
+                test_size=0.5, shuffle=False, train_cache_file_name=tmp_file, test_cache_file_name=tmp_file_2
+            )
+            self.assertListEqual(list(dset_dict.keys()), ["train", "test"])
+            dset_train = dset_dict["train"]
+            dset_test = dset_dict["test"]
+
+            self.assertEqual(len(dset_train), 15)
+            self.assertEqual(len(dset_test), 15)
+            self.assertEqual(dset_train[0]["filename"], "my_name-train_0")
+            self.assertEqual(dset_train[-1]["filename"], "my_name-train_14")
+            self.assertEqual(dset_test[0]["filename"], "my_name-train_15")
+            self.assertEqual(dset_test[-1]["filename"], "my_name-train_29")
+
+            tmp_file = os.path.join(tmp_dir, "test_5.arrow")
+            tmp_file_2 = os.path.join(tmp_dir, "test_6.arrow")
+            dset_dict = dset.train_test_split(
+                train_size=10, shuffle=False, train_cache_file_name=tmp_file, test_cache_file_name=tmp_file_2
+            )
+            self.assertListEqual(list(dset_dict.keys()), ["train", "test"])
+            dset_train = dset_dict["train"]
+            dset_test = dset_dict["test"]
+
+            self.assertEqual(len(dset_train), 10)
+            self.assertEqual(len(dset_test), 20)
+            self.assertEqual(dset_train[0]["filename"], "my_name-train_0")
+            self.assertEqual(dset_train[-1]["filename"], "my_name-train_9")
+            self.assertEqual(dset_test[0]["filename"], "my_name-train_10")
+            self.assertEqual(dset_test[-1]["filename"], "my_name-train_29")
+
+            tmp_file = os.path.join(tmp_dir, "test_7.arrow")
+            tmp_file_2 = os.path.join(tmp_dir, "test_8.arrow")
+            dset_dict = dset.train_test_split(
+                train_size=10, train_cache_file_name=tmp_file, test_cache_file_name=tmp_file_2
+            )
+            self.assertListEqual(list(dset_dict.keys()), ["train", "test"])
+            dset_train = dset_dict["train"]
+            dset_test = dset_dict["test"]
+
+            self.assertEqual(len(dset_train), 10)
+            self.assertEqual(len(dset_test), 20)
+            self.assertNotEqual(dset_train[0]["filename"], "my_name-train_0")
+            self.assertNotEqual(dset_train[-1]["filename"], "my_name-train_9")
+            self.assertNotEqual(dset_test[0]["filename"], "my_name-train_10")
+            self.assertNotEqual(dset_test[-1]["filename"], "my_name-train_29")
