56c1a25a598fe4972e3d37397e3d19153790cfe9
Mariama Drame <mariama@debmower_ajd>
2020-06-22 09:32:07 +0200

    update cmrc2018 to fix consistant citation

--- datasets/cmrc2018/cmrc2018.py
+++ datasets/cmrc2018/cmrc2018.py
@@ -19,7 +19,7 @@ _CITATION = """\
       Chen, Zhipeng  and
       Ma, Wentao  and
       Wang, Shijin  and
-      Hu, Guoping },
+      Hu, Guoping},
     booktitle = {Proceedings of the 2019 Conference on Empirical Methods in Natural Language Processing and the 9th International Joint Conference on Natural Language Processing (EMNLP-IJCNLP)},
     month = {nov},
     year = {2019},
@@ -27,8 +27,7 @@ _CITATION = """\
     publisher = {Association for Computational Linguistics},
     url = {https://www.aclweb.org/anthology/D19-1600},
     doi = {10.18653/v1/D19-1600},
-    pages = {5886--5891},
-}
+    pages = {5886--5891}}
 """
 
 # TODO(cmrc2018):
