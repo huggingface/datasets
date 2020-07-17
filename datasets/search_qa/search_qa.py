5a092ad20f2a7d1b0ed2e9743803d81108dc1b64
Quentin Lhoest <lhoest.q@gmail.com>
2020-07-17 11:08:03 +0200

    style

--- datasets/search_qa/search_qa.py
+++ datasets/search_qa/search_qa.py
@@ -24,6 +24,7 @@ import os
 
 import nlp
 
+
 _CITATION = """
     @article{DBLP:journals/corr/DunnSHGCC17,
     author    = {Matthew Dunn and
@@ -58,12 +59,11 @@ Following this approach, we built SearchQA, which consists of more than 140k que
 """
 
 _DL_URLS = {
-    
     "raw_jeopardy": "https://drive.google.com/uc?export=download&id=1U7WdBpd9kJ85S7BbBhWUSiy9NnXrKdO6",
     "train_test_val": "https://drive.google.com/uc?export=download&id=1aHPVfC5TrlnUjehtagVZoDfq4VccgaNT",
-    
 }
 # pylint: enable=line-too-long
+
 
 class SearchQaConfig(nlp.BuilderConfig):
     """BuilderConfig for SearchQA."""
@@ -74,24 +74,20 @@ class SearchQaConfig(nlp.BuilderConfig):
         Args:
           **kwargs: keyword arguments forwarded to super.
         """
-        super(SearchQaConfig, self).__init__(version=nlp.Version("1.0.0", "New split API (https://tensorflow.org/datasets/splits)"), **kwargs)
+        super(SearchQaConfig, self).__init__(
+            version=nlp.Version("1.0.0", "New split API (https://tensorflow.org/datasets/splits)"), **kwargs
+        )
         self.data_url = data_url
+
 
 class SearchQa(nlp.GeneratorBasedBuilder):
     """Search QA Dataset."""
-    
-    BUILDER_CONFIGS = [
-        SearchQaConfig(
-            name= name,
-            description='',
-            data_url = _DL_URLS[name]
-            
-        ) for name in _DL_URLS.keys()
-    ]
-    
+
+    BUILDER_CONFIGS = [SearchQaConfig(name=name, description="", data_url=_DL_URLS[name]) for name in _DL_URLS.keys()]
+
     def _info(self):
         return nlp.DatasetInfo(
-            description=_DESCRIPTION + '\n' + self.config.description,
+            description=_DESCRIPTION + "\n" + self.config.description,
             features=nlp.Features(
                 {
                     "category": nlp.Value("string"),
@@ -102,107 +98,91 @@ class SearchQa(nlp.GeneratorBasedBuilder):
                     "round": nlp.Value("string"),
                     "category": nlp.Value("string"),
                     "show_number": nlp.Value("int32"),
-                    "search_results": nlp.features.Sequence({
-                        'urls': nlp.Value('string'),
-                        "snippets": nlp.Value('string'),
-                        "titles": nlp.Value('string'),
-                        "related_links": nlp.Value('string')
-                    })
+                    "search_results": nlp.features.Sequence(
+                        {
+                            "urls": nlp.Value("string"),
+                            "snippets": nlp.Value("string"),
+                            "titles": nlp.Value("string"),
+                            "related_links": nlp.Value("string"),
+                        }
+                    )
                     # These are the features of your dataset like images, labels ...
                 }
             ),
             homepage="https://github.com/nyu-dl/dl4ir-searchQA",
             citation=_CITATION,
         )
-    
+
     def _split_generators(self, dl_manager):
         """Returns SplitGenerators."""
         # TODO(jeopardy): Downloads the data and defines the splits
         # dl_manager is a nlp.download.DownloadManager that can be used to
         # download and extract URLs
-        
-        if self.config.name == 'raw_jeopardy':
-            filepath = dl_manager.download_and_extract(_DL_URLS['raw_jeopardy'])
-            sub_folders = sorted(os.listdir(os.path.join(filepath, 'jeopardy')))
-            all_files = []     
+
+        if self.config.name == "raw_jeopardy":
+            filepath = dl_manager.download_and_extract(_DL_URLS["raw_jeopardy"])
+            sub_folders = sorted(os.listdir(os.path.join(filepath, "jeopardy")))
+            all_files = []
             for zip_folder in sub_folders:
-                if 'lock' in zip_folder:
+                if "lock" in zip_folder:
                     continue
-                zip_folder_path = os.path.join(filepath, 'jeopardy', zip_folder)
+                zip_folder_path = os.path.join(filepath, "jeopardy", zip_folder)
                 file_path = dl_manager.extract(zip_folder_path)
-                zip_folder = zip_folder.split('.')[0]
+                zip_folder = zip_folder.split(".")[0]
                 if os.path.isdir(os.path.join(file_path, zip_folder)):
                     file_path = os.path.join(file_path, zip_folder)
-                    
+
                 else:
-                    #in some cases the subfolder name contains sapces as 050000 - 059999   and 050000-059999
-                    parts = zip_folder.split('-')
-                    zip_folder =parts[0]+' - '+parts[1]
+                    # in some cases the subfolder name contains sapces as 050000 - 059999   and 050000-059999
+                    parts = zip_folder.split("-")
+                    zip_folder = parts[0] + " - " + parts[1]
                     if os.path.isdir(os.path.join(file_path, zip_folder)):
                         file_path = os.path.join(file_path, zip_folder)
-                           
+
                 files = sorted(os.listdir(file_path))
-                
-                files_paths = [os.path.join(file_path, file) for file in files if '__MACOSX' not in file]
-                all_files.extend(files_paths) 
-            
+
+                files_paths = [os.path.join(file_path, file) for file in files if "__MACOSX" not in file]
+                all_files.extend(files_paths)
+
             return [
-                nlp.SplitGenerator(
-                    name=nlp.Split.TRAIN, 
-                    gen_kwargs={
-                        "filepaths": all_files
-                        }
-                    ),
+                nlp.SplitGenerator(name=nlp.Split.TRAIN, gen_kwargs={"filepaths": all_files}),
             ]
-        elif self.config.name == 'train_test_val':
-            filepath = dl_manager.download_and_extract(_DL_URLS['train_test_val'])
-            train_path = dl_manager.extract(os.path.join(filepath, 'data_json', 'train.zip'))
-            test_path = dl_manager.extract(os.path.join(filepath, 'data_json', 'test.zip'))
-            val_path = dl_manager.extract(os.path.join(filepath, 'data_json', 'val.zip'))
-            
+        elif self.config.name == "train_test_val":
+            filepath = dl_manager.download_and_extract(_DL_URLS["train_test_val"])
+            train_path = dl_manager.extract(os.path.join(filepath, "data_json", "train.zip"))
+            test_path = dl_manager.extract(os.path.join(filepath, "data_json", "test.zip"))
+            val_path = dl_manager.extract(os.path.join(filepath, "data_json", "val.zip"))
+
             train_files = [os.path.join(train_path, file) for file in sorted(os.listdir(train_path))]
             test_files = [os.path.join(test_path, file) for file in sorted(os.listdir(test_path))]
             val_files = [os.path.join(val_path, file) for file in sorted(os.listdir(val_path))]
             return [
-                nlp.SplitGenerator(
-                    name=nlp.Split.TRAIN, 
-                    gen_kwargs={
-                        "filepaths": train_files
-                        }
-                    ),
-                nlp.SplitGenerator(
-                    name=nlp.Split.TEST, 
-                    gen_kwargs={
-                        "filepaths": test_files
-                        }
-                    ),
-                nlp.SplitGenerator(
-                    name=nlp.Split.VALIDATION, 
-                    gen_kwargs={
-                        "filepaths": val_files
-                        }
-                    )
+                nlp.SplitGenerator(name=nlp.Split.TRAIN, gen_kwargs={"filepaths": train_files}),
+                nlp.SplitGenerator(name=nlp.Split.TEST, gen_kwargs={"filepaths": test_files}),
+                nlp.SplitGenerator(name=nlp.Split.VALIDATION, gen_kwargs={"filepaths": val_files}),
             ]
-            
+
     def _generate_examples(self, filepaths):
         """Yields examples."""
         # TODO(searchQa): Yields (key, example) tuples from the dataset
         for i, filepath in enumerate(filepaths):
             with open(filepath) as f:
-                
-                data = json.load(f)   
+
+                data = json.load(f)
                 category = data["category"]
                 air_date = data["air_date"]
                 question = data["question"]
-                value = data['value']
+                value = data["value"]
                 answer = data["answer"]
                 round_ = data["round"]
                 show_number = int(data["show_number"])
-                search_results = data['search_results']
-                urls = [result['url'] for result in search_results]
-                snippets = [result['snippet'] for result in search_results]
-                titles = [result['title'] for result in search_results]
-                related_links = [result['related_links'] if result['related_links'] else '' for result in search_results]
+                search_results = data["search_results"]
+                urls = [result["url"] for result in search_results]
+                snippets = [result["snippet"] for result in search_results]
+                titles = [result["title"] for result in search_results]
+                related_links = [
+                    result["related_links"] if result["related_links"] else "" for result in search_results
+                ]
                 yield i, {
                     "category": category,
                     "air_date": air_date,
@@ -213,9 +193,9 @@ class SearchQa(nlp.GeneratorBasedBuilder):
                     "category": category,
                     "show_number": show_number,
                     "search_results": {
-                        'urls': urls,
+                        "urls": urls,
                         "snippets": snippets,
                         "titles": titles,
-                        "related_links": related_links
-                    }
+                        "related_links": related_links,
+                    },
                 }
