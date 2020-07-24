55f6b41b47a6c1ad86be55332973504026c4d9d4
Quentin Lhoest <lhoest.q@gmail.com>
2020-07-24 11:29:25 +0200

    quality

--- tests/test_hf_gcp.py
+++ tests/test_hf_gcp.py
@@ -41,7 +41,7 @@ def list_datasets_on_hf_gcp_parameters(with_config=True):
         ]
     else:
         return [
-            {"testcase_name": dataset, "dataset": dataset,}
+            {"testcase_name": dataset, "dataset": dataset}
             for dataset in set(d["dataset"] for d in DATASETS_ON_HF_GCP)
         ]
 
