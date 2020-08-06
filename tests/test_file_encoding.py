7129f4e4383d86802ed712e88de57a95fb5dc001
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2020-08-06 23:26:01 +0200

    Extend regexp to exclude CSV reader and add docstring to helper function

--- tests/test_file_encoding.py
+++ tests/test_file_encoding.py
@@ -3,10 +3,21 @@ from pathlib import Path
 
 
 def no_encoding_on_file_open(filepath: str):
-    """Find all instances where a non-binary file is opened without UTF-8 encoding."""
+    r"""Find all instances where a non-binary file is opened without UTF-8 encoding.
+
+    Regex explanation
+
+    (?!.*\b(?:encoding|rb|w|wb|w+|wb+|ab|ab+)\b): Lookahead and discard match if `encoding` or `rb` etc are args of open(...)
+
+    (?<!\.): Lookbehind and discard match if open(...) preceded by `.`. Used to exclude gzip.open(...)
+
+    (?<!csv.reader\(): Lookahead and discard match if open(...) preceded by `csv.reader(`
+
+    (open)\((.*)\): Capture everything in braces of open(...)
+    """
 
     with open(filepath, "r", encoding="utf-8") as input_file:
-        regexp = re.compile(r"""(?!.*\b(?:encoding|rb|w|wb|w+|wb+|ab|ab+)\b)(?<!\.)(open)\((.*)\)""")
+        regexp = re.compile(r"(?!.*\b(?:encoding|rb|w|wb|w+|wb+|ab|ab+)\b)(?<!\.)(?<!csv.reader\()(open)\((.*)\)")
         input_text = input_file.read()
         match = regexp.search(input_text)
 
@@ -19,4 +30,4 @@ def test_encoding_on_file_open():
 
     for dataset in dataset_files:
         if no_encoding_on_file_open(str(dataset)):
-            raise ValueError(f"UTF-8 encoding missing in dataset {dataset}")
+            raise ValueError(f"open(...) must use utf-8 encoding in {dataset}")
