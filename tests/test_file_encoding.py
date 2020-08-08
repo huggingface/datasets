95d69e1ea97a48e62c1394106b19d138837893a7
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2020-08-08 14:31:28 +0200

    Improve docstring in test function

--- tests/test_file_encoding.py
+++ tests/test_file_encoding.py
@@ -2,18 +2,20 @@ import re
 from pathlib import Path
 
 
-def no_encoding_on_file_open(filepath: str):
+def _no_encoding_on_file_open(filepath: str):
     r"""Find all instances where a non-binary file is opened without UTF-8 encoding.
 
-    Regex explanation
+    This function uses regular expressions to find instances where Python's ``open()`` function is used to open
+    non-binary files. See below for an explanation of the regular expression:
 
-    (?!.*\b(?:encoding|rb|w|wb|w+|wb+|ab|ab+)\b): Lookahead and discard match if `encoding` or `rb` etc are args of open(...)
+    (?!.*\b(?:encoding|rb|w|wb|w+|wb+|ab|ab+)\b): Lookahead and discard match if ``encoding`` or ``rb`` etc are
+    arguments of ``open()``.
 
-    (?<!\.): Lookbehind and discard match if open(...) preceded by `.`. Used to exclude gzip.open(...)
+    (?<!\.): Lookbehind and discard match if ``open()`` preceded by ``.``. Used to exclude ``gzip.open()``
 
-    (?<!csv.reader\(): Lookahead and discard match if open(...) preceded by `csv.reader(`
+    (?<!csv.reader\(): Lookahead and discard match if ``open()`` preceded by ``csv.reader(``
 
-    (open)\((.*)\): Capture everything in braces of open(...)
+    (open)\((.*)\): Capture everything in braces of ``open()``
     """
 
     with open(filepath, "r", encoding="utf-8") as input_file:
@@ -29,5 +31,5 @@ def test_no_encoding_on_file_open():
     dataset_files = list(dataset_paths.absolute().glob("**/*.py"))
 
     for dataset in dataset_files:
-        if no_encoding_on_file_open(str(dataset)):
+        if _no_encoding_on_file_open(str(dataset)):
             raise ValueError(f"open(...) must use utf-8 encoding in {dataset}")
