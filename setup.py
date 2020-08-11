444dd64b4f0dfde9531ef481fe70d31e25c05bb1
Quentin Lhoest <lhoest.q@gmail.com>
2020-08-11 12:05:06 +0200

    pin sphinx version

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ EXTRAS_REQUIRE = {
     'dev': TESTS_REQUIRE + QUALITY_REQUIRE,
     'tests': TESTS_REQUIRE,
     'quality': QUALITY_REQUIRE,
-    'docs': ["recommonmark", "sphinx", "sphinx-markdown-tables", "sphinx-rtd-theme==0.4.3", "sphinx-copybutton"]
+    'docs': ["recommonmark", "sphinx>=3.1.2", "sphinx-markdown-tables", "sphinx-rtd-theme==0.4.3", "sphinx-copybutton"]
 }
 
 setup(
