2e9c799f075898c337ee6b4226dfbd56ef43b520
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2020-08-12 21:39:56 +0200

    :lipstick: Remove double backticks from doctring

--- tests/test_file_encoding.py
+++ tests/test_file_encoding.py
@@ -5,17 +5,17 @@ from pathlib import Path
 def _no_encoding_on_file_open(filepath: str):
     r"""Find all instances where a non-binary file is opened without UTF-8 encoding.
 
-    This function uses regular expressions to find instances where Python's ``open()`` function is used to open
+    This function uses regular expressions to find instances where Python's `open()` function is used to open
     non-binary files. See below for an explanation of the regular expression:
 
-    (?!.*\b(?:encoding|rb|w|wb|w+|wb+|ab|ab+)\b): Lookahead and discard match if ``encoding`` or ``rb`` etc are
-    arguments of ``open()``.
+    (?!.*\b(?:encoding|rb|w|wb|w+|wb+|ab|ab+)\b): Lookahead and discard match if `encoding` or `rb` etc are
+    arguments of `open()`.
 
-    (?<!\.): Lookbehind and discard match if ``open()`` preceded by ``.``. Used to exclude ``gzip.open()``
+    (?<!\.): Lookbehind and discard match if `open()` preceded by `.`. Used to exclude `gzip.open()`
 
-    (?<!csv.reader\(): Lookahead and discard match if ``open()`` preceded by ``csv.reader(``
+    (?<!csv.reader\(): Lookahead and discard match if `open()` preceded by `csv.reader(`
 
-    (open)\((.*)\): Capture everything in braces of ``open()``
+    (open)\((.*)\): Capture everything in braces of `open()`
     """
 
     with open(filepath, "r", encoding="utf-8") as input_file:
