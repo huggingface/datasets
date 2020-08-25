2b9b639efad15e865dd44fa3219e60e4e79062c4
Quentin Lhoest <lhoest.q@gmail.com>
2020-08-25 17:33:33 +0200

    typo

--- tests/test_array_xd.py
+++ tests/test_array_xd.py
@@ -50,7 +50,7 @@ def generate_examples(features: dict, num_examples=100, seq_shapes=None):
                 while isinstance(v, nlp.Sequence):
                     depth += 1
                     v = v.feature
-                shape = seq_shapes[k][:]
+                shape = seq_shapes[k][:depth]
                 data = np.random.rand(*shape).astype(v.dtype)
             example[k] = data
             dummy_data.append((i, example))
