9fdb4eda2e2615cd1ca52d9a84f619a1b6a04cff
Nicolas Patry <patry.nicolas@protonmail.com>
2020-09-01 22:19:21 +0200

    Make style with `pip install .[quality]`

--- src/nlp/utils/logging.py
+++ src/nlp/utils/logging.py
@@ -16,6 +16,9 @@
 
 import logging
 import threading
+from typing import Optional
+
+
 from logging import CRITICAL  # NOQA
 from logging import DEBUG  # NOQA
 from logging import ERROR  # NOQA
@@ -24,7 +27,6 @@ from logging import INFO  # NOQA
 from logging import NOTSET  # NOQA
 from logging import WARN  # NOQA
 from logging import WARNING  # NOQA
-from typing import Optional
 
 
 _lock = threading.Lock()
