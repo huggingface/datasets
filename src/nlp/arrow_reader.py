034c1c00cbf750766b0cb8825fb107a0d911057d
Quentin Lhoest <lhoest.q@gmail.com>
2020-09-07 17:59:30 +0200

    allow writer to write empty tables

--- src/nlp/arrow_reader.py
+++ src/nlp/arrow_reader.py
@@ -288,7 +288,8 @@ class ArrowReader(BaseReader):
         mmap = pa.memory_map(filename)
         f = pa.ipc.open_stream(mmap)
         pa_table = f.read_all()
-        if skip is not None and take is not None:
+        # here we don't want to slice an empty table, or it may segfault
+        if skip is not None and take is not None and not (skip == 0 and take == len(pa_table)):
             pa_table = pa_table.slice(skip, take)
         return pa_table
 
@@ -317,7 +318,8 @@ class ParquetReader(BaseReader):
             filename_skip_take["take"] if "take" in filename_skip_take else None,
         )
         pa_table = pa.parquet.read_table(filename, memory_map=True)
-        if skip is not None and take is not None:
+        # here we don't want to slice an empty table, or it may segfault
+        if skip is not None and take is not None and not (skip == 0 and take == len(pa_table)):
             pa_table = pa_table.slice(skip, take)
         return pa_table
 
