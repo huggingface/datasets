be41801f63504f1f45d334042d214687f387643d
Vladimir <dovlex@gmail.com>
2020-09-11 06:25:57 -0400

    Fix CI failures

--- datasets/conll2003/conll2003.py
+++ datasets/conll2003/conll2003.py
@@ -34,15 +34,15 @@ _CITATION = """\
 """
 
 _DESCRIPTION = """\
-The shared task of CoNLL-2003 concerns language-independent named entity recognition. We will concentrate on 
-four types of named entities: persons, locations, organizations and names of miscellaneous entities that do 
-not belong to the previous three groups. 
+The shared task of CoNLL-2003 concerns language-independent named entity recognition. We will concentrate on
+four types of named entities: persons, locations, organizations and names of miscellaneous entities that do
+not belong to the previous three groups.
 
-The CoNLL-2003 shared task data files contain four columns separated by a single space. Each word has been put on 
-a separate line and there is an empty line after each sentence. The first item on each line is a word, the second 
-a part-of-speech (POS) tag, the third a syntactic chunk tag and the fourth the named entity tag. The chunk tags 
-and the named entity tags have the format I-TYPE which means that the word is inside a phrase of type TYPE. Only 
-if two phrases of the same type immediately follow each other, the first word of the second phrase will have tag 
+The CoNLL-2003 shared task data files contain four columns separated by a single space. Each word has been put on
+a separate line and there is an empty line after each sentence. The first item on each line is a word, the second
+a part-of-speech (POS) tag, the third a syntactic chunk tag and the fourth the named entity tag. The chunk tags
+and the named entity tags have the format I-TYPE which means that the word is inside a phrase of type TYPE. Only
+if two phrases of the same type immediately follow each other, the first word of the second phrase will have tag
 B-TYPE to show that it starts a new phrase. A word with tag O is not part of a phrase.
 
 For more details see https://www.clips.uantwerpen.be/conll2003/ner/ and https://www.aclweb.org/anthology/W03-0419
