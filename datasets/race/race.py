f6a28db66bc33bbe73e5fd6deed747df5f48d158
Andre Barbosa <andre.barbosa@quintoandar.com.br>
2020-09-14 15:48:14 -0300

    fix checksum and uses attributes for different scenarios

--- datasets/race/race.py
+++ datasets/race/race.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, division, print_function
 
 import json
 import os
+from pathlib import Path
 
 import datasets
 
@@ -30,10 +31,19 @@ _URL = "http://www.cs.cmu.edu/~glai1/data/race/RACE.tar.gz"
 class Race(datasets.GeneratorBasedBuilder):
     """ReAding Comprehension Dataset From Examination dataset from CMU"""
 
-    VERSION = datasets.Version("0.1.0")
+    VERSION = datasets.Version("0.2.0")
+
+    BUILDER_CONFIGS = [
+        datasets.BuilderConfig(name="high", description="Exams designed for high school students", version=VERSION),
+        datasets.BuilderConfig(
+            name="middle", description="Exams designed for middle school students", version=VERSION
+        ),
+        datasets.BuilderConfig(
+            name="all", description="Exams designed for both high school and middle school students", version=VERSION
+        ),
+    ]
 
     def _info(self):
-        # TODO(race): Specifies the datasets.DatasetInfo object
         return datasets.DatasetInfo(
             # This is the description that will appear on the datasets page.
             description=_DESCRIPTION,
@@ -59,47 +69,48 @@ class Race(datasets.GeneratorBasedBuilder):
 
     def _split_generators(self, dl_manager):
         """Returns SplitGenerators."""
-        #Downloads the data and defines the splits
+        # Downloads the data and defines the splits
         # dl_manager is a datasets.download.DownloadManager that can be used to
         dl_dir = dl_manager.download_and_extract(_URL)
+        case = str(self.config.name)
+        if case == "all":
+            case = ""
         return [
             datasets.SplitGenerator(
                 name=datasets.Split.TEST,
                 # These kwargs will be passed to _generate_examples
-                gen_kwargs={"train_test_or_eval": os.path.join(dl_dir, "RACE/test")},
+                gen_kwargs={"train_test_or_eval": os.path.join(dl_dir, f"RACE/test/{case}")},
             ),
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN,
                 # These kwargs will be passed to _generate_examples
-                gen_kwargs={"train_test_or_eval": os.path.join(dl_dir, "RACE/train")},
+                gen_kwargs={"train_test_or_eval": os.path.join(dl_dir, f"RACE/train/{case}")},
             ),
             datasets.SplitGenerator(
                 name=datasets.Split.VALIDATION,
                 # These kwargs will be passed to _generate_examples
-                gen_kwargs={"train_test_or_eval": os.path.join(dl_dir, "RACE/dev")},
+                gen_kwargs={"train_test_or_eval": os.path.join(dl_dir, f"RACE/dev/{case}")},
             ),
         ]
 
     def _generate_examples(self, train_test_or_eval):
         """Yields examples."""
-        middle_or_high = os.listdir(train_test_or_eval)
-        for case in middle_or_high:
-            files_dir = os.path.join(train_test_or_eval, case)
-            for file in sorted(os.listdir(files_dir)):
-                filepath = os.path.join(files_dir, file)
-                with open(filepath, encoding="utf-8") as f:
-                    data = json.load(f)
-                    questions = data["questions"]
-                    answers = data["answers"]
-                    options = data["options"]
-                    for i in range(len(questions)):
-                        question = questions[i]
-                        answer = answers[i]
-                        option = options[i]
-                        yield i, {
-                            "example_id": data["id"],
-                            "article": data["article"],
-                            "question": question,
-                            "answer": answer,
-                            "options": option,
-                        }
+        current_path = Path(train_test_or_eval)
+        files_in_dir = [str(f.absolute()) for f in current_path.glob("**/*.txt")]
+        for file in sorted(files_in_dir):
+            with open(file, encoding="utf-8") as f:
+                data = json.load(f)
+                questions = data["questions"]
+                answers = data["answers"]
+                options = data["options"]
+                for i in range(len(questions)):
+                    question = questions[i]
+                    answer = answers[i]
+                    option = options[i]
+                    yield i, {
+                        "example_id": data["id"],
+                        "article": data["article"],
+                        "question": question,
+                        "answer": answer,
+                        "options": option,
+                    }
