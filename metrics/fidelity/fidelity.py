110bc04c700e0d490ea664e934ab5f3624063681
Anirudh Singh Rathore <singh16anirudh@gmail.com>
2020-10-30 12:23:52 -0600

    moved y_hat, prob_y_hat, prob_y_hat_alpha and null difference inside predictions and added some more lines in kwargs description

--- metrics/fidelity/fidelity.py
+++ metrics/fidelity/fidelity.py
@@ -48,16 +48,23 @@ References:
 """
 
 _KWARGS_DESCRIPTION = """
-Under Construction
 Args:
-    `predictions`: (numpy array) y_hat
-    `prob_y_hat`: (numpy array) P(y_hat/x)
-    `prob_y_hat_alpha`: (numpy array) P(y_hat/x, alpha)
-    `null_difference`: (numpy array) max(0, P(y_hat/x) - P(y_hat/x, 0))
+    `predictions`: (dict) 
+    keys
+        `y_hat`: (numpy array) y_hat is the predicted label by the model on a particular input
+        `prob_y_hat`: (numpy array) P(y_hat/x) is the probability of the predicted label when the input contains
+         full information
+        `prob_y_hat_alpha`: (numpy array) P(y_hat/x, alpha) is the probability of the predicted label when information
+        is reduced subject to alpha. Alpha denotes whether a token from the input is included in the input to the model
+        or not. alpha in [0, 1]. We use a binarization threshold which is 0.5 by default by which we binarize the alpha
+        values for either selecting the token or omitting it.
+        `null_difference`: (numpy array) max(0, P(y_hat/x) - P(y_hat/x, 0)) is the difference between the probability 
+        of the predicted label when full information is provided and the probability when zero information is provided(
+        when alpha is 0 for all the input tokens)
     `model`: (pytorch nn Module) model to compute predictions
-    `pad_token_id`: padding token id for the corresponding tokenizer
+    `pad_token_id`: padding token id for the corresponding tokenizer. After reducing the information from the input
     `input_ids`: (torch.tensor) tensor of input ids
-    `alpha`: (list) 0s and 1s corresponding to the presence of a token in the rationale
+    `alpha`: (list) list of fractional values in [0, 1] corresponding to the presence of a token in the rationale
     `attention_masks`: [Optional] (torch.tensor) tensor of attention masks
     `fidelity_type`: [Default="sufficiency"] (str) sufficiency or comprehensiveness
     `clip`: [Default=True] (bool) true for clipping the fidelity values between 0 and 1
@@ -77,22 +84,16 @@ class Fidelity(datasets.Metric):
             inputs_description=_KWARGS_DESCRIPTION,
             features=datasets.Features(
                 {
-                    "predictions": datasets.Value("float", id="sequence"),
-                    "prob_y_hat": datasets.Value("float", id="sequence"),
-                    "prob_y_hat_alpha": datasets.Value("float", id="sequence"),
-                    "null_difference": datasets.Value("float", id="sequence"),
-                    "mode": datasets.Value("string", id="sequence"),
-                    "normalization": datasets.Value("bool", id="sequence"),
+                    "predictions": datasets.Sequence(
+                        datasets.Sequence(datasets.Value("float", id="token"), id="sequence"), id="references"
+                    )
                 }
             ),
         )
 
     def _compute(
         self,
-        predictions: np.ndarray = None,
-        prob_y_hat: np.ndarray = None,
-        prob_y_hat_alpha: np.ndarray = None,
-        null_difference: np.ndarray = None,
+        predictions: dict = None,
         model: torch.nn.Module = None,
         pad_token_id: int = None,
         input_ids: torch.Tensor = None,
@@ -104,60 +105,49 @@ class Fidelity(datasets.Metric):
         reduction: str = "mean",
         binarization_threshold: float = 0.5,
     ):
-
-        if normalization:
-            if (
-                (predictions is None)
-                and (prob_y_hat is None or prob_y_hat_alpha is None or null_difference is None)
-                and (model is None or input_ids is None or alpha is None)
-            ):
-                return "Please provide either predictions or model and inputs to compute predictions"
-        else:
-            if (
-                (predictions is None)
-                and (prob_y_hat is None or prob_y_hat_alpha is None)
-                and (model is None or input_ids is None or alpha is None)
-            ):
-                return "Please provide either predictions or model and inputs to compute predictions"
-
-        if prob_y_hat is None:
-            predictions, prob_y_hat = utility.compute_predictions(
+        if "prob_y_hat" not in predictions.keys() or predictions["prob_y_hat"] is None:
+            predictions["y_hat"], predictions["prob_y_hat"] = utility.compute_predictions(
                 input_ids=input_ids, model=model, attention_masks=attention_masks
             )
-            prob_y_hat = prob_y_hat[np.arange(len(prob_y_hat)), predictions]
+            predictions["prob_y_hat"] = predictions["prob_y_hat"][np.arange(len(predictions["prob_y_hat"])),
+                                                                  predictions["y_hat"]]
 
-        if prob_y_hat_alpha is None:
+        if "prob_y_hat_alpha" not in predictions.keys() or predictions["prob_y_hat_alpha"] is None:
             input_ids_reduced, attention_masks_reduced = utility.reduce_input_with_rationale(
                 input_ids=input_ids, alpha=alpha, pad_token_id=pad_token_id, fidelity_type=fidelity_type
             )
 
-            predictions_alpha, prob_y_hat_alpha = utility.compute_predictions(
+            predictions["predictions_alpha"], predictions["prob_y_hat_alpha"] = utility.compute_predictions(
                 input_ids=input_ids_reduced, model=model, attention_masks=attention_masks_reduced
             )
-            prob_y_hat_alpha = prob_y_hat_alpha[np.arange(len(prob_y_hat_alpha)), predictions]
+            predictions["prob_y_hat_alpha"] = predictions["prob_y_hat_alpha"][np.arange(
+                len(predictions["prob_y_hat_alpha"])), predictions["y_hat"]]
 
         # Calculating fidelity value
         fidelity = utility.compute_fidelity(
-            prob_y_hat=prob_y_hat,
-            prob_y_hat_alpha=prob_y_hat_alpha,
+            prob_y_hat=predictions["prob_y_hat"],
+            prob_y_hat_alpha=predictions["prob_y_hat_alpha"],
             fidelity_type=fidelity_type,
             dataset_level=False,
             clip=clip,
         )
 
         if normalization:
-            if null_difference is None:
-                null_difference = utility.compute_null_diff(
+            if "null_difference" not in predictions.keys() or predictions["null_difference"] is None:
+                predictions["null_difference"] = utility.compute_null_diff(
                     input_ids=input_ids,
                     model=model,
-                    predictions=predictions,
-                    prob_y_hat=prob_y_hat,
+                    predictions=predictions["y_hat"],
+                    prob_y_hat=predictions["prob_y_hat"],
                     pad_token_id=pad_token_id,
                 )
 
             # Normalizing fidelity value
             fidelity = utility.normalize_item_set_fidelity(
-                fidelity=fidelity, null_difference=null_difference, fidelity_type=fidelity_type, clip=clip
+                fidelity=fidelity,
+                null_difference=predictions["null_difference"],
+                fidelity_type=fidelity_type,
+                clip=clip
             )
 
         if reduction == "mean":
