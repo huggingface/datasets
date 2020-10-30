821b0b4b0223f004a2037115c55960fc11cf0ee7
Mkrtchyan, Sergey <sergey.mkrtchyan@cerence.com>
2020-10-30 15:05:33 -0400

    Make feature names more explicit

--- datasets/asnq/asnq.py
+++ datasets/asnq/asnq.py
@@ -14,11 +14,14 @@
 # limitations under the License.
 """Answer-Sentence Natural Questions (ASNQ)
 
-ASNQ is a dataset for answer sentence selection derived from Google Natural Questions (NQ)
-dataset (Kwiatkowski et al. 2019). The dataset details can be found in the paper at
+ASNQ is a dataset for answer sentence selection derived from Google's
+Natural Questions (NQ) dataset (Kwiatkowski et al. 2019). It converts
+NQ's dataset into an AS2 (answer-sentence-selection) format.
+
+The dataset details can be found in the paper at
 https://arxiv.org/abs/1911.04118
 
-ASNQ is used to transfer the pre-trained models in the paper and can be downloaded at
+The dataset can be downloaded at
 https://wqa-public.s3.amazonaws.com/tanda-aaai-2020/data/asnq.tar
 """
 
@@ -41,7 +44,19 @@ _CITATION = """\
 
 _DESCRIPTION = """\
 ASNQ is a dataset for answer sentence selection derived from
-Google Natural Questions (NQ) dataset (Kwiatkowski et al. 2019).
+Google's Natural Questions (NQ) dataset (Kwiatkowski et al. 2019).
+
+Each example contains a question, candidate sentence, label indicating whether or not
+the sentence answers the question, and two additional features -- 
+sentence_in_long_answer and short_answer_in_sentence indicating whether ot not the 
+candidate sentence is contained in the long_answer and if the short_answer is in the candidate sentence.
+
+For more details please see 
+https://arxiv.org/pdf/1911.04118.pdf
+
+and 
+
+https://research.google/pubs/pub47761/
 """
 
 _URL = "https://wqa-public.s3.amazonaws.com/tanda-aaai-2020/data/asnq.tar"
@@ -50,9 +65,10 @@ _URL = "https://wqa-public.s3.amazonaws.com/tanda-aaai-2020/data/asnq.tar"
 class ASNQ(datasets.GeneratorBasedBuilder):
     """ASNQ is a dataset for answer sentence selection derived
     ASNQ is a dataset for answer sentence selection derived from
-    Google Natural Questions (NQ) dataset (Kwiatkowski et al. 2019).
+    Google's Natural Questions (NQ) dataset (Kwiatkowski et al. 2019).
 
-    The dataset details can be found in the `paper <https://arxiv.org/abs/1911.04118>`.
+    The dataset details can be found in the paper:
+    https://arxiv.org/abs/1911.04118
     """
 
     VERSION = datasets.Version("1.0.0")
@@ -65,9 +81,11 @@ class ASNQ(datasets.GeneratorBasedBuilder):
             # This defines the different columns of the dataset and their types
             features=datasets.Features(
                 {
-                    "sentence1": datasets.Value("string"),
-                    "sentence2": datasets.Value("string"),
-                    "label": datasets.Value("int32"),
+                    "question": datasets.Value("string"),
+                    "sentence": datasets.Value("string"),
+                    "label": datasets.ClassLabel(names=["neg", "pos"]),
+                    "sentence_in_long_answer": datasets.Value("bool"),
+                    "short_answer_in_sentence": datasets.Value("bool")
                 }
             ),
             # No default supervised_keys
@@ -103,9 +121,27 @@ class ASNQ(datasets.GeneratorBasedBuilder):
         ]
 
     def _generate_examples(self, filepath, split):
-        """ Yields examples. """
+        """ Yields examples.
+
+        Original dataset contains labels '1', '2', '3' and '4', with labels
+        '1', '2' and '3' considered negative (sentence does not answer the question),
+        and label '4' considered positive (sentence does answer the question).
+        We map these labels to two classes, returning the other properties as additional
+        features."""
+
+        # Mapping of dataset's original labels to a tuple of
+        # (label, sentence_in_long_answer, short_answer_in_sentence)
+        label_map = {'1': ('neg', False, False),
+                     '2': ('neg', False, True),
+                     '3': ('neg', True, False),
+                     '4': ('pos', True, True)}
         with open(filepath, encoding="utf-8") as tsvfile:
             tsvreader = csv.reader(tsvfile, delimiter="\t")
             for id_, row in enumerate(tsvreader):
-                sentence1, sentence2, label = row
-                yield id_, {"sentence1": sentence1, "sentence2": sentence2, "label": label}
+                question, sentence, orig_label = row
+                label, sentence_in_long_answer, short_answer_in_sentence = label_map[orig_label]
+                yield id_, {"question": question,
+                            "sentence": sentence,
+                            "label": label,
+                            "sentence_in_long_answer": sentence_in_long_answer,
+                            "short_answer_in_sentence": short_answer_in_sentence}
