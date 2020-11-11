8d6adb7353ad7f3d522e873616a298b04443ca45
sumanthd17 <dsumanth17@gmail.com>
2020-11-11 23:08:03 +0530

    indic_glue/inltkh

--- datasets/indic_glue/indic_glue.py
+++ datasets/indic_glue/indic_glue.py
@@ -76,7 +76,7 @@ _DESCRIPTIONS = {
         The dataset has 400k examples across 11 Indian languages.
         """
     ),
-    "clsr": textwrap.dedent(
+    "inltkh": textwrap.dedent(
         """
         REPLACE
         """
@@ -109,7 +109,7 @@ _CITATIONS = {
         REPLACE
         """
     ),
-    "clsr": textwrap.dedent(
+    "inltkh": textwrap.dedent(
         """
         REPLACE
         """
@@ -130,7 +130,7 @@ _TEXT_FEATURES = {
         "titleD": "titleD",
         "url": "url",
     },
-    "clsr": {},
+    "inltkh": {"label": "label", "text": "text"},
 }
 
 _DATA_URLS = {
@@ -139,7 +139,7 @@ _DATA_URLS = {
     "sna": "https://storage.googleapis.com/ai4bharat-public-indic-nlp-corpora/evaluations/soham-articles.tar.gz",
     "csqa": "https://storage.googleapis.com/ai4bharat-public-indic-nlp-corpora/evaluations/wiki-cloze.tar.gz",
     "wstp": "https://storage.googleapis.com/ai4bharat-public-indic-nlp-corpora/evaluations/wiki-section-titles.tar.gz",
-    "clsr": "",
+    "inltkh": "https://storage.googleapis.com/ai4bharat-public-indic-nlp-corpora/evaluations/inltk-headlines.tar.gz",
 }
 
 _URLS = {
@@ -148,7 +148,7 @@ _URLS = {
     "sna": "https://indicnlp.ai4bharat.org/indic-glue/#news-category-classification",
     "csqa": "https://indicnlp.ai4bharat.org/indic-glue/#cloze-style-question-answering",
     "wstp": "https://indicnlp.ai4bharat.org/indic-glue/#wikipedia-section-title-prediction",
-    "clsr": "",
+    "inltkh": "https://indicnlp.ai4bharat.org/indic-glue/#news-category-classification",
 }
 
 _INDIC_GLUE_URL = "https://indicnlp.ai4bharat.org/indic-glue/"
@@ -158,7 +158,7 @@ _COPA_LANGS = ["en", "hi", "gu", "mr"]
 _SNA_LANGS = ["bn"]
 _CSQA_LANGS = ["as", "bn", "gu", "hi", "kn", "ml", "mr", "or", "pa", "ta", "te"]
 _WSTP_LANGS = ["as", "bn", "gu", "hi", "kn", "ml", "mr", "or", "pa", "ta", "te"]
-_CLSR_LANGS = []
+_iNLTKH_LANGS = ["gu", "ml", "mr", "ta", "te"]
 
 _NAMES = []
 
@@ -177,8 +177,8 @@ for lang in _CSQA_LANGS:
 for lang in _WSTP_LANGS:
     _NAMES.append(f"wstp.{lang}")
 
-# for lang in _CLSR_LANGS:
-#     _NAMES.append(f'clsr.{lang}')
+for lang in _iNLTKH_LANGS:
+    _NAMES.append(f"inltkh.{lang}")
 
 
 class IndicGlueConfig(datasets.BuilderConfig):
@@ -404,6 +404,35 @@ class IndicGlue(datasets.GeneratorBasedBuilder):
                 ),
             ]
 
+        if self.config.name.startswith("inltkh"):
+            dl_dir = dl_manager.download_and_extract(self.config.data_url)
+            task_name = self._get_task_name_from_data_url(self.config.data_url)
+            dl_dir = os.path.join(dl_dir, task_name + "/" + self.config.name.split(".")[1])
+
+            return [
+                datasets.SplitGenerator(
+                    name=datasets.Split.TRAIN,
+                    gen_kwargs={
+                        "datafile": os.path.join(dl_dir, f"{self.config.name.split('.')[1]}-train.csv"),
+                        "split": datasets.Split.TRAIN,
+                    },
+                ),
+                datasets.SplitGenerator(
+                    name=datasets.Split.VALIDATION,
+                    gen_kwargs={
+                        "datafile": os.path.join(dl_dir, f"{self.config.name.split('.')[1]}-valid.csv"),
+                        "split": datasets.Split.VALIDATION,
+                    },
+                ),
+                datasets.SplitGenerator(
+                    name=datasets.Split.TEST,
+                    gen_kwargs={
+                        "datafile": os.path.join(dl_dir, f"{self.config.name.split('.')[1]}-test.csv"),
+                        "split": datasets.Split.TEST,
+                    },
+                ),
+            ]
+
     def _generate_examples(self, **args):
         """Yields examples."""
         filepath = args["datafile"]
@@ -462,6 +491,11 @@ class IndicGlue(datasets.GeneratorBasedBuilder):
                     "titleD": row["titleD"],
                     "url": row["url"],
                 }
+
+        if self.config.name.startswith("inltkh"):
+            df = pd.read_csv(filepath, names=["label", "text"])
+            for id_, row in df.iterrows():
+                yield id_, {"text": row["text"], "label": row["label"]}
 
     def _get_task_name_from_data_url(self, data_url):
         return data_url.split("/")[-1].split(".")[0]
