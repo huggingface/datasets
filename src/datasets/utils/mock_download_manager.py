0b4575b10761dc663bb440ce73492d9740a45e12
Quentin Lhoest <lhoest.q@gmail.com>
2020-11-27 18:33:42 +0100

    fix nested urls

--- src/datasets/utils/mock_download_manager.py
+++ src/datasets/utils/mock_download_manager.py
@@ -139,14 +139,20 @@ class MockDownloadManager(object):
 
     def create_dummy_data_dict(self, path_to_dummy_data, data_url):
         dummy_data_dict = {}
-        for key, single_url in data_url.items():
+        for key, single_urls in data_url.items():
             for download_callback in self.download_callbacks:
-                download_callback(single_url)
+                if isinstance(single_urls, list):
+                    for single_url in single_urls:
+                        download_callback(single_url)
+                else:
+                    single_url = single_urls
+                    download_callback(single_url)
             # we force the name of each key to be the last file / folder name of the url path
             # if the url has arguments, we need to encode them with urllib.parse.quote_plus
-            if isinstance(single_url, list):
-                value = [os.path.join(path_to_dummy_data, urllib.parse.quote_plus(Path(x).name)) for x in single_url]
+            if isinstance(single_urls, list):
+                value = [os.path.join(path_to_dummy_data, urllib.parse.quote_plus(Path(x).name)) for x in single_urls]
             else:
+                single_url = single_urls
                 value = os.path.join(path_to_dummy_data, urllib.parse.quote_plus(Path(single_url).name))
             dummy_data_dict[key] = value
 
