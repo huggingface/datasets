7035af519bf65eefb914b6a9177622cc7b8faf14
Joel Niklaus <me@joelniklaus.ch>
2020-11-28 01:23:14 +0100

    removed whitespace to please flake8

--- datasets/ler/ler.py
+++ datasets/ler/ler.py
@@ -43,13 +43,13 @@ _CITATION = """\
 """
 
 _DESCRIPTION = """\
-We describe a dataset developed for Named Entity Recognition in German federal court decisions. 
-It consists of approx. 67,000 sentences with over 2 million tokens. 
-The resource contains 54,000 manually annotated entities, mapped to 19 fine-grained semantic classes: 
-person, judge, lawyer, country, city, street, landscape, organization, company, institution, court, brand, law, 
-ordinance, European legal norm, regulation, contract, court decision, and legal literature. 
-The legal documents were, furthermore, automatically annotated with more than 35,000 TimeML-based time expressions. 
-The dataset, which is available under a CC-BY 4.0 license in the CoNNL-2002 format, 
+We describe a dataset developed for Named Entity Recognition in German federal court decisions.
+It consists of approx. 67,000 sentences with over 2 million tokens.
+The resource contains 54,000 manually annotated entities, mapped to 19 fine-grained semantic classes:
+person, judge, lawyer, country, city, street, landscape, organization, company, institution, court, brand, law,
+ordinance, European legal norm, regulation, contract, court decision, and legal literature.
+The legal documents were, furthermore, automatically annotated with more than 35,000 TimeML-based time expressions.
+The dataset, which is available under a CC-BY 4.0 license in the CoNNL-2002 format,
 was developed for training an NER service for German legal documents in the EU project Lynx.
 """
 
