c87577a208d515e3f572f13a977378fc2a62eb1b
Thomas Wolf <thomas@huggingface.co>
2020-11-30 17:42:13 +0100

    make the dataset template more clear and complete with an example with several config

--- templates/new_dataset_script.py
+++ templates/new_dataset_script.py
@@ -24,6 +24,7 @@ import datasets
 
 
 # TODO: Add BibTeX citation
+# Find for instance the citation on arxiv or on the dataset repo/website
 _CITATION = """\
 @InProceedings{huggingface:dataset,
 title = {A great new dataset},
@@ -34,30 +35,21 @@ year={2020}
 """
 
 # TODO: Add description of the dataset here
+# You can copy an official description
 _DESCRIPTION = """\
 This new dataset is designed to solve this great NLP task and is crafted with a lot of care. 
 """
 
-_URL = "https://huggingface.co/great-new-dataset.zip"
+# TODO: Add link to the official dataset URLs here
+# The HuggingFace dataset library don't host the datasets but only point to the original files
+# This can be an arbitrary nested dict/list of URLs (see below in `_split_generators` method)
+_URLs = {
+    'first_domain': "https://huggingface.co/great-new-dataset-first_domain.zip",
+    'second_domain': "https://huggingface.co/great-new-dataset-second_domain.zip",
+}
 
 
 # TODO: Name of the dataset usually match the script name with CamelCase instead of snake_case
-# Using a specific configuration class is optional, you can also use the base class if you don't need
-# to add specific attributes.
-# here we give an example for three sub-set of the dataset with difference sizes.
-class NewDatasetConfig(datasets.BuilderConfig):
-    """ BuilderConfig for NewDataset"""
-
-    def __init__(self, data_size, **kwargs):
-        """
-
-        Args:
-            data_size: the size of the training set we want to us (xs, s, m, l, xl)
-            **kwargs: keyword arguments forwarded to super.
-        """
-        self.data_size = data_size
-
-
 class NewDataset(datasets.GeneratorBasedBuilder):
     """TODO: Short description of my dataset."""
 
@@ -66,26 +58,44 @@ class NewDataset(datasets.GeneratorBasedBuilder):
     # This is an example of a dataset with multiple configurations.
     # If you don't want/need to define several sub-sets in your dataset,
     # just remove the BUILDER_CONFIG_CLASS and the BUILDER_CONFIGS attributes.
-    BUILDER_CONFIG_CLASS = NewDatasetConfig
+
+    # If you need to make complex sub-parts in the datasets with configurable options
+    # You can create your own builder configuration class to store attribute, inheriting from datasets.BuilderConfig
+    # BUILDER_CONFIG_CLASS = MyBuilderConfig
+
+    # You will be able to load one or the other configurations in the following list with
+    # data = datasets.load_dataset('my_dataset', 'first_domain')
+    # data = datasets.load_dataset('my_dataset', 'second_domain')
     BUILDER_CONFIGS = [
-        NewDatasetConfig(name="my_dataset_" + size, description="A small dataset", data_size=size) for size in ["small", "medium", "large"]
+        datasets.BuilderConfig(name="first_domain", description="This part of my dataset covers a first domain"),
+        datasets.BuilderConfig(name="second_domain", description="This part of my dataset covers a second domain"),
     ]
 
     def _info(self):
-        # TODO: Specifies the datasets.DatasetInfo object
-        return datasets.DatasetInfo(
-            # This is the description that will appear on the datasets page.
-            description=_DESCRIPTION,
-            # This defines the different columns of the dataset and their types
-            features=datasets.Features(
+        # TODO: This method pecifies the datasets.DatasetInfo object which contains informations and typings for the dataset
+        if self.config.name == "first_domain":  # This is the name of the configuration selected in BUILDER_CONFIGS above 
+            features = datasets.Features(
                 {
                     "sentence": datasets.Value("string"),
                     "option1": datasets.Value("string"),
-                    "option2": datasets.Value("string"),
                     "answer": datasets.Value("string")
                     # These are the features of your dataset like images, labels ...
                 }
             ),
+        else:  # This is an example to show how to have different features for "first_domain" and "second_domain"
+            features = datasets.Features(
+                {
+                    "sentence": datasets.Value("string"),
+                    "option2": datasets.Value("string"),
+                    "second_domain_answer": datasets.Value("string")
+                    # These are the features of your dataset like images, labels ...
+                }
+            ),
+        return datasets.DatasetInfo(
+            # This is the description that will appear on the datasets page.
+            description=_DESCRIPTION,
+            # This defines the different columns of the dataset and their types
+            features=features,  # Here we define them above because they are different between the two configurations
             # If there's a common (input, target) tuple from the features,
             # specify them here. They'll be used if as_supervised=True in
             # builder.as_dataset.
@@ -97,32 +107,36 @@ class NewDataset(datasets.GeneratorBasedBuilder):
 
     def _split_generators(self, dl_manager):
         """Returns SplitGenerators."""
-        # TODO: Downloads the data and defines the splits
-        # dl_manager is a datasets.download.DownloadManager that can be used to
-        # download and extract URLs
-        dl_dir = dl_manager.download_and_extract(_URL)
-        data_dir = os.path.join(dl_dir, "great-new-dataset")
+        # TODO: This method is tasked with downloading/extracting the data and defining the splits depending on the configuration
+        # If several configurations are possible (listed in BUILDER_CONFIGS), the configuration selected by the user is in self.config.name
+
+        # dl_manager is a datasets.download.DownloadManager that can be used to download and extract URLs
+        # It can accept any type or nested list/dict and will give back the same structure with the url replaced with path to local files.
+        # By default the archives will be extracted and a path to a cached folder where they are extracted is returned instead of the archive 
+        my_urls = _URLs[self.config.name]
+        data_dir = dl_manager.download_and_extract(my_urls)
         return [
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN,
                 # These kwargs will be passed to _generate_examples
                 gen_kwargs={
-                    "filepath": os.path.join(data_dir, "train_{}.jsonl".format(self.config.data_size)),
-                    #'labelpath': os.path.join(data_dir, 'train_{}-labels.lst'.format(self.config.data_size)),
+                    "filepath": os.path.join(data_dir, "train.jsonl"),
                     "split": "train",
                 },
             ),
             datasets.SplitGenerator(
                 name=datasets.Split.TEST,
                 # These kwargs will be passed to _generate_examples
-                gen_kwargs={"filepath": os.path.join(data_dir, "test.jsonl"), "split": "test"},
+                gen_kwargs={
+                    "filepath": os.path.join(data_dir, "test.jsonl"),
+                    "split": "test"
+                },
             ),
             datasets.SplitGenerator(
                 name=datasets.Split.VALIDATION,
                 # These kwargs will be passed to _generate_examples
                 gen_kwargs={
                     "filepath": os.path.join(data_dir, "dev.jsonl"),
-                    #'labelpath': os.path.join(data_dir, 'dev-labels.lst'),
                     "split": "dev",
                 },
             ),
@@ -130,21 +144,22 @@ class NewDataset(datasets.GeneratorBasedBuilder):
 
     def _generate_examples(self, filepath, split):
         """ Yields examples. """
-        # TODO: Yields (key, example) tuples from the dataset
+        # TODO: This method will receive as arguments the `gen_kwargs` defined in the previous `_split_generators` method.
+        # It is in charge of opening the given file and yielding (key, example) tuples from the dataset
+        # The key is not important, it's more here for legacy reason (legacy from tfds)
+
         with open(filepath) as f:
             for id_, row in enumerate(f):
                 data = json.loads(row)
-                if split == "test":
+                if self.config.name == "first_domain":
                     yield id_, {
                         "sentence": data["sentence"],
                         "option1": data["option1"],
-                        "option2": data["option2"],
-                        "answer": "",
+                        "answer": "" if split == "test" else data["answer"],
                     }
                 else:
                     yield id_, {
                         "sentence": data["sentence"],
-                        "option1": data["option1"],
                         "option2": data["option2"],
-                        "answer": data["answer"],
+                        "second_domain_answer": "" if split == "test" else data["second_domain_answer"],
                     }
