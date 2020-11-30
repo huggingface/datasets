48037c4cf1c2a9957531bbd7fcd8e6d16b56fe0a
zaidalyafeai <alyafey22@gmail.com>
2020-11-30 03:56:03 +0300

    fix conflicts

--- datasets/metrec/metrec.py
+++ datasets/metrec/metrec.py
@@ -32,17 +32,10 @@ The train dataset contains 47,124 records and the test dataset contains 8316 rec
 """
 
 _CITATION = """\
-<<<<<<< HEAD
 @article{metrec2020,
   title={MetRec: A dataset for meter classification of arabic poetry},
   author={Al-shaibani, Maged S and Alyafeai, Zaid and Ahmad, Irfan},
   journal={Data in Brief},
-=======
-@article{al2020meter,
-  title={Meter Classification of Arabic Poems Using Deep Bidirectional Recurrent Neural Networks},
-  author={Al-shaibani, Maged S and Alyafeai, Zaid and Ahmad, Irfan},
-  journal={Pattern Recognition Letters},
->>>>>>> 7eb3846c61d3ccd13c64304a13e18c16b1f865e6
   year={2020},
   publisher={Elsevier}
 }
