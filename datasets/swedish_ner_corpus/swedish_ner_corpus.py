5126bdb1ffa6c761c07dfef1f60c7835a9a7e82c
abhishekkrthakur <abhishek4@gmail.com>
2020-11-30 13:10:37 +0100

    swedish ner

--- datasets/swedish_ner_corpus/swedish_ner_corpus.py
+++ datasets/swedish_ner_corpus/swedish_ner_corpus.py
@@ -1,7 +1,23 @@
+# coding=utf-8
+# Copyright 2020 HuggingFace Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
 import datasets
 
 _DESCRIPTION = """\
-    Webbnyheter 2012 from Spraakbanken, semi-manually annotated and adapted for CoreNLP Swedish NER. Semi-manually defined in this case as: Bootstrapped from Swedish Gazetters then manually correcte/reviewed by two independent native speaking swedish annotators. No annotator agreement calculated.
+Webbnyheter 2012 from Spraakbanken, semi-manually annotated and adapted for CoreNLP Swedish NER. Semi-manually defined in this case as: Bootstrapped from Swedish Gazetters then manually correcte/reviewed by two independent native speaking swedish annotators. No annotator agreement calculated.
 """
 _HOMEPAGE_URL = "https://github.com/klintan/swedish-ner-corpus"
 _TRAIN_URL = "https://raw.githubusercontent.com/klintan/swedish-ner-corpus/master/train_corpus.txt"
@@ -17,8 +33,12 @@ class SwedishNERCorpusDataset(datasets.GeneratorBasedBuilder):
             features=datasets.Features(
                 {
                     "id": datasets.Value("string"),
-                    "words": datasets.Sequence(datasets.Value("string")),
-                    "labels": datasets.Sequence(datasets.Value("string")),
+                    "tokens": datasets.Sequence(datasets.Value("string")),
+                    "ner_tags": datasets.Sequence(
+                        datasets.features.ClassLabel(
+                            names=["0", "LOC", "MISC", "ORG", "PER"]
+                        )
+                    ),
                 },
             ),
             supervised_keys=None,
@@ -45,30 +65,38 @@ class SwedishNERCorpusDataset(datasets.GeneratorBasedBuilder):
             current_labels = []
             for row in f:
                 row = row.rstrip()
-                row_split = row.split("\t")
-                if len(row_split) > 1:
+                row_split = row.split()
+                if len(row_split) == 2:
                     token, label = row_split
                     current_words.append(token)
                     current_labels.append(label)
                 else:
                     if not current_words:
                         continue
-                    assert len(current_words) == len(current_labels)
+                    assert len(current_words) == len(
+                        current_labels
+                    ), "word len doesnt match label length"
                     sentence = (
                         sentence_counter,
                         {
                             "id": str(sentence_counter),
-                            "words": current_words,
-                            "labels": current_labels,
+                            "tokens": current_words,
+                            "ner_tags": current_labels,
                         },
                     )
                     sentence_counter += 1
                     current_words = []
                     current_labels = []
                     yield sentence
+
+            # if something remains:
             if current_words:
-                yield sentence_counter, {
-                    "id": str(sentence_counter),
-                    "words": current_words,
-                    "labels": current_labels,
-                }
+                sentence = (
+                    sentence_counter,
+                    {
+                        "id": str(sentence_counter),
+                        "tokens": current_words,
+                        "ner_tags": current_labels,
+                    },
+                )
+                yield sentence
