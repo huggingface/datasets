a3e7579d5052d36683eb94559f89b6d860170699
reshinthadithyan <reshinther@gmail.com>
2020-11-30 20:15:03 +0530

    reformatted code

--- datasets/codexglue_concode/codexglue_concode.py
+++ datasets/codexglue_concode/codexglue_concode.py
@@ -1,36 +1,44 @@
-"""concode - code2text pairs dataset"""
+"""TODO concode - code2text pairs dataset"""
 import datasets
 import json
 import logging
 
 _CITATION = """\
-@article{iyer2018mapping,
-  title={Mapping language to code in programmatic context},
-  author={Iyer, Srinivasan and Konstas, Ioannis and Cheung, Alvin and Zettlemoyer, Luke},
-  journal={arXiv preprint arXiv:1808.09588},
-  year=2018,
+@article{iyer2018mapping, 
+  title={Mapping language to code in programmatic context}, 
+  author={Iyer, Srinivasan and Konstas, Ioannis and Cheung, Alvin and Zettlemoyer, Luke}, 
+  journal={arXiv preprint arXiv: 1808.09588}, 
+  year=2018, 
 }
 """
-_URL = "https://raw.githubusercontent.com/microsoft/CodeXGLUE/main/Text-Code/text-to-code/dataset/concode/"
+_URL = "https: //raw.githubusercontent.com/microsoft/CodeXGLUE/main/Text-Code/text-to-code/dataset/concode/"
 _URLS = {
     "train": _URL + "train.json",
     "dev": _URL + "dev.json",
     "test": _URL + "test.json",
 }
-_DESCRIPTION = """Mapping Language to Code in a Programmatic Context"""
+_DESCRIPTION = """Mapping Language to Code in a Programmatic Context.
+concode dataset which a widely used code generation dataset from Iyer's EMNLP 2018 paper
+Generate source code of class member functions in Java, given natural language description 
+and class environment. Class environment is the programmatic context provided by the 
+rest of the class, including other member variables and member functions in class.
+"""
+
+
 class CodeXGlue_ConcodeConfig(datasets.BuilderConfig):
     """BuilderConfig for Concode Dataset."""
 
     def __init__(self, **kwargs):
         """BuilderConfig for Concode.
-
         Args:
           **kwargs: keyword arguments forwarded to super.
         """
         super(CodeXGlue_ConcodeConfig, self).__init__(**kwargs)
 
+
 class Concode(datasets.GeneratorBasedBuilder):
     """ConCode: code generation dataset for java NL queries(pre-processed)"""
+
     VERSION = datasets.Version("1.0.0")
 
     BUILDER_CONFIGS = [
@@ -38,23 +46,26 @@ class Concode(datasets.GeneratorBasedBuilder):
             name="plain_text",
             version=VERSION,
             description="Plain text",
-        ),
+        )
     ]
+
     def _info(self):
-            return datasets.DatasetInfo(
-                description=_DESCRIPTION,
-                features=datasets.Features(
-                    {
-                        "nl": datasets.Value("string"),
-                        "code": datasets.Value("string"),
-                    }),
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "natural_lang": datasets.Value("string"),
+                    "code": datasets.Value("string"),
+                }
+            ),
             supervised_keys=("ml4code", "source code"),
-            homepage="https://github.com/sriniiyer/concode",
+            homepage="https: //github.com/sriniiyer/concode",
             citation=_CITATION,
-            )
+        )
+
     def _split_generators(self, dl_manager):
         """Returns SplitGenerators."""
-        dl_path = dl_manager.download(_URLS) 
+        dl_path = dl_manager.download(_URLS)
         return [
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN,
@@ -62,31 +73,27 @@ class Concode(datasets.GeneratorBasedBuilder):
             ),
             datasets.SplitGenerator(
                 name=datasets.Split.VALIDATION,
-                gen_kwargs={
-                    "filename": dl_path["dev"]},
+                gen_kwargs={"filename": dl_path["dev"]},
             ),
             datasets.SplitGenerator(
                 name=datasets.Split.TEST,
-                gen_kwargs={
-                    "filename":dl_path["test"]},
+                gen_kwargs={"filename": dl_path["test"]},
             ),
         ]
-    def _generate_examples(self,filename):
+
+    def _generate_examples(self, filename):
         """Yields examples.
-
-        Each example contains a Nl Query and the corresponding Code.
-
+        Each example contains a Natural Language Query and the corresponding Code.
         Args:
-          filename: name of the file to be read.
-
-
+          filename: path of the file to be read.
         Yields:
-          A dictionary of features.
+          A dictionary of natural_lang and corresponding code in code field
         """
-        logging.info("Generating examples from from %s split ",filename)
+        logging.info("Generating examples from from %s split ", filename)
         id = 0
-        with open(filename) as f:
+        with open(filename, encoding="utf-8") as f:
             for elem_data in f:
                 elem_data = json.loads(elem_data)
+                elem_data = {"natural_lang": elem_data["nl"], "code": elem_data["code"]}
                 yield id, elem_data
                 id += 1
