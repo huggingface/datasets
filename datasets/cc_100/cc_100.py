507e6d058791d244fdd780bd6b64e1ebe158788d
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2020-11-30 18:18:21 +0100

    Add docstrings

--- datasets/cc_100/cc_100.py
+++ datasets/cc_100/cc_100.py
@@ -1,3 +1,19 @@
+# coding=utf-8
+# Copyright 2020 The HuggingFace Datasets Authors and the current dataset script contributor.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""CC-100: Monolingual Datasets from Web Crawl Data."""
+
 import datasets
 
 
@@ -178,12 +194,21 @@ _LANGUAGES = [
 
 
 class Cc100Config(datasets.BuilderConfig):
+    """ BuilderConfig for Cc100."""
+
     def __init__(self, language=None, **kwargs):
+        """
+
+        Args:
+            language: language of the subdataset.
+            **kwargs: keyword arguments forwarded to super.
+        """
         super().__init__(name=language, description=f"CC-100 dataset for language {language}.", **kwargs)
         self.language = language
 
 
 class Cc100(datasets.GeneratorBasedBuilder):
+    """CC-100 dataset."""
 
     BUILDER_CONFIG_CLASS = Cc100Config
     BUILDER_CONFIGS = [Cc100Config(language=language) for language in _LANGUAGES]
