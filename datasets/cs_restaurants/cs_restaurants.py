231ccf3df7985b7bf053e406f8874ee5a28ed9c7
TevenLeScao <teven.lescao@gmail.com>
2020-12-01 18:04:08 +0100

    CS Restaurants NLG dataset

--- datasets/cs_restaurants/cs_restaurants.py
+++ datasets/cs_restaurants/cs_restaurants.py
@@ -25,7 +25,11 @@ import datasets
 _CITATION = """\
 @article{DBLP:journals/corr/abs-1910-05298,
   author    = {Ondrej Dusek and
+<<<<<<< HEAD
                Filip Jurcicek},
+=======
+               Filip Jurc{\'{\i}}cek},
+>>>>>>> CS Restaurants NLG dataset
   title     = {Neural Generation for Czech: Data and Baselines},
   journal   = {CoRR},
   volume    = {abs/1910.05298},
@@ -40,7 +44,11 @@ _CITATION = """\
 """
 
 _DESCRIPTION = """\
+<<<<<<< HEAD
 This is a dataset for NLG in task-oriented spoken dialogue systems with Czech as the target language. It originated as
+=======
+This is a dataset for NLG in task-oriented spoken dialogue systems with Czech as the target language. It originated as 
+>>>>>>> CS Restaurants NLG dataset
 a translation of the English San Francisco Restaurants dataset by Wen et al. (2015).
 """
 
