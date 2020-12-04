264f851c7333c0c97ee3cdb33b51d904c53e69a7
Abhishek Thakur <abhishek4@gmail.com>
2020-12-04 11:35:19 +0100

    initial commit

---
+++ datasets/great_code/great_code.py
@@ -0,0 +1,170 @@
+# coding=utf-8
+# Copyright 2020 HuggingFace Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+import json
+import os
+
+import datasets
+
+
+_DESCRIPTION = """\
+The dataset for the variable-misuse task, described in the ICLR 2020 paper 'Global Relational Models of Source Code' [https://openreview.net/forum?id=B1lnbRNtwr]
+
+This is the public version of the dataset used in that paper. The original, used to produce the graphs in the paper, could not be open-sourced due to licensing issues. See the public associated code repository [https://github.com/VHellendoorn/ICLR20-Great] for results produced from this dataset.
+
+This dataset was generated synthetically from the corpus of Python code in the ETH Py150 Open dataset [https://github.com/google-research-datasets/eth_py150_open].
+"""
+_HOMEPAGE_URL = ""
+_CITATION = """\
+@inproceedings{DBLP:conf/iclr/HellendoornSSMB20,
+  author    = {Vincent J. Hellendoorn and
+               Charles Sutton and
+               Rishabh Singh and
+               Petros Maniatis and
+               David Bieber},
+  title     = {Global Relational Models of Source Code},
+  booktitle = {8th International Conference on Learning Representations, {ICLR} 2020,
+               Addis Ababa, Ethiopia, April 26-30, 2020},
+  publisher = {OpenReview.net},
+  year      = {2020},
+  url       = {https://openreview.net/forum?id=B1lnbRNtwr},
+  timestamp = {Thu, 07 May 2020 17:11:47 +0200},
+  biburl    = {https://dblp.org/rec/conf/iclr/HellendoornSSMB20.bib},
+  bibsource = {dblp computer science bibliography, https://dblp.org}
+}
+"""
+_TRAIN_URLS = [
+    f"https://github.com/google-research-datasets/great/blob/master/train/train__VARIABLE_MISUSE__SStuB.txt-{x:05d}-of-00300"
+    for x in range(300)
+]
+_TEST_URLS = [
+    f"https://github.com/google-research-datasets/great/blob/master/eval/eval__VARIABLE_MISUSE__SStuB.txt-{x:05d}-of-00300"
+    for x in range(300)
+]
+_VALID_URLS = [
+    f"https://github.com/google-research-datasets/great/blob/master/dev/dev__VARIABLE_MISUSE__SStuB.txt-{x:05d}-of-00300"
+    for x in range(300)
+]
+
+
+class GreatCode(datasets.GeneratorBasedBuilder):
+    VERSION = datasets.Version("1.0.0")
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "id": datasets.Value("int32"),
+                    "source_tokens": datasets.Value("string"),
+                    "has_bug": datasets.Value("bool"),
+                    "error_location": datasets.Value("int32"),
+                    "repair_candidates": datasets.Sequence("string"),
+                    "bug_kind": datasets.Value("int32"),
+                    "bug_kind_name": datasets.Value("string"),
+                    "repair_targets": datasets.Sequence("int32"),
+                    "edges": [
+                        [
+                            {
+                                "before_index": datasets.Value("int32"),
+                                "after_index": datasets.Value("int32"),
+                                "edge_type": datasets.Value("int32"),
+                                "edge_type_name": datasets.Value("string"),
+                            }
+                        ]
+                    ],
+                    "provenances": [
+                        {
+                            "datasetProvenance": {
+                                "datasetName": datasets.Value("string"),
+                                "filepath": datasets.Value("string"),
+                                "license": datasets.Value("string"),
+                                "note": datasets.Value("string"),
+                            }
+                        }
+                    ],
+                },
+            ),
+            supervised_keys=None,
+            homepage=_HOMEPAGE_URL,
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        train_path = dl_manager.download_and_extract(_TRAIN_URLS)
+        valid_path = dl_manager.download_and_extract(_TEST_URLS)
+        test_path = dl_manager.download_and_extract(_VALID_URLS)
+        print("*************")
+        print(train_path)
+        print(valid_path)
+        print(test_path)
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                gen_kwargs={
+                    "datapath": train_path,
+                    "datatype": "train",
+                },
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.VALIDATION,
+                gen_kwargs={
+                    "datapath": valid_path,
+                    "datatype": "valid",
+                },
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.TEST,
+                gen_kwargs={
+                    "datapath": test_path,
+                    "datatype": "test",
+                },
+            ),
+        ]
+
+    def _generate_examples(self, datapath, datatype):
+        with open(datapath, "r") as json_file:
+            json_list = list(json_file)
+
+        for example_counter, json_str in enumerate(json_list):
+            result = json.loads(json_str)
+            response = {
+                "id": example_counter,
+                "table_page_title": result["table_page_title"],
+                "table_webpage_url": result["table_webpage_url"],
+                "table_section_title": result["table_section_title"],
+                "table_section_text": result["table_section_text"],
+                "table": result["table"],
+                "highlighted_cells": result["highlighted_cells"],
+                "example_id": str(result["example_id"]),
+            }
+            if datatype == "train":
+                response["overlap_subset"] = "none"
+            else:
+                response["overlap_subset"] = str(result["overlap_subset"])
+
+            if datatype == "test":
+                response["sentence_annotations"] = [
+                    {
+                        "original_sentence": "none",
+                        "sentence_after_deletion": "none",
+                        "sentence_after_ambiguity": "none",
+                        "final_sentence": "none",
+                    }
+                ]
+            else:
+                response["sentence_annotations"] = result["sentence_annotations"]
+            yield example_counter, response
