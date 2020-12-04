7ad71507e8a4a95f52000e1d65602872cf36ebed
chaitnayabasava <viswachaitanya.b16@iiits.in>
2020-12-04 21:05:05 +0530

    Add urdu fake news dataset.

---
+++ datasets/urdu_fake_news/README.md
@@ -0,0 +1,140 @@
+---
+annotations_creators:
+  - expert-generated
+language_creators:
+  - expert-generated
+languages:
+  - ur
+licenses: []
+multilinguality:
+  - monolingual
+size_categories:
+  - n<1K
+source_datasets:
+  - original
+task_categories:
+  - text-classification
+task_ids:
+  - fact-checking
+  - intent-classification
+---
+
+# Dataset Card for Bend the Truth (Urdu Fake News)
+
+## Table of Contents
+
+- [Dataset Description](#dataset-description)
+  - [Dataset Summary](#dataset-summary)
+  - [Supported Tasks](#supported-tasks-and-leaderboards)
+  - [Languages](#languages)
+- [Dataset Structure](#dataset-structure)
+  - [Data Instances](#data-instances)
+  - [Data Fields](#data-instances)
+  - [Data Splits](#data-instances)
+- [Dataset Creation](#dataset-creation)
+  - [Curation Rationale](#curation-rationale)
+  - [Source Data](#source-data)
+  - [Annotations](#annotations)
+  - [Personal and Sensitive Information](#personal-and-sensitive-information)
+- [Considerations for Using the Data](#considerations-for-using-the-data)
+  - [Social Impact of Dataset](#social-impact-of-dataset)
+  - [Discussion of Biases](#discussion-of-biases)
+  - [Other Known Limitations](#other-known-limitations)
+- [Additional Information](#additional-information)
+  - [Dataset Curators](#dataset-curators)
+  - [Licensing Information](#licensing-information)
+  - [Citation Information](#citation-information)
+
+## Dataset Description
+
+- **Homepage:**
+- **Repository:** https://github.com/MaazAmjad/Datasets-for-Urdu-news
+- **Paper:**
+- **Leaderboard:**
+- **Point of Contact:** <a href="https://github.com/MaazAmjad">Maaz Amjad</a>
+
+### Dataset Summary
+
+[More Information Needed]
+
+### Supported Tasks and Leaderboards
+
+[More Information Needed]
+
+### Languages
+
+[More Information Needed]
+
+## Dataset Structure
+
+### Data Instances
+
+[More Information Needed]
+
+### Data Fields
+
+- news: a string in urdu
+- label: the label indicating whethere the provided news is real or fake.
+- category: The intent of the news being presented. The available 5 classes are Sports, Health, Technology, Entertainment, and Business.
+
+### Data Splits
+
+[More Information Needed]
+
+## Dataset Creation
+
+### Curation Rationale
+
+[More Information Needed]
+
+### Source Data
+
+#### Initial Data Collection and Normalization
+
+[More Information Needed]
+
+#### Who are the source language producers?
+
+[More Information Needed]
+
+### Annotations
+
+#### Annotation process
+
+[More Information Needed]
+
+#### Who are the annotators?
+
+[More Information Needed]
+
+### Personal and Sensitive Information
+
+[More Information Needed]
+
+## Considerations for Using the Data
+
+### Social Impact of Dataset
+
+[More Information Needed]
+
+### Discussion of Biases
+
+[More Information Needed]
+
+### Other Known Limitations
+
+[More Information Needed]
+
+## Additional Information
+
+### Dataset Curators
+
+[More Information Needed]
+
+### Licensing Information
+
+[More Information Needed]
+
+### Citation Information
+
+[More Information Needed]
---
+++ datasets/urdu_fake_news/urdu_fake_news.py
@@ -0,0 +1,98 @@
+"""Urdu Fake News Dataset"""
+
+from __future__ import absolute_import, division, print_function
+
+import glob
+import os
+
+import datasets
+
+
+_CITATION = """
+@article{MaazUrdufake2020,
+author = {Amjad, Maaz and Sidorov, Grigori and Zhila, Alisa and  G’{o}mez-Adorno, Helena and Voronkov, Ilia  and Gelbukh, Alexander},
+title = {Bend the Truth: A Benchmark Dataset for Fake News Detection in Urdu and Its Evaluation},
+journal={Journal of Intelligent & Fuzzy Systems},
+volume={39},
+number={2},
+pages={2457-2469},
+doi = {10.3233/JIFS-179905},
+year={2020},
+publisher={IOS Press}
+}
+"""
+
+_DESCRIPTION = """
+Urdu fake news datasets that contain news of 5 different news domains.
+These domains are Sports, Health, Technology, Entertainment, and Business.
+The real news are collected by combining manual approaches.
+"""
+
+_URL = "https://github.com/MaazAmjad/Datasets-for-Urdu-news/blob/master/"
+_URL += "Urdu%20Fake%20News%20Dataset.zip?raw=true"
+
+
+class UrduFakeNews(datasets.GeneratorBasedBuilder):
+    VERSION = datasets.Version("1.0.0")
+
+    category_list = [
+        "bus",
+        "hlth",
+        "sp",
+        "tch",
+        "sbz",
+    ]
+
+    def _info(self):
+        labels_list = ["Fake", "Real"]
+
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "news": datasets.Value("string"),
+                    "label": datasets.ClassLabel(names=labels_list),
+                    "category": datasets.ClassLabel(names=self.category_list),
+                }
+            ),
+            homepage="https://github.com/MaazAmjad/Datasets-for-Urdu-news",
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        """Returns SplitGenerators."""
+        dl_path = dl_manager.download_and_extract(_URL)
+        input_path = os.path.join(dl_path, "1.Corpus")
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                gen_kwargs={"pattern": os.path.join(input_path, "Train", "*", "*.txt")},
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.TEST,
+                gen_kwargs={"pattern": os.path.join(input_path, "Test", "*", "*.txt")},
+            ),
+        ]
+
+    def _generate_examples(self, pattern=None):
+        """Yields examples."""
+        for filename in sorted(glob.glob(pattern)):
+
+            with open(filename, encoding="utf-8") as f:
+                news = ""
+                for line in f:
+                    if line == "\n":
+                        continue
+                    news += line
+
+            name = os.path.basename(filename)
+            key = name.rstrip(".txt")
+
+            _class = 1 if ("Real" in filename) else 0
+            category = "".join([i for i in key if not i.isdigit()])
+            if category != "":
+                category = self.category_list.index(category)
+            else:
+                category = 0
+
+            yield key, {"news": news, "label": _class, "category": category}
