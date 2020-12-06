1535ecb64329da63f9113f80b6f8ee4384de3774
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2020-12-06 16:06:58 +0100

    Add metadata to dataset script

--- datasets/large_spanish_corpus/large_spanish_corpus.py
+++ datasets/large_spanish_corpus/large_spanish_corpus.py
@@ -12,72 +12,62 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""TODO: Add a description here."""
+"""The Large Spanish Corpus is a compilation of Spanish corpora spanning Wikipedia to European parliament notes."""
 
 from __future__ import absolute_import, division, print_function
 
-import csv
-import json
 import os
 
 import datasets
 
 
-# TODO: Add BibTeX citation
-# Find for instance the citation on arxiv or on the dataset repo/website
 _CITATION = """\
-@InProceedings{huggingface:dataset,
-title = {A great new dataset},
-authors={huggingface, Inc.
-},
-year={2020}
+@dataset{jose_canete_2019_3247731,
+  author       = {José Cañete},
+  title        = {Compilation of Large Spanish Unannotated Corpora},
+  month        = may,
+  year         = 2019,
+  publisher    = {Zenodo},
+  doi          = {10.5281/zenodo.3247731},
+  url          = {https://doi.org/10.5281/zenodo.3247731}
 }
 """
 
-# TODO: Add description of the dataset here
-# You can copy an official description
 _DESCRIPTION = """\
-This new dataset is designed to solve this great NLP task and is crafted with a lot of care. 
+The Large Spanish Corpus is a compilation of Spanish corpora spanning Wikipedia to European parliament notes. It is especially useful for pretraining language models.
 """
 
-# TODO: Add a link to an official homepage for the dataset here
-_HOMEPAGE = ""
+_HOMEPAGE = "https://github.com/josecannete/spanish-corpora"
 
-# TODO: Add the licence for the dataset here if you can find it
 _LICENSE = ""
 
-# TODO: Add link to the official dataset URLs here
-# The HuggingFace dataset library don't host the datasets but only point to the original files
-# This can be an arbitrary nested dict/list of URLs (see below in `_split_generators` method)
 _URL = "https://zenodo.org/record/3247731/files/raw.tar.bz2"
 
-_CORPORA = ['JRC',
- 'EMEA',
- 'GlobalVoices',
- 'ECB',
- 'DOGC',
- 'all_wikis',
- 'TED',
- 'multiUN',
- 'Europarl',
- 'NewsCommentary11',
- 'UN',
- 'EUBookShop',
- 'ParaCrawl',
- 'OpenSubtitles2018',
- 'DGT']
+_CORPORA = [
+    "JRC",
+    "EMEA",
+    "GlobalVoices",
+    "ECB",
+    "DOGC",
+    "all_wikis",
+    "TED",
+    "multiUN",
+    "Europarl",
+    "NewsCommentary11",
+    "UN",
+    "EUBookShop",
+    "ParaCrawl",
+    "OpenSubtitles2018",
+    "DGT",
+]
 
-_CORPORA_FILEPATHS = {
-     corpus: os.path.join(
-        "spanish-corpora/raw",
-        f"{corpus}.txt"
-        )
-        for corpus in _CORPORA
-        }
+# the untarred archive has the structure spanish-corpora/raw/{corpus_name}.txt
+_CORPORA_FILEPATHS = {corpus: os.path.join("spanish-corpora/raw", f"{corpus}.txt") for corpus in _CORPORA}
 
 _VERSION = "1.1.0"
 
 _COMBINED = "combined"
+
 
 class LargeSpanishCorpusConfig(datasets.BuilderConfig):
     def __init__(self, corpora=None, **kwargs):
@@ -89,9 +79,8 @@ class LargeSpanishCorpusConfig(datasets.BuilderConfig):
         return [_CORPORA_FILEPATHS[corpus] for corpus in self.corpora]
 
 
-# TODO: Name of the dataset usually match the script name with CamelCase instead of snake_case
 class LargeSpanishCorpus(datasets.GeneratorBasedBuilder):
-    """TODO: Short description of my dataset."""
+    """The Large Spanish Corpus."""
 
     BUILDER_CONFIGS = [
         LargeSpanishCorpusConfig(name=corpus, corpora=[corpus], description=f"Spanish examples in corpus {corpus}.")
@@ -123,25 +112,14 @@ class LargeSpanishCorpus(datasets.GeneratorBasedBuilder):
         )
 
     def _split_generators(self, dl_manager):
-        """Returns SplitGenerators."""
-        # TODO: This method is tasked with downloading/extracting the data and defining the splits depending on the configuration
-        # If several configurations are possible (listed in BUILDER_CONFIGS), the configuration selected by the user is in self.config.name
-
-        # dl_manager is a datasets.download.DownloadManager that can be used to download and extract URLs
-        # It can accept any type or nested list/dict and will give back the same structure with the url replaced with path to local files.
-        # By default the archives will be extracted and a path to a cached folder where they are extracted is returned instead of the archive 
         data_dir = dl_manager.download_and_extract(_URL)
         return [datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"data_dir": data_dir})]
 
     def _generate_examples(self, data_dir):
-        """ Yields examples. """
-        # TODO: This method will receive as arguments the `gen_kwargs` defined in the previous `_split_generators` method.
-        # It is in charge of opening the given file and yielding (key, example) tuples from the dataset
-        # The key is not important, it's more here for legacy reason (legacy from tfds)
         _id = 0
         print(self.config.filepaths)
         for filepath, corpus in zip(self.config.filepaths, self.config.corpora):
             filepath = os.path.join(data_dir, filepath)
-            with open(filepath, mode='r', encoding="utf-8") as f:
+            with open(filepath, mode="r", encoding="utf-8") as f:
                 yield _id, {"title": corpus, "text": f.read()},
                 _id += 1
