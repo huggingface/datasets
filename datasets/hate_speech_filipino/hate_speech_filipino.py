eb90daa9baf42fbf330c5526bf200a1cf3090028
anaerobeth <anaerobeth@gmail.com>
2020-12-06 21:03:43 -0500

    add Readme and tags for Hate Speech in Filipino

--- datasets/hate_speech_filipino/hate_speech_filipino.py
+++ datasets/hate_speech_filipino/hate_speech_filipino.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Text Classification Dataset in Filipino."""
+"""Hate Speech Text Classification Dataset in Filipino."""
 
 import csv
 import os
