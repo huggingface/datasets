513149f51ee5acac6f23b08b68cfe24fc363408e
purvimisal <purvivmisal@gmail.com>
2020-12-07 13:53:13 -0800

    Adding final changes

--- datasets/onestop_english/onestop_english.py
+++ datasets/onestop_english/onestop_english.py
@@ -41,7 +41,7 @@ _HOMEPAGE = "https://github.com/nishkalavallabhi/OneStopEnglishCorpus"
 
 _LICENSE = "Creative Commons Attribution-ShareAlike 4.0 International License"
 
-_URL = "https://github.com/nishkalavallabhi/OneStopEnglishCorpus/archive/master.zip"
+_URL = "https://github.com/purvimisal/OneStopCorpus-Compiled/raw/main/Texts-SeparatedByReadingLevel.zip"
 
 
 # TODO: Name of the dataset usually match the script name with CamelCase instead of snake_case
@@ -55,8 +55,8 @@ class OneStopEnglish(datasets.GeneratorBasedBuilder):
         return datasets.DatasetInfo(
             description=_DESCRIPTION,
             features=datasets.Features(
-            {"text": datasets.Value("string"), "label": datasets.features.ClassLabel(names=["ele", "int", "adv"])}
-            ), 
+                {"text": datasets.Value("string"), "label": datasets.features.ClassLabel(names=["ele", "int", "adv"])}
+            ),
             supervised_keys=[""],
             homepage=_HOMEPAGE,
             license=_LICENSE,
@@ -86,54 +86,60 @@ class OneStopEnglish(datasets.GeneratorBasedBuilder):
         ]
 
     def _get_examples_from_split(self, split_key, data_dir):
-        """ Reads the downloaded and extracted files and combines the individual text files to one dataset and split it into 80% train, 
-        10% validation and 10% test data. """
+        """Reads the downloaded and extracted files and combines the individual text files to one dataset and split it into 80% train,
+        10% validation and 10% test data."""
 
-        data_dir = os.path.join(data_dir, "OneStopEnglishCorpus-master/Texts-SeparatedByReadingLevel")
+        data_dir = os.path.join(data_dir, "Texts-SeparatedByReadingLevel")
 
         ele_samples = []
-        dir_path = data_dir + '/Ele-Txt/'
+        dir_path = data_dir + "/Ele-Txt/"
         files = os.listdir(dir_path)
+        print("DIRPATH", dir_path)
         for f in files:
             try:
-                with open(dir_path+f, encoding='utf-8-sig') as myfile:
+                with open(dir_path + f, encoding="utf-8-sig") as myfile:
                     text = myfile.read().strip()
                     ele_samples.append(text)
             except Exception as e:
-                print("Error with:", dir_path+f, e)
+                print("Error with:", dir_path + f, e)
 
         int_samples = []
-        dir_path = data_dir + '/Int-Txt/'
+        dir_path = data_dir + "/Int-Txt/"
+        print("DIRPATH", dir_path)
         files = os.listdir(dir_path)
         for f in files:
             try:
-                with open(dir_path+f, encoding='utf-8-sig') as myfile:
+                with open(dir_path + f, encoding="utf-8-sig") as myfile:
                     text = myfile.read().strip()
                     int_samples.append(text)
             except Exception as e:
-                print("Error with:", dir_path+f, e)
+                print("Error with:", dir_path + f, e)
 
         adv_samples = []
-        dir_path = data_dir + '/Adv-Txt/'
+        dir_path = data_dir + "/Adv-Txt/"
+        print("DIRPATH", dir_path)
         files = os.listdir(dir_path)
         for f in files:
             try:
-                with open(dir_path+f, encoding='utf-8-sig') as myfile:
+                with open(dir_path + f, encoding="utf-8-sig") as myfile:
                     text = myfile.read().strip()
                     adv_samples.append(text)
             except Exception as e:
-                print("Error with:", dir_path+f, e)    
+                print("Error with:", dir_path + f, e)
 
-        # 80/ 10/ 10 split of dataset 
-        i1 = int(len(ele_samples)*0.8 + 0.5)
-        i2 = int(len(ele_samples)*0.9 + 0.5)
+        # 80/ 10/ 10 split of dataset
+        i1 = int(len(ele_samples) * 0.8 + 0.5)
+        i2 = int(len(ele_samples) * 0.9 + 0.5)
         train_samples = ele_samples[:i1] + int_samples[:i1] + adv_samples[:i1]
         train_labels = (["ele"] * i1) + (["int"] * i1) + (["adv"] * i1)
         validation_samples = ele_samples[i1:i2] + int_samples[i1:i2] + adv_samples[i1:i2]
         validation_labels = (["ele"] * (i2 - i1)) + (["int"] * (i2 - i1)) + (["adv"] * (i1 - i1))
         test_samples = ele_samples[i2:] + int_samples[i2:] + adv_samples[i2:]
-        test_labels = (["ele"] * (len(ele_samples) - i2)) + (["int"] * (len(ele_samples) - i2)) + (["adv"] * (len(ele_samples) - i2))
-
+        test_labels = (
+            (["ele"] * (len(ele_samples) - i2))
+            + (["int"] * (len(ele_samples) - i2))
+            + (["adv"] * (len(ele_samples) - i2))
+        )
 
         if split_key == "train":
             return (train_samples, train_labels)
@@ -143,7 +149,7 @@ class OneStopEnglish(datasets.GeneratorBasedBuilder):
             return (test_samples, test_labels)
         else:
             raise ValueError(f"Invalid split key {split_key}")
-        
+
     def _generate_examples(self, split_key, data_dir):
         """Yields examples for a given split of dataset."""
         split_text, split_labels = self._get_examples_from_split(split_key, data_dir)
