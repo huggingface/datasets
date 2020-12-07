bef1f7d9658d9bc5321b700e5fb9f293bbc536fe
sandip <sandip.aero@gmail.com>
2020-12-07 16:32:48 +0530

    Opus montenegrinsubs (#1166)
    * Opus MontenegrinSubs dataset for machine translation task, for language pair en-me.
    
    * Update datasets/opus_montenegrinsubs/README.md
    
    * Update datasets/opus_montenegrinsubs/opus_montenegrinsubs.py
    
    * Update datasets/opus_montenegrinsubs/README.md
    
    * Update datasets/opus_montenegrinsubs/dataset_infos.json
    
    Co-authored-by: Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>

---
+++ datasets/opus_montenegrinsubs/opus_montenegrinsubs.py
@@ -0,0 +1,91 @@
+# coding=utf-8
+# Copyright 2020 The HuggingFace Datasets Authors and the current dataset script contributor.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Opus MontenegrinSubs dataset for machine translation task, for language pair en-me."""
+
+from __future__ import absolute_import, division, print_function
+
+import os
+
+import datasets
+
+
+_CITATION = """\
+J. Tiedemann, 2012, Parallel Data, Tools and Interfaces in OPUS. In Proceedings of the 8th International Conference \
+on Language Resources and Evaluation (LREC 2012)"""
+
+
+_DESCRIPTION = """\
+Opus MontenegrinSubs dataset for machine translation task, for language pair en-me: english and montenegrin
+"""
+
+
+_HOMEPAGE = "http://opus.nlpl.eu/MontenegrinSubs.php"
+
+
+_LICENSE = ""
+
+_URLs = {"train": "https://object.pouta.csc.fi/OPUS-MontenegrinSubs/v1/moses/en-me.txt.zip"}
+
+
+class OpusMontenegrinsubs(datasets.GeneratorBasedBuilder):
+
+    VERSION = datasets.Version("1.0.0")
+
+    BUILDER_CONFIGS = [datasets.BuilderConfig(name="en-me", version=VERSION)]
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {"translation": datasets.features.Translation(languages=tuple(self.config.name.split("-")))}
+            ),
+            supervised_keys=None,
+            homepage="http://opus.nlpl.eu/MontenegrinSubs.php",
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        """Returns SplitGenerators."""
+        data_dir = dl_manager.download_and_extract(_URLs)
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                # These kwargs will be passed to _generate_examples
+                gen_kwargs={
+                    "source_file": os.path.join(data_dir["train"], "MontenegrinSubs.en-me.en"),
+                    "target_file": os.path.join(data_dir["train"], "MontenegrinSubs.en-me.me"),
+                    "split": "train",
+                },
+            ),
+        ]
+
+    def _generate_examples(self, source_file, target_file, split):
+        """This function returns the examples in the raw (text) form."""
+        with open(source_file, encoding="utf-8") as f:
+            source_sentences = f.read().split("\n")
+        with open(target_file, encoding="utf-8") as f:
+            target_sentences = f.read().split("\n")
+
+        assert len(target_sentences) == len(source_sentences), "Sizes do not match: %d vs %d for %s vs %s." % (
+            len(source_sentences),
+            len(target_sentences),
+            source_file,
+            target_file,
+        )
+
+        source, target = tuple(self.config.name.split("-"))
+        for idx, (l1, l2) in enumerate(zip(source_sentences, target_sentences)):
+            result = {"translation": {source: l1, target: l2}}
+            yield idx, result
