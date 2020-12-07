1a9e845bf4f91c7a5c4454063f4abfbc8923c166
SBrandeis <s.brandeis@orange.fr>
2020-12-07 14:40:15 +0100

    Add code_search_net script
    Small refactor
    
    Remove unused split gen_kwarg
    
    Remove 'all' config
    
    Re-add the 'all' configuration
    
    Add description in VESRION
    
    Reformatting bibtex citation
    
    rename fields
    
    style changes

---
+++ datasets/code_search_net/code_search_net.py
@@ -0,0 +1,216 @@
+# coding=utf-8
+# Copyright 2020 The HuggingFace Datasets Authors and the current dataset script contributor.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""CodeSearchNet corpus: proxy dataset for semantic code search"""
+
+# TODO: add licensing info in the examples
+# TODO: log richer informations (especially while extracting the jsonl.gz files)
+# TODO: enable custom configs; such as: "java+python"
+# TODO: enable fetching examples with a given license, eg: "java_MIT"
+
+from __future__ import absolute_import, division, print_function
+
+import csv
+import json
+import os
+
+import datasets
+
+
+_CITATION = """\
+@article{husain2019codesearchnet,
+    title={{CodeSearchNet} challenge: Evaluating the state of semantic code search},
+    author={Husain, Hamel and Wu, Ho-Hsiang and Gazit, Tiferet and Allamanis, Miltiadis and Brockschmidt, Marc},
+    journal={arXiv preprint arXiv:1909.09436},
+    year={2019}
+}
+"""
+
+_DESCRIPTION = """\
+CodeSearchNet corpus contains about 6 million functions from open-source code \
+spanning six programming languages (Go, Java, JavaScript, PHP, Python, and Ruby). \
+The CodeSearchNet Corpus also contains automatically generated query-like \
+natural language for 2 million functions, obtained from mechanically scraping \
+and preprocessing associated function documentation.
+"""
+
+_HOMEPAGE = "https://github.com/github/CodeSearchNet"
+
+_LICENSE = "Various"
+
+_S3_BUCKET_URL = "https://s3.amazonaws.com/code-search-net/CodeSearchNet/v2/"
+_AVAILABLE_LANGUAGES = ["python", "java", "javascript", "go", "ruby", "php"]
+_URLs = {language: [(language, _S3_BUCKET_URL + f"{language}.zip")] for language in _AVAILABLE_LANGUAGES}
+# URLs for "all" are juste the concatenation of URLs for all languages
+_URLs["all"] = sum(_URLs.values(), [])
+
+
+class CodeSearchNet(datasets.GeneratorBasedBuilder):
+    """"CodeSearchNet corpus: proxy dataset for semantic code search."""
+
+    VERSION = datasets.Version("1.0.0", "Add CodeSearchNet corpus dataset")
+    BUILDER_CONFIGS = [
+        datasets.BuilderConfig(
+            name="all",
+            version=VERSION,
+            description="All available languages: Java, Go, Javascript, Python, PHP, Ruby",
+        ),
+        datasets.BuilderConfig(
+            name="java",
+            version=VERSION,
+            description="Java language",
+        ),
+        datasets.BuilderConfig(
+            name="go",
+            version=VERSION,
+            description="Go language",
+        ),
+        datasets.BuilderConfig(
+            name="python",
+            version=VERSION,
+            description="Pyhton language",
+        ),
+        datasets.BuilderConfig(
+            name="javascript",
+            version=VERSION,
+            description="Javascript language",
+        ),
+        datasets.BuilderConfig(
+            name="ruby",
+            version=VERSION,
+            description="Ruby language",
+        ),
+        datasets.BuilderConfig(
+            name="php",
+            version=VERSION,
+            description="PHP language",
+        ),
+    ]
+
+    DEFAULT_CONFIG_NAME = "all"
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "repository_name": datasets.Value("string"),
+                    "func_path_in_repository": datasets.Value("string"),
+                    "func_name": datasets.Value("string"),
+                    "whole_func_string": datasets.Value("string"),
+                    "language": datasets.Value("string"),
+                    "func_code_string": datasets.Value("string"),
+                    "func_code_tokens": datasets.Sequence(datasets.Value("string")),
+                    "func_documentation_string": datasets.Value("string"),
+                    "func_documentation_tokens": datasets.Sequence(datasets.Value("string")),
+                    "split_name": datasets.Value("string"),
+                    "func_code_url": datasets.Value("string"),
+                    # TODO - add licensing info in the examples
+                }
+            ),
+            # No default supervised keys
+            supervised_keys=None,
+            homepage=_HOMEPAGE,
+            license=_LICENSE,
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        """Returns SplitGenerators.
+
+        Note: The original data is stored in S3, and follows this unusual directory structure:
+            ```
+            .
+            ├── <language_name>  # e.g. python
+            │   └── final
+            │       └── jsonl
+            │           ├── test
+            │           │   └── <language_name>_test_0.jsonl.gz
+            │           ├── train
+            │           │   ├── <language_name>_train_0.jsonl.gz
+            │           │   ├── <language_name>_train_1.jsonl.gz
+            │           │   ├── ...
+            │           │   └── <language_name>_train_n.jsonl.gz
+            │           └── valid
+            │               └── <language_name>_valid_0.jsonl.gz
+            ├── <language_name>_dedupe_definitions_v2.pkl
+            └── <language_name>_licenses.pkl
+            ```
+        """
+        data_urls = _URLs[self.config.name]
+        # Extracts the language archives
+        lang_names_and_dirs = [(language, dl_manager.download_and_extract(url)) for language, url in data_urls]
+        data_dirs = [
+            os.path.join(lang_dir, lang_name, "final", "jsonl") for lang_name, lang_dir in lang_names_and_dirs
+        ]
+        split_dirs = {
+            split_name: [os.path.join(data_dir, split_name) for data_dir in data_dirs]
+            for split_name in ["train", "test", "valid"]
+        }
+
+        split_paths = {
+            split_name: [
+                dl_manager.extract(entry.path)
+                for split_dir in split_dirs[split_name]
+                for entry in os.scandir(split_dir)
+                if entry.is_file() and entry.name.endswith(".jsonl.gz")
+            ]
+            for split_name in ["train", "test", "valid"]
+        }
+
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                gen_kwargs={
+                    "filepaths": split_paths["train"],
+                },
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.TEST,
+                gen_kwargs={
+                    "filepaths": split_paths["test"],
+                },
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.VALIDATION,
+                gen_kwargs={
+                    "filepaths": split_paths["valid"],
+                },
+            ),
+        ]
+
+    def _generate_examples(self, filepaths):
+        """Yields the examples by iterating through the available jsonl files."""
+        for file_id_, filepath in enumerate(filepaths):
+            with open(filepath, encoding="utf-8") as f:
+                for row_id_, row in enumerate(f):
+                    # Key of the example = dir_id_ + entry_id + row_id,
+                    # to ensure all examples have a distinct key
+                    id_ = file_id_ + row_id_
+                    data = json.loads(row)
+                    yield id_, {
+                        "repository_name": data["repo"],
+                        "func_path_in_repository": data["path"],
+                        "func_name": data["func_name"],
+                        "whole_func_string": data["original_string"],
+                        "language": data["language"],
+                        "func_code_string": data["code"],
+                        "func_code_tokens": data["code_tokens"],
+                        "func_documentation_string": data["docstring"],
+                        "func_documentation_tokens": data["docstring_tokens"],
+                        "split_name": data["partition"],
+                        "func_code_url": data["url"],
+                    }
