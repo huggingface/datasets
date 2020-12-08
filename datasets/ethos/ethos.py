14a10cdd79701005a7c40e4b000f4efa3529719b
John Mollas <iamollas@csd.auth.gr>
2020-12-08 20:01:00 +0200

    ethos plz pass the test

--- datasets/ethos/ethos.py
+++ datasets/ethos/ethos.py
@@ -1,13 +1,9 @@
 """Ethos  dataset"""
 
 from __future__ import absolute_import, division, print_function
-
 import logging
-
 import pandas as pd
-
 import datasets
-
 
 _CITATION = """
 @misc{mollas2020ethos,
@@ -54,8 +50,7 @@ class EthosConfig(datasets.BuilderConfig):
         elif variation.lower() == 'multilabel':
             self.variation = 'multilabel'
         else:
-            logging.warning("Wrong variation. Could be either \
-                'binary' or 'multilabel', using 'binary' instead.")
+            logging.warning("Wrong variation. Could be either 'binary' or 'multilabel', using 'binary' instead.")
             self.variation = 'binary'
         super(EthosConfig, self).__init__(**kwargs)
 
@@ -80,46 +75,43 @@ class Ethos(datasets.GeneratorBasedBuilder):
 
     def _info(self):
         if self.config.variation == 'binary':
-            f=datasets.Features(
-            {
-                "text": datasets.Value("string"),
-                "label": datasets.features.ClassLabel(names=["no_hate_speech", "hate_speech"]),
-            })
+            f = datasets.Features(
+                {
+                    "text": datasets.Value("string"),
+                    "label": datasets.features.ClassLabel(names=["no_hate_speech", "hate_speech"]),
+                })
         else:
-            f=datasets.Features(
-            {
-                "text": datasets.Value("string"),
-                "violence": datasets.Value("int32"),
-                "directed_vs_generalized": datasets.Value("int32"),
-                "gender": datasets.Value("int32"),
-                "race": datasets.Value("int32"),
-                "national_origin": datasets.Value("int32"),
-                "disability": datasets.Value("int32"),
-                "religion": datasets.Value("int32"),
-                "sexual_orientation": datasets.Value("int32"),
-            })
+            f = datasets.Features(
+                {
+                    "text": datasets.Value("string"),
+                    "violence": datasets.Value("int32"),
+                    "directed_vs_generalized": datasets.Value("int32"),
+                    "gender": datasets.Value("int32"),
+                    "race": datasets.Value("int32"),
+                    "national_origin": datasets.Value("int32"),
+                    "disability": datasets.Value("int32"),
+                    "religion": datasets.Value("int32"),
+                    "sexual_orientation": datasets.Value("int32"),
+                })
         return datasets.DatasetInfo(
             features=f,
             supervised_keys=None,
             homepage=
-            "https://github.com/intelligence-csd-auth-gr/Ethos-Hate-Speech-Dataset/tree/master\
-                /ethos/ethos_data",
+            "https://github.com/intelligence-csd-auth-gr/Ethos-Hate-Speech-Dataset/tree/masterethos/ethos_data",
             citation=_CITATION,
         )
 
     def _split_generators(self, dl_manager):
         if self.config.variation == 'binary':
-            url = { 'train':  
-            'https://raw.githubusercontent.com/intelligence-csd-auth-gr/Ethos-Hate-Speech-Dataset/master\
-                /ethos/ethos_data/Ethos_Dataset_Binary.csv'}
+            url = {'train': 'https://raw.githubusercontent.com/intelligence-csd-auth-gr/Ethos-Hate-Speech-Dataset'
+                            '/master/ethos/ethos_data/Ethos_Dataset_Binary.csv'}
         else:
-            url = {'train': 
-            'https://raw.githubusercontent.com/intelligence-csd-auth-gr/Ethos-Hate-Speech-Dataset/master\
-                /ethos/ethos_data/Ethos_Dataset_Multi_Label.csv'}
+            url = {'train': 'https://raw.githubusercontent.com/intelligence-csd-auth-gr/Ethos-Hate-Speech-Dataset'
+                            '/master/ethos/ethos_data/Ethos_Dataset_Multi_Label.csv'}
         downloaded_files = dl_manager.download_and_extract(url)
-        
-        return [datasets.SplitGenerator( name=datasets.Split.TRAIN, 
-                                         gen_kwargs={"filepath": downloaded_files["train"]})]
+
+        return [datasets.SplitGenerator(name=datasets.Split.TRAIN,
+                                        gen_kwargs={"filepath": downloaded_files["train"]})]
 
     def _generate_examples(self, filepath):
         """Yields examples."""
@@ -127,33 +119,33 @@ class Ethos(datasets.GeneratorBasedBuilder):
         data = pd.read_csv(filepath, delimiter=';')
         if self.config.variation == 'binary':
 
-            X = data['comment'].values
-            y = [1 if i >=0.5 else 0 for i in data['isHate'].values]
+            x = data['comment'].values
+            y = [1 if i >= 0.5 else 0 for i in data['isHate'].values]
             class_names = ["no_hate_speech", "hate_speech"]
-            for i in range(len(X)):
+            for i in range(len(x)):
                 _id = i
-                yield _id, { 'text': X[i], 'label':class_names[y[i]]}
+                yield _id, {'text': x[i], 'label': class_names[y[i]]}
         else:
-            X = data['comment'].values
-            yT = data.loc[:,data.columns != 'comment'].values
+            x = data['comment'].values
+            y_temp = data.loc[:, data.columns != 'comment'].values
             y = []
-            for yt in yT:
+            for yt in y_temp:
                 yi = []
                 for i in yt:
-                    if i>=0.5:
+                    if i >= 0.5:
                         yi.append(int(1))
                     else:
                         yi.append(int(0))
                 y.append(yi)
-            for i in range(len(X)):
+            for i in range(len(x)):
                 _id = i
-                print(X[i],y[i])
-                yield _id, { "text": X[i],
-                "violence": y[i][0],
-                "directed_vs_generalized": y[i][1],
-                "gender": y[i][2],
-                "race": y[i][3],
-                "national_origin": y[i][4],
-                "disability": y[i][5],
-                "religion": y[i][6],
-                "sexual_orientation": y[i][7]}
+                print(x[i], y[i])
+                yield _id, {"text": x[i],
+                            "violence": y[i][0],
+                            "directed_vs_generalized": y[i][1],
+                            "gender": y[i][2],
+                            "race": y[i][3],
+                            "national_origin": y[i][4],
+                            "disability": y[i][5],
+                            "religion": y[i][6],
+                            "sexual_orientation": y[i][7]}
