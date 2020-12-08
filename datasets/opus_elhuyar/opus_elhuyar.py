975798815638de1033e309ed470236bffa4ec7a3
sandip <sandip.aero@gmail.com>
2020-12-08 21:40:31 +0530

    Update opus_elhuyar.py

--- datasets/opus_elhuyar/opus_elhuyar.py
+++ datasets/opus_elhuyar/opus_elhuyar.py
@@ -22,8 +22,12 @@ import datasets
 
 
 _CITATION = """\
-J. Tiedemann, 2012, Parallel Data, Tools and Interfaces in OPUS. In Proceedings of the 8th International \
-Conference on Language Resources and Evaluation (LREC 2012)"""
+@InProceedings{opus:Elhuyar,
+title = {Parallel Data, Tools and Interfaces in OPUS. In Proceedings of the 8th International \
+Conference on Language Resources and Evaluation (LREC 2012)},
+authors={J. Tiedemann},
+year={2012}
+}"""
 
 
 _DESCRIPTION = """\
