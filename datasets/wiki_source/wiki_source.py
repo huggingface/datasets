845572bb0f0156f5ef1358cea188763a644e53f9
abhishek thakur <abhishekkrthakur@users.noreply.github.com>
2020-12-10 16:28:43 +0100

    Update wiki_source.py

--- datasets/wiki_source/wiki_source.py
+++ datasets/wiki_source/wiki_source.py
@@ -107,5 +107,4 @@ class WikiSource(datasets.GeneratorBasedBuilder):
                         "translation": {l1: x, l2: y},
                     },
                 )
-                sentence_counter += 1
                 yield result
