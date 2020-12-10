f6cc4e0fc6a36b6ccb80cce506be1c833f67ad30
abhishek thakur <abhishekkrthakur@users.noreply.github.com>
2020-12-10 11:06:01 +0100

    Update tilde_model.py

--- datasets/tilde_model/tilde_model.py
+++ datasets/tilde_model/tilde_model.py
@@ -70,12 +70,12 @@ class TildeModelConfig(datasets.BuilderConfig):
 
 
 class TildeModel(datasets.GeneratorBasedBuilder):
-    VERSION = datasets.Version(_VERSION)
     BUILDER_CONFIGS = [
         TildeModelConfig(
             lang1=lang1,
             lang2=lang2,
             description=f"Translating {lang1} to {lang2} or vice versa",
+            version=datasets.Version(_VERSION),
         )
         for lang1, lang2 in _LANGUAGE_PAIRS
     ]
