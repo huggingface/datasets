644c6feb0eb7d5fcedeffa98d36dc5a8d0643e1b
abhishek thakur <abhishekkrthakur@users.noreply.github.com>
2020-12-10 11:02:13 +0100

    Update multi_para_crawl.py

--- datasets/multi_para_crawl/multi_para_crawl.py
+++ datasets/multi_para_crawl/multi_para_crawl.py
@@ -77,6 +77,7 @@ class MultiParaCrawl(datasets.GeneratorBasedBuilder):
             lang1=lang1,
             lang2=lang2,
             description=f"Translating {lang1} to {lang2} or vice versa",
+            version=datasets.Version(_VERSION),
         )
         for lang1, lang2 in _LANGUAGE_PAIRS
     ]
