a5b0565837a98860317d349321aed58f978fa248
abhishek thakur <abhishekkrthakur@users.noreply.github.com>
2020-12-10 16:28:26 +0100

    Update ofis_publik.py

--- datasets/ofis_publik/ofis_publik.py
+++ datasets/ofis_publik/ofis_publik.py
@@ -108,5 +108,4 @@ class OfisPublik(datasets.GeneratorBasedBuilder):
                         "translation": {l1: x, l2: y},
                     },
                 )
-                sentence_counter += 1
                 yield result
