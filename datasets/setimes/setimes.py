47221fa347c8c44f8a190c663a7a908107627736
abhishek thakur <abhishekkrthakur@users.noreply.github.com>
2020-12-10 11:05:22 +0100

    Update setimes.py

--- datasets/setimes/setimes.py
+++ datasets/setimes/setimes.py
@@ -95,12 +95,12 @@ class SetimesConfig(datasets.BuilderConfig):
 
 
 class Setimes(datasets.GeneratorBasedBuilder):
-    VERSION = datasets.Version(_VERSION)
     BUILDER_CONFIGS = [
         SetimesConfig(
             lang1=lang1,
             lang2=lang2,
             description=f"Translating {lang1} to {lang2} or vice versa",
+            version=datasets.Version(_VERSION),
         )
         for lang1, lang2 in _LANGUAGE_PAIRS
     ]
