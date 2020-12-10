40b19aaf4c894a6721530a2e1dc919d91e87b2ae
abhishek thakur <abhishekkrthakur@users.noreply.github.com>
2020-12-10 11:06:48 +0100

    Update books.py

--- datasets/books/books.py
+++ datasets/books/books.py
@@ -128,12 +128,12 @@ class BooksConfig(datasets.BuilderConfig):
 
 
 class Books(datasets.GeneratorBasedBuilder):
-    VERSION = datasets.Version(_VERSION)
     BUILDER_CONFIGS = [
         BooksConfig(
             lang1=lang1,
             lang2=lang2,
             description=f"Translating {lang1} to {lang2} or vice versa",
+            version=datasets.Version(_VERSION),
         )
         for lang1, lang2 in _LANGUAGE_PAIRS
     ]
