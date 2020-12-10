12e057ed7fb7750faaf6ee5920342bd808516865
Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>
2020-12-10 16:10:21 +0100

    Update datasets/php/php.py

--- datasets/php/php.py
+++ datasets/php/php.py
@@ -30,7 +30,7 @@ total number of sentence fragments: 1.38M
 _HOMEPAGE_URL = "http://opus.nlpl.eu/PHP.php"
 _CITATION = """\
 @InProceedings{TIEDEMANN12.463,
-  author = {J�rg Tiedemann},
+  author = {J{\"o}rg Tiedemann},
   title = {Parallel Data, Tools and Interfaces in OPUS},
   booktitle = {Proceedings of the Eight International Conference on Language Resources and Evaluation (LREC'12)},
   year = {2012},
