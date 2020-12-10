1fca902bac0065a78dad76d03eab0dfbf1c00de9
abhishek thakur <abhishekkrthakur@users.noreply.github.com>
2020-12-10 10:34:59 +0100

    Update spc.py

--- datasets/spc/spc.py
+++ datasets/spc/spc.py
@@ -62,7 +62,6 @@ class SpcConfig(datasets.BuilderConfig):
         super().__init__(
             *args,
             name=f"{lang1}-{lang2}",
-            version=datasets.Version(_VERSION, ""),
             **kwargs,
         )
         self.lang1 = lang1
@@ -70,6 +69,7 @@ class SpcConfig(datasets.BuilderConfig):
 
 
 class Spc(datasets.GeneratorBasedBuilder):
+    VERSION = datasets.Version(_VERSION)
     BUILDER_CONFIGS = [
         SpcConfig(
             lang1=lang1,
