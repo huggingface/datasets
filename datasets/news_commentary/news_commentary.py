6c9994d088f59e06e3c966bc7f26da4e8d727a69
abhishek thakur <abhishekkrthakur@users.noreply.github.com>
2020-12-10 11:04:39 +0100

    Update news_commentary.py

--- datasets/news_commentary/news_commentary.py
+++ datasets/news_commentary/news_commentary.py
@@ -127,12 +127,12 @@ class NewsCommentaryConfig(datasets.BuilderConfig):
 
 
 class NewsCommentary(datasets.GeneratorBasedBuilder):
-    VERSION = datasets.Version(_VERSION)
     BUILDER_CONFIGS = [
         NewsCommentaryConfig(
             lang1=lang1,
             lang2=lang2,
             description=f"Translating {lang1} to {lang2} or vice versa",
+            version=datasets.Version(_VERSION),
         )
         for lang1, lang2 in _LANGUAGE_PAIRS
     ]
