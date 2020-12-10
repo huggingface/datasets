84886e29f39ad0c08bdc18ac4f69e01bc24f3910
abhishek thakur <abhishekkrthakur@users.noreply.github.com>
2020-12-10 11:05:41 +0100

    Update emea.py

--- datasets/emea/emea.py
+++ datasets/emea/emea.py
@@ -60,12 +60,12 @@ class EmeaConfig(datasets.BuilderConfig):
 
 
 class Emea(datasets.GeneratorBasedBuilder):
-    VERSION = datasets.Version(_VERSION)
     BUILDER_CONFIGS = [
         EmeaConfig(
             lang1=lang1,
             lang2=lang2,
             description=f"Translating {lang1} to {lang2} or vice versa",
+            version=datasets.Version(_VERSION),
         )
         for lang1, lang2 in _LANGUAGE_PAIRS
     ]
