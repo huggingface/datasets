08b1a80079b2323dcb00eeaefd443daf0a76bbcb
abhishek thakur <abhishekkrthakur@users.noreply.github.com>
2020-12-10 11:00:47 +0100

    Update wiki_source.py

--- datasets/wiki_source/wiki_source.py
+++ datasets/wiki_source/wiki_source.py
@@ -57,6 +57,7 @@ class WikiSource(datasets.GeneratorBasedBuilder):
             lang1=lang1,
             lang2=lang2,
             description=f"Translating {lang1} to {lang2} or vice versa",
+            version=datasets.Version(_VERSION),
         )
         for lang1, lang2 in _LANGUAGE_PAIRS
     ]
