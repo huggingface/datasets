3a3eef3cfbadfa873d3afc64e17211a1db66883e
Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>
2020-12-14 10:44:02 +0100

    remove comments

--- datasets/samsum/samsum.py
+++ datasets/samsum/samsum.py
@@ -23,8 +23,6 @@ import py7zr
 import datasets
 
 
-# TODO: Add BibTeX citation
-# Find for instance the citation on arxiv or on the dataset repo/website
 _CITATION = """
 @article{gliwa2019samsum,
   title={SAMSum Corpus: A Human-annotated Dialogue Dataset for Abstractive Summarization},
@@ -34,8 +32,6 @@ _CITATION = """
 }
 """
 
-# TODO: Add description of the dataset here
-# You can copy an official description
 _DESCRIPTION = """
 SAMSum Corpus contains over 16k chat dialogues with manually annotated
 summaries.
@@ -45,76 +41,42 @@ There are two features:
   - id: id of a example.
 """
 
-# TODO: Add a link to an official homepage for the dataset here
 _HOMEPAGE = "https://arxiv.org/abs/1911.12237v2"
 
-# TODO: Add the licence for the dataset here if you can find it
 _LICENSE = "CC BY-NC-ND 4.0"
 
-# TODO: Add link to the official dataset URLs here
-# The HuggingFace dataset library don't host the datasets but only point to the original files
-# This can be an arbitrary nested dict/list of URLs (see below in `_split_generators` method)
 _URLs = "https://arxiv.org/src/1911.12237v2/anc/corpus.7z"
 
 
-# TODO: Name of the dataset usually match the script name with CamelCase instead of snake_case
 class Samsum(datasets.GeneratorBasedBuilder):
     """SAMSum Corpus dataset."""
 
     VERSION = datasets.Version("1.1.0")
 
-    # This is an example of a dataset with multiple configurations.
-    # If you don't want/need to define several sub-sets in your dataset,
-    # just remove the BUILDER_CONFIG_CLASS and the BUILDER_CONFIGS attributes.
-
-    # If you need to make complex sub-parts in the datasets with configurable options
-    # You can create your own builder configuration class to store attribute, inheriting from datasets.BuilderConfig
-    # BUILDER_CONFIG_CLASS = MyBuilderConfig
-
-    # You will be able to load one or the other configurations in the following list with
-    # data = datasets.load_dataset('my_dataset', 'first_domain')
-    # data = datasets.load_dataset('my_dataset', 'second_domain')
     BUILDER_CONFIGS = [
         datasets.BuilderConfig(name="samsum"),
     ]
 
-    # DEFAULT_CONFIG_NAME = "first_domain"  # It's not mandatory to have a default configuration. Just use one if it make sense.
 
     def _info(self):
-        # TODO: This method specifies the datasets.DatasetInfo object which contains informations and typings for the dataset
         features = datasets.Features(
             {
                 "id": datasets.Value("string"),
                 "dialogue": datasets.Value("string"),
                 "summary": datasets.Value("string"),
-                # These are the features of your dataset like images, labels ...
             }
         )
         return datasets.DatasetInfo(
-            # This is the description that will appear on the datasets page.
             description=_DESCRIPTION,
-            # This defines the different columns of the dataset and their types
-            features=features,  # Here we define them above because they are different between the two configurations
-            # If there's a common (input, target) tuple from the features,
-            # specify them here. They'll be used if as_supervised=True in
-            # builder.as_dataset.
+            features=features,
             supervised_keys=None,
-            # Homepage of the dataset for documentation
             homepage=_HOMEPAGE,
-            # License for the dataset if available
             license=_LICENSE,
-            # Citation for the dataset
             citation=_CITATION,
         )
 
     def _split_generators(self, dl_manager):
         """Returns SplitGenerators."""
-        # TODO: This method is tasked with downloading/extracting the data and defining the splits depending on the configuration
-        # If several configurations are possible (listed in BUILDER_CONFIGS), the configuration selected by the user is in self.config.name
-
-        # dl_manager is a datasets.download.DownloadManager that can be used to download and extract URLs
-        # It can accept any type or nested list/dict and will give back the same structure with the url replaced with path to local files.
-        # By default the archives will be extracted and a path to a cached folder where they are extracted is returned instead of the archive
         my_urls = _URLs
         path = dl_manager.download_and_extract(my_urls)
         return [
@@ -146,9 +108,6 @@ class Samsum(datasets.GeneratorBasedBuilder):
 
     def _generate_examples(self, filepath, split):
         """ Yields examples. """
-        # TODO: This method will receive as arguments the `gen_kwargs` defined in the previous `_split_generators` method.
-        # It is in charge of opening the given file and yielding (key, example) tuples from the dataset
-        # The key is not important, it's more here for legacy reason (legacy from tfds)
 
         path, fname = filepath
 
