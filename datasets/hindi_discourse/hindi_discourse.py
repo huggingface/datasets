1755e0bd24dd8e730798b43b2b1688f1e381741b
Hritwik Dutta <hritwikdutta@Hritwiks-MacBook-Pro.local>
2020-12-14 23:50:56 +0530

    fixed whitespace issues

--- datasets/hindi_discourse/hindi_discourse.py
+++ datasets/hindi_discourse/hindi_discourse.py
@@ -32,10 +32,10 @@ _CITATION = """\
 """
 
 _DESCRIPTION = """\
-The Hindi Discourse Analysis dataset is a corpus for analyzing discourse modes present in its sentences. 
-It contains sentences from stories written by 11 famous authors from the 20th Century. 
-4-5 stories by each author have been selected which were available in the public domain resulting 
-in a collection of 53 stories. Most of these short stories were originally written in Hindi 
+The Hindi Discourse Analysis dataset is a corpus for analyzing discourse modes present in its sentences.
+It contains sentences from stories written by 11 famous authors from the 20th Century.
+4-5 stories by each author have been selected which were available in the public domain resulting
+in a collection of 53 stories. Most of these short stories were originally written in Hindi
 but some of them were written in other Indian languages and later translated to Hindi.
 """
 
