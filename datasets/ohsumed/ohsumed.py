1fb939b6e776067e40d3dd586023f446f3951cb9
skyprince999 <aakashg80@gmail.com>
2020-12-15 18:33:13 +0530

    Update datasets/ohsumed/ohsumed.py deleted print line
    Co-authored-by: Simon Brandeis <33657802+SBrandeis@users.noreply.github.com>

--- datasets/ohsumed/ohsumed.py
+++ datasets/ohsumed/ohsumed.py
@@ -150,7 +150,6 @@ class Ohsumed(datasets.GeneratorBasedBuilder):
         # TODO: This method will receive as arguments the `gen_kwargs` defined in the previous `_split_generators` method.
         # It is in charge of opening the given file and yielding (key, example) tuples from the dataset
         # The key is not important, it's more here for legacy reason (legacy from tfds)
-        print(filepath)
         with open(filepath, encoding="utf-8") as f:
             abstracts = json.load(f)
             for id_, data in enumerate(abstracts):
