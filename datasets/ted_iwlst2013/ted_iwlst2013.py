b3e6536fafdbc2f77ed55126e297de627e5b8275
Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>
2020-12-16 17:43:45 +0100

    Update ted_iwlst2013.py

--- datasets/ted_iwlst2013/ted_iwlst2013.py
+++ datasets/ted_iwlst2013/ted_iwlst2013.py
@@ -111,5 +111,4 @@ class TedIwlst2013(datasets.GeneratorBasedBuilder):
                         "translation": {l1: x, l2: y},
                     },
                 )
-                sentence_counter += 1
                 yield result
