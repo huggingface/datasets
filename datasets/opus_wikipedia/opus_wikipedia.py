ff9373c749a0cd5bae7a7fc3112cf53e24aa5de4
Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>
2020-12-17 15:24:40 +0100

    Update opus_wikipedia.py

--- datasets/opus_wikipedia/opus_wikipedia.py
+++ datasets/opus_wikipedia/opus_wikipedia.py
@@ -29,7 +29,7 @@ total number of sentence fragments: 25.90M
 _HOMEPAGE_URL = "http://opus.nlpl.eu/Wikipedia.php"
 _CITATION = """\
 @InProceedings{TIEDEMANN12.463,
-  author = {J�rg Tiedemann},
+  author = {J{\"o}rg Tiedemann},
   title = {Parallel Data, Tools and Interfaces in OPUS},
   booktitle = {Proceedings of the Eight International Conference on Language Resources and Evaluation (LREC'12)},
   year = {2012},
