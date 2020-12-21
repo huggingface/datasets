5d6316d715327fc8709e5381e8e11ca7b4c1e4cb
abdulelahsm <abdulelah.almesfer@gmail.com>
2020-12-21 22:54:42 +0300

    added 17 more batches and made minor adjustments to README.md

--- datasets/saudinewsnet/saudinewsnet.py
+++ datasets/saudinewsnet/saudinewsnet.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""The dataset contains a set of 2,972 Arabic newspaper articles alongwith metadata, extracted from various online Saudi newspapers."""
+"""The dataset contains a set of 31,030 Arabic newspaper articles alongwith metadata, extracted from various online Saudi newspapers."""
 
 from __future__ import absolute_import, division, print_function
 
@@ -32,7 +32,7 @@ url = "http://github.com/ParallelMazen/SaudiNewsNet"
 """
 
 
-_DESCRIPTION = """The dataset contains a set of 2,972 Arabic newspaper articles alongwith metadata, \
+_DESCRIPTION = """The dataset contains a set of 31,030 Arabic newspaper articles alongwith metadata, \
 extracted from various online Saudi newspapers and written in MSA."""
 
 
@@ -42,11 +42,51 @@ _HOMEPAGE = "https://github.com/parallelfold/SaudiNewsNet"
 _LICENSE = "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License."
 
 
-_URL = "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-07-21.zip"
+_URLs = [
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-07-21.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-07-22.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-07-23.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-07-24.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-07-25.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-07-26.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-07-27.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-07-31.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-08-01.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-08-02.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-08-03.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-08-04.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-08-06.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-08-07.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-08-08.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-08-09.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-08-10.zip",
+    "https://github.com/parallelfold/SaudiNewsNet/raw/master/dataset/2015-08-11.zip",
+]
+
+_dirs = [
+    "2015-07-21.json",
+    "2015-07-22.json",
+    "2015-07-23.json",
+    "2015-07-24.json",
+    "2015-07-25.json",
+    "2015-07-26.json",
+    "2015-07-27.json",
+    "2015-07-31.json",
+    "2015-08-01.json",
+    "2015-08-02.json",
+    "2015-08-03.json",
+    "2015-08-04.json",
+    "2015-08-06.json",
+    "2015-08-07.json",
+    "2015-08-08.json",
+    "2015-08-09.json",
+    "2015-08-10.json",
+    "2015-08-11.json",
+]
 
 
-class SaudiNewsNet(datasets.GeneratorBasedBuilder):
-    """a set of 2972 Arabic newspaper articles along with metadata"""
+class Saudinewsnet(datasets.GeneratorBasedBuilder):
+    """a set of 31,030 Arabic newspaper articles along with metadata"""
 
     def _info(self):
         return datasets.DatasetInfo(
@@ -72,31 +112,35 @@ class SaudiNewsNet(datasets.GeneratorBasedBuilder):
 
     def _split_generators(self, dl_manager):
         """Returns SplitGenerators."""
-        datadir = dl_manager.download_and_extract(_URL)
+        datadir = dl_manager.download_and_extract(_URLs)
+        pt = []
+        for dd, d in zip(datadir, _dirs):
+            pt.append(os.path.join(dd, d))
         return [
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN,
-                gen_kwargs={"filepath": os.path.join(datadir, "2015-07-21.json"), "split": "train"},
+                gen_kwargs={"filepath": pt, "split": "train"},
             )
         ]
 
     def _generate_examples(self, filepath, split):
         """Generates examples"""
-        with open(filepath, encoding="utf-8") as f:
-            articles = json.load(f)
-            for _id, article in enumerate(articles):
-                title = article.get("title", "")
-                source = article["source"]
-                dt = article["date_extracted"]
-                link = article["url"]
-                author = article.get("author", "").strip("&nbsp;")
-                content = article["content"].strip("/n")
+        for path in filepath:
+            with open(path, encoding="utf-8") as f:
+                articles = json.load(f)
+                for _id, article in enumerate(articles):
+                    title = article.get("title", "")
+                    source = article["source"]
+                    dt = article["date_extracted"]
+                    link = article["url"]
+                    author = article.get("author", "").strip("&nbsp;")
+                    content = article["content"].strip("/n")
 
-                yield _id, {
-                    "title": title,
-                    "source": source,
-                    "date_extracted": dt,
-                    "url": link,
-                    "author": author,
-                    "content": content,
-                }
+                    yield _id, {
+                        "title": title,
+                        "source": source,
+                        "date_extracted": dt,
+                        "url": link,
+                        "author": author,
+                        "content": content,
+                    }
