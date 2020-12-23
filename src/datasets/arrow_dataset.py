f4f366f8571d01470c84b7bd940fdd0499da2e4f
Noa Onoszko <noaono@hotmail.com>
2020-12-23 14:30:00 +0100

    Fixed bug in the shape property

--- src/datasets/arrow_dataset.py
+++ src/datasets/arrow_dataset.py
@@ -528,7 +528,7 @@ class Dataset(DatasetInfoMixin, IndexableMixin):
     def shape(self) -> Tuple[int]:
         """Shape of the dataset (number of columns, number of rows)."""
         if self._indices is not None:
-            return tuple(self._indices.num_rows, self._data.num_columns)
+            return (self._indices.num_rows, self._data.num_columns)
         return self._data.shape
 
     def unique(self, column: str) -> List[Any]:
