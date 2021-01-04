dae68804ec3feb97c5c74d6fb761a1c91fd3bd15
Quentin Lhoest <lhoest.q@gmail.com>
2021-01-04 19:30:06 +0100

    Release: 1.2.0

--- setup.py
+++ setup.py
@@ -139,7 +139,7 @@ EXTRAS_REQUIRE = {
 
 setup(
     name='datasets',
-    version="1.1.3",
+    version="1.2.0",
     description=DOCLINES[0],
     long_description='\n'.join(DOCLINES[2:]),
     author='HuggingFace Inc.',
