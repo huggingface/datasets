fd987f742ba2f97b1c29372999c7f5b04759e145
Song Feng <sngfng@gmail.com>
2021-01-06 11:27:15 -0500

    reg sync
    - add utterance to answers
    - add bart for doc2dial

--- datasets/doc2dial/doc2dial.py
+++ datasets/doc2dial/doc2dial.py
@@ -93,12 +93,7 @@ class Doc2dial(datasets.GeneratorBasedBuilder):
                             "turn_id": datasets.Value("int32"),
                             "role": datasets.Value("string"),
                             "da": datasets.Value("string"),
-                            "reference": [
-                                {
-                                    "keys": datasets.Value("string"),
-                                    "values": datasets.Value("string"),
-                                }
-                            ],
+                            "reference": [{"keys": datasets.Value("string"), "values": datasets.Value("string"),}],
                             "utterance": datasets.Value("string"),
                         }
                     ],
@@ -138,6 +133,8 @@ class Doc2dial(datasets.GeneratorBasedBuilder):
                     "answers": datasets.features.Sequence(
                         {
                             "text": datasets.Value("string"),
+                            "utterance": datasets.Value("string"),
+                            "da": datasets.Value("string"),
                             "answer_start": datasets.Value("int32"),
                             "answer_end": datasets.Value("int32"),
                             "sp_id": datasets.features.Sequence(datasets.Value("string")),
@@ -169,11 +166,7 @@ class Doc2dial(datasets.GeneratorBasedBuilder):
             )
 
         return datasets.DatasetInfo(
-            description=_DESCRIPTION,
-            features=features,
-            supervised_keys=None,
-            homepage=_HOMEPAGE,
-            citation=_CITATION,
+            description=_DESCRIPTION, features=features, supervised_keys=None, homepage=_HOMEPAGE, citation=_CITATION,
         )
 
     def _split_generators(self, dl_manager):
@@ -200,9 +193,7 @@ class Doc2dial(datasets.GeneratorBasedBuilder):
             return [
                 datasets.SplitGenerator(
                     name=datasets.Split.TRAIN,
-                    gen_kwargs={
-                        "filepath": os.path.join(data_dir, "doc2dial/v0.9/data/doc2dial_doc.json"),
-                    },
+                    gen_kwargs={"filepath": os.path.join(data_dir, "doc2dial/v0.9/data/doc2dial_doc.json"),},
                 )
             ]
         elif self.config.name == "doc2dial_rc":
@@ -219,12 +210,7 @@ class Doc2dial(datasets.GeneratorBasedBuilder):
                     name=datasets.Split.TRAIN,
                     gen_kwargs={
                         "filepath": os.path.join(
-                            data_dir,
-                            "doc2dial",
-                            "v0.9",
-                            "data",
-                            "woOOD",
-                            "doc2dial_dial_train.json",
+                            data_dir, "doc2dial", "v0.9", "data", "woOOD", "doc2dial_dial_train.json",
                         ),
                     },
                 ),
@@ -291,10 +277,7 @@ class Doc2dial(datasets.GeneratorBasedBuilder):
                                         "role": i["role"],
                                         "da": i["da"],
                                         "reference": [
-                                            {
-                                                "keys": ref,
-                                                "values": str(i["reference"][ref]),
-                                            }
+                                            {"keys": ref, "values": str(i["reference"][ref]),}
                                             for ref in i["reference"]
                                         ],
                                         "utterance": i["utterance"],
@@ -352,10 +335,9 @@ class Doc2dial(datasets.GeneratorBasedBuilder):
                 for domain, d_doc_dials in dial_data.items():
                     for doc_id, dials in d_doc_dials.items():
                         doc = doc_data[domain][doc_id]
-                        (
-                            start_pos_char_candidates,
-                            end_pos_char_candidates,
-                        ) = self._get_start_end_candidates_rc(doc["spans"])
+                        (start_pos_char_candidates, end_pos_char_candidates,) = self._get_start_end_candidates_rc(
+                            doc["spans"]
+                        )
                         for dial in dials:
                             all_prev_utterances = []
                             all_prev_turns = []
@@ -395,7 +377,12 @@ class Doc2dial(datasets.GeneratorBasedBuilder):
                                     qa["is_impossible"] = True
                                 else:
                                     qa["is_impossible"] = False
-                                    qa["answers"] = turn_to_predict["references"]
+                                    qa["answers"] = []
+                                    for ele in turn_to_predict["references"]:
+                                        e = dict(ele)
+                                        e["utterance"] = turn_to_predict["utterance"]
+                                        e["da"] = turn_to_predict["da"]
+                                        qa["answers"].append(e)
                                     assert (
                                         len((qa["answers"])) >= 1
                                     ), "Ensure the answers are not empty if the question is answerable"
--- dgd_utils/my_run.py
+++ dgd_utils/my_run.py
@@ -5,28 +5,13 @@ from collections import defaultdict
 from datasets import load_dataset
 
 
-def load_doc2dial():
-    dataset = load_dataset("datasets/doc2dial", "doc2dial_rc", split="train")
-    # dataset = load_dataset("datasets/doc2dial", "dialogue_domain", split="train")
-    for ele in dataset:
-        # print(json.dumps(ele, indent=4))
-        # break
-        # if len(ele["doc_text"].split()) < 200:
-        #     # ele["spans"] = ele["spans"][:3]
-        #     print(json.dumps(ele, indent=4))
-        #     break
-        if len(ele["doc_context"].split()) < 200:
-            print(json.dumps(ele, indent=4))
-            break
-
-
 def btag(tag, text):
     text = text.replace("\n", "\t")
     return "<{}> {}".format(tag, text)
 
 
 def load_sharc_bart(split, out_path=None):
-    dataset = load_dataset("sharc", split="train")
+    dataset = load_dataset("sharc", split=split)
     source = []
     target = []
     if out_path and not os.path.exists(out_path):
@@ -56,7 +41,7 @@ def load_sharc_bart(split, out_path=None):
 
 
 def load_doqa_bart(split, out_path=None):
-    dataset = load_dataset("doqa", name="cooking", split="train")
+    dataset = load_dataset("doqa", name="cooking", split=split)
     source = []
     target = []
     if out_path and not os.path.exists(out_path):
@@ -89,7 +74,42 @@ def load_doqa_bart(split, out_path=None):
         fp.close()
 
 
+def load_doc2dial_bart(split, out_path=None):
+    dataset = load_dataset(
+        "/Users/songfeng/work/projects/hf_datasets/datasets/doc2dial/", name="doc2dial_rc", split=split
+    )
+    source = []
+    target = []
+    if out_path and not os.path.exists(out_path):
+        os.makedirs(out_path)
+    contexts = []
+    dial_id_pre = None
+    for ex in dataset:
+        if ex["is_impossible"]:
+            continue
+        dial_id, turn_id = ex["id"].split("_")
+        user_context = btag("user", ex["question"])
+        da_context = btag("da", ex["answers"]["da"][0])
+        agent_context = btag("agent", ex["answers"]["utterance"][0])
+        agent_utterance = ex["answers"]["utterance"][0]
+        if dial_id_pre != dial_id:
+            contexts = [
+                btag("title", ex["title"]),
+                btag("doc_context", ex["doc_context"]),
+            ]
+        dial_id_pre = dial_id
+        source.append("\t".join(contexts + [user_context, da_context]))
+        target.append(agent_utterance)
+        contexts.extend([user_context, agent_context])
+    with open(os.path.join(out_path, "{}.source".format(split)), "w") as fp:
+        fp.write("\n".join(source))
+        fp.close()
+    with open(os.path.join(out_path, "{}.target".format(split)), "w") as fp:
+        fp.write("\n".join(target))
+        fp.close()
+
+
 if __name__ == "__main__":
-    load_doqa_bart("train", "~/work/projects/data/my_bart_doqa/")
+    # load_doqa_bart("train", "~/work/projects/data/my_bart_doqa/")
     # load_sharc_bart("train", "~/work/projects/data/my_bart_sharc/")
-    # load_doc2dial()
+    load_doc2dial_bart("validation", "my_bart_doc2dial/")
