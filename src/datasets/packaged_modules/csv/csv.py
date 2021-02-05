0761e0a0b81c453be84028804765813066946b57
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-02-05 12:03:00 +0100

    Print error message with filename when malformed CSV

--- src/datasets/packaged_modules/csv/csv.py
+++ src/datasets/packaged_modules/csv/csv.py
@@ -126,9 +126,15 @@ class Csv(datasets.ArrowBasedBuilder):
                 float_precision=self.config.float_precision,
                 chunksize=self.config.chunksize,
             )
-            for batch_idx, df in enumerate(csv_file_reader):
-                pa_table = pa.Table.from_pandas(df, schema=schema)
-                # Uncomment for debugging (will print the Arrow table size and elements)
-                # logger.warning(f"pa_table: {pa_table} num rows: {pa_table.num_rows}")
-                # logger.warning('\n'.join(str(pa_table.slice(i, 1).to_pydict()) for i in range(pa_table.num_rows)))
-                yield (file_idx, batch_idx), pa_table
+
+            try:
+                for batch_idx, df in enumerate(csv_file_reader):
+                    pa_table = pa.Table.from_pandas(df, schema=schema)
+                    # Uncomment for debugging (will print the Arrow table size and elements)
+                    # logger.warning(f"pa_table: {pa_table} num rows: {pa_table.num_rows}")
+                    # logger.warning('\n'.join(str(pa_table.slice(i, 1).to_pydict()) for i in range(pa_table.num_rows)))
+                    yield (file_idx, batch_idx), pa_table
+            except ValueError as e:
+                # logger.error(f"Failed to read file '{csv_file_reader.f}' with error {type(e)}: {e}")
+                print(f"Failed to read file '{csv_file_reader.f}' with error {type(e)}: {e}")
+                raise
