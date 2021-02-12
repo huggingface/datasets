30339eb0bbdf38f97e84fae247ecd5a8019d122d
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-02-12 09:58:36 +0100

    Always use memory_map for Parquet

--- src/datasets/arrow_reader.py
+++ src/datasets/arrow_reader.py
@@ -341,14 +341,15 @@ class ParquetReader(BaseReader):
         super().__init__(path, info)
         self._filetype_suffix = "parquet"
 
-    def _get_dataset_from_filename(self, filename_skip_take, in_memory=False):
+    def _get_dataset_from_filename(self, filename_skip_take, **kwargs):
         """Returns a Dataset instance from given (filename, skip, take)."""
         filename, skip, take = (
             filename_skip_take["filename"],
             filename_skip_take["skip"] if "skip" in filename_skip_take else None,
             filename_skip_take["take"] if "take" in filename_skip_take else None,
         )
-        pa_table = pa.parquet.read_table(filename, memory_map=not in_memory)
+        # Parquet read_table always loads data in memory, independently of memory_map
+        pa_table = pa.parquet.read_table(filename, memory_map=True)
         # here we don't want to slice an empty table, or it may segfault
         if skip is not None and take is not None and not (skip == 0 and take == len(pa_table)):
             pa_table = pa_table.slice(skip, take)
