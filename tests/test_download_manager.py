26b23fb229fcc521f406294b272e3d0cd5f86e78
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-02-15 09:18:58 +0100

    Test multiple download/extract calls

--- tests/test_download_manager.py
+++ tests/test_download_manager.py
@@ -26,12 +26,19 @@ def mock_request(*args, **kwargs):
     return MockResponse()
 
 
-@pytest.mark.parametrize("urls", [URL, [URL], {"train": URL}])
-def test_download_manager_download(urls, tmp_path, monkeypatch):
+@pytest.mark.parametrize("urls_type", [str, list, dict])
+def test_download_manager_download(urls_type, tmp_path, monkeypatch):
     import requests
 
     monkeypatch.setattr(requests, "request", mock_request)
 
+    url = URL
+    if issubclass(urls_type, str):
+        urls = url
+    elif issubclass(urls_type, list):
+        urls = [url]
+    elif issubclass(urls_type, dict):
+        urls = {"train": url}
     dataset_name = "dummy"
     cache_subdir = "downloads"
     cache_dir_root = str(tmp_path)
@@ -40,26 +47,28 @@ def test_download_manager_download(urls, tmp_path, monkeypatch):
         use_etag=False,
     )
     dl_manager = DownloadManager(dataset_name=dataset_name, download_config=download_config)
-    downloaded_paths = dl_manager.download(urls)
-    for downloaded_paths in [downloaded_paths, dl_manager.downloaded_paths]:
-        if isinstance(urls, str):
-            downloaded_paths = [downloaded_paths]
-        elif isinstance(urls, dict):
-            assert "train" in downloaded_paths.keys()
-            downloaded_paths = downloaded_paths.values()
-        assert downloaded_paths
-        for downloaded_path in downloaded_paths:
-            downloaded_path = Path(downloaded_path)
-            parts = downloaded_path.parts
-            assert parts[-1] == HASH
-            assert parts[-2] == cache_subdir
-            assert downloaded_path.exists()
-            content = downloaded_path.read_text()
-            assert content == CONTENT
-            metadata_downloaded_path = downloaded_path.with_suffix(".json")
-            assert metadata_downloaded_path.exists()
-            metadata_content = json.loads(metadata_downloaded_path.read_text())
-            assert metadata_content == {"url": URL, "etag": None}
+    num_download_calls = 2
+    for download_call in range(num_download_calls):
+        downloaded_paths = dl_manager.download(urls)
+        for downloaded_paths in [downloaded_paths, dl_manager.downloaded_paths[download_call]]:
+            if isinstance(urls, str):
+                downloaded_paths = [downloaded_paths]
+            elif isinstance(urls, dict):
+                assert "train" in downloaded_paths.keys()
+                downloaded_paths = downloaded_paths.values()
+            assert downloaded_paths
+            for downloaded_path in downloaded_paths:
+                downloaded_path = Path(downloaded_path)
+                parts = downloaded_path.parts
+                assert parts[-1] == HASH
+                assert parts[-2] == cache_subdir
+                assert downloaded_path.exists()
+                content = downloaded_path.read_text()
+                assert content == CONTENT
+                metadata_downloaded_path = downloaded_path.with_suffix(".json")
+                assert metadata_downloaded_path.exists()
+                metadata_content = json.loads(metadata_downloaded_path.read_text())
+                assert metadata_content == {"url": URL, "etag": None}
 
 
 @pytest.mark.parametrize("paths_type", [str, list, dict])
@@ -79,23 +88,25 @@ def test_download_manager_extract(paths_type, xz_file, text_file):
         use_etag=False,
     )
     dl_manager = DownloadManager(dataset_name=dataset_name, download_config=download_config)
-    extracted_paths = dl_manager.extract(paths)
-    input_paths = paths
-    for extracted_paths in [extracted_paths, dl_manager.extracted_paths]:
-        if isinstance(paths, str):
-            extracted_paths = [extracted_paths]
-            input_paths = [paths]
-        elif isinstance(paths, dict):
-            assert "train" in extracted_paths.keys()
-            extracted_paths = extracted_paths.values()
-            input_paths = paths.values()
-        assert extracted_paths
-        for extracted_path, input_path in zip(extracted_paths, input_paths):
-            extracted_path = Path(extracted_path)
-            parts = extracted_path.parts
-            assert parts[-1] == hash_url_to_filename(input_path, etag=None)
-            assert parts[-2] == extracted_subdir
-            assert extracted_path.exists()
-            extracted_file_content = extracted_path.read_text()
-            expected_file_content = text_file.read_text()
-            assert extracted_file_content == expected_file_content
+    num_extract_calls = 2
+    for extract_call in range(num_extract_calls):
+        extracted_paths = dl_manager.extract(paths)
+        input_paths = paths
+        for extracted_paths in [extracted_paths, dl_manager.extracted_paths[extract_call]]:
+            if isinstance(paths, str):
+                extracted_paths = [extracted_paths]
+                input_paths = [paths]
+            elif isinstance(paths, dict):
+                assert "train" in extracted_paths.keys()
+                extracted_paths = extracted_paths.values()
+                input_paths = paths.values()
+            assert extracted_paths
+            for extracted_path, input_path in zip(extracted_paths, input_paths):
+                extracted_path = Path(extracted_path)
+                parts = extracted_path.parts
+                assert parts[-1] == hash_url_to_filename(input_path, etag=None)
+                assert parts[-2] == extracted_subdir
+                assert extracted_path.exists()
+                extracted_file_content = extracted_path.read_text()
+                expected_file_content = text_file.read_text()
+                assert extracted_file_content == expected_file_content
