043e92983049616f37239279f3ac58a935bfb408
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-02-19 10:32:29 +0100

    Fix style

--- src/datasets/utils/file_utils.py
+++ src/datasets/utils/file_utils.py
@@ -528,7 +528,6 @@ def get_from_cache(
             if url.startswith("ftp://"):
                 ftp_get(url, temp_file, proxies=proxies, resume_size=resume_size, headers=headers, cookies=cookies)
             else:
-                callback = lambda chunk: temp_file.write(chunk)
                 RemoteManager.http_get(
                     url,
                     proxies=proxies,
@@ -536,7 +535,7 @@ def get_from_cache(
                     headers=headers,
                     cookies=cookies,
                     max_retries=max_retries,
-                    callback=callback,
+                    callback=lambda chunk: temp_file.write(chunk),
                 )
 
         logger.info("storing %s in cache at %s", url, cache_path)
