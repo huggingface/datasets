4ef4aff5ef42c4f694f6b4fdc2eb456707f96366
Justin <justin@yan.family>
2021-02-20 14:01:06 -0800

    Standardizing datasets dtypes to a defined set

--- tests/test_features.py
+++ tests/test_features.py
@@ -10,6 +10,7 @@ from datasets.features import (
     Features,
     Sequence,
     Value,
+    _arrow_to_datasets_dtype,
     _cast_to_python_objects,
     cast_to_python_objects,
     string_to_arrow,
@@ -45,21 +46,22 @@ class FeaturesTest(TestCase):
             pa.timestamp("ns", tz="America/New_York"),
             pa.string(),
             pa.int32(),
+            pa.float64(),
         ]
         for dt in supported_pyarrow_datatypes:
-            self.assertEqual(dt, string_to_arrow(str(dt)))
+            self.assertEqual(dt, string_to_arrow(_arrow_to_datasets_dtype(dt)))
 
         unsupported_pyarrow_datatypes = [pa.list_(pa.float64())]
         for dt in unsupported_pyarrow_datatypes:
             with self.assertRaises(ValueError):
-                string_to_arrow(str(dt))
+                string_to_arrow(_arrow_to_datasets_dtype(dt))
 
-        supported_pyarrow_dtypes = ["timestamp[ns]", "timestamp[ns, tz=+07:30]", "int32"]
-        for sdt in supported_pyarrow_dtypes:
-            self.assertEqual(sdt, str(string_to_arrow(sdt)))
+        supported_datasets_dtypes = ["timestamp[ns]", "timestamp[ns, tz=+07:30]", "int32", "float64"]
+        for sdt in supported_datasets_dtypes:
+            self.assertEqual(sdt, _arrow_to_datasets_dtype(string_to_arrow(sdt)))
 
-        unsupported_pyarrow_dtypes = ["timestamp[blob]", "timestamp[[ns]]", "timestamp[ns, tz=[ns]]", "int"]
-        for sdt in unsupported_pyarrow_dtypes:
+        unsupported_datasets_dtypes = ["timestamp[blob]", "timestamp[[ns]]", "timestamp[ns, tz=[ns]]", "int"]
+        for sdt in unsupported_datasets_dtypes:
             with self.assertRaises(ValueError):
                 string_to_arrow(sdt)
 
