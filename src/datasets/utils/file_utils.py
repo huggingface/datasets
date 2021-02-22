b3f45b0e88f33c024d54ea63b0c58f02060a2145
mariosasko <mariosasko777@gmail.com>
2021-02-23 00:01:03 +0100

    Remove unused import

--- src/datasets/utils/file_utils.py
+++ src/datasets/utils/file_utils.py
@@ -21,7 +21,7 @@ from dataclasses import dataclass
 from functools import partial
 from hashlib import sha256
 from pathlib import Path
-from typing import Dict, List, Optional, Union
+from typing import Dict, Optional, Union
 from urllib.parse import urlparse
 from zipfile import ZipFile, is_zipfile
 
