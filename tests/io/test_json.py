7582e6c5c86fe270947fb6927969a6cbfd41e228
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-02-25 10:02:49 +0100

    Test JsonlDatasetReader features

--- tests/io/test_json.py
+++ tests/io/test_json.py
@@ -2,15 +2,15 @@ import json
 
 import pytest
 
-from datasets import NamedSplit
+from datasets import DatasetInfo, Features, NamedSplit, Value
 from datasets.io.json import JsonlDatasetReader
 
 
 DATA = [
-    {"col_1": "a", "col_2": 0, "col_3": 0.0},
-    {"col_1": "b", "col_2": 1, "col_3": 1.0},
-    {"col_1": "c", "col_2": 2, "col_3": 2.0},
-    {"col_1": "d", "col_2": 3, "col_3": 3.0},
+    {"col_1": "0", "col_2": 0, "col_3": 0.0},
+    {"col_1": "1", "col_2": 1, "col_3": 1.0},
+    {"col_1": "2", "col_2": 2, "col_3": 2.0},
+    {"col_1": "3", "col_2": 3, "col_3": 3.0},
 ]
 
 
@@ -23,14 +23,28 @@ def jsonl_path(tmp_path_factory):
     return path
 
 
+@pytest.mark.parametrize(
+    "features",
+    [
+        None,
+        {"col_1": "string", "col_2": "int64", "col_3": "float64"},
+        {"col_1": "string", "col_2": "string", "col_3": "string"},
+        {"col_1": "int32", "col_2": "int32", "col_3": "int32"},
+        {"col_1": "float32", "col_2": "float32", "col_3": "float32"},
+    ],
+)
 @pytest.mark.parametrize("split, expected_split", [(None, None), (NamedSplit("train"), "train")])
-def test_dataset_jsonl_reader(split, expected_split, jsonl_path):
+def test_dataset_jsonl_reader(split, expected_split, features, jsonl_path):
     path = jsonl_path
-    ds = JsonlDatasetReader(path, split=split).read()
+    default_expected_features = {"col_1": "string", "col_2": "int64", "col_3": "float64"}
+    expected_features = features.copy() if features else default_expected_features
+    features = Features({feature: Value(dtype) for feature, dtype in features.items()}) if features else None
+    info = DatasetInfo(features=features) if features else None
+
+    ds = JsonlDatasetReader(path, split=split, info=info).read()
     assert ds.num_rows == 4
     assert ds.num_columns == 3
-    assert ds.column_names == ["col_1", "col_2", "col_3"]
-    assert ds.features["col_1"].dtype == "string"
-    assert ds.features["col_2"].dtype == "int64"
-    assert ds.features["col_3"].dtype == "float64"
+    assert ds.column_names == ["col_1", "col_2", "col_3"] # feature_names
+    for feature, expected_dtype in expected_features.items():
+        assert ds.features[feature].dtype == expected_dtype
     assert ds.split == expected_split
