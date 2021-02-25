13dc2060f6805d4b147e34835103f8c04672e542
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-02-25 17:18:02 +0100

    Try-catch pyarrow exception

--- src/datasets/io/json.py
+++ src/datasets/io/json.py
@@ -1,3 +1,5 @@
+import json
+
 import pyarrow as pa
 import pyarrow.json as paj
 
@@ -13,9 +15,18 @@ class JsonlDatasetReader(AbstractDatasetReader):
 
     def _read_table(self):
         schema = pa.schema(self.info.features.type) if self.info and self.info.features else None
-        # try:
         with open(self.path, "rb") as f:
-            table = paj.read_json(f, parse_options=paj.ParseOptions(explicit_schema=schema))
+            try:
+                table = paj.read_json(f, parse_options=paj.ParseOptions(explicit_schema=schema))
+            except pa.ArrowInvalid:
+                with open(self.path, encoding="utf-8") as f:
+                    dataset = json.load(f)
+                raise ValueError(
+                    f"Not able to read records in the JSON file at {self.path}. "
+                    f"You should probably indicate the field of the JSON file containing your records. "
+                    f"This JSON file contain the following fields: {str(list(dataset.keys()))}. "
+                    f"Select the correct one and provide it as `field='XXX'` to the `load_dataset` method. "
+                )
         return table
 
     # def _cast_table_to_info_features(self, table):
