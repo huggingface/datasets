eb45f5c86d143e45e21887faeb58d2bf6c4aea21
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-02-25 16:39:03 +0100

    Test CsvDatasetReader features

--- tests/io/test_csv.py
+++ tests/io/test_csv.py
@@ -2,7 +2,7 @@ import csv
 
 import pytest
 
-from datasets import NamedSplit
+from datasets import DatasetInfo, Features, NamedSplit, Value
 from datasets.io.csv import CsvDatasetReader
 
 
@@ -25,12 +25,29 @@ def csv_path(tmp_path_factory):
     return path
 
 
+@pytest.mark.parametrize(
+    "features",
+    [
+        None,
+        {"col_1": "string", "col_2": "int64", "col_3": "float64"},
+        {"col_1": "string", "col_2": "string", "col_3": "string"},
+        {"col_1": "int32", "col_2": "int32", "col_3": "int32"},
+        {"col_1": "float32", "col_2": "float32", "col_3": "float32"},
+    ],
+)
 @pytest.mark.parametrize("split", [None, NamedSplit("train")])
-def test_dataset_csv_reader(split, csv_path):
+def test_dataset_csv_reader(split, features, csv_path):
     path = csv_path
+    # CSV file loses col_1 string dtype information: default now is "int64" instead of "string"
+    default_expected_features = {"col_1": "int64", "col_2": "int64", "col_3": "float64"}
+    expected_features = features.copy() if features else default_expected_features
+    features = Features({feature: Value(dtype) for feature, dtype in features.items()}) if features else None
+    info = DatasetInfo(features=features) if features else None
 
-    ds = CsvDatasetReader(path, split=split).read()
+    ds = CsvDatasetReader(path, split=split, info=info).read()
     assert ds.num_rows == 4
     assert ds.num_columns == 3
     assert ds.column_names == ["col_1", "col_2", "col_3"]
     assert ds.split == split
+    for feature, expected_dtype in expected_features.items():
+        assert ds.features[feature].dtype == expected_dtype
