c7b14dde436074efc6842bfc2a25ab53cc6f9087
mariosasko <mariosasko777@gmail.com>
2021-02-26 15:18:37 +0100

    Add cross-platform support for datasets-cli

--- src/datasets/commands/datasets_cli.py
+++ src/datasets/commands/datasets_cli.py
@@ -3,15 +3,16 @@ from argparse import ArgumentParser
 
 from datasets.commands.convert import ConvertCommand
 from datasets.commands.download import DownloadCommand
-from datasets.commands.env import EnvironmentCommand
-from datasets.commands.test import TestCommand
-from datasets.commands.run_beam import RunBeamCommand
 from datasets.commands.dummy_data import DummyDataCommand
+from datasets.commands.env import EnvironmentCommand
+from datasets.commands.run_beam import RunBeamCommand
+from datasets.commands.test import TestCommand
 from datasets.utils.logging import set_verbosity_info
 
-if __name__ == '__main__':
-    parser = ArgumentParser('HuggingFace Datasets CLI tool', usage='datasets-cli <command> [<args>]')
-    commands_parser = parser.add_subparsers(help='datasets-cli command helpers')
+
+def main():
+    parser = ArgumentParser("HuggingFace Datasets CLI tool", usage="datasets-cli <command> [<args>]")
+    commands_parser = parser.add_subparsers(help="datasets-cli command helpers")
     set_verbosity_info()
 
     # Register commands
@@ -25,10 +26,14 @@ if __name__ == '__main__':
     # Let's go
     args = parser.parse_args()
 
-    if not hasattr(args, 'func'):
+    if not hasattr(args, "func"):
         parser.print_help()
         exit(1)
 
     # Run
     service = args.func(args)
     service.run()
+
+
+if __name__ == "__main__":
+    main()
