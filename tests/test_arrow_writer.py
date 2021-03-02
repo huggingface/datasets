d2e123dcc27dc3e35a90b04c50c9db4d428d492b
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-03-02 10:31:20 +0100

    Use ArrowWriter as context manager in tests

--- tests/test_arrow_writer.py
+++ tests/test_arrow_writer.py
@@ -69,10 +69,10 @@ class ArrowWriterTest(TestCase):
 
     def test_write_no_schema(self):
         output = pa.BufferOutputStream()
-        writer = ArrowWriter(stream=output)
-        writer.write({"col_1": "foo", "col_2": 1})
-        writer.write({"col_1": "bar", "col_2": 2})
-        num_examples, num_bytes = writer.finalize()
+        with ArrowWriter(stream=output) as writer:
+            writer.write({"col_1": "foo", "col_2": 1})
+            writer.write({"col_1": "bar", "col_2": 2})
+            num_examples, num_bytes = writer.finalize()
         self.assertEqual(num_examples, 2)
         self.assertGreater(num_bytes, 0)
         fields = {"col_1": pa.string(), "col_2": pa.int64()}
@@ -82,10 +82,10 @@ class ArrowWriterTest(TestCase):
     def test_write_schema(self):
         fields = {"col_1": pa.string(), "col_2": pa.int64()}
         output = pa.BufferOutputStream()
-        writer = ArrowWriter(stream=output, schema=pa.schema(fields))
-        writer.write({"col_1": "foo", "col_2": 1})
-        writer.write({"col_1": "bar", "col_2": 2})
-        num_examples, num_bytes = writer.finalize()
+        with ArrowWriter(stream=output, schema=pa.schema(fields)) as writer:
+            writer.write({"col_1": "foo", "col_2": 1})
+            writer.write({"col_1": "bar", "col_2": 2})
+            num_examples, num_bytes = writer.finalize()
         self.assertEqual(num_examples, 2)
         self.assertGreater(num_bytes, 0)
         self.assertEqual(writer._schema, pa.schema(fields, metadata=writer._schema.metadata))
@@ -93,9 +93,9 @@ class ArrowWriterTest(TestCase):
 
     def test_write_batch_no_schema(self):
         output = pa.BufferOutputStream()
-        writer = ArrowWriter(stream=output)
-        writer.write_batch({"col_1": ["foo", "bar"], "col_2": [1, 2]})
-        num_examples, num_bytes = writer.finalize()
+        with ArrowWriter(stream=output) as writer:
+            writer.write_batch({"col_1": ["foo", "bar"], "col_2": [1, 2]})
+            num_examples, num_bytes = writer.finalize()
         self.assertEqual(num_examples, 2)
         self.assertGreater(num_bytes, 0)
         fields = {"col_1": pa.string(), "col_2": pa.int64()}
@@ -105,9 +105,9 @@ class ArrowWriterTest(TestCase):
     def test_write_batch_schema(self):
         fields = {"col_1": pa.string(), "col_2": pa.int64()}
         output = pa.BufferOutputStream()
-        writer = ArrowWriter(stream=output, schema=pa.schema(fields))
-        writer.write_batch({"col_1": ["foo", "bar"], "col_2": [1, 2]})
-        num_examples, num_bytes = writer.finalize()
+        with ArrowWriter(stream=output, schema=pa.schema(fields)) as writer:
+            writer.write_batch({"col_1": ["foo", "bar"], "col_2": [1, 2]})
+            num_examples, num_bytes = writer.finalize()
         self.assertEqual(num_examples, 2)
         self.assertGreater(num_bytes, 0)
         self.assertEqual(writer._schema, pa.schema(fields, metadata=writer._schema.metadata))
@@ -117,9 +117,9 @@ class ArrowWriterTest(TestCase):
         with tempfile.TemporaryDirectory() as tmp_dir:
             fields = {"col_1": pa.string(), "col_2": pa.int64()}
             output = os.path.join(tmp_dir, "test.arrow")
-            writer = ArrowWriter(path=output, schema=pa.schema(fields))
-            writer.write_batch({"col_1": ["foo", "bar"], "col_2": [1, 2]})
-            num_examples, num_bytes = writer.finalize()
+            with ArrowWriter(path=output, schema=pa.schema(fields)) as writer:
+                writer.write_batch({"col_1": ["foo", "bar"], "col_2": [1, 2]})
+                num_examples, num_bytes = writer.finalize()
             self.assertEqual(num_examples, 2)
             self.assertGreater(num_bytes, 0)
             self.assertEqual(writer._schema, pa.schema(fields, metadata=writer._schema.metadata))
