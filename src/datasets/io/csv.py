c819e4982ed489b1c77cb4fb8538706d0d2c8604
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-03-02 15:46:55 +0100

    Remove CsvDatasetReader and unused attributes

--- src/datasets/io/csv.py
+++ src/datasets/io/csv.py
@@ -1,50 +1,25 @@
-import pandas as pd
-import pyarrow as pa
-
-from ..arrow_dataset import Dataset
 from ..packaged_modules.csv.csv import Csv
-from .abc import AbstractDatasetReader
+from .abc import AbstractDatasetBuilder
 
 
-class CsvDatasetReader(AbstractDatasetReader):
-    def read(self):
-        table = self._read_table()
-        return Dataset(table, info=self.info, split=self.split)
-
-    def _read_table(self):
-        schema = pa.schema(self.info.features.type) if self.info and self.info.features else None
-        dtype = {name: dtype.to_pandas_dtype() for name, dtype in zip(schema.names, schema.types)} if schema else None
-        df = pd.read_csv(self.path, dtype=dtype, **self.kwargs)  # dtype allows reading an int column as str
-        table = pa.Table.from_pandas(df, schema=schema)
-        return table
-
-
-class CsvDatasetBuilder:
+class CsvDatasetBuilder(AbstractDatasetBuilder):
     def __init__(
         self,
         path,
-        name=None,
-        data_dir=None,
-        data_files=None,
         split=None,
-        cache_dir=None,
         features=None,
+        cache_dir=None,
         **config_kwargs,
     ):
         self.split = split
         self.builder = Csv(
             cache_dir=cache_dir,
-            name=name,
-            data_dir=data_dir,
-            data_files=path or data_files,
-            hash=hash,
+            data_files=path,
             features=features,
             **config_kwargs,
         )
 
     def build(self):
-        # split = "train"  # None  # if None: num_rows = {'train': 4} instead of 4
-
         download_config = None
         download_mode = None
         ignore_verifications = False
@@ -55,8 +30,6 @@ class CsvDatasetBuilder:
         save_infos = False
 
         base_path = None
-
-        # import pdb;pdb.set_trace()
 
         self.builder.download_and_prepare(
             download_config=download_config,
