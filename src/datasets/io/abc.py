62b24b4d2149d60af41281bf11b4d76244c7fd12
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-03-04 16:47:04 +0100

    Make explicit that path can be NestedDataStructure-like

--- src/datasets/io/abc.py
+++ src/datasets/io/abc.py
@@ -3,20 +3,20 @@ from typing import Optional
 
 from .. import Features, NamedSplit
 from ..arrow_dataset import Dataset
-from ..utils.typing import PathLike
+from ..utils.typing import NestedDataStructureLike, PathLike
 
 
 class AbstractDatasetReader(ABC):
     def __init__(
         self,
-        path: PathLike,
+        path_or_paths: NestedDataStructureLike[PathLike],
         split: Optional[NamedSplit] = None,
         features: Optional[Features] = None,
         cache_dir: str = None,
         keep_in_memory: bool = False,
         **kwargs,
     ):
-        self.path = path
+        self.path_or_paths = path_or_paths
         self.split = split
         self.features = features
         self.cache_dir = cache_dir
--- src/datasets/io/json.py
+++ src/datasets/io/json.py
@@ -2,14 +2,14 @@ from typing import Optional
 
 from .. import Features, NamedSplit
 from ..packaged_modules.json.json import Json
-from ..utils.typing import PathLike
+from ..utils.typing import NestedDataStructureLike, PathLike
 from .abc import AbstractDatasetReader
 
 
 class JsonDatasetReader(AbstractDatasetReader):
     def __init__(
         self,
-        path: PathLike,
+        path_or_paths: NestedDataStructureLike[PathLike],
         split: Optional[NamedSplit] = None,
         features: Optional[Features] = None,
         cache_dir: str = None,
@@ -17,11 +17,11 @@ class JsonDatasetReader(AbstractDatasetReader):
         **kwargs,
     ):
         super().__init__(
-            path, split=split, features=features, cache_dir=cache_dir, keep_in_memory=keep_in_memory, **kwargs
+            path_or_paths, split=split, features=features, cache_dir=cache_dir, keep_in_memory=keep_in_memory, **kwargs
         )
         self.builder = Json(
             cache_dir=cache_dir,
-            data_files=path,
+            data_files=path_or_paths,
             features=features,
             **kwargs,
         )
