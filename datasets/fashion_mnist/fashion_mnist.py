ac3d42546a48a841f27e509b71b6b1da1ebb5a45
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-03-07 03:09:22 +0530

    Fix flake8 issue

--- datasets/fashion_mnist/fashion_mnist.py
+++ datasets/fashion_mnist/fashion_mnist.py
@@ -45,10 +45,10 @@ _CITATION = """\
 """
 
 _DESCRIPTION = """\
-Fashion-MNIST is a dataset of Zalando's article images—consisting of a training set of 
-60,000 examples and a test set of 10,000 examples. Each example is a 28x28 grayscale image, 
-associated with a label from 10 classes. We intend Fashion-MNIST to serve as a direct drop-in 
-replacement for the original MNIST dataset for benchmarking machine learning algorithms. 
+Fashion-MNIST is a dataset of Zalando's article images—consisting of a training set of
+60,000 examples and a test set of 10,000 examples. Each example is a 28x28 grayscale image,
+associated with a label from 10 classes. We intend Fashion-MNIST to serve as a direct drop-in
+replacement for the original MNIST dataset for benchmarking machine learning algorithms.
 It shares the same image size and structure of training and testing splits.
 """
 
