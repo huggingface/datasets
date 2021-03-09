ed6e46b06b402491d22b5abe725ef6199bff8624
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-03-09 12:10:24 +0100

    Test JSON DatasetDict Reader for dict path

--- tests/io/test_json.py
+++ tests/io/test_json.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from datasets import Dataset, Features, NamedSplit, Value
+from datasets import Dataset, DatasetDict, Features, NamedSplit, Value
 from datasets.io.json import JsonDatasetReader
 
 
@@ -54,5 +54,39 @@ def test_json_dataset_reader(path_type, split, jsonl_path, features, tmp_path):
     assert ds.num_columns == 3
     assert ds.column_names == ["col_1", "col_2", "col_3"]
     assert ds.split == expected_split
+    for feature, expected_dtype in expected_features.items():
+        assert ds.features[feature].dtype == expected_dtype
+
+
+@pytest.mark.parametrize(
+    "features",
+    [
+        None,
+        {"col_1": "string", "col_2": "int64", "col_3": "float64"},
+        {"col_1": "string", "col_2": "string", "col_3": "string"},
+        {"col_1": "int32", "col_2": "int32", "col_3": "int32"},
+        {"col_1": "float32", "col_2": "float32", "col_3": "float32"},
+    ],
+)
+@pytest.mark.parametrize("split", [None, "train", "test"])
+def test_json_datasetdict_reader(split, jsonl_path, features, tmp_path):
+    if split:
+        path = {split: jsonl_path}
+    else:
+        split = "train"
+        path = {"train": jsonl_path, "test": jsonl_path}
+    cache_dir = tmp_path / "cache"
+
+    default_expected_features = {"col_1": "string", "col_2": "int64", "col_3": "float64"}
+    expected_features = features.copy() if features else default_expected_features
+    features = Features({feature: Value(dtype) for feature, dtype in features.items()}) if features else None
+
+    ds = JsonDatasetReader(path, features=features, cache_dir=cache_dir).read()
+    assert isinstance(ds, DatasetDict)
+    ds = ds[split]
+    assert ds.num_rows == 4
+    assert ds.num_columns == 3
+    assert ds.column_names == ["col_1", "col_2", "col_3"]
+    assert ds.split == split
     for feature, expected_dtype in expected_features.items():
         assert ds.features[feature].dtype == expected_dtype
