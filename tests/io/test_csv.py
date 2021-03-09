c26ed08f26963dda905e7ae5c41bfb7642a095cb
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-03-09 10:37:03 +0100

    Test CSV DatasetDict Reader for dict path

--- tests/io/test_csv.py
+++ tests/io/test_csv.py
@@ -2,7 +2,7 @@ import csv
 
 import pytest
 
-from datasets import Dataset, Features, NamedSplit, Value
+from datasets import Dataset, DatasetDict, Features, NamedSplit, Value
 from datasets.io.csv import CsvDatasetReader
 
 
@@ -57,5 +57,40 @@ def test_csv_dataset_reader(path_type, split, csv_path, features, tmp_path):
     assert ds.num_columns == 3
     assert ds.column_names == ["col_1", "col_2", "col_3"]
     assert ds.split == expected_split
+    for feature, expected_dtype in expected_features.items():
+        assert ds.features[feature].dtype == expected_dtype
+
+
+@pytest.mark.parametrize(
+    "features",
+    [
+        None,
+        {"col_1": "string", "col_2": "int64", "col_3": "float64"},
+        {"col_1": "string", "col_2": "string", "col_3": "string"},
+        {"col_1": "int32", "col_2": "int32", "col_3": "int32"},
+        {"col_1": "float32", "col_2": "float32", "col_3": "float32"},
+    ],
+)
+@pytest.mark.parametrize("split", [None, "train", "test"])
+def test_csv_datasetdict_reader(split, csv_path, features, tmp_path):
+    if split:
+        path = {split: csv_path}
+    else:
+        split = "train"
+        path = {"train": csv_path, "test": csv_path}
+    cache_dir = tmp_path / "cache"
+
+    # CSV file loses col_1 string dtype information: default now is "int64" instead of "string"
+    default_expected_features = {"col_1": "int64", "col_2": "int64", "col_3": "float64"}
+    expected_features = features.copy() if features else default_expected_features
+    features = Features({feature: Value(dtype) for feature, dtype in features.items()}) if features else None
+
+    ds = CsvDatasetReader(path, features=features, cache_dir=cache_dir).read()
+    assert isinstance(ds, DatasetDict)
+    ds = ds[split]
+    assert ds.num_rows == 4
+    assert ds.num_columns == 3
+    assert ds.column_names == ["col_1", "col_2", "col_3"]
+    assert ds.split == split
     for feature, expected_dtype in expected_features.items():
         assert ds.features[feature].dtype == expected_dtype
