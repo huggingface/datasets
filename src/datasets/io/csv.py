b86e7cdab70f20f68d66b061417d2bda92b80b29
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-03-09 10:26:20 +0100

    Pass paths as dict to allow split renaming

--- src/datasets/io/csv.py
+++ src/datasets/io/csv.py
@@ -19,6 +19,7 @@ class CsvDatasetReader(AbstractDatasetReader):
         super().__init__(
             path_or_paths, split=split, features=features, cache_dir=cache_dir, keep_in_memory=keep_in_memory, **kwargs
         )
+        path_or_paths = path_or_paths if isinstance(path_or_paths, dict) else {self.split: path_or_paths}
         self.builder = Csv(
             cache_dir=cache_dir,
             data_files=path_or_paths,
