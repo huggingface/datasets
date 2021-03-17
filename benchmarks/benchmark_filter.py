fe31f573014ae4eadffea0a209a60b3a444b3a82
theo <theo@matussie.re>
2021-03-17 16:15:10 +0100

    log & no go through dataset dict

--- benchmarks/benchmark_filter.py
+++ benchmarks/benchmark_filter.py
@@ -1,11 +1,21 @@
+import logging
+import sys
 import time
 
-from datasets import load_dataset
+from datasets import load_dataset, set_caching_enabled
 
 
 if __name__ == "__main__":
+    set_caching_enabled(False)
+    logging.basicConfig(level=logging.DEBUG)
+
     bc = load_dataset("bookcorpus")
+
     now = time.time()
-    bc.filter(lambda x: len(x["text"]) < 64)
+    try:
+        bc["train"].filter(lambda x: len(x["text"]) < 64, num_proc=int(sys.argv[1]))
+    except Exception as e:
+        print(f"cancelled: {e}")
     elapsed = time.time() - now
+
     print(elapsed)
