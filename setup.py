43d13438754b7bf7eb79a2ab0c225236a1d7baf3
Quentin Lhoest <lhoest.q@gmail.com>
2021-03-18 17:10:15 +0100

    deploy docs

--- setup.py
+++ setup.py
@@ -189,7 +189,7 @@ EXTRAS_REQUIRE = {
 
 setup(
     name="datasets",
-    version="1.5.0",
+    version="1.5.0dev0",
     description=DOCLINES[0],
     long_description="\n".join(DOCLINES[2:]),
     author="HuggingFace Inc.",
