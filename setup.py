32ed1984517b0eed0f9b74851d2804fc49598cae
SBrandeis <s.brandeis@protonmail.com>
2021-03-18 11:53:31 +0100

    Bump huggingface_hub version

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ REQUIRED_PKGS = [
     # for saving datsets to local
     "fsspec",
     # To get datasets from the Datasets Hub on huggingface.co
-    "huggingface_hub==0.0.2",
+    "huggingface_hub==0.0.6",
 ]
 
 BENCHMARKS_REQUIRE = [
