8d18933bf2a68865af6856cb04520d1b7c7a80bf
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-03-18 12:12:44 +0100

    Fix docstring of load_dataset

--- src/datasets/load.py
+++ src/datasets/load.py
@@ -666,12 +666,12 @@ def load_dataset(
 
     Args:
 
-        path (``str``):
-            path to the dataset processing script with the dataset builder. Can be either:
-                - a local path to processing script or the directory containing the script (if the script has the same name as the directory),
-                    e.g. ``'./dataset/squad'`` or ``'./dataset/squad/squad.py'``
-                - a dataset identifier in the HuggingFace Datasets Hub (list all available datasets and ids with ``datasets.list_datasets()``)
-                    e.g. ``'squad'``, ``'glue'`` or ``'openai/webtext'``
+        path (``str``): Path to the dataset processing script with the dataset builder. Can be either:
+
+            - a local path to processing script or the directory containing the script (if the script has the same name as the directory),
+              e.g. ``'./dataset/squad'`` or ``'./dataset/squad/squad.py'``.
+            - a dataset identifier in the HuggingFace Datasets Hub (list all available datasets and ids with ``datasets.list_datasets()``)
+              e.g. ``'squad'``, ``'glue'`` or ``'openai/webtext'``.
         name (Optional ``str``): defining the name of the dataset configuration
         data_files (Optional ``str``): defining the data_files of the dataset configuration
         data_dir (Optional ``str``): defining the data_dir of the dataset configuration
@@ -681,18 +681,19 @@ def load_dataset(
             Splits can be combined and specified like in tensorflow-datasets.
         cache_dir (Optional ``str``): directory to read/write data. Defaults to "~/datasets".
         features (Optional ``datasets.Features``): Set the features type to use for this dataset.
-        download_config (Optional ``datasets.DownloadConfig``: specific download configuration parameters.
+        download_config (Optional ``datasets.DownloadConfig``): specific download configuration parameters.
         download_mode (Optional `datasets.GenerateMode`): select the download/generate mode - Default to REUSE_DATASET_IF_EXISTS
         ignore_verifications (bool): Ignore the verifications of the downloaded/processed dataset information (checksums/size/splits/...)
         keep_in_memory (bool, default=False): Whether to copy the data in-memory.
         save_infos (bool): Save the dataset information (checksums/size/splits/...)
         script_version (Optional ``Union[str, datasets.Version]``): Version of the dataset script to load:
+
             - For canonical datasets in the `huggingface/datasets` library like "squad", the default version of the module is the local version fo the lib.
-            You can specify a different version from your local version of the lib (e.g. "master" or "1.2.0") but it might cause compatibility issues.
+              You can specify a different version from your local version of the lib (e.g. "master" or "1.2.0") but it might cause compatibility issues.
             - For community provided datasets like "lhoestq/squad" that have their own git repository on the Datasets Hub, the default version "main" corresponds to the "main" branch.
-            You can specify a different version that the default "main" by using a commit sha or a git tag of the dataset repository.
+              You can specify a different version that the default "main" by using a commit sha or a git tag of the dataset repository.
         use_auth_token (Optional ``Union[str, bool]``): Optional string or boolean to use as Bearer token for remote files on the Datasets Hub.
-            If True, will get token from ~/.huggingface.
+            If True, will get token from `~/.huggingface`.
         **config_kwargs (Optional ``dict``): keyword arguments to be passed to the ``datasets.BuilderConfig`` and used in the ``datasets.DatasetBuilder``.
 
     Returns:
