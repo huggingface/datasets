4cdc1ae3ddba35fb2ecbe8f6b270e0162588d0b3
Yongrae Jo <dreamgonfly@gmail.com>
2021-03-25 19:37:46 +0900

    Fix incorrect assertion in builder.py

--- src/datasets/builder.py
+++ src/datasets/builder.py
@@ -981,7 +981,7 @@ class GeneratorBasedBuilder(DatasetBuilder):
             finally:
                 num_examples, num_bytes = writer.finalize()
 
-        assert num_examples == num_examples, f"Expected to write {split_info.num_examples} but wrote {num_examples}"
+        assert split_info.num_examples == num_examples, f"Expected to write {split_info.num_examples} but wrote {num_examples}"
         split_generator.split_info.num_examples = num_examples
         split_generator.split_info.num_bytes = num_bytes
 
