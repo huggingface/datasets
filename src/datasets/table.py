fd067b9803f294742ae22ecffeeac0d48df4185d
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-04-06 14:55:37 +0200

    Create _consolidate_blocks

--- src/datasets/table.py
+++ src/datasets/table.py
@@ -548,6 +548,15 @@ class ConcatenationTable(Table):
         return merged_blocks
 
     @classmethod
+    def _consolidate_blocks(cls, blocks: TableBlockContainer):
+        if isinstance(blocks, TableBlock):
+            return blocks
+        elif isinstance(blocks[0], TableBlock):
+            return cls._merge_blocks(blocks, axis=0)
+        else:
+            return cls._merge_blocks(blocks)
+
+    @classmethod
     def from_blocks(cls, blocks: TableBlockContainer) -> "ConcatenationTable":
         if isinstance(blocks, TableBlock):
             table = blocks
