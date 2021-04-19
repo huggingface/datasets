748e2e9bffeea7aab68182bfe6c6f71d4305e1c4
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-04-19 11:40:46 +0200

    Revert "tmp"
    This reverts commit 8a1af5acec2372db854bbcd4b5988411bd55e78b.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,12 @@
 import csv
 import json
 import lzma
-import os
-import shutil
 import textwrap
 
-import numpy as np
 import pytest
 
 from datasets.arrow_dataset import Dataset
-from datasets.features import Array2D, Array3D, ClassLabel, Features, Sequence, Value
+from datasets.features import ClassLabel, Features, Sequence, Value
 
 from .s3_fixtures import *  # noqa: load s3 fixtures
 
@@ -180,49 +177,3 @@ def text_path(tmp_path_factory):
         for item in data:
             f.write(item + "\n")
     return path
-
-
-@pytest.fixture
-def tmp_dir(tmp_path):
-    yield str(tmp_path)
-    shutil.rmtree(tmp_path)
-
-
-@pytest.fixture
-def create_dummy_dataset(tmp_dir):
-    created_datasets = []
-
-    def _create_dummy_dataset(in_memory: bool = None, multiple_columns=False, array_features=False):
-        if multiple_columns:
-            if array_features:
-                data = {
-                    "col_1": [[[True, False], [False, True]]] * 4,  # 2D
-                    "col_2": [[[["a", "b"], ["c", "d"]], [["e", "f"], ["g", "h"]]]] * 4,  # 3D array
-                    "col_3": [[3, 2, 1, 0]] * 4,  # Sequence
-                }
-                features = Features(
-                    {
-                        "col_1": Array2D(shape=(2, 2), dtype="bool"),
-                        "col_2": Array3D(shape=(2, 2, 2), dtype="string"),
-                        "col_3": Sequence(feature=Value("int64")),
-                    }
-                )
-            else:
-                data = {"col_1": [3, 2, 1, 0], "col_2": ["a", "b", "c", "d"], "col_3": [False, True, False, True]}
-                features = None
-            dset = Dataset.from_dict(data, features=features)
-        else:
-            dset = Dataset.from_dict({"filename": ["my_name-train" + "_" + str(x) for x in np.arange(30).tolist()]})
-        if in_memory:
-            dset = dset.map(keep_in_memory=True)
-        else:
-            start = 0
-            while os.path.isfile(os.path.join(tmp_dir, f"dataset{start}.arrow")):
-                start += 1
-            dset = dset.map(cache_file_name=os.path.join(tmp_dir, f"dataset{start}.arrow"))
-        created_datasets.append(dset)
-        return dset
-
-    yield _create_dummy_dataset
-    for dataset in created_datasets:
-        dataset.__del__()
