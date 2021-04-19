ffe2750a5002f4113eccc8d54e27e474c318f21e
Philipp <wiesenbach@cl.uni-heidelberg.de>
2021-04-19 23:01:03 +0200

    adding bbaw egyptian dataset

---
+++ datasets/bbaw_egyptian/bbaw_egyptian.py
@@ -0,0 +1,90 @@
+# coding=utf-8
+# Copyright 2020 The HuggingFace Datasets Authors and the current dataset script contributor.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import json
+
+import datasets
+
+
+_CITATION = """\
+@misc{OPUS4-2919,
+title     = {Teilauszug der Datenbank des Vorhabens "Strukturen und Transformationen des Wortschatzes der {\"a}gyptischen Sprache" vom Januar 2018},
+institution = {Akademienvorhaben Strukturen und Transformationen des Wortschatzes der {\"a}gyptischen Sprache. Text- und Wissenskultur im alten {\"A}gypten},
+type      = {other},
+year        = {2018},
+}
+"""
+
+
+_DESCRIPTION = """\
+The project `Strukturen und Transformationen des Wortschatzes der ägyptischen Sprache`
+is compiling an extensively annotated digital corpus of Egyptian texts.
+This publication comprises an excerpt of the internal database's contents.
+"""
+
+_HOMEPAGE = "https://aaew.bbaw.de/tla/index.html"
+
+_LICENSE = "Creative Commons-Lizenz - CC BY-SA - 4.0 International"
+
+
+class BbawEgyptian(datasets.GeneratorBasedBuilder):
+    """
+    The project `Strukturen und Transformationen des Wortschatzes der ägyptischen Sprache`
+    is compiling an extensively annotated digital corpus of Egyptian texts.
+    This publication comprises an excerpt of the internal database's contents.
+    """
+
+    _URL = "https://phiwi.github.io/"
+    _URLS = {"all": _URL + "all.json"}
+
+    def _info(self):
+        features = datasets.Features(
+            {
+                "transcription": datasets.Value("string"),
+                "translation": datasets.Value("string"),
+                "hieroglyphs": datasets.Value("string"),
+            }
+        )
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=features,
+            supervised_keys=None,
+            homepage=_HOMEPAGE,
+            license=_LICENSE,
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        """Returns SplitGenerators."""
+        my_urls = self._URLS
+        data_dir = dl_manager.download_and_extract(my_urls)
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                gen_kwargs={"filepath": data_dir["all"]},
+            )
+        ]
+
+    def _generate_examples(self, filepath):
+        """ Yields examples as (key, example) tuples. """
+        with open(filepath, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        for id_, row in enumerate(data):
+            yield id_, {
+                "translation": row["translation"],
+                "transcription": row["transcription"],
+                "hieroglyphs": row["hieroglyphs"],
+            }
