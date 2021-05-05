e6e8ff100fd06b1b42bd7a1f4fe70c3f64c7df95
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-05-05 18:41:21 +0200

    Ignore flake8 errors

--- src/datasets/features/__init__.py
+++ src/datasets/features/__init__.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 from .audio import Audio
 from .features import Array2D, Array3D, Array4D, Array5D, Audio, ClassLabel, Features, Sequence, Value
 from .translation import Translation, TranslationVariableLanguages
