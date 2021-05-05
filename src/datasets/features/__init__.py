fcdb1102d3fa20121ee26ee751b26304f4b71994
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-05-05 18:26:41 +0200

    Fix imports

--- src/datasets/features/__init__.py
+++ src/datasets/features/__init__.py
@@ -1,3 +1,3 @@
-from .audio import *
-from .features import *
-from .translation import *
+from .audio import Audio
+from .features import Array2D, Array3D, Array4D, Array5D, Audio, ClassLabel, Features, Sequence, Value
+from .translation import Translation, TranslationVariableLanguages
