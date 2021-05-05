827b2638d188b0a2149dc2e4f6c76a503ea77ffb
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-05-05 19:51:51 +0200

    Fix imports of private classes/functions

--- src/datasets/features/__init__.py
+++ src/datasets/features/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
 from .audio import Audio
 from .features import *
-from .features import _ArrayXD, _ArrayXDExtensionType, _arrow_to_datasets_dtype
+from .features import _ArrayXD, _ArrayXDExtensionType, _arrow_to_datasets_dtype, _cast_to_python_objects
 from .translation import Translation, TranslationVariableLanguages
