e7d89045f231b6b581c5a301a335792215b839a8
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-05-05 19:09:07 +0200

    Fix imports

--- src/datasets/features/__init__.py
+++ src/datasets/features/__init__.py
@@ -1,17 +1,5 @@
 # flake8: noqa
 from .audio import Audio
-from .features import (
-    Array2D,
-    Array3D,
-    Array4D,
-    Array5D,
-    Audio,
-    ClassLabel,
-    Features,
-    Sequence,
-    Value,
-    _ArrayXDExtensionType,
-    cast_to_python_objects,
-    pandas_types_mapper,
-)
+from .features import *
+from .features import _ArrayXDExtensionType
 from .translation import Translation, TranslationVariableLanguages
