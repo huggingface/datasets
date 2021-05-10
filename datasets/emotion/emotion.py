95d395e9642c07e1b548cb5f01f5af2cf3e41689
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2021-05-10 18:26:16 +0200

    Revert emotion dataset for real!

--- datasets/emotion/emotion.py
+++ datasets/emotion/emotion.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import, division, print_function
-
 import csv
 
 import datasets
@@ -26,9 +24,7 @@ _CITATION = """\
 """
 
 _DESCRIPTION = """\
-Emotion is a dataset of English Twitter messages with eight basic emotions: anger, anticipation,
-disgust, fear, joy, sadness, surprise, and trust. For more detailed information please refer to the
-paper.
+Emotion is a dataset of English Twitter messages with six basic emotions: anger, fear, joy, love, sadness, and surprise. For more detailed information please refer to the paper.
 """
 _URL = "https://github.com/dair-ai/emotion_dataset"
 # use dl=1 to force browser to download data instead of displaying it
@@ -39,9 +35,12 @@ _TEST_DOWNLOAD_URL = "https://www.dropbox.com/s/ikkqxfdbdec3fuj/test.txt?dl=1"
 
 class Emotion(datasets.GeneratorBasedBuilder):
     def _info(self):
+        class_names = ["sadness", "joy", "love", "anger", "fear", "surprise"]
         return datasets.DatasetInfo(
             description=_DESCRIPTION,
-            features=datasets.Features({"text": datasets.Value("string"), "label": datasets.Value("string")}),
+            features=datasets.Features(
+                {"text": datasets.Value("string"), "label": datasets.ClassLabel(names=class_names)}
+            ),
             supervised_keys=("text", "label"),
             homepage=_URL,
             citation=_CITATION,
