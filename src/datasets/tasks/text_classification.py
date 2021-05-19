db7ba349c7b98fdcbcb31874e01fc93111f727a8
mariosasko <mariosasko777@gmail.com>
2021-05-20 01:41:05 +0200

    Lazy initialize label schema in text classification task

--- src/datasets/tasks/text_classification.py
+++ src/datasets/tasks/text_classification.py
@@ -5,13 +5,28 @@ from ..features import ClassLabel, Features, Value
 from .base import TaskTemplate
 
 
+class FeaturesWithLazyClassLabel:
+    def __init__(self, features, label_column="labels"):
+        assert label_column in features, f"Key '{label_column}' missing in features {features}"
+        self._features = features
+        self._label_column = label_column
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self._features
+
+        assert hasattr(obj, self._label_column), f"Object has no attribute '{self._label_column}'"
+        features = self._features.copy()
+        features["labels"] = ClassLabel(names=getattr(obj, self._label_column))
+        return features
+
+
 @dataclass(frozen=True)
 class TextClassification(TaskTemplate):
     task: ClassVar[str] = "text-classification"
     input_schema: ClassVar[Features] = Features({"text": Value("string")})
-    # TODO(lewtun): Since we update this in __post_init__ do we need to set a default? We'll need it for __init__ so
-    # investigate if there's a more elegant approach.
-    label_schema: ClassVar[Features] = Features({"labels": ClassLabel})
+    # TODO(lewtun): Find a more elegant approach without descriptors.
+    label_schema: ClassVar[Features] = FeaturesWithLazyClassLabel(Features({"labels": ClassLabel}))
     labels: List[str]
     text_column: str = "text"
     label_column: str = "labels"
@@ -20,7 +35,6 @@ class TextClassification(TaskTemplate):
         assert len(self.labels) == len(set(self.labels)), "Labels must be unique"
         # Cast labels to tuple to allow hashing
         self.__dict__["labels"] = tuple(sorted(self.labels))
-        self.label_schema["labels"] = ClassLabel(names=self.labels)
 
     @property
     def column_mapping(self) -> Dict[str, str]:
