0182b80f5a9310f5d428579788930366574a1973
Benjamin Akera <akeraben@gmail.com>
2021-05-20 21:59:44 -0400

    added sunbird datasets

--- datasets/sunbird/sunbird.py
+++ datasets/sunbird/sunbird.py
@@ -30,44 +30,36 @@ _CITATION = """\
         TO ADD: Sunbird citation
 """
 
-_DATA_URL = "https://github.com/SunbirdAI/toy-datasets/blob/main/dummy-enlg.zip"
+_VERSION = "1.0.0"
+_TRAIN_EN = "https://raw.githubusercontent.com/SunbirdAI/toy-datasets/main/train.en"
+_TEST_EN = "https://raw.githubusercontent.com/SunbirdAI/toy-datasets/main/test.en"
+_VAL_EN = "https://raw.githubusercontent.com/SunbirdAI/toy-datasets/main/val.en"
+_TRAIN_LG = "https://raw.githubusercontent.com/SunbirdAI/toy-datasets/main/train.lg"
+_TEST_LG = "https://raw.githubusercontent.com/SunbirdAI/toy-datasets/main/test.lg"
+_VAL_LG = "https://raw.githubusercontent.com/SunbirdAI/toy-datasets/main/val.lg"
 
+_LANGUAGE_PAIRS = [
+    ("lg", "en"),
+]
+
+
+# https://raw.githubusercontent.com/IgnatiusEzeani/IGBONLP/master/ig_en_mt/benchmark_dataset/train.en
 # Tuple that describes a single pair of files with matching translations.
 # language_to_file is the map from language (2 letter string: example 'en')
 # to the file path in the extracted directory.
-TranslateData = collections.namedtuple("TranslateData", ["url", "language_to_file"])
+# TranslateData = collections.namedtuple("TranslateData", ["url", "language_to_file"])
 
 
 class SunbirdConfig(datasets.BuilderConfig):
     """BuilderConfig for Sunbird."""
-
-    def __init__(self, language_pair=(None, None), **kwargs):
-        """BuilderConfig for Sunbird.
-
-        Args:
-            for the `datasets.features.text.TextEncoder` used for the features feature.
-          language_pair: pair of languages that will be used for translation. Should
-            contain 2-letter coded strings. First will be used at source and second
-            as target in supervised mode. For example: ("lg", "en").
-          **kwargs: keyword arguments forwarded to super.
-        """
-        name = "%s%s" % (language_pair[0], language_pair[1])
-
-        description = ("Translation dataset from %s to %s") % (language_pair[0], language_pair[1])
-        super(SunbirdConfig, self).__init__(
-            name=name,
-            description=description,
-            version=datasets.Version("1.1.0", ""),
+    def __init__(self, *args, lang1=None, lang2=None, **kwargs):
+        super().__init__(
+            *args,
+            name=f"{lang1}-{lang2}",
             **kwargs,
         )
-
-        # Validate language pair.
-        assert "en" in language_pair, ("Config language pair must contain `en`, got: %s", language_pair)
-        source, target = language_pair
-        non_en = source if target == "en" else target
-        assert non_en in ["lg", "luo"], ("Invalid non-en language in pair: %s", non_en)
-
-        self.language_pair = language_pair
+        self.lang1 = lang1
+        self.lang2 = lang2
 
 
 class Sunbird(datasets.GeneratorBasedBuilder):
@@ -75,61 +67,66 @@ class Sunbird(datasets.GeneratorBasedBuilder):
 
     BUILDER_CONFIGS = [
         SunbirdConfig(
-            language_pair=("lg", "en"),
-        ),
-       # SunbirdConfig(
-        #    language_pair=("luo", "lg"),
-       # ),
+            lang1=lang1,
+            lang2= lang2,
+            description = f"Translating {lang1} to {lang2} or vice versa",
+            version=datasets.Version(_VERSION),
+        )
+        for lang1, lang2 in _LANGUAGE_PAIRS
     ]
+    BUILDER_CONFIG_CLASS = SunbirdConfig
+
 
     def _info(self):
-        source, target = self.config.language_pair
         return datasets.DatasetInfo(
             description=_DESCRIPTION,
             features=datasets.Features(
-                {"translation": datasets.features.Translation(languages=self.config.language_pair)}
+                {
+                    "id": datasets.Value("string"),
+                    "translation": datasets.Translation(languages=(self.config.lang1, self.config.lang2)),
+                 },
             ),
-            supervised_keys=(source, target),
+            supervised_keys=None,
             homepage="https://sunbird.ai/",
             citation=_CITATION,
         )
 
     def _split_generators(self, dl_manager):
-        dl_dir = dl_manager.download_and_extract(_DATA_URL)
+        train_en = dl_manager.download_and_extract(_TRAIN_EN)
+        train_lg = dl_manager.download_and_extract(_TRAIN_LG)
 
-        source, target = self.config.language_pair
-        non_en = source if target == "en" else target
-        path_tmpl = "{dl_dir}/train." "{lang}"
+        valid_en = dl_manager.download_and_extract(_VAL_EN)
+        valid_lg = dl_manager.download_and_extract(_VAL_LG)
 
-        files = {}
-        for split in ("dev", "devtest"):
-            files[split] = {
-                "source_file": path_tmpl.format(dl_dir=dl_dir, split=split, non_en=non_en, lang=source),
-                "target_file": path_tmpl.format(dl_dir=dl_dir, split=split, non_en=non_en, lang=target),
-            }
+        test_en = dl_manager.download_and_extract(_TEST_EN)
+        test_lg = dl_manager.download_and_extract(_TEST_LG)
 
         return [
-            datasets.SplitGenerator(name=datasets.Split.VALIDATION, gen_kwargs=files["dev"]),
-            datasets.SplitGenerator(name=datasets.Split.TEST, gen_kwargs=files["devtest"]),
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                gen_kwargs={"lg_datapath": train_lg, "en_datapath": train_en},
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.VALIDATION,
+                gen_kwargs={"lg_datapath": valid_lg, "en_datapath": valid_en},
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.TEST,
+                gen_kwargs={"lg_datapath": test_lg, "en_datapath": test_en},
+            ),
         ]
 
-    def _generate_examples(self, source_file, target_file):
-        """This function returns the examples in the raw (text) form."""
-        with open(source_file, encoding="utf-8") as f:
-            source_sentences = f.read().split("\n")
-        with open(target_file, encoding="utf-8") as f:
-            target_sentences = f.read().split("\n")
-
-        assert len(target_sentences) == len(source_sentences), "Sizes do not match: %d vs %d for %s vs %s." % (
-            len(source_sentences),
-            len(target_sentences),
-            source_file,
-            target_file,
-        )
-
-        source, target = self.config.language_pair
-        for idx, (l1, l2) in enumerate(zip(source_sentences, target_sentences)):
-            result = {"translation": {source: l1, target: l2}}
-            # Make sure that both translations are non-empty.
-            if all(result.values()):
-                yield idx, result
+    def _generate_examples(self, lg_datapath, en_datapath):
+        with open(lg_datapath, encoding="utf-8") as f1, open(en_datapath, encoding="utf-8") as f2:
+            for sentence_counter, (x, y) in enumerate(zip(f1, f2)):
+                x = x.strip()
+                y = y.strip()
+                print(x)
+                result = (
+                    sentence_counter,
+                    {
+                        "id": str(sentence_counter),
+                        "translation": {"lg": x, "en": y},
+                    },
+                )
+                yield result
