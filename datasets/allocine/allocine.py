2f359813d1b59263f351a70bbd037a0c5d0eb5c3
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2021-05-28 10:25:09 +0200

    Revert "Insert task template to allocine dataset"
    This reverts commit c57714970fda37d801ef8e9dfc53d9a3a5b39107.

--- datasets/allocine/allocine.py
+++ datasets/allocine/allocine.py
@@ -5,7 +5,6 @@ import json
 import os
 
 import datasets
-from datasets.tasks import TextClassification
 
 
 _CITATION = """\
@@ -66,9 +65,6 @@ class AllocineDataset(datasets.GeneratorBasedBuilder):
             supervised_keys=None,
             homepage="https://github.com/TheophileBlard/french-sentiment-analysis-with-bert",
             citation=_CITATION,
-            task_templates=[
-                TextClassification(task="text-classification", text_column="review", label_column="label", labels=None)
-            ],
         )
 
     def _split_generators(self, dl_manager):
