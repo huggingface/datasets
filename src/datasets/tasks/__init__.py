caf050c662096379ae6ca6842ab924998eaec4ee
lewtun <lewis.c.tunstall@gmail.com>
2021-05-28 13:37:32 +0200

    Update text classification template labels in DatasetInfo __post_init__ (#2392)
    * Update labels in DatasetInfo __post_init__
    
    * Add labels to TextClassification __post_init__
    
    * Refactor tests to account for label update in `DatasetInfo`, add test
    
    * Raise ValueError if TextClassification template has None or incompatible labels
    
    * Add decorator to share docstrings across multiple functions
    
    * Remove custom feature with lazy classlabel
    
    Co-authored-by: Quentin Lhoest <lhoest.q@gmail.com>

--- src/datasets/tasks/__init__.py
+++ src/datasets/tasks/__init__.py
@@ -1,11 +1,14 @@
 from typing import Optional
 
+from ..utils.logging import get_logger
 from .base import TaskTemplate
 from .question_answering import QuestionAnswering
 from .text_classification import TextClassification
 
 
 __all__ = ["TaskTemplate", "QuestionAnswering", "TextClassification"]
+
+logger = get_logger(__name__)
 
 
 NAME2TEMPLATE = {QuestionAnswering.task: QuestionAnswering, TextClassification.task: TextClassification}
@@ -15,8 +18,7 @@ def task_template_from_dict(task_template_dict: dict) -> Optional[TaskTemplate]:
     """Create one of the supported task templates in :py:mod:`datasets.tasks` from a dictionary."""
     task_name = task_template_dict.get("task")
     if task_name is None:
+        logger.warning(f"Couldn't find template for task '{task_name}'. Available templates: {list(NAME2TEMPLATE)}")
         return None
     template = NAME2TEMPLATE.get(task_name)
-    if template is None:
-        return None
     return template.from_dict(task_template_dict)
