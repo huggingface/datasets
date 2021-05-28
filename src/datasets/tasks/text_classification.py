40e440080e8f94b2e80b9f93c0d23bee2826ff90
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2021-05-28 09:25:28 +0200

    Remove custom feature with lazy classlabel
    No longer needed since we create a new instance of the task template during the `DatasetInfo.__post_init__`

--- src/datasets/tasks/text_classification.py
+++ src/datasets/tasks/text_classification.py
@@ -1,25 +1,8 @@
-import copy
 from dataclasses import dataclass
 from typing import ClassVar, Dict, Optional, Tuple
 
 from ..features import ClassLabel, Features, Value
 from .base import TaskTemplate
-
-
-class FeaturesWithLazyClassLabel:
-    def __init__(self, features, label_column="labels"):
-        assert label_column in features, f"Key '{label_column}' missing in features {features}"
-        self._features = features
-        self._label_column = label_column
-
-    def __get__(self, obj, objtype=None):
-        if obj is None:
-            return self._features
-
-        assert hasattr(obj, self._label_column), f"Object has no attribute '{self._label_column}'"
-        features = self._features.copy()
-        features["labels"] = ClassLabel(names=getattr(obj, self._label_column))
-        return features
 
 
 @dataclass(frozen=True)
@@ -28,7 +11,7 @@ class TextClassification(TaskTemplate):
     task: str = "text-classification"
     input_schema: ClassVar[Features] = Features({"text": Value("string")})
     # TODO(lewtun): Find a more elegant approach without descriptors.
-    label_schema: ClassVar[Features] = FeaturesWithLazyClassLabel(Features({"labels": ClassLabel}))
+    label_schema: ClassVar[Features] = Features({"labels": ClassLabel})
     text_column: str = "text"
     label_column: str = "labels"
     labels: Optional[Tuple[str]] = None
@@ -38,7 +21,7 @@ class TextClassification(TaskTemplate):
             assert len(self.labels) == len(set(self.labels)), "Labels must be unique"
             # Cast labels to tuple to allow hashing
             self.__dict__["labels"] = tuple(sorted(self.labels))
-            self.__dict__["label_schema"] = copy.deepcopy(self.label_schema)
+            self.__dict__["label_schema"] = self.label_schema.copy()
             self.label_schema["labels"] = ClassLabel(names=self.labels)
 
     @property
