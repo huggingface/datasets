e42acac9dcb863253d08d1bf9d4cbc213f23d1b1
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2021-05-31 15:05:00 +0200

    Fix task name attribute

--- src/datasets/tasks/question_answering.py
+++ src/datasets/tasks/question_answering.py
@@ -8,7 +8,7 @@ from .base import TaskTemplate
 @dataclass(frozen=True)
 class QuestionAnsweringExtractive(TaskTemplate):
     # `task` is not a ClassVar since we want it to be part of the `asdict` output for JSON serialization
-    task: str = "question-answering"
+    task: str = "question-answering-extractive"
     input_schema: ClassVar[Features] = Features({"question": Value("string"), "context": Value("string")})
     label_schema: ClassVar[Features] = Features(
         {
