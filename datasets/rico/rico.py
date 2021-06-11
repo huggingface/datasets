d44bbb0a9984b9a1c3be99d5f377ec78b84d2645
ncoop57 <nacooper01@email.wm.edu>
2021-06-11 13:26:09 -0400

    Rename folder and file names

--- datasets/rico/rico.py
+++ datasets/rico/rico.py
@@ -16,7 +16,10 @@
 
 
 import csv
+import glob
 import json
+import imageio
+import pickle
 import os
 
 import datasets
@@ -40,7 +43,7 @@ This new dataset is designed to solve this great NLP task and is crafted with a 
 """
 
 # TODO: Add a link to an official homepage for the dataset here
-_HOMEPAGE = ""
+_HOMEPAGE = "http://interactionmining.org/rico"
 
 # TODO: Add the licence for the dataset here if you can find it
 _LICENSE = ""
@@ -48,14 +51,19 @@ _LICENSE = ""
 # TODO: Add link to the official dataset URLs here
 # The HuggingFace dataset library don't host the datasets but only point to the original files
 # This can be an arbitrary nested dict/list of URLs (see below in `_split_generators` method)
-_URLs = {
-    'first_domain': "https://huggingface.co/great-new-dataset-first_domain.zip",
-    'second_domain': "https://huggingface.co/great-new-dataset-second_domain.zip",
+_DATA_URLs = {
+    "screenshots_hierarchies": "https://storage.googleapis.com/crowdstf-rico-uiuc-4540/rico_dataset_v0.1/unique_uis.tar.gz",
+    "ui_metadata": "https://storage.googleapis.com/crowdstf-rico-uiuc-4540/rico_dataset_v0.1/ui_details.csv",
+    "layout_vectors": "https://storage.googleapis.com/crowdstf-rico-uiuc-4540/rico_dataset_v0.1/ui_layout_vectors.zip",
+    "traces": "https://storage.googleapis.com/crowdstf-rico-uiuc-4540/rico_dataset_v0.1/traces.tar.gz",
+    "animations": "https://storage.googleapis.com/crowdstf-rico-uiuc-4540/rico_dataset_v0.1/animations.tar.gz",
+    "playstore_metadata": "https://storage.googleapis.com/crowdstf-rico-uiuc-4540/rico_dataset_v0.1/app_details.csv",
+    "screenshots_semantic_hierarchies": "https://storage.cloud.google.com/crowdstf-rico-uiuc-4540/rico_dataset_v0.1/semantic_annotations.zip",
 }
 
 
 # TODO: Name of the dataset usually match the script name with CamelCase instead of snake_case
-class NewDataset(datasets.GeneratorBasedBuilder):
+class RicoDataset(datasets.GeneratorBasedBuilder):
     """TODO: Short description of my dataset."""
 
     VERSION = datasets.Version("1.1.0")
@@ -72,46 +80,37 @@ class NewDataset(datasets.GeneratorBasedBuilder):
     # data = datasets.load_dataset('my_dataset', 'first_domain')
     # data = datasets.load_dataset('my_dataset', 'second_domain')
     BUILDER_CONFIGS = [
-        datasets.BuilderConfig(name="first_domain", version=VERSION, description="This part of my dataset covers a first domain"),
-        datasets.BuilderConfig(name="second_domain", version=VERSION, description="This part of my dataset covers a second domain"),
+        datasets.BuilderConfig(
+            name="screenshots_hierarchies",
+            version=VERSION,
+            description="Contains 66k+ unique UI screens. For each UI, we present a screenshot (PNG file) and a detailed view hierarchy (JSON object)."
+        ),
+        datasets.BuilderConfig(name="ui_metadata", version=VERSION, description=""),
+        datasets.BuilderConfig(name="layout_vectors", version=VERSION, description=""),
+        datasets.BuilderConfig(name="traces", version=VERSION, description=""),
+        datasets.BuilderConfig(name="animations", version=VERSION, description=""),
+        datasets.BuilderConfig(name="playstore_metadata", version=VERSION, description=""),
+        datasets.BuilderConfig(name="screenshots_semantic_hierarchies", version=VERSION, description=""),
     ]
 
-    DEFAULT_CONFIG_NAME = "first_domain"  # It's not mandatory to have a default configuration. Just use one if it make sense.
+    DEFAULT_CONFIG_NAME = "screenshots_hierarchies"  # It's not mandatory to have a default configuration. Just use one if it make sense.
 
     def _info(self):
-        # TODO: This method specifies the datasets.DatasetInfo object which contains informations and typings for the dataset
-        if self.config.name == "first_domain":  # This is the name of the configuration selected in BUILDER_CONFIGS above
+        if self.config.name == "screenshots_hierarchies":
             features = datasets.Features(
                 {
-                    "sentence": datasets.Value("string"),
-                    "option1": datasets.Value("string"),
-                    "answer": datasets.Value("string")
-                    # These are the features of your dataset like images, labels ...
+                    "screenshot": datasets.Array3D(shape=(1440, 2560, 3), dtype="uint8"),
+                    # This is a JSON obj, but will be coded as a string
+                    "hierarchy": datasets.Value("string"),
                 }
             )
-        else:  # This is an example to show how to have different features for "first_domain" and "second_domain"
-            features = datasets.Features(
-                {
-                    "sentence": datasets.Value("string"),
-                    "option2": datasets.Value("string"),
-                    "second_domain_answer": datasets.Value("string")
-                    # These are the features of your dataset like images, labels ...
-                }
-            )
+
         return datasets.DatasetInfo(
-            # This is the description that will appear on the datasets page.
             description=_DESCRIPTION,
-            # This defines the different columns of the dataset and their types
-            features=features,  # Here we define them above because they are different between the two configurations
-            # If there's a common (input, target) tuple from the features,
-            # specify them here. They'll be used if as_supervised=True in
-            # builder.as_dataset.
+            features=features,
             supervised_keys=None,
-            # Homepage of the dataset for documentation
             homepage=_HOMEPAGE,
-            # License for the dataset if available
             license=_LICENSE,
-            # Citation for the dataset
             citation=_CITATION,
         )
 
@@ -123,33 +122,17 @@ class NewDataset(datasets.GeneratorBasedBuilder):
         # dl_manager is a datasets.download.DownloadManager that can be used to download and extract URLs
         # It can accept any type or nested list/dict and will give back the same structure with the url replaced with path to local files.
         # By default the archives will be extracted and a path to a cached folder where they are extracted is returned instead of the archive
-        my_urls = _URLs[self.config.name]
+        my_urls = _DATA_URLs[self.config.name]
         data_dir = dl_manager.download_and_extract(my_urls)
         return [
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN,
                 # These kwargs will be passed to _generate_examples
                 gen_kwargs={
-                    "filepath": os.path.join(data_dir, "train.jsonl"),
+                    "filepath": os.path.join(data_dir, "combined"),
                     "split": "train",
                 },
-            ),
-            datasets.SplitGenerator(
-                name=datasets.Split.TEST,
-                # These kwargs will be passed to _generate_examples
-                gen_kwargs={
-                    "filepath": os.path.join(data_dir, "test.jsonl"),
-                    "split": "test"
-                },
-            ),
-            datasets.SplitGenerator(
-                name=datasets.Split.VALIDATION,
-                # These kwargs will be passed to _generate_examples
-                gen_kwargs={
-                    "filepath": os.path.join(data_dir, "dev.jsonl"),
-                    "split": "dev",
-                },
-            ),
+            )
         ]
 
     def _generate_examples(
@@ -158,19 +141,29 @@ class NewDataset(datasets.GeneratorBasedBuilder):
         """ Yields examples as (key, example) tuples. """
         # This method handles input defined in _split_generators to yield (key, example) tuples from the dataset.
         # The `key` is here for legacy reason (tfds) and is not important in itself.
+        print(filepath)
+        screen_glob = sorted(glob.glob(os.path.join(filepath, "*.jpg")))
+        hierarchy_glob = sorted(glob.glob(os.path.join(filepath, "*.json")))
+        for idx, (screen_filepath, hierarchy_filepath) in enumerate(zip(screen_glob, hierarchy_glob)):
+            screen = imageio.imread(screen_filepath)
+            with open(hierarchy_filepath, "r") as f:
+                hierarchy = f.read()
 
-        with open(filepath, encoding="utf-8") as f:
-            for id_, row in enumerate(f):
-                data = json.loads(row)
-                if self.config.name == "first_domain":
-                    yield id_, {
-                        "sentence": data["sentence"],
-                        "option1": data["option1"],
-                        "answer": "" if split == "test" else data["answer"],
-                    }
-                else:
-                    yield id_, {
-                        "sentence": data["sentence"],
-                        "option2": data["option2"],
-                        "second_domain_answer": "" if split == "test" else data["second_domain_answer"],
-                    }
+            yield idx, {"screenshot": screen, "hierarchy": hierarchy}
+        #     pass
+
+        # with open(filepath, encoding="utf-8") as f:
+        #     for id_, row in enumerate(f):
+        #         data = json.loads(row)
+        #         if self.config.name == "first_domain":
+        #             yield id_, {
+        #                 "sentence": data["sentence"],
+        #                 "option1": data["option1"],
+        #                 "answer": "" if split == "test" else data["answer"],
+        #             }
+        #         else:
+        #             yield id_, {
+        #                 "sentence": data["sentence"],
+        #                 "option2": data["option2"],
+        #                 "second_domain_answer": "" if split == "test" else data["second_domain_answer"],
+        #             }
