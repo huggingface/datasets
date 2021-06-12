dfb8d41c7fc7ffade08cac5a0b820f2de4e55064
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-06-12 07:29:12 +0200

    Test extracted datasets path

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,12 +15,14 @@ from .s3_fixtures import *  # noqa: load s3 fixtures
 def set_test_cache_config(tmp_path_factory, monkeypatch):
     # test_hf_cache_home = tmp_path_factory.mktemp("cache")  # TODO: why a cache dir per test function does not work?
     test_hf_cache_home = tmp_path_factory.getbasetemp() / "cache"
-    test_hf_datasets_cache = str(test_hf_cache_home / "datasets")
-    test_hf_metrics_cache = str(test_hf_cache_home / "metrics")
-    test_hf_modules_cache = str(test_hf_cache_home / "modules")
-    monkeypatch.setattr("datasets.config.HF_DATASETS_CACHE", test_hf_datasets_cache)
-    monkeypatch.setattr("datasets.config.HF_METRICS_CACHE", test_hf_metrics_cache)
-    monkeypatch.setattr("datasets.config.HF_MODULES_CACHE", test_hf_modules_cache)
+    test_hf_datasets_cache = test_hf_cache_home / "datasets"
+    test_hf_metrics_cache = test_hf_cache_home / "metrics"
+    test_hf_modules_cache = test_hf_cache_home / "modules"
+    monkeypatch.setattr("datasets.config.HF_DATASETS_CACHE", str(test_hf_datasets_cache))
+    monkeypatch.setattr("datasets.config.HF_METRICS_CACHE", str(test_hf_metrics_cache))
+    monkeypatch.setattr("datasets.config.HF_MODULES_CACHE", str(test_hf_modules_cache))
+    test_extracted_datasets_path = test_hf_datasets_cache / "downloads" / "extracted"
+    monkeypatch.setattr("datasets.config.EXTRACTED_DATASETS_PATH", str(test_extracted_datasets_path))
 
 
 FILE_CONTENT = """\
