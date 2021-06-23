0c537ea23b4f6cc2e9f1034de800a932e4d11e6c
Quentin Lhoest <lhoest.q@gmail.com>
2021-06-23 18:26:05 +0200

    bump to 3.0.0

--- setup.py
+++ setup.py
@@ -73,9 +73,9 @@ REQUIRED_PKGS = [
     # We use numpy>=1.17 to have np.random.Generator (Dataset shuffling)
     "numpy>=1.17",
     # Backend and serialization.
-    # Minimum 2.0.0 to support nested types in parquet format
+    # Minimum 3.0.0 to support mix of struct and list types in parquet format
     # pyarrow 4.0.0 introduced segfault bug, see: https://github.com/huggingface/datasets/pull/2268
-    "pyarrow>=2.0.0,!=4.0.0",
+    "pyarrow>=3.0.0,!=4.0.0",
     # For smart caching dataset processing
     "dill",
     # For performance gains with apache arrow
