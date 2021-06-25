158c5451577d47dd4c57710f0c3e127152c58b87
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-06-25 16:51:52 +0200

    Add coding_format attribute

--- src/datasets/features/audio.py
+++ src/datasets/features/audio.py
@@ -6,11 +6,15 @@ import pyarrow as pa
 
 @dataclass
 class Audio:
+    coding_format: str = None
     id: Optional[str] = None
     # Automatically constructed
     dtype: ClassVar[str] = "dict"
     pa_type: ClassVar[Any] = None
     _type: str = field(default="Audio", init=False, repr=False)
+
+    def __post_init__(self):
+        self.coding_format = self.coding_format.upper() if self.coding_format else None
 
     def __call__(self):
         return pa.struct({"array": pa.array(), "sample_rate": pa.int32()})
@@ -19,5 +23,5 @@ class Audio:
         import soundfile as sf
 
         with open(value, "rb") as f:
-            array, sample_rate = sf.read(f)
+            array, sample_rate = sf.read(f, format=self.coding_format)
         return {"array": array, "sample_rate": sample_rate}
