e51a8a989d517c307d5f4e992d41b89137ffade0
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-06-28 15:46:06 +0200

    Test Audio encode example

--- tests/features/test_audio.py
+++ tests/features/test_audio.py
@@ -7,3 +7,12 @@ def test_audio_instantiation():
     assert audio.dtype == "dict"
     assert audio.pa_type is None
     assert audio._type == "Audio"
+
+
+def test_audio_encode_example(shared_datadir):
+    audio_path = str(shared_datadir / "test_audio_44100.wav")
+    audio = Audio()
+    encoded_example = audio.encode_example(audio_path)
+    assert encoded_example.keys() == {'array', 'sample_rate'}
+    assert encoded_example["array"].shape == (202311, 2)
+    assert encoded_example["sample_rate"] == 44100
