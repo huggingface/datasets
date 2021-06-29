42308436f541b78a785a9c78d781893d94e0239a
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-06-29 09:10:14 +0200

    Test dataset with Audio feature

--- tests/features/test_audio.py
+++ tests/features/test_audio.py
@@ -2,7 +2,8 @@ import sys
 
 import pytest
 
-from datasets.features import Audio
+from datasets import Dataset
+from datasets.features import Audio, Features
 
 
 def test_audio_instantiation():
@@ -23,3 +24,19 @@ def test_audio_encode_example(shared_datadir):
     assert encoded_example.keys() == {"array", "sample_rate"}
     assert encoded_example["array"].shape == (404622,)  # TODO: (202311, 2)
     assert encoded_example["sample_rate"] == 44100
+
+
+@pytest.mark.skipif(
+    sys.platform == "linux", reason="linux requires libsndfile installed using distribution package manager"
+)
+def test_dataset_with_audio_feature(shared_datadir):
+    audio_path = str(shared_datadir / "test_audio_44100.wav")
+    data = {"file": [audio_path]}
+    features = Features({"file": Audio()})
+    dset = Dataset.from_dict(data, features=features)
+    assert dset.shape == (1, 1)
+    assert dset.column_names == ["file"]
+    assert isinstance(dset[0]["file"], dict)
+    assert dset[0]["file"].keys() == {"array", "sample_rate"}
+    assert len(dset[0]["file"]["array"]) == 404622  # TODO: to numpy? .shape == (202311, 2)
+    assert dset[0]["file"]["sample_rate"] == 44100
