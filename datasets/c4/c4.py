5310653c25834415f52ec0a24c419b232ee0e9d8
Quentin Lhoest <lhoest.q@gmail.com>
2021-07-01 17:05:33 +0200

    pin data revision

--- datasets/c4/c4.py
+++ datasets/c4/c4.py
@@ -41,7 +41,7 @@ _N_SHARDS_PER_SPLIT = {
 }
 
 _DATA_URL = (
-    "https://huggingface.co/datasets/allenai/c4/resolve/main/{name}/c4-{split}.{index:05d}-of-{n_shards:05d}.json.gz"
+    "https://huggingface.co/datasets/allenai/c4/resolve/1ddc917116b730e1859edef32896ec5c16be51d0/{name}/c4-{split}.{index:05d}-of-{n_shards:05d}.json.gz"
 )
 
 
