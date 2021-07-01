f369c9f2a48b8a2291c7468669b8312b5c6ab6a8
Quentin Lhoest <lhoest.q@gmail.com>
2021-07-01 11:44:26 +0200

    add infos

--- datasets/c4/c4.py
+++ datasets/c4/c4.py
@@ -1,19 +1,3 @@
-# coding=utf-8
-# Copyright 2020 The TensorFlow Datasets Authors and the HuggingFace Datasets Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Lint as: python3
 """C4 dataset based on Common Crawl."""
 
 
@@ -32,8 +16,8 @@ A colossal, cleaned version of Common Crawl's web crawl corpus.
 Based on Common Crawl dataset: "https://commoncrawl.org".
 
 This is the processed version of Google's C4 dataset by AllenAI.
-
 """
+
 _CITATION = """
 @article{2019t5,
     author = {Colin Raffel and Noam Shazeer and Adam Roberts and Katherine Lee and Sharan Narang and Michael Matena and Yanqi Zhou and Wei Li and Peter J. Liu},
@@ -44,6 +28,7 @@ _CITATION = """
     eprint = {1910.10683},
 }
 """
+
 _URL = "https://github.com/allenai/allennlp/discussions/5056"
 
 _VARIANTS = ["en", "realnewslike", "en.noblocklist", "en.noclean"]
@@ -88,11 +73,12 @@ class C4(datasets.GeneratorBasedBuilder):
                 _DATA_URL.format(name=self.config.name, split=split, index=index, n_shards=n_shards)
                 for index in range(n_shards)
             ]
-        downloaded_files = dl_manager.download(data_urls)
+        train_downloaded_files = dl_manager.download(data_urls["train"])
+        validation_downloaded_files = dl_manager.download(data_urls["train"])
         return [
-            datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepaths": downloaded_files["train"]}),
+            datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepaths": train_downloaded_files}),
             datasets.SplitGenerator(
-                name=datasets.Split.VALIDATION, gen_kwargs={"filepaths": downloaded_files["validation"]}
+                name=datasets.Split.VALIDATION, gen_kwargs={"filepaths": validation_downloaded_files}
             ),
         ]
 
