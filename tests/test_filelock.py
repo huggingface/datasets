02a716052a59184e074a20627bc61ae9724be482
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-07-11 18:19:58 +0200

    Fix tests

--- tests/test_filelock.py
+++ tests/test_filelock.py
@@ -7,8 +7,8 @@ from datasets.utils.filelock import FileLock, Timeout
 
 
 def test_filelock(tmpdir):
-    lock1 = FileLock(tmpdir / "foo.lock")
-    lock2 = FileLock(tmpdir / "foo.lock")
+    lock1 = FileLock(str(tmpdir / "foo.lock"))
+    lock2 = FileLock(str(tmpdir / "foo.lock"))
     timeout = 0.01
     with lock1.acquire():
         with pytest.raises(Timeout):
@@ -19,7 +19,7 @@ def test_filelock(tmpdir):
 
 def test_long_filename(tmpdir):
     filename = "a" * 1000 + ".lock"
-    lock1 = FileLock(tmpdir / filename)
+    lock1 = FileLock(str(tmpdir / filename))
     assert lock1._lock_file.endswith(".lock")
     assert not lock1._lock_file.endswith(filename)
     assert len(os.path.basename(lock1._lock_file)) <= 255
