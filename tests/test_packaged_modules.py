a9140e8d438a6c0532a92543e62e2459989c1c7e
Quentin Lhoest <lhoest.q@gmail.com>
2021-07-13 16:41:54 +0200

    fix test on windows

--- tests/test_packaged_modules.py
+++ tests/test_packaged_modules.py
@@ -1,3 +1,4 @@
+import os
 import textwrap
 
 import pyarrow as pa
@@ -62,7 +63,7 @@ def test_csv_generate_tables_raises_error_with_malformed_csv(csv_file, malformed
     assert any(
         record.levelname == "ERROR"
         and f"Failed to read file" in record.message
-        and malformed_csv_file in record.message
+        and os.path.basename(malformed_csv_file) in record.message
         for record in caplog.records
     )
 
