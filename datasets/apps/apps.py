e3d8970f5d8fe079fe9ff51e8a7a09e110bb6620
arampacha <aruthart@gmail.com>
2021-07-17 17:56:39 +0000

    adds formatted dataset config

--- datasets/apps/apps.py
+++ datasets/apps/apps.py
@@ -69,15 +69,16 @@ class APPS(datasets.GeneratorBasedBuilder):
     # You will be able to load one or the other configurations in the following list with
     # data = datasets.load_dataset('my_dataset', 'first_domain')
     # data = datasets.load_dataset('my_dataset', 'second_domain')
-    # BUILDER_CONFIGS = [
-    #     datasets.BuilderConfig(name="first_domain", version=VERSION, description="This part of my dataset covers a first domain"),
-    #     datasets.BuilderConfig(name="second_domain", version=VERSION, description="This part of my dataset covers a second domain"),
-    # ]
+    BUILDER_CONFIGS = [
+        datasets.BuilderConfig(name="raw", version=VERSION, description="Outputs separate entry for question, starter code et."),
+        datasets.BuilderConfig(name="formatted", version=VERSION, description="Outputs a formatted qestions for APPS task."),
+    ]
 
-    # DEFAULT_CONFIG_NAME = "first_domain"
+    DEFAULT_CONFIG_NAME = "formatted"
 
     def _info(self):
-        features = datasets.Features(
+        if self.config.name == "raw":
+            features = datasets.Features(
                 {
                     "id": datasets.Value("int64"),
                     "question": datasets.Value("string"),
@@ -85,7 +86,18 @@ class APPS(datasets.GeneratorBasedBuilder):
                     "starter_code": datasets.Value("string"),
                     "answer_type": datasets.Value("string"),
                 }
-                )
+            )
+        elif self.config.name == "formatted":
+            features = datasets.Features(
+                {
+                    "id": datasets.Value("int64"),
+                    "question": datasets.Value("string"),
+                    "answer": datasets.Value("string"),
+                }
+            )
+        else:
+            raise ValueError(f"{self.config.name} is not a proper config for APPS dataset. Available configs are: 'raw', 'formatted'.")
+        
         return datasets.DatasetInfo(
             description=_DESCRIPTION,
             features=features,
@@ -120,7 +132,6 @@ class APPS(datasets.GeneratorBasedBuilder):
             sols_fname = os.path.join(problem, "solutions.json")
             starter_code = os.path.join(problem, "starter_code.py")
 
-            # print(question_fname)
 
             if os.path.exists(starter_code):
                 answer_type = "\nUse Call-Based format\n"
@@ -141,20 +152,33 @@ class APPS(datasets.GeneratorBasedBuilder):
             with open(question_fname, 'r') as f:
                 question_str = f.read()
 
+            if self.config.name == "raw":
+                prompt_dict = {
+                    "question":question_str, 
+                    "starter_code":starter_code, 
+                    "answer_type":answer_type,
+                }
+            else:
+                question_str = (
+                    "\nQUESTION:\n" + 
+                    question_str + "\n" + 
+                    starter_code + "\n" + 
+                    answer_type + 
+                    "\nANSWER:\n"
+                )
+                prompt_dict = {"question":question_str}
             # Read all the solutions
             with open(sols_fname, 'r') as f:
                 sols_str_list = json.load(f)
                 for sol_str in sols_str_list:
                     sol_str = reindent_code(sol_str)
-
                     yield id_, {
                         "id":id_, 
-                        "question":question_str, 
-                        "starter_code":starter_code, 
-                        "answer_type":answer_type,
-                        "answer":sol_str
+                        "answer":sol_str,
+                        **prompt_dict
                     }
                     id_ += 1
+
 
 
 def generate_prompt(
