486d9467f06f9693b0f6990d9b5fefd8fb3f5668
ggdupont <ger.dupont@gmail.com>
2021-07-20 22:46:27 +0200

    WIP dataset from ES query

--- src/datasets/packaged_modules/elasticsearch/elasticsearch.py
+++ src/datasets/packaged_modules/elasticsearch/elasticsearch.py
@@ -23,9 +23,10 @@ class ElasticsearchConfig(datasets.BuilderConfig):
     port: Optional[int] = None,
     es_index_name: Optional[str] = None,
     es_index_config: Optional[dict] = None,
+    query: Optional[str] = None,
 
 
-class ElasticsearchBuilder(datasets.ArrowBasedBuilder):
+class ElasticsearchBuilder(datasets.GeneratorBasedBuilder):
     """Elasticsearch based Builder to load datasets based on an Elasticsearch index and a filter query."""
 
     BUILDER_CONFIG_CLASS = ElasticsearchConfig
@@ -56,14 +57,42 @@ class ElasticsearchBuilder(datasets.ArrowBasedBuilder):
             raise Exception(msg)
 
         # TODO load index mapping to set self.config.feature
+        # self.es_client.indices.get_mapping(index=self.config.es_index_name)
 
     def _info(self):
-        return datasets.DatasetInfo()
+        # TODO use data from mapping to have more detailed dataset info
+        es_dataset_info = datasets.DatasetInfo()
+
+        return es_dataset_info
 
     def _split_generators(self, dl_manager):
-        pass
+        query = "*" if self.config.query is None else self.config.query
 
-    def _generate_tables(self, query: str):
+        # open point in time to "freeze" index state
+        point_in_time = self.es_client.open_point_in_time(index=self.config.es_index_name, keep_alive="5m")
+
+        # probe the search results to get the total number of results
+        response = self.es_client.search(
+            index=self.config.es_index_name,
+            body={"query": {"multi_match": {"query": query, "fields": ["text"], "type": "cross_fields"}}, "size": 0},
+            params={point_in_time},
+        )
+        total_number_of_results = response["hits"]["total"]["value"]
+
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                # These kwargs will be passed to _generate_examples
+                gen_kwargs={
+                    "query": query,
+                    "point_in_time": point_in_time,
+                    "max_k": total_number_of_results,
+                },
+            ),
+        ]
+
+    def _generate_examples(self, query, point_in_time, max_k):
+        # TODO load results page by page eventually loading page in background while yielding
         k = 10
         response = self.es_client.search(
             index=self.config.es_index_name,
