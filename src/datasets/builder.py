f18a57e6dcf8d2e9125b2104f4fd85b60641b77e
Quentin Lhoest <lhoest.q@gmail.com>
2021-07-20 17:55:48 +0200

    don't use data_dir if it's None for config_id

--- src/datasets/builder.py
+++ src/datasets/builder.py
@@ -123,6 +123,8 @@ class BuilderConfig:
         # it was previously ignored before the introduction of config id because we didn't want
         # to change the config name. Now it's fine to take it into account for the config id.
         # config_kwargs_to_add_to_suffix.pop("data_dir", None)
+        if "data_dir" in config_kwargs_to_add_to_suffix and config_kwargs_to_add_to_suffix["data_dir"] is None:
+            del config_kwargs_to_add_to_suffix["data_dir"]
         if config_kwargs_to_add_to_suffix:
             # we don't care about the order of the kwargs
             config_kwargs_to_add_to_suffix = {
