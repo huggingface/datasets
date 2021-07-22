cea1a295a4325242988645bf121841e70c317303
Quentin Lhoest <lhoest.q@gmail.com>
2021-07-22 12:04:25 +0200

    Release: 1.10.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ author = "HuggingFace"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "1.10.1"
+release = "1.10.2"
 
 
 # -- General configuration ---------------------------------------------------
