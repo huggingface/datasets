35b5e4bc0cb2ed896e40f3eb2a4aa3de1cb1a6c5
Mario Šaško <mariosasko777@gmail.com>
2021-07-28 01:53:18 -0700

    Deal with the bad check in test_load.py (#2721)
    * Remove bad check in test_load.py
    
    * Fix import

--- tests/test_load.py
+++ tests/test_load.py
@@ -227,8 +227,6 @@ def test_load_dataset_local(dataset_loading_script_dir, data_dir, keep_in_memory
         datasets.load_dataset("_dummy")
     m_combined_path = re.search(fr"\S*{re.escape(os.path.join('_dummy', '_dummy.py'))}\b", str(exc_info.value))
     assert m_combined_path is not None and os.path.isabs(m_combined_path.group())
-    m_path = re.search(r"\S*_dummy\b", str(exc_info.value))
-    assert m_path is not None and os.path.isabs(m_path.group())
 
 
 @require_streaming
