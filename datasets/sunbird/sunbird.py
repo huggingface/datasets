5c43cc299ff25480ee9a3d269a36fd1c1bc725e8
Benjamin Akera <akeraben@gmail.com>
2021-07-31 17:01:33 -0400

    Update sunbird.py

--- datasets/sunbird/sunbird.py
+++ datasets/sunbird/sunbird.py
@@ -250,7 +250,10 @@ class Sunbird(datasets.GeneratorBasedBuilder):
             dl_dir = dl_manager.download_and_extract(_URL["supervised"].format(lang_pair))
         
         data_dir = os.path.join(dl_dir, os.path.join("v1-dataset/v1.0", domain, lang_pair))
+       
+        print("########",src_tag)
         print("########",data_dir)
+        print("$$$$$",os.path.join(data_dir, f"test.{src_tag}"))
         
         output=[]
 
