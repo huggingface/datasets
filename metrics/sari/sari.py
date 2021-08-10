f4455632793e7a62e74a002c47fba065306dd00f
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-08-10 11:20:35 +0200

    Fix sacrebleu tokenizers

--- metrics/sari/sari.py
+++ metrics/sari/sari.py
@@ -239,7 +239,7 @@ def normalize(sentence, lowercase: bool = True, tokenizer: str = "13a", return_s
         sentence = sentence.lower()
 
     if tokenizer in ["13a", "intl"]:
-        normalized_sent = sacrebleu.TOKENIZERS[tokenizer]()(sentence)
+        normalized_sent = sacrebleu.metrics.bleu._get_tokenizer(tokenizer)(sentence)
     elif tokenizer == "moses":
         normalized_sent = sacremoses.MosesTokenizer().tokenize(sentence, return_str=True, escape=False)
     elif tokenizer == "penn":
