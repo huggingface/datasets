4795f2350ee93e1b7c902015661cf7bb88bfed73
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-08-11 13:16:56 +0200

    Support streaming xz compressed files

--- src/datasets/utils/streaming_download_manager.py
+++ src/datasets/utils/streaming_download_manager.py
@@ -15,7 +15,7 @@ from .logging import get_logger
 
 logger = get_logger(__name__)
 BASE_KNOWN_EXTENSIONS = ["txt", "csv", "json", "jsonl", "tsv", "conll", "conllu", "parquet", "pkl", "pickle", "xml"]
-COMPRESSION_KNOWN_EXTENSIONS = ["zst"]
+COMPRESSION_KNOWN_EXTENSIONS = ["xz", "zst"]
 
 
 def xjoin(a, *p):
