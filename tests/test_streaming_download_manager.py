a5a3f4803adf23b1b59b0a6f33b152516129e51f
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-08-16 12:34:00 +0200

    Fix streaming zip files from canonical datasets (#2805)
    * Test streaming zip file with join
    
    * Simplify streaming gzip and zip files
    
    * Simplify xopen
    
    * Fix tests after simplifying streaming gzip/zip
    
    * Test xjoin
    
    * Fix xjoin for paths without leading protocol in path
    
    * Remove unused import
    
    * Fix xjoin
    
    * Refactor xjoin

--- tests/test_streaming_download_manager.py
+++ tests/test_streaming_download_manager.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 
 from .utils import require_streaming
@@ -7,6 +5,25 @@ from .utils import require_streaming
 
 TEST_URL = "https://huggingface.co/datasets/lhoestq/test/raw/main/some_text.txt"
 TEST_URL_CONTENT = "foo\nbar\nfoobar"
+
+
+@require_streaming
+@pytest.mark.parametrize(
+    "input_path, paths_to_join, expected_path",
+    [
+        ("https://host.com/archive.zip", ("file.txt",), "zip://file.txt::https://host.com/archive.zip"),
+        (
+            "zip://folder::https://host.com/archive.zip",
+            ("file.txt",),
+            "zip://folder/file.txt::https://host.com/archive.zip",
+        ),
+    ],
+)
+def test_xjoin(input_path, paths_to_join, expected_path):
+    from datasets.utils.streaming_download_manager import xjoin
+
+    output_path = xjoin(input_path, *paths_to_join)
+    assert output_path == expected_path
 
 
 @require_streaming
@@ -56,11 +73,13 @@ def test_streaming_dl_manager_download_and_extract_no_extraction(urlpath):
 
 @require_streaming
 def test_streaming_dl_manager_extract(text_gz_path):
-    from datasets.utils.streaming_download_manager import StreamingDownloadManager
+    from datasets.utils.streaming_download_manager import StreamingDownloadManager, xopen
 
     dl_manager = StreamingDownloadManager()
-    path = os.path.basename(text_gz_path).rstrip(".gz")
-    assert dl_manager.extract(text_gz_path) == f"gzip://{path}::{text_gz_path}"
+    output_path = dl_manager.extract(text_gz_path)
+    assert output_path == text_gz_path
+    fsspec_open_file = xopen(output_path)
+    assert fsspec_open_file.compression == "gzip"
 
 
 @require_streaming
@@ -68,8 +87,23 @@ def test_streaming_dl_manager_download_and_extract_with_extraction(text_gz_path,
     from datasets.utils.streaming_download_manager import StreamingDownloadManager, xopen
 
     dl_manager = StreamingDownloadManager()
-    filename = os.path.basename(text_gz_path).rstrip(".gz")
-    out = dl_manager.download_and_extract(text_gz_path)
-    assert out == f"gzip://{filename}::{text_gz_path}"
-    with xopen(out, encoding="utf-8") as f, open(text_path, encoding="utf-8") as expected_file:
+    output_path = dl_manager.download_and_extract(text_gz_path)
+    assert output_path == text_gz_path
+    fsspec_open_file = xopen(output_path, encoding="utf-8")
+    assert output_path == text_gz_path
+    with fsspec_open_file as f, open(text_path, encoding="utf-8") as expected_file:
         assert f.read() == expected_file.read()
+
+
+@require_streaming
+@pytest.mark.parametrize(
+    "input_path, filename, expected_path",
+    [("https://domain.org/archive.zip", "filename.jsonl", "zip://filename.jsonl::https://domain.org/archive.zip")],
+)
+def test_streaming_dl_manager_download_and_extract_with_join(input_path, filename, expected_path):
+    from datasets.utils.streaming_download_manager import StreamingDownloadManager, xjoin
+
+    dl_manager = StreamingDownloadManager()
+    extracted_path = dl_manager.download_and_extract(input_path)
+    output_path = xjoin(extracted_path, filename)
+    assert output_path == expected_path
