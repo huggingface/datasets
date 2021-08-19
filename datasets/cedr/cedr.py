17eaa47981edce8981c4ec20974b2bc3f9c51169
Aleksandr <sanya-naumov@mail.ru>
2021-08-19 14:56:11 +0300

    fix typo

--- datasets/cedr/cedr.py
+++ datasets/cedr/cedr.py
@@ -94,7 +94,7 @@ class Cedr(datasets.GeneratorBasedBuilder):
             features = datasets.Features(
                 {
                     "text": datasets.Value("string"),
-                    "labels": datasets.features.Sequence(datasets.ClassLabel(names=["joy," "sadness", "surprise", "fear", "anger"])),
+                    "labels": datasets.features.Sequence(datasets.ClassLabel(names=["joy", "sadness", "surprise", "fear", "anger"])),
                     "source": datasets.Value("string"),
                     # These are the features of your dataset like images, labels ...
                 }
@@ -103,7 +103,7 @@ class Cedr(datasets.GeneratorBasedBuilder):
             features = datasets.Features(
                 {
                     "text": datasets.Value("string"),
-                    "labels": datasets.features.Sequence(datasets.ClassLabel(names=["joy," "sadness", "surprise", "fear", "anger"])),
+                    "labels": datasets.features.Sequence(datasets.ClassLabel(names=["joy", "sadness", "surprise", "fear", "anger"])),
                     "source": datasets.Value("string"),
                     "sentences": [[{"forma": datasets.Value("string"), "lemma": datasets.Value("string"),}]]
                     # These are the features of your dataset like images, labels ...
