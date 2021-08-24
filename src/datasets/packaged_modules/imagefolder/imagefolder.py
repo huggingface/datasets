cf4294bf0a558d25e74802484c4d99cd61bddcc4
nateraw <nxr9266@g.rit.edu>
2021-08-23 18:50:35 -0600

    :white_check_mark: Update tests

--- src/datasets/packaged_modules/imagefolder/imagefolder.py
+++ src/datasets/packaged_modules/imagefolder/imagefolder.py
@@ -26,23 +26,26 @@ class ImageFolder(datasets.GeneratorBasedBuilder):
     BUILDER_CONFIG_CLASS = ImageFolderConfig
 
     def _info(self):
-        return datasets.DatasetInfo(features=self.config.features)
+        return datasets.DatasetInfo(
+            features=datasets.Features(
+                {
+                    "image_file": datasets.Value("string"),
+                    "labels": datasets.Value("string")
+                }
+            ),
+        )
 
     def _split_generators(self, dl_manager):
         if not self.config.data_files:
             raise ValueError(f"At least one data file must be specified, but got data_files={self.config.data_files}")
 
         data_files = dl_manager.download_and_extract(self.config.data_files)
-        if isinstance(data_files, (str, list, tuple)):
-            files = data_files
-            if isinstance(files, str):
-                files = [files]
-            return [datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"archive_path": data_files})]
+        if isinstance(data_files, str):
+            folder = data_files
+            return [datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"archive_path": folder})]
         splits = []
-        for split_name, files in data_files.items():
-            if isinstance(files, str):
-                files = [files]
-            splits.append(datasets.SplitGenerator(name=split_name, gen_kwargs={"archive_path": files}))
+        for split_name, folder in data_files.items():
+            splits.append(datasets.SplitGenerator(name=split_name, gen_kwargs={"archive_path": folder}))
         return splits
 
     def _generate_examples(self, archive_path):
@@ -50,4 +53,4 @@ class ImageFolder(datasets.GeneratorBasedBuilder):
         extensions = {".jpg", ".jpeg", ".png", ".ppm", ".bmp", ".pgm", ".tif", ".tiff", ".webp"}
         for i, path in enumerate(Path(archive_path).glob("**/*")):
             if path.suffix in extensions:
-                yield i, {"file": path.as_posix(), "labels": path.parent.name.lower()}
+                yield i, {"image_file": path.as_posix(), "labels": path.parent.name.lower()}
