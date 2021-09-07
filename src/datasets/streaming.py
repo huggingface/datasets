7c75ceb62bc1f00817628a44c0e6c7aa1ffdd264
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-09-07 09:05:17 +0200

    Use xpathjoin to patch Path joinpath/__truediv__

--- src/datasets/streaming.py
+++ src/datasets/streaming.py
@@ -1,10 +1,11 @@
 import importlib
 from functools import partial
 from typing import Optional, Union
+from unittest.mock import patch
 
 from .utils.logging import get_logger
 from .utils.patching import patch_submodule
-from .utils.streaming_download_manager import xjoin, xopen
+from .utils.streaming_download_manager import xjoin, xopen, xpathjoin
 
 
 logger = get_logger(__name__)
@@ -13,7 +14,7 @@ logger = get_logger(__name__)
 def extend_module_for_streaming(module_path, use_auth_token: Optional[Union[str, bool]] = None):
     """
     Extend the `open` and `os.path.join` functions of the module to support data streaming.
-    They rare replaced by `xopen` and `xjoin` defined to work with the StreamingDownloadManager.
+    They are replaced by `xopen` and `xjoin` defined to work with the StreamingDownloadManager.
 
     We use fsspec to extend `open` to be able to read remote files.
     To join paths and navigate in remote compressed archives, we use the "::" separator.
@@ -27,3 +28,6 @@ def extend_module_for_streaming(module_path, use_auth_token: Optional[Union[str,
         patch_submodule(module, "open", xopen).start()
     # allow to navigate in remote zip files
     patch_submodule(module, "os.path.join", xjoin).start()
+    if hasattr(module, "Path"):
+        patch.object(module.Path, "joinpath", xpathjoin).start()
+        patch.object(module.Path, "__truediv__", xpathjoin).start()
