561e2bb40ecde4f0d637fea8be3d111c64adf61b
anton-l <aglozhkov@gmail.com>
2021-09-09 14:59:35 +0300

    Style checks

--- datasets/superb/superb.py
+++ datasets/superb/superb.py
@@ -301,17 +301,17 @@ class Superb(datasets.GeneratorBasedBuilder):
             ```
             mkdir VoxCeleb1
             cd VoxCeleb1
-            
+
             wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox1_dev_wav_partaa
             wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox1_dev_wav_partab
             wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox1_dev_wav_partac
             wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox1_dev_wav_partad
             cat vox1_dev* > vox1_dev_wav.zip
             unzip vox1_dev_wav.zip
-            
+
             wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox1_test_wav.zip
             unzip vox1_test_wav.zip
-            
+
             # download the official SUPERB train-dev-test split
             wget https://raw.githubusercontent.com/s3prl/s3prl/master/s3prl/downstream/voxceleb1/veri_test_class.txt
             ```"""
