ced158c53fcc142eb83ddc6d56b0c8a58d4bef61
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-09-09 10:15:12 +0200

    Use context manager for file opening

--- datasets/docred/docred.py
+++ datasets/docred/docred.py
@@ -100,8 +100,11 @@ class DocRed(datasets.GeneratorBasedBuilder):
 
     def _generate_examples(self, filepath, rel_info):
         """Generate DocRED examples."""
-        relation_name_map = json.load(open(rel_info, encoding="utf-8"))
-        data = json.load(open(filepath, encoding="utf-8"))
+
+        with open(rel_info, encoding="utf-8") as f:
+            relation_name_map = json.load(f)
+        with open(filepath, encoding="utf-8") as f:
+            data = json.load(f)
 
         for idx, example in enumerate(data):
 
