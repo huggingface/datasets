a7071cce988808cd048378f9c2bc2614cf2c8cf7
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-09-21 09:52:01 +0200

    Compose Formatter with ArrowFeaturesDecoder

--- src/datasets/formatting/formatting.py
+++ src/datasets/formatting/formatting.py
@@ -213,6 +213,7 @@ class Formatter(Generic[RowFormat, ColumnFormat, BatchFormat]):
     def __init__(self, features=None):
         self.features = features
         self.python_features_decoder = PythonFeaturesDecoder(self.features)
+        self.arrow_features_decoder = ArrowFeaturesDecoder(self.features)
 
     def __call__(self, pa_table: pa.Table, query_type: str) -> Union[RowFormat, ColumnFormat, BatchFormat]:
         if query_type == "row":
