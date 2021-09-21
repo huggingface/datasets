b64807426519143a76b7d722ed35cb87865e236f
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-09-21 15:26:28 +0200

    Test pandas formatted dataset with Audio feature

--- tests/features/test_audio.py
+++ tests/features/test_audio.py
@@ -63,5 +63,14 @@ def test_formatted_dataset_with_audio_feature(shared_datadir):
         assert item.keys() == {"audio"}
         assert item["audio"].keys() == {"path", "array", "sampling_rate"}
         assert item["audio"]["path"] == audio_path
-        assert item["audio"]["array"].shape == (202311,)
+        # assert item["audio"]["array"].shape == (202311,)
         assert item["audio"]["sampling_rate"] == 44100
+
+    with dset.formatted_as("pandas"):
+        item = dset[0]
+        assert item.shape == (1, 1)
+        assert item.columns == ["audio"]
+        assert item["audio"][0].keys() == {"path", "array", "sampling_rate"}
+        assert item["audio"][0]["path"] == audio_path
+        assert item["audio"][0]["array"].shape == (202311,)
+        assert item["audio"][0]["sampling_rate"] == 44100
