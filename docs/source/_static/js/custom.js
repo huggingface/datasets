568db594d51110da9e23d224abded2a976b3c8c7
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-10-14 18:07:42 +0200

    Release docs

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -1,6 +1,6 @@
 // These two things need to be updated at each release for the version selector.
 // Last stable version
-const stableVersion = "v1.13.1"
+const stableVersion = "v1.13.2"
 // Dictionary doc folder to label
 const versionMapping = {
     "master": "master",
@@ -33,6 +33,7 @@ const versionMapping = {
     "v1.12.1": "v1.12.1",
     "v1.13.0": "v1.13.0",
     "v1.13.1": "v1.13.1",
+    "v1.13.2": "v1.13.2",
 }
 
 function addIcon() {
--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -18,7 +18,7 @@
 # pylint: enable=line-too-long
 # pylint: disable=g-import-not-at-top,g-bad-import-order,wrong-import-position
 
-__version__ = "1.13.2"
+__version__ = "1.13.3.dev0"
 
 import pyarrow
 from packaging import version as _version
