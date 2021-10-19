f1ba17094f26e848b14ea052d13cb96b3cc26f1d
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2021-10-19 17:31:08 +0300

    Add documentation

--- metrics/google_bleu/google_bleu.py
+++ metrics/google_bleu/google_bleu.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 """ Google BLEU (aka GLEU) metric. """
 
-from typing import Dict, Any, List
+from typing import Dict, List
 
 from nltk.translate import gleu_score
 
@@ -22,15 +22,80 @@ import datasets
 from datasets import MetricInfo
 
 _CITATION = """\
-TODO
+@misc{wu2016googles,
+      title={Google's Neural Machine Translation System: Bridging the Gap between Human and Machine Translation}, 
+      author={Yonghui Wu and Mike Schuster and Zhifeng Chen and Quoc V. Le and Mohammad Norouzi and Wolfgang Macherey
+              and Maxim Krikun and Yuan Cao and Qin Gao and Klaus Macherey and Jeff Klingner and Apurva Shah and Melvin
+              Johnson and Xiaobing Liu and Łukasz Kaiser and Stephan Gouws and Yoshikiyo Kato and Taku Kudo and Hideto
+              Kazawa and Keith Stevens and George Kurian and Nishant Patil and Wei Wang and Cliff Young and
+              Jason Smith and Jason Riesa and Alex Rudnick and Oriol Vinyals and Greg Corrado and Macduff Hughes
+              and Jeffrey Dean},
+      year={2016},
+      eprint={1609.08144},
+      archivePrefix={arXiv},
+      primaryClass={cs.CL}
+}
 """
 
 _DESCRIPTION = """\
-TODO
+The BLEU score has some undesirable properties when used for single
+sentences, as it was designed to be a corpus measure. We therefore
+use a slightly different score for our RL experiments which we call
+the 'GLEU score'. For the GLEU score, we record all sub-sequences of
+1, 2, 3 or 4 tokens in output and target sequence (n-grams). We then
+compute a recall, which is the ratio of the number of matching n-grams
+to the number of total n-grams in the target (ground truth) sequence,
+and a precision, which is the ratio of the number of matching n-grams
+to the number of total n-grams in the generated output sequence. Then
+GLEU score is simply the minimum of recall and precision. This GLEU
+score's range is always between 0 (no matches) and 1 (all match) and
+it is symmetrical when switching output and target. According to
+our experiments, GLEU score correlates quite well with the BLEU
+metric on a corpus level but does not have its drawbacks for our per
+sentence reward objective.
 """
 
 _KWARGS_DESCRIPTION = """\
-TODO
+Computes corpus-level Google BLEU (GLEU) score of translated segments against one or more references.
+Instead of averaging the sentence level GLEU scores (i.e. macro-average precision), Wu et al. (2016) sum up the matching
+tokens and the max of hypothesis and reference tokens for each sentence, then compute using the aggregate values.
+
+Args:
+    predictions: list of translations to score.
+        Each translation should be tokenized into a list of tokens.
+    references: list of lists of references for each translation.
+        Each reference should be tokenized into a list of tokens.
+    min_len: The minimum order of n-gram this function should extract.
+    max_len: The maximum order of n-gram this function should extract.
+
+Returns:
+    'google_bleu': bleu score
+    
+Examples:
+    >>> hyp1 = ['It', 'is', 'a', 'guide', 'to', 'action', 'which',
+    ...         'ensures', 'that', 'the', 'military', 'always',
+    ...         'obeys', 'the', 'commands', 'of', 'the', 'party']
+    >>> ref1a = ['It', 'is', 'a', 'guide', 'to', 'action', 'that',
+    ...          'ensures', 'that', 'the', 'military', 'will', 'forever',
+    ...          'heed', 'Party', 'commands']
+    >>> ref1b = ['It', 'is', 'the', 'guiding', 'principle', 'which',
+    ...          'guarantees', 'the', 'military', 'forces', 'always',
+    ...          'being', 'under', 'the', 'command', 'of', 'the', 'Party']
+    >>> ref1c = ['It', 'is', 'the', 'practical', 'guide', 'for', 'the',
+    ...          'army', 'always', 'to', 'heed', 'the', 'directions',
+    ...          'of', 'the', 'party']
+
+    >>> hyp2 = ['he', 'read', 'the', 'book', 'because', 'he', 'was',
+    ...         'interested', 'in', 'world', 'history']
+    >>> ref2a = ['he', 'was', 'interested', 'in', 'world', 'history',
+    ...          'because', 'he', 'read', 'the', 'book']
+
+    >>> list_of_references = [[ref1a, ref1b, ref1c], [ref2a]]
+    >>> hypotheses = [hyp1, hyp2]
+    >>> google_bleu = datasets.load_metric("google_bleu")
+    >>> results = google_bleu.compute(predictions=hypotheses, references=list_of_references)
+    >>> print(results["google_bleu"])
+    0.5673076923076923
 """
 
 
@@ -55,9 +120,9 @@ class GoogleBleu(datasets.Metric):
     def _compute(self,
                  predictions: List[List[List[str]]],
                  references: List[List[str]],
-                 min_len=1,
-                 max_len=4,
-                 ) -> Dict[str, Any]:
+                 min_len: int = 1,
+                 max_len: int = 4,
+                 ) -> Dict[str, float]:
         return {
             "google_bleu": gleu_score.corpus_gleu(
                 list_of_references=references, hypotheses=predictions, min_len=min_len, max_len=max_len
