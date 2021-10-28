4e56539c19f14dbb185a90c0ad39e3733f225ee7
Josep Oriol <oriol.ayats@gmail.com>
2021-10-28 14:10:24 +0200

    Replaced conditional by not in

--- src/datasets/info.py
+++ src/datasets/info.py
@@ -332,7 +332,7 @@ class MetricInfo:
     experiment_id: Optional[str] = None
 
     def __post_init__(self):
-        if not "predictions" in self.features:
+        if "predictions" not in self.features:
             raise ValueError("Need to have at least a 'predictions' field in 'features'.")
         if self.format is not None:
             for key, value in self.features.items():
