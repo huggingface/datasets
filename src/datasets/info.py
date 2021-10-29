d6becb57e03df777845caa10d006be8e277f5f63
Josep Oriol Ayats <oriol.ayats@gmail.com>
2021-10-29 15:08:43 +0200

    Asserts replaced by exceptions (huggingface#3171) (#3174)
    * Asserts replaced by exceptions
    
    * Replaced conditional by not in

--- src/datasets/info.py
+++ src/datasets/info.py
@@ -332,7 +332,8 @@ class MetricInfo:
     experiment_id: Optional[str] = None
 
     def __post_init__(self):
-        assert "predictions" in self.features, "Need to have at least a 'predictions' field in 'features'."
+        if "predictions" not in self.features:
+            raise ValueError("Need to have at least a 'predictions' field in 'features'.")
         if self.format is not None:
             for key, value in self.features.items():
                 if not isinstance(value, Value):
