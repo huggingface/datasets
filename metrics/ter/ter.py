4dc891bae5b67ad806fae99f066526a4abd795e1
Bram Vanroy <Bram.Vanroy@UGent.be>
2021-10-31 08:54:13 +0100

    Update ter.py

--- metrics/ter/ter.py
+++ metrics/ter/ter.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2020 The HuggingFace Datasets Authors.
+# Copyright 2021 The HuggingFace Datasets Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,22 +14,13 @@
 # limitations under the License.
 """ TER metric as available in sacrebleu. """
 import sacrebleu as scb
+from packaging import version
+from sacrebleu import TER
 
 import datasets
 
 
 _CITATION = """\
-@inproceedings{post-2018-call,
-    title = "A Call for Clarity in Reporting {BLEU} Scores",
-    author = "Post, Matt",
-    booktitle = "Proceedings of the Third Conference on Machine Translation: Research Papers",
-    month = oct,
-    year = "2018",
-    address = "Belgium, Brussels",
-    publisher = "Association for Computational Linguistics",
-    url = "https://www.aclweb.org/anthology/W18-6319",
-    pages = "186--191",
-}
 @inproceedings{snover-etal-2006-study,
     title = "A Study of Translation Edit Rate with Targeted Human Annotation",
     author = "Snover, Matthew  and
@@ -46,13 +37,28 @@ _CITATION = """\
     pages = "223--231",
     abstract = "We examine a new, intuitive measure for evaluating machine-translation output that avoids the knowledge intensiveness of more meaning-based approaches, and the labor-intensiveness of human judgments. Translation Edit Rate (TER) measures the amount of editing that a human would have to perform to change a system output so it exactly matches a reference translation. We show that the single-reference variant of TER correlates as well with human judgments of MT quality as the four-reference variant of BLEU. We also define a human-targeted TER (or HTER) and show that it yields higher correlations with human judgments than BLEU{---}even when BLEU is given human-targeted references. Our results indicate that HTER correlates with human judgments better than HMETEOR and that the four-reference variants of TER and HTER correlate with human judgments as well as{---}or better than{---}a second human judgment does.",
 }
+@inproceedings{post-2018-call,
+    title = "A Call for Clarity in Reporting {BLEU} Scores",
+    author = "Post, Matt",
+    booktitle = "Proceedings of the Third Conference on Machine Translation: Research Papers",
+    month = oct,
+    year = "2018",
+    address = "Belgium, Brussels",
+    publisher = "Association for Computational Linguistics",
+    url = "https://www.aclweb.org/anthology/W18-6319",
+    pages = "186--191",
+}
 """
 
 _DESCRIPTION = """\
 Besides calculating BLEU scores, SacreBLEU is also capable in calculating TER scores, among other things.
 Specifically, it is inspired by the TERCOM implementation which can be found here: https://github.com/jhclark/tercom
 
-See the [README.md] file at https://github.com/mjpost/sacreBLEU#ter for more information.
+The implementation here is slightly different from sacrebleu in terms of the required input format. The length of
+the references and hypotheses lists need to be the same, so you may need to transpose your references compared to
+sacrebleu's required input format. See https://github.com/huggingface/datasets/issues/3154#issuecomment-950746534
+
+See the README.md file at https://github.com/mjpost/sacreBLEU#ter for more information.
 """
 
 _KWARGS_DESCRIPTION = """
@@ -61,15 +67,15 @@ Produces TER scores alongside the number of edits and reference length.
 Args:
     predictions: The system stream (a sequence of segments).
     references: A list of one or more reference streams (each a sequence of segments).
-   normalized: Whether to apply basic tokenization to sentences.
-   no_punct: Whether to remove punctuations from sentences.
-   asian_support: Whether to support Asian character processing.
-   case_sensitive: Whether to disable lowercasing.
+    normalized: Whether to apply basic tokenization to sentences.
+    no_punct: Whether to remove punctuations from sentences.
+    asian_support: Whether to support Asian character processing.
+    case_sensitive: Whether to disable lowercasing.
 
 Returns:
-    'score': TER score (num_edits / sum_ref_lengths * 100)
-    'num_edits': number of edits required
-    'ref_length': reference length
+    'score': TER score (num_edits / sum_ref_lengths * 100),
+    'num_edits': The cumulative number of edits,
+    'ref_length': The cumulative average reference length.
 
 Examples:
 
@@ -82,8 +88,12 @@ Examples:
 
 @datasets.utils.file_utils.add_start_docstrings(_DESCRIPTION, _KWARGS_DESCRIPTION)
 class Ter(datasets.Metric):
-    """Largely borrowed from the sacrebleu implementation in `datasets`"""
     def _info(self):
+        if version.parse(scb.__version__) < version.parse("1.4.12"):
+            raise ImportWarning(
+                "To use `sacrebleu`, the module `sacrebleu>=1.4.12` is required, and the current version of `sacrebleu` doesn't match this condition.\n"
+                'You can install it with `pip install "sacrebleu>=1.4.12"`.'
+            )
         return datasets.MetricInfo(
             description=_DESCRIPTION,
             citation=_CITATION,
@@ -103,25 +113,21 @@ class Ter(datasets.Metric):
             ],
         )
 
-    def _compute(self,
-                 predictions,
-                 references,
-                 normalized: bool = False,
-                 no_punct: bool = False,
-                 asian_support: bool = False,
-                 case_sensitive: bool = False
-                 ):
-        output = scb.corpus_ter(
-            predictions,
-            references,
-            normalized=normalized,
-            no_punct=no_punct,
-            asian_support=asian_support,
-            case_sensitive=case_sensitive
-        )
-        output_dict = {
-            "score": output.score,
-            "num_edits": output.num_edits,
-            "ref_length": output.ref_length
-        }
-        return output_dict
+    def _compute(
+        self,
+        predictions,
+        references,
+        normalized: bool = False,
+        no_punct: bool = False,
+        asian_support: bool = False,
+        case_sensitive: bool = False,
+    ):
+        references_per_prediction = len(references[0])
+        if any(len(refs) != references_per_prediction for refs in references):
+            raise ValueError("Sacrebleu requires the same number of references for each prediction")
+        transformed_references = [[refs[i] for refs in references] for i in range(references_per_prediction)]
+
+        sb_ter = TER(normalized, no_punct, asian_support, case_sensitive)
+        output = sb_ter.corpus_score(predictions, transformed_references)
+
+        return {"score": output.score, "num_edits": output.num_edits, "ref_length": output.ref_length}
