212b8bae3f31c521beed58b47454869dafdfe723
Quentin Lhoest <lhoest.q@gmail.com>
2021-11-02 22:25:57 +0100

    docs

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -1,6 +1,6 @@
 // These two things need to be updated at each release for the version selector.
 // Last stable version
-const stableVersion = "v1.14.0"
+const stableVersion = "v1.15.0"
 // Dictionary doc folder to label
 const versionMapping = {
     "master": "master",
@@ -36,6 +36,7 @@ const versionMapping = {
     "v1.13.2": "v1.13.2",
     "v1.13.3": "v1.13.3",
     "v1.14.0": "v1.14.0",
+    "v1.15.0": "v1.15.0",
 }
 
 function addIcon() {
--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -18,7 +18,7 @@
 # pylint: enable=line-too-long
 # pylint: disable=g-import-not-at-top,g-bad-import-order,wrong-import-position
 
-__version__ = "1.15.0"
+__version__ = "1.15.0.dev0"
 
 import pyarrow
 from packaging import version as _version
