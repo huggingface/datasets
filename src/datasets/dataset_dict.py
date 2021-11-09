6b1cd61338c279f0ceed6f2ba06583eb956be783
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2021-11-09 15:30:39 +0100

    Change the signature of callback fed to filter when batched=True

--- src/datasets/dataset_dict.py
+++ src/datasets/dataset_dict.py
@@ -523,8 +523,10 @@ class DatasetDict(dict):
 
         Args:
             function (`callable`): with one of the following signature:
-                - `function(example: Dict) -> bool` if `with_indices=False`
-                - `function(example: Dict, indices: int) -> bool` if `with_indices=True`
+                - ``function(example: Union[Dict, Any]) -> bool`` if ``with_indices=False, batched=False``
+                - ``function(example: Union[Dict, Any], indices: int) -> bool`` if ``with_indices=True, batched=False``
+                - ``function(example: Union[Dict, Any]) -> List[bool]`` if ``with_indices=False, batched=True``
+                - ``function(example: Union[Dict, Any], indices: int) -> List[bool]`` if ``with_indices=True, batched=True``
             with_indices (`bool`, defaults to `False`): Provide example indices to `function`. Note that in this case the signature of `function` should be `def function(example, idx): ...`.
             input_columns (`Optional[Union[str, List[str]]]`, defaults to `None`): The columns to be passed into `function` as
                 positional arguments. If `None`, a dict mapping to all formatted columns is passed as one argument.
