5b26bb51f46143b29853defe37295ef32e2b855c
Mario Šaško <mario@huggingface.co>
2021-11-12 15:02:44 +0100

    Remove isort module placement (#3243)

--- src/datasets/utils/streaming_download_manager.py
+++ src/datasets/utils/streaming_download_manager.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import posixpath
 import re
 import tarfile
 import time
@@ -8,7 +9,6 @@ from pathlib import Path, PurePosixPath
 from typing import Optional, Tuple, Union
 
 import fsspec
-import posixpath
 from aiohttp.client_exceptions import ClientError
 
 from .. import config
