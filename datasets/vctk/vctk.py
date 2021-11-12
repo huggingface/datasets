aeb4fb8d9bf6923b28ec4c1bb7aaa117ebdf128e
Jake Tae <>
2021-11-12 05:17:06 -0500

    feature: add vctk corpus

---
+++ datasets/vctk/vctk.py
@@ -0,0 +1,118 @@
+# coding=utf-8
+# Copyright 2021 The TensorFlow Datasets Authors and the HuggingFace Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""VCTK dataset."""
+
+
+import os
+
+import datasets
+from datasets.tasks import AutomaticSpeechRecognition
+
+
+_CITATION = """\
+@inproceedings{Veaux2017CSTRVC,
+	title        = {CSTR VCTK Corpus: English Multi-speaker Corpus for CSTR Voice Cloning Toolkit},
+	author       = {Christophe Veaux and Junichi Yamagishi and Kirsten MacDonald},
+	year         = 2017
+}
+"""
+
+_DESCRIPTION = """\
+"""
+
+_URL = "https://datashare.ed.ac.uk/handle/10283/3443"
+_DL_URL = "https://datashare.is.ed.ac.uk/bitstream/handle/10283/3443/VCTK-Corpus-0.92.zip"
+
+
+class VCTK(datasets.GeneratorBasedBuilder):
+    """VCTK dataset."""
+
+    VERSION = datasets.Version("0.9.2")
+
+    BUILDER_CONFIGS = [
+        datasets.BuilderConfig(name="main", version=VERSION, description="VCTK dataset"),
+    ]
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "speaker_id": datasets.Value("string"),
+                    "audio": datasets.features.Audio(sampling_rate=48000),
+                    "file": datasets.Value("string"),
+                    "text": datasets.Value("string"),
+                    "text_id": datasets.Value("string"),
+                    "age": datasets.Value("string"), 
+                    "gender": datasets.Value("string"),
+                    "accent": datasets.Value("string"), 
+                    "region": datasets.Value("string"), 
+                    "comment": datasets.Value("string"),
+                }
+            ),
+            supervised_keys=("file", "text"),
+            homepage=_URL,
+            citation=_CITATION,
+            task_templates=[
+                AutomaticSpeechRecognition(audio_file_path_column="file", transcription_column="text")
+            ],
+        )
+
+    def _split_generators(self, dl_manager):
+        root_path = dl_manager.download_and_extract(_DL_URL)
+        root_path = os.path.join(root_path, "VCTK-Corpus-0.92")
+
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN, gen_kwargs={"root_path": root_path}
+            ),
+        ]
+
+    def _generate_examples(self, root_path):
+        """Generate examples from the VCTK corpus root path."""
+
+        meta_path = os.path.join(root_path, "speaker-info.txt")
+        txt_root = os.path.join(root_path, "txt")
+        wav_root = os.path.join(root_path, "wav48_silence_trimmed")
+        fields = ["speaker_id", "age", "gender", "accent", "region", "comment"]
+
+        key = 0
+        with open(meta_path) as meta_file:
+            _ = next(iter(meta_file))
+            for line in meta_file:
+                data = {}
+                values = line.split()
+                for i, field in enumerate(fields):
+                    data[field] = values[i] if i < len(values) else ""
+                speaker_id = data["speaker_id"]
+                speaker_txt_path = os.path.join(txt_root, speaker_id)
+                speaker_wav_path = os.path.join(wav_root, speaker_id)
+                for txt_file in sorted(os.listdir(speaker_txt_path)):
+                    filename, _ = os.path.splitext(txt_file)
+                    _, text_id = filename.split("_")
+                    for i in [1, 2]:
+                        wav_file = os.path.join(speaker_wav_path, f"{filename}_mic{i}.flac")
+                        with open(os.path.join(speaker_txt_path, txt_file)) as text_file:
+                            text = text_file.readline().strip()
+                            more_data = {
+                                "file": wav_file,
+                                "audio": wav_file,
+                                "text": text,
+                                "text_id": text_id,
+                            }
+                            yield key, {**data, **more_data}
+                        key += 1
