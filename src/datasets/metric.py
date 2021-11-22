93da837c4668859aa49d9c5cbe6c051901829fe2
Mario Šaško <mario@huggingface.co>
2021-11-22 11:08:17 +0100

    Fix check in metric.py

--- src/datasets/metric.py
+++ src/datasets/metric.py
@@ -169,7 +169,7 @@ class Metric(MetricInfoMixin):
         # Safety checks on num_process and process_id
         if not isinstance(process_id, int) or process_id < 0:
             raise ValueError("'process_id' should be a number greater than 0")
-        if not isinstance(num_process, int) or num_process < process_id:
+        if not isinstance(num_process, int) or num_process <= process_id:
             raise ValueError("'num_process' should be a number greater than process_id")
         if keep_in_memory and num_process != 1:
             raise ValueError("Using 'keep_in_memory' is not possible in distributed setting (num_process > 1).")
