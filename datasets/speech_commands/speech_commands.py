3fd603c8d6831b2e90ae26652f8a1f662cc4fcba
pkazakova <pkazakova@embedika.ru>
2021-11-28 16:29:49 +0300

    add two configs: for version 0.01 and 0.02

--- datasets/speech_commands/speech_commands.py
+++ datasets/speech_commands/speech_commands.py
@@ -19,17 +19,60 @@ import glob
 import os
 
 import datasets
-from datasets.tasks import AutomaticSpeechRecognition
 
 
-_URL = "https://www.tensorflow.org/datasets/catalog/speech_commands"
-_DL_URL = "http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz"
-_DESCRIPTION = "dummy description"  # TODO
-_CITATION = "dummy citation"  # TODO
-_LICENSE = "dummy license"  # TODO
+_CITATION = """
+@article{speechcommandsv2,
+   author = { {Warden}, P.},
+    title = "{Speech Commands: A Dataset for Limited-Vocabulary Speech Recognition}",
+  journal = {ArXiv e-prints},
+  archivePrefix = "arXiv",
+  eprint = {1804.03209},
+  primaryClass = "cs.CL",
+  keywords = {Computer Science - Computation and Language, Computer Science - Human-Computer Interaction},
+    year = 2018,
+    month = apr,
+    url = {https://arxiv.org/abs/1804.03209},
+}
+"""
 
+_DESCRIPTION = """
+An audio dataset of spoken words designed to help train and evaluate keyword spotting systems. 
+This is version 0.02 of the data set containing 105,829 audio files, released on
+April 11th 2018.
+The audio files were collected using crowdsourcing, see
+[aiyprojects.withgoogle.com/open_speech_recording](https://github.com/petewarden/extract_loudest_section)
+for some of the open source audio collection code we used (and please consider
+contributing to enlarge this data set). The goal was to gather examples of
+people speaking single-word commands, rather than conversational sentences, so
+they were prompted for individual words over the course of a five minute
+session. Twenty four core command words were recorded, with most speakers saying each
+of them five times. The core words in version 0.02 are "Yes", "No", "Up", "Down", "Left",
+"Right", "On", "Off", "Stop", "Go", "Zero", "One", "Two", "Three", "Four",
+"Five", "Six", "Seven", "Eight", "Nine", "Backward", "Forward", "Follow", "Learn". 
+To help distinguish unrecognized words, there are also several auxiliary ("_unknown_") words, which most speakers only said once.
+These include "Bed", "Bird", "Cat", "Dog", "Happy", "House", "Marvin", "Sheila", "Tree", and "Wow".
+Its primary goal is to provide a way to build and test small models that detect when a single word is spoken, 
+from a set of target words, with as few false positives as possible from background noise or unrelated speech. 
+Note that in the train and validation set, the label "unknown" is much more prevalent than the labels 
+of the target words or background noise. One difference from the release version is the handling of silent segments. 
+While in the test set the silence segments are regular 1 second files, in the training they are provided 
+as long segments under "_background_noise_" folder. Here we split these background noise into 1 second clips, 
+and also keep one of the files for the validation set.
+"""
 
-WORDS = [
+_LICENSE = "Creative Commons BY 4.0 License"
+
+_URL = "https://www.tensorflow.org/datasets/catalog/speech_commands"  # TODO: check
+
+_DL_URL = "http://download.tensorflow.org/data/speech_commands_{}.tar.gz"
+
+# _DL_TEST_URL = {
+#     "v0.01": "http://download.tensorflow.org/data/speech_commands_test_set_v0.01.tar.gz",
+#     "v0.02": "http://download.tensorflow.org/data/speech_commands_test_set_v0.02.tar.gz"
+# }
+
+WORDS_V1 = [
     "yes",
     "no",
     "up",
@@ -40,6 +83,9 @@ WORDS = [
     "off",
     "stop",
     "go",
+]
+
+WORDS_V2 = WORDS_V1 + [
     "zero",
     "one",
     "two",
@@ -53,34 +99,51 @@ WORDS = [
     "backward",
     "forward",
     "follow",
-    "learn"
-
+    "learn",
 ]
 UNKNOWN = "_unknown_"
-BACKGROUND = "_background_noise_"
-LABELS = WORDS + [UNKNOWN, BACKGROUND]
+BACKGROUND = "_background_noise_"  # TODO: _silence_?
+LABELS_V1 = WORDS_V1 + [UNKNOWN, BACKGROUND]
+LABELS_V2 = WORDS_V2 + [UNKNOWN, BACKGROUND]
 
 
-# class SpeechCommandsConfig(datasets.BuilderConfig):
-#     #  TODO: likely I don't need a custom config
-#     """BuilderConfig for SpeechCommands. """
-#
-#     def __init__(self, *args, **kwargs):
-#         super(SpeechCommandsConfig, self).__init__(version=datasets.Version("0.0.2"), **kwargs)
+class SpeechCommandsConfig(datasets.BuilderConfig):
+    """BuilderConfig for SpeechCommands. """
+
+    def __init__(self, data_url, labels, **kwargs):  # TODO: url?
+        super(SpeechCommandsConfig, self).__init__(**kwargs)
+        # self.url = url
+        self.data_url = data_url
+        self.labels = labels
 
 
 class SpeechCommands(datasets.GeneratorBasedBuilder):
     # DEFAULT_WRITER_BATCH_SIZE = 256  # TODO: do I need it?
-    # BUILDER_CONFIGS = [SpeechCommandsConfig()]
+    BUILDER_CONFIGS = [
+        SpeechCommandsConfig(
+            name="v0.01",
+            description="",  #TODO
+            labels=LABELS_V1,
+            data_url=_DL_URL.format("v0.01"),
+            version=datasets.Version("0.0.1")
+        ),
+        SpeechCommandsConfig(
+            name="v0.02",
+            description="",  # TODO
+            labels=LABELS_V2,
+            data_url=_DL_URL.format("v0.02"),
+            version=datasets.Version("0.0.2")
+        ),
+    ]
 
     def _info(self):
         return datasets.DatasetInfo(
-            description=_DESCRIPTION,
+            description=_DESCRIPTION,  # TODO: or self.config.description?
             features=datasets.Features(
                 {
                     "file": datasets.Value("string"),
                     "audio": datasets.features.Audio(sampling_rate=16_000),
-                    "label": datasets.ClassLabel(names=LABELS),
+                    "label": datasets.ClassLabel(names=self.config.labels),
                     "speaker_id": datasets.Value("string"),
                     "utterance_id": datasets.Value("int8"),
                 }
@@ -89,26 +152,26 @@ class SpeechCommands(datasets.GeneratorBasedBuilder):
             homepage=_URL,
             citation=_CITATION,
             license=_LICENSE,
-            version=datasets.Version("0.0.2"),
+            version=self.config.version,
         )
 
     def _split_generators(self, dl_manager):
-        archive_path = dl_manager.download_and_extract(_DL_URL)
-        split_to_paths = _split_files(archive_path)
+        archive_path = dl_manager.download_and_extract(self.config.data_url)
+        splits_to_paths = _split_files(archive_path)
 
         # TODO: make `archive_path` a class attribute / self.config attr since it's the same for each split?
         return [
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN,
-                gen_kwargs={"archive_path": archive_path, "filenames": split_to_paths["train"]}
+                gen_kwargs={"archive_path": archive_path, "filenames": splits_to_paths["train"]}
             ),
             datasets.SplitGenerator(
                 name=datasets.Split.VALIDATION,
-                gen_kwargs={"archive_path": archive_path, "filenames": split_to_paths["val"]}
+                gen_kwargs={"archive_path": archive_path, "filenames": splits_to_paths["val"]}
             ),
             datasets.SplitGenerator(
                 name=datasets.Split.TEST,
-                gen_kwargs={"archive_path": archive_path, "filenames": split_to_paths["test"]}
+                gen_kwargs={"archive_path": archive_path, "filenames": splits_to_paths["test"]}
             ),
         ]
 
@@ -126,7 +189,7 @@ class SpeechCommands(datasets.GeneratorBasedBuilder):
             }
                 continue
 
-            elif word in WORDS:
+            elif word in self.config.labels[:-2]:  # the last two are _unknown_ and _background_
                 label = word
             else:
                 label = UNKNOWN
