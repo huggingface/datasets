61f718a6663a5d91cc2d52eded935938199a7482
mariosasko <mariosasko777@gmail.com>
2021-12-09 19:02:55 +0100

    Style

--- datasets/wider_face/wider_face.py
+++ datasets/wider_face/wider_face.py
@@ -25,21 +25,21 @@ _LICENSE = "Unknown"
 
 _CITATION = """\
 @inproceedings{yang2016wider,
-	Author = {Yang, Shuo and Luo, Ping and Loy, Chen Change and Tang, Xiaoou},
-	Booktitle = {IEEE Conference on Computer Vision and Pattern Recognition (CVPR)},
-	Title = {WIDER FACE: A Face Detection Benchmark},
-	Year = {2016}}
+    Author = {Yang, Shuo and Luo, Ping and Loy, Chen Change and Tang, Xiaoou},
+    Booktitle = {IEEE Conference on Computer Vision and Pattern Recognition (CVPR)},
+    Title = {WIDER FACE: A Face Detection Benchmark},
+    Year = {2016}}
 """
 
 _DESCRIPTION = """\
-WIDER FACE dataset is a face detection benchmark dataset, of which images are 
-selected from the publicly available WIDER dataset. We choose 32,203 images and 
-label 393,703 faces with a high degree of variability in scale, pose and 
-occlusion as depicted in the sample images. WIDER FACE dataset is organized 
-based on 61 event classes. For each event class, we randomly select 40%/10%/50% 
-data as training, validation and testing sets. We adopt the same evaluation 
+WIDER FACE dataset is a face detection benchmark dataset, of which images are
+selected from the publicly available WIDER dataset. We choose 32,203 images and
+label 393,703 faces with a high degree of variability in scale, pose and
+occlusion as depicted in the sample images. WIDER FACE dataset is organized
+based on 61 event classes. For each event class, we randomly select 40%/10%/50%
+data as training, validation and testing sets. We adopt the same evaluation
 metric employed in the PASCAL VOC dataset. Similar to MALF and Caltech datasets,
-we do not release bounding box ground truth for the test images. Users are 
+we do not release bounding box ground truth for the test images. Users are
 required to submit final prediction files, which we shall proceed to evaluate.
 """
 
