8113f12716a8216d461e2dddbe6a5e01f4b20349
lhbonifacio <luizhbonifacio@gmail.com>
2021-12-12 15:06:38 +0000

    Adding mMARCO dataset

--- datasets/mmarco/mmarco.py
+++ datasets/mmarco/mmarco.py
@@ -16,14 +16,12 @@
 # Lint as: python3
 """mMARCO dataset."""
 
-
-import json
 import datasets
 
 
 _CITATION = """
 @misc{bonifacio2021mmarco,
-      title={mMARCO: A Multilingual Version of the MS MARCO Passage Ranking Dataset}, 
+      title={mMARCO: A Multilingual Version of the MS MARCO Passage Ranking Dataset},
       author={Luiz Henrique Bonifacio and Israel Campiotti and Vitor Jeronymo and Roberto Lotufo and Rodrigo Nogueira},
       year={2021},
       eprint={2108.13897},
@@ -38,13 +36,14 @@ mMARCO translated datasets
 
 _V1_BASE_URLS = {
     "collections": "https://huggingface.co/datasets/unicamp-dl/mmarco/resolve/main/data/v1.1/collections/",
-    "train":       "https://huggingface.co/datasets/unicamp-dl/mmarco/resolve/main/data/v1.1/queries/train/",
-    "dev":         "https://huggingface.co/datasets/unicamp-dl/mmarco/resolve/main/data/v1.1/queries/dev/",
+    "train": "https://huggingface.co/datasets/unicamp-dl/mmarco/resolve/main/data/v1.1/queries/train/",
+    "dev": "https://huggingface.co/datasets/unicamp-dl/mmarco/resolve/main/data/v1.1/queries/dev/",
 }
 
 
 class MsMarcoConfig(datasets.BuilderConfig):
     """BuilderConfig for MS MARCO."""
+
     language: str = "english"
 
     def __init__(self, **kwargs):
@@ -63,53 +62,32 @@ class MsMarco(datasets.GeneratorBasedBuilder):
             name="v1.1",
             description="""version v1.1""",
             version=datasets.Version("1.1.0", ""),
-        ),
-        MsMarcoConfig(
-            name="v2.1",
-            description="""version v2.1""",
-            version=datasets.Version("2.1.0", ""),
-        ),
+        )
     ]
 
     def _info(self):
         return datasets.DatasetInfo(
             description=_DESCRIPTION + "\n" + self.config.description,
-            features=datasets.Features(
-                {
-                    "doc_id": datasets.Value("int32"),
-                    "document": datasets.Value("string")
-                }
-            ),
+            features=datasets.Features({"doc_id": datasets.Value("int32"), "document": datasets.Value("string")}),
             homepage="https://github.com/unicamp-dl/mMARCO",
-            citation=_CITATION
+            citation=_CITATION,
         )
 
     def _split_generators(self, dl_manager):
         """Returns SplitGenerators."""
-        
-        if self.config.name == "v2.1":
-            dl_path = dl_manager.download_and_extract(_V2_URLS)
-        else:
+
+        if self.config.name == "v1.1":
             _V1_URLS = {
-                "collections": _V1_BASE_URLS["collections"] + self.config.language + '_collection.tsv',
-                "train": _V1_BASE_URLS["train"] + self.config.language + '_queries.train.tsv',
-                "dev": _V1_BASE_URLS["dev"] + self.config.language + '_queries.dev.tsv'
+                "collections": _V1_BASE_URLS["collections"] + self.config.language + "_collection.tsv",
+                "train": _V1_BASE_URLS["train"] + self.config.language + "_queries.train.tsv",
+                "dev": _V1_BASE_URLS["dev"] + self.config.language + "_queries.dev.tsv",
             }
             dl_path = dl_manager.download_and_extract(_V1_URLS)
-        
+
         return [
-            datasets.SplitGenerator(
-                name="collection",
-                gen_kwargs={"filepath": dl_path["collections"]}
-            ),
-            datasets.SplitGenerator(
-                name=datasets.Split.TRAIN,
-                gen_kwargs={"filepath": dl_path["train"]}
-            ),
-            datasets.SplitGenerator(
-                name=datasets.Split.VALIDATION,
-                gen_kwargs={"filepath": dl_path["dev"]}
-            ),
+            datasets.SplitGenerator(name="collection", gen_kwargs={"filepath": dl_path["collections"]}),
+            datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": dl_path["train"]}),
+            datasets.SplitGenerator(name=datasets.Split.VALIDATION, gen_kwargs={"filepath": dl_path["dev"]}),
         ]
 
     def _generate_examples(self, filepath):
@@ -117,9 +95,6 @@ class MsMarco(datasets.GeneratorBasedBuilder):
         with open(filepath, encoding="utf-8") as f:
             if self.config.name == "v1.1":
                 for line in f:
-                    doc_id, doc = line.rstrip().split('\t')
-                    features = {
-                        "doc_id": doc_id,
-                        "document": doc
-                    }
+                    doc_id, doc = line.rstrip().split("\t")
+                    features = {"doc_id": doc_id, "document": doc}
                     yield doc_id, features
