8beb12f5132fc6edeb279fa2ef0ba7fd33276c6f
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-12-14 16:52:18 +0100

    Pass iter_files generator

--- src/datasets/packaged_modules/csv/csv.py
+++ src/datasets/packaged_modules/csv/csv.py
@@ -138,13 +138,13 @@ class Csv(datasets.ArrowBasedBuilder):
             files = data_files
             if isinstance(files, str):
                 files = [files]
-            files = [file for file in dl_manager.iter_files(files)]
+            files = dl_manager.iter_files(files)
             return [datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"files": files})]
         splits = []
         for split_name, files in data_files.items():
             if isinstance(files, str):
                 files = [files]
-            files = [file for file in dl_manager.iter_files(files)]
+            files = dl_manager.iter_files(files)
             splits.append(datasets.SplitGenerator(name=split_name, gen_kwargs={"files": files}))
         return splits
 
