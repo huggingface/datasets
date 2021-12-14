88b1c5dd77fe09365de226498050378613119026
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-12-14 16:07:59 +0100

    Remove unnecessary condition in csv module

--- src/datasets/packaged_modules/csv/csv.py
+++ src/datasets/packaged_modules/csv/csv.py
@@ -139,15 +139,13 @@ class Csv(datasets.ArrowBasedBuilder):
             files = data_files
             if isinstance(files, str):
                 files = [files]
-            if any(os.path.isdir(file) for file in files):
-                files = [file for file in dl_manager.iter_files(files)]
+            files = [file for file in dl_manager.iter_files(files)]
             return [datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"files": files})]
         splits = []
         for split_name, files in data_files.items():
             if isinstance(files, str):
                 files = [files]
-            if any(os.path.isdir(file) for file in files):
-                files = [file for file in dl_manager.iter_files(files)]
+            files = [file for file in dl_manager.iter_files(files)]
             splits.append(datasets.SplitGenerator(name=split_name, gen_kwargs={"files": files}))
         return splits
 
