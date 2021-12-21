a19ea56e4ef73a2c9f50077e06d803ca2c38148a
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2021-12-21 07:24:01 +0100

    Update swahili_news dataset (#3463)
    * Update swahili_news dataset
    
    * Update swahili_news dataset card
    
    * Update dummy data
    
    * Update JSON metadata
    
    * Fix style
    
    * Add pretty_name tag to dataset card

--- datasets/swahili_news/swahili_news.py
+++ datasets/swahili_news/swahili_news.py
@@ -14,55 +14,50 @@
 # limitations under the License.
 """Swahili : News Classification Dataset"""
 
-
 import csv
 
 import datasets
 
 
-# TODO: Add BibTeX citation
-# Find for instance the citation on arxiv or on the dataset repo/website
 _CITATION = """\
-@dataset{davis_david_2020_4300294,
+@dataset{davis_david_2020_5514203,
   author       = {Davis David},
   title        = {Swahili : News Classification Dataset},
   month        = dec,
   year         = 2020,
+  note         = {{The news version contains both train and test sets.}},
   publisher    = {Zenodo},
-  version      = {0.1},
-  doi          = {10.5281/zenodo.4300294},
-  url          = {https://doi.org/10.5281/zenodo.4300294}
+  version      = {0.2},
+  doi          = {10.5281/zenodo.5514203},
+  url          = {https://doi.org/10.5281/zenodo.5514203}
 }
 """
 
-# TODO: Add description of the dataset here
-# You can copy an official description
 _DESCRIPTION = """\
 Swahili is spoken by 100-150 million people across East Africa. In Tanzania, it is one of two national languages (the other is English) and it is the official language of instruction in all schools. News in Swahili is an important part of the media sphere in Tanzania.
 
 News contributes to education, technology, and the economic growth of a country, and news in local languages plays an important cultural role in many Africa countries. In the modern age, African languages in news and other spheres are at risk of being lost as English becomes the dominant language in online spaces.
 
- The Swahili news dataset was created to reduce the gap of using the Swahili language to create NLP technologies and help AI practitioners in Tanzania and across Africa continent to practice their NLP skills to solve different problems in organizations or societies related to Swahili language. Swahili News were collected from different websites that provide news in the Swahili language. I was able to find some websites that provide news in Swahili only and others in different languages including Swahili.
+The Swahili news dataset was created to reduce the gap of using the Swahili language to create NLP technologies and help AI practitioners in Tanzania and across Africa continent to practice their NLP skills to solve different problems in organizations or societies related to Swahili language. Swahili News were collected from different websites that provide news in the Swahili language. I was able to find some websites that provide news in Swahili only and others in different languages including Swahili.
 
 The dataset was created for a specific task of text classification, this means each news content can be categorized into six different topics (Local news, International news , Finance news, Health news, Sports news, and Entertainment news). The dataset comes with a specified train/test split. The train set contains 75% of the dataset and test set contains 25% of the dataset.
 """
 
-
 _HOMEPAGE = "https://zenodo.org/record/4300294#.X84BQdgzZPb"
-
 
 _LICENSE = "Creative Commons Attribution 4.0 International"
 
-# The HuggingFace dataset library don't host the datasets but only point to the original files
-# This can be an arbitrary nested dict/list of URLs (see below in `_split_generators` method)
-_URL = "https://zenodo.org/record/4300294/files/train.csv?download=1"
+# The HuggingFace Datasets library don't host the datasets but only point to the original files
+_URLS = {
+    "train": "https://zenodo.org/record/5514203/files/train_v0.2.csv?download=1",
+    "test": "https://zenodo.org/record/5514203/files/test_v0.2.csv?download=1",
+}
 
 
-# TODO: Name of the dataset usually match the script name with CamelCase instead of snake_case
 class SwahiliNews(datasets.GeneratorBasedBuilder):
     """Swahili : News Classification Dataset"""
 
-    VERSION = datasets.Version("1.0.0")
+    VERSION = datasets.Version("0.2.0")
 
     BUILDER_CONFIGS = [
         datasets.BuilderConfig(
@@ -73,14 +68,10 @@ class SwahiliNews(datasets.GeneratorBasedBuilder):
     ]
 
     def _info(self):
-
         return datasets.DatasetInfo(
-            # This is the description that will appear on the datasets page.
             description=_DESCRIPTION,
-            # This defines the different columns of the dataset and their types
             features=datasets.Features(
                 {
-                    "id": datasets.Value("string"),
                     "text": datasets.Value("string"),
                     "label": datasets.features.ClassLabel(
                         names=["uchumi", "kitaifa", "michezo", "kimataifa", "burudani", "afya"]
@@ -88,27 +79,22 @@ class SwahiliNews(datasets.GeneratorBasedBuilder):
                 }
             ),
             supervised_keys=None,
-            # Homepage of the dataset for documentation
             homepage=_HOMEPAGE,
-            # License for the dataset if available
             license=_LICENSE,
-            # Citation for the dataset
             citation=_CITATION,
         )
 
     def _split_generators(self, dl_manager):
-        """Returns SplitGenerators."""
-        train_path = dl_manager.download_and_extract(_URL)
-
+        paths = dl_manager.download_and_extract(_URLS)
         return [
-            datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": train_path}),
+            datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": paths["train"]}),
+            datasets.SplitGenerator(name=datasets.Split.TEST, gen_kwargs={"filepath": paths["test"]}),
         ]
 
     def _generate_examples(self, filepath):
-
         with open(filepath, encoding="utf-8") as csv_file:
             csv_reader = csv.DictReader(
                 csv_file, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True
             )
             for id_, row in enumerate(csv_reader):
-                yield id_, {"id": row["id"], "text": row["content"], "label": row["category"]}
+                yield id_, row
