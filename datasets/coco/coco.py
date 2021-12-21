19883a9ab76b785c8820c30dd2c7089f41ff8221
mariosasko <mariosasko777@gmail.com>
2021-12-21 14:47:46 +0100

    Style

--- datasets/coco/coco.py
+++ datasets/coco/coco.py
@@ -54,7 +54,7 @@ _CITATION = """\
 
 
 _DESCRIPTION = """\
-The MS COCO (Microsoft Common Objects in Context) dataset is a large-scale object detection, 
+The MS COCO (Microsoft Common Objects in Context) dataset is a large-scale object detection,
 segmentation, key-point detection, and captioning dataset. The dataset consists of 328K images of variable shapes.
 """
 
