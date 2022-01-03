c7dbedd43e2c49b6d1811a2d87e8fcc42ea16795
VictorSanh <victorsanh@gmail.com>
2022-01-03 16:42:51 +0000

    changing to the adresses to which it was automatically re-directing

--- datasets/asset/asset.py
+++ datasets/asset/asset.py
@@ -51,14 +51,14 @@ _HOMEPAGE = "https://github.com/facebookresearch/asset"
 _LICENSE = "Creative Common Attribution-NonCommercial 4.0 International"
 
 _URL_LIST = [
-    ("human_ratings.csv", "https://github.com/facebookresearch/asset/raw/main/human_ratings/human_ratings.csv"),
-    ("asset.valid.orig", "https://github.com/facebookresearch/asset/raw/main/dataset/asset.valid.orig"),
-    ("asset.test.orig", "https://github.com/facebookresearch/asset/raw/main/dataset/asset.test.orig"),
+    ("human_ratings.csv", "https://raw.githubusercontent.com/facebookresearch/asset/main/human_ratings/human_ratings.csv"),
+    ("asset.valid.orig", "https://raw.githubusercontent.com/facebookresearch/asset/main/dataset/asset.valid.orig"),
+    ("asset.test.orig", "https://raw.githubusercontent.com/facebookresearch/asset/main/dataset/asset.test.orig"),
 ]
 _URL_LIST += [
     (
         f"asset.{spl}.simp.{i}",
-        f"https://github.com/facebookresearch/asset/raw/main/dataset/asset.{spl}.simp.{i}",
+        f"https://raw.githubusercontent.com/facebookresearch/asset/main/dataset/asset.{spl}.simp.{i}",
     )
     for spl in ["valid", "test"]
     for i in range(10)
