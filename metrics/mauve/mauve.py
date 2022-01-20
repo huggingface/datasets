425894a33b9bd84205796d51bfcb8ca8199ca246
Quentin Lhoest <lhoest.q@gmail.com>
2022-01-20 15:40:37 +0100

    minor stuff

--- metrics/mauve/mauve.py
+++ metrics/mauve/mauve.py
@@ -19,7 +19,7 @@ import numpy  # Here to have a nice missing dependency error message early on
 import requests  # Here to have a nice missing dependency error message early on
 import sklearn  # Here to have a nice missing dependency error message early on
 import tqdm  # Here to have a nice missing dependency error message early on
-from mauve import compute_mauve
+from mauve import compute_mauve  # From: mauve-text
 
 import datasets
 
@@ -73,12 +73,13 @@ Returns:
     q_hist: same as above, but with q_text.
 Examples:
 
+    >>> # faiss segfaults in doctest for some reason, so the .compute call is not tested with doctest
     >>> import datasets
     >>> mauve = datasets.load_metric('mauve')
     >>> predictions = ["hello there", "general kenobi"]
     >>> references = ["hello there", "general kenobi"]
-    >>> out = mauve.compute(predictions=predictions, references=references)
-    >>> print(out.mauve)
+    >>> out = mauve.compute(predictions=predictions, references=references) # doctest: +SKIP
+    >>> print(out.mauve) # doctest: +SKIP
     1.0
 """
 
@@ -104,6 +105,45 @@ class Mauve(datasets.Metric):
             ],
         )
 
-    def _compute(self, predictions, references, *args, **kwargs):
-        out = compute_mauve(p_text=predictions, q_text=references, *args, **kwargs)
+    def _compute(
+        self,
+        predictions,
+        references,
+        p_features=None,
+        q_features=None,
+        p_tokens=None,
+        q_tokens=None,
+        num_buckets="auto",
+        pca_max_data=-1,
+        kmeans_explained_var=0.9,
+        kmeans_num_redo=5,
+        kmeans_max_iter=500,
+        featurize_model_name="gpt2-large",
+        device_id=-1,
+        max_text_length=1024,
+        divergence_curve_discretization_size=25,
+        mauve_scaling_factor=5,
+        verbose=True,
+        seed=25,
+    ):
+        out = compute_mauve(
+            p_text=predictions,
+            q_text=references,
+            p_features=p_features,
+            q_features=q_features,
+            p_tokens=p_tokens,
+            q_tokens=q_tokens,
+            num_buckets=num_buckets,
+            pca_max_data=pca_max_data,
+            kmeans_explained_var=kmeans_explained_var,
+            kmeans_num_redo=kmeans_num_redo,
+            kmeans_max_iter=kmeans_max_iter,
+            featurize_model_name=featurize_model_name,
+            device_id=device_id,
+            max_text_length=max_text_length,
+            divergence_curve_discretization_size=divergence_curve_discretization_size,
+            mauve_scaling_factor=mauve_scaling_factor,
+            verbose=verbose,
+            seed=seed,
+        )
         return out
