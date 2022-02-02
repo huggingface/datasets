415f151087506706eac3a36e80b2d924c0b0c59a
Quentin Lhoest <lhoest.q@gmail.com>
2022-02-02 15:23:44 +0100

    docs

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -17,7 +17,7 @@
 # pylint: enable=line-too-long
 # pylint: disable=g-import-not-at-top,g-bad-import-order,wrong-import-position
 
-__version__ = "1.18.3"
+__version__ = "1.18.4.dev0"
 
 import pyarrow
 from packaging import version as _version
