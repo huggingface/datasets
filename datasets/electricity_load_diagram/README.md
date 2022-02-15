4d4c8b3058317f09f45b01bc64f1e50736ce327a
Kashif Rasul <kashif.rasul@gmail.com>
2022-02-15 15:29:02 +0100

    added electricity load diagram

---
+++ datasets/electricity_load_diagram/README.md
@@ -0,0 +1,266 @@
+---
+annotations_creators:
+- no-annotation
+language_creators:
+- found
+languages: []
+licenses:
+- unknown
+multilinguality: []
+pretty_name: Electricity Load Diagram
+size_categories:
+- unknown
+source_datasets:
+- original
+task_categories:
+- other
+task_ids:
+- other-other-time-series-forecasting
+---
+
+# Dataset Card for Electricity Load Diagrams
+
+## Table of Contents
+- [Table of Contents](#table-of-contents)
+- [Dataset Description](#dataset-description)
+  - [Dataset Summary](#dataset-summary)
+  - [Supported Tasks and Leaderboards](#supported-tasks-and-leaderboards)
+  - [Languages](#languages)
+- [Dataset Structure](#dataset-structure)
+  - [Data Instances](#data-instances)
+  - [Data Fields](#data-fields)
+  - [Data Splits](#data-splits)
+- [Dataset Creation](#dataset-creation)
+  - [Curation Rationale](#curation-rationale)
+  - [Source Data](#source-data)
+  - [Annotations](#annotations)
+  - [Personal and Sensitive Information](#personal-and-sensitive-information)
+- [Considerations for Using the Data](#considerations-for-using-the-data)
+  - [Social Impact of Dataset](#social-impact-of-dataset)
+  - [Discussion of Biases](#discussion-of-biases)
+  - [Other Known Limitations](#other-known-limitations)
+- [Additional Information](#additional-information)
+  - [Dataset Curators](#dataset-curators)
+  - [Licensing Information](#licensing-information)
+  - [Citation Information](#citation-information)
+  - [Contributions](#contributions)
+
+## Dataset Description
+
+- **Homepage:** [Electricity Load Diagrams 2011-2014](https://archive.ics.uci.edu/ml/datasets/ElectricityLoadDiagrams20112014)
+- **Paper:** [Modeling Long- and Short-Term Temporal Patterns with Deep Neural Networks
+](https://dl.acm.org/doi/10.1145/3209978.3210006)
+- **Point of Contact:** [Artur Trindade](mailto:artur.trindade@elergone.pt)
+
+### Dataset Summary
+
+This dataset contains hourly kW electricity consumption time series of 370 Portuguese clients from 2011 to 2014.
+
+### Supported Tasks and Leaderboards
+
+- `time-series-forecasting`: The time series forecasting tasks involves learning the future `target` values of time series in a dataset for the `prediction_length` time steps. The results of the forecasts can then be validated via the ground truth in the `validation` split and tested via the `test` split.
+
+
+### Languages
+
+[More Information Needed]
+
+## Dataset Structure
+
+Data set has no missing values. The raw values are in kW of each 15 min interval and are resampled to hourly frequency. 
+Each time series represent one client. Some clients were created after 2011. In these cases consumption were considered zero. All time labels report to Portuguese hour, however all days contain 96 measurements (24*4). Every year in March time change day (which has only 23 hours) the values between 1:00 am and 2:00 am are zero for all points. Every year in October time change day (which has 25 hours) the values between 1:00 am and 2:00 am aggregate the consumption of two hours.
+
+
+### Data Instances
+
+A sample from the training set is provided below:
+
+```python
+{
+  'start': '2012-01-01 00:00:00',
+  'target': [14.0, 18.0, 21.0, 20.0, 22.0, 20.0, 20.0, 20.0, 13.0, 11.0], # <= this target array is a concatenated sample
+  'feat_static_cat': [0], 
+  'item_id': '0'
+}
+```
+
+We have two configurations `uci` and `lstnet`, which are specified as follows. 
+
+The time series are resampled to hourly frequency. We test on 7 rolling windows of prediction length of 24. 
+
+The `uci` validation therefore ends 24*7 time steps before the end of each time series. The training split ends 24 time steps before the end of the validation split. 
+
+For the `lsnet` configuration we split the training window so that it is 0.6-th of the full time series and the validation is  0.8-th of the full time series and the last 0.2-th length time windows is used as the test set of 7 rolling windows of the 24 time steps each. Finally, as in the LSTNet paper, we only consider time series that are active in the year 2012--2014, which leaves us with 320 time series.
+
+### Data Fields
+
+For this univariate regular time series we have:
+
+- `start`: a `datetime` of the first entry of each time serie in the dataset
+- `target`: an `array[float32]` of the actual target values
+- `feat_static_cat`: an `array[uint64]` which contains a categorical identifier of each time series in the dataset
+- `item_id`: a string identifier of each time series in a dataset for reference
+
+Given the `freq` and the `start` datetime, we can assign a datetime to each entry in the target array.
+
+
+### Data Splits
+
+#### UCI configuration
+
+```python
+DatasetDict({
+    train: Dataset({
+        features: ['start', 'target', 'feat_static_cat', 'item_id'],
+        num_rows: 370
+    })
+    test: Dataset({
+        features: ['start', 'target', 'feat_static_cat', 'item_id'],
+        num_rows: 2590
+    })
+    validation: Dataset({
+        features: ['start', 'target', 'feat_static_cat', 'item_id'],
+        num_rows: 370
+    })
+})
+```
+
+### LSTNet configuration
+
+
+```python
+DatasetDict({
+    train: Dataset({
+        features: ['start', 'target', 'feat_static_cat', 'item_id'],
+        num_rows: 320
+    })
+    test: Dataset({
+        features: ['start', 'target', 'feat_static_cat', 'item_id'],
+        num_rows: 2240
+    })
+    validation: Dataset({
+        features: ['start', 'target', 'feat_static_cat', 'item_id'],
+        num_rows: 320
+    })
+})
+```
+
+## Dataset Creation
+
+The Electricity Load Diagrams 2011–2014 Dataset was developed by Artur Trindade and shared in UCI Machine Learning Repository. This dataset covers the electricity load of 370 substations in Portugal from the start of 2011 to the end of 2014 with a sampling period of 15 min. We will resample this to hourly time series.
+
+### Curation Rationale
+
+Research and development of load forecasting methods. In particular short-term electricity forecasting.
+
+### Source Data
+
+This dataset covers the electricity load of 370 sub-stations in Portugal from the start of 2011 to the end of 2014 with a sampling period of 15 min.
+
+#### Initial Data Collection and Normalization
+
+[More Information Needed]
+
+#### Who are the source language producers?
+
+[More Information Needed]
+
+### Annotations
+
+#### Annotation process
+
+[More Information Needed]
+
+#### Who are the annotators?
+
+[More Information Needed]
+
+### Personal and Sensitive Information
+
+[More Information Needed]
+
+## Considerations for Using the Data
+
+The default metadata for each configuration is:
+
+### UCI Metadata:
+
+```json
+{
+    "freq": "1H",
+    "prediction_length": 24,
+    "feat_static_cat": [
+        {
+            "name": "feat_static_cat",
+            "cardinality": "370"
+        }
+    ]
+}
+```
+
+### LSTNet Metadata:
+
+```json
+{
+    "freq": "1H",
+    "prediction_length": 24,
+    "feat_static_cat": [
+        {
+            "name": "feat_static_cat",
+            "cardinality": "320"
+        }
+    ]
+}
+```
+
+### Notes
+
+- Data set has no missing values.
+- Values are in kW of each 15 min rescaled to hourly. To convert values in kWh values must be divided by 4.
+- All time labels report to Portuguese hour, however all days present 96 measures (24*4). 
+- Every year in March time change day (which has only 23 hours) the values between 1:00 am and 2:00 am are zero for all points. 
+- Every year in October time change day (which has 25 hours) the values between 1:00 am and 2:00 am aggregate the consumption of two hours.
+
+### Dataset Usage
+
+We have the following configuration parameters corresponding to the default setting:
+
+- `freq` is the time series frequency at which we resample (default: `"1H"`)
+- `prediction_length` is the forecast horizon for this task which is used to make the validation and test splits (default: `24`)
+- `rolling_evaluations` is the number of rolling window time series in the test split for evaluation purposes (default: `7`)
+
+To define your own configuration different from those used in the papers you can always specify it, for example:
+
+```python
+load_dataset("electricity_load_diagram", "uci", rolling_evaluations=10)
+```
+
+### Social Impact of Dataset
+
+[More Information Needed]
+
+### Discussion of Biases
+
+[More Information Needed]
+
+### Other Known Limitations
+
+[More Information Needed]
+
+## Additional Information
+
+### Dataset Curators
+
+[More Information Needed]
+
+### Licensing Information
+
+[More Information Needed]
+
+### Citation Information
+
+[More Information Needed]
+
+### Contributions
+
+Thanks to [@kashif](https://github.com/kashif) for adding this dataset.
---
+++ datasets/electricity_load_diagram/electricity_load_diagram.py
@@ -0,0 +1,222 @@
+# coding=utf-8
+# Copyright 2020 The HuggingFace Datasets Authors and the current dataset script contributor.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Electricity Load Diagrams 2011-2014 time series dataset."""
+from pathlib import Path
+
+import pandas as pd
+
+import datasets
+
+from .utils import to_dict
+
+
+_CITATION = """\
+@inproceedings{10.1145/3209978.3210006,
+    author = {Lai, Guokun and Chang, Wei-Cheng and Yang, Yiming and Liu, Hanxiao},
+    title = {Modeling Long- and Short-Term Temporal Patterns with Deep Neural Networks},
+    year = {2018},
+    isbn = {9781450356572},
+    publisher = {Association for Computing Machinery},
+    address = {New York, NY, USA},
+    url = {https://doi.org/10.1145/3209978.3210006},
+    doi = {10.1145/3209978.3210006},
+    booktitle = {The 41st International ACM SIGIR Conference on Research & Development in Information Retrieval},
+    pages = {95--104},
+    numpages = {10},
+    location = {Ann Arbor, MI, USA},
+    series = {SIGIR '18}
+}
+"""
+
+# You can copy an official description
+_DESCRIPTION = """\
+This new dataset contains hourly kW electricity consumption time series of 370 Portuguese clients from 2011 to 2014.
+"""
+
+_HOMEPAGE = "https://archive.ics.uci.edu/ml/datasets/ElectricityLoadDiagrams20112014"
+
+_LICENSE = ""
+
+# The HuggingFace Datasets library doesn't host the datasets but only points to the original files.
+# This can be an arbitrary nested dict/list of URLs (see below in `_split_generators` method)
+_URL = "https://archive.ics.uci.edu/ml/machine-learning-databases/00321/LD2011_2014.txt.zip"
+
+
+class ElectrictyLoadDiagramConfig(datasets.BuilderConfig):
+    """A builder config with some added meta data."""
+
+    freq: str = "1H"
+    prediction_length: int = 24
+    rolling_evaluations: int = 7
+
+
+class ElectrictyLoadDiagram(datasets.GeneratorBasedBuilder):
+    """Hourly electricity consumption of 370 points/clients."""
+
+    VERSION = datasets.Version("1.0.0")
+
+    BUILDER_CONFIGS = [
+        ElectrictyLoadDiagramConfig(
+            name="uci",
+            version=VERSION,
+            description="Original UCI time series.",
+        ),
+        ElectrictyLoadDiagramConfig(
+            name="lstnet",
+            version=VERSION,
+            description="Electricity time series preporcessed as in LSTNet paper.",
+        ),
+    ]
+
+    DEFAULT_CONFIG_NAME = "lstnet"
+
+    def _info(self):
+        features = datasets.Features(
+            {
+                "start": datasets.Value("timestamp[s]"),
+                "target": datasets.Sequence(datasets.Value("float32")),
+                "feat_static_cat": datasets.Sequence(datasets.Value("uint64")),
+                # "feat_static_real":  datasets.Sequence(datasets.Value("float32")),
+                # "feat_dynamic_real":  datasets.Sequence(datasets.Sequence(datasets.Value("uint64"))),
+                # "feat_dynamic_cat": datasets.Sequence(datasets.Sequence(datasets.Value("uint64"))),
+                "item_id": datasets.Value("string"),
+            }
+        )
+        return datasets.DatasetInfo(
+            # This is the description that will appear on the datasets page.
+            description=_DESCRIPTION,
+            # This defines the different columns of the dataset and their types
+            features=features,  # Here we define them above because they are different between the two configurations
+            # If there's a common (input, target) tuple from the features, uncomment supervised_keys line below and
+            # specify them. They'll be used if as_supervised=True in builder.as_dataset.
+            # supervised_keys=("sentence", "label"),
+            # Homepage of the dataset for documentation
+            homepage=_HOMEPAGE,
+            # License for the dataset if available
+            license=_LICENSE,
+            # Citation for the dataset
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        # dl_manager is a datasets.download.DownloadManager that can be used to download and extract URLS
+        # It can accept any type or nested list/dict and will give back the same structure with the url replaced with path to local files.
+        # By default the archives will be extracted and a path to a cached folder where they are extracted is returned instead of the archive
+        data_dir = dl_manager.download_and_extract(_URL)
+
+        train_ts = []
+        val_ts = []
+        test_ts = []
+
+        df = pd.read_csv(
+            Path(data_dir) / "LD2011_2014.txt",
+            sep=";",
+            index_col=0,
+            parse_dates=True,
+            decimal=",",
+        )
+        df.sort_index(inplace=True)
+        df = df.resample(self.config.freq).sum()
+        unit = pd.tseries.frequencies.to_offset(self.config.freq).name
+
+        if self.config.name == "uci":
+            val_end_date = df.index.max() - pd.Timedelta(
+                self.config.prediction_length * self.config.rolling_evaluations, unit
+            )
+            train_end_date = val_end_date - pd.Timedelta(self.config.prediction_length, unit)
+        else:
+            # concate the time series to be from 2012 till 2014
+            df = df[(df.index.year >= 2012) & (df.index.year <= 2014)]
+
+            # drop time series which are zero at the start
+            df = df.T[df.iloc[0] > 0].T
+
+            # tran/val/test split from LSTNet paper
+            # validation ends at 8/10-th of the time series
+            val_end_date = df.index[int(len(df) * (8 / 10)) - 1]
+            # training ends at 6/10-th of the time series
+            train_end_date = df.index[int(len(df) * (6 / 10)) - 1]
+
+        for cat, (ts_id, ts) in enumerate(df.iteritems()):
+            start_date = ts.ne(0).idxmax()
+
+            sliced_ts = ts[start_date:train_end_date]
+            train_ts.append(
+                to_dict(
+                    target_values=sliced_ts.values,
+                    start=start_date,
+                    cat=[cat],
+                    item_id=ts_id,
+                )
+            )
+
+            sliced_ts = ts[start_date:val_end_date]
+            val_ts.append(
+                to_dict(
+                    target_values=sliced_ts.values,
+                    start=start_date,
+                    cat=[cat],
+                    item_id=ts_id,
+                )
+            )
+
+        for i in range(self.config.rolling_evaluations):
+            for cat, (ts_id, ts) in enumerate(df.iteritems()):
+                start_date = ts.ne(0).idxmax()
+
+                test_end_date = val_end_date + pd.Timedelta(self.config.prediction_length * (i + 1), unit)
+                sliced_ts = ts[start_date:test_end_date]
+                test_ts.append(
+                    to_dict(
+                        target_values=sliced_ts.values,
+                        start=start_date,
+                        cat=[cat],
+                        item_id=ts_id,
+                    )
+                )
+
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                # These kwargs will be passed to _generate_examples
+                gen_kwargs={
+                    "split": train_ts,
+                    "filepath": None,
+                },
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.TEST,
+                # These kwargs will be passed to _generate_examples
+                gen_kwargs={
+                    "split": test_ts,
+                    "filepath": None,
+                },
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.VALIDATION,
+                # These kwargs will be passed to _generate_examples
+                gen_kwargs={
+                    "split": val_ts,
+                    "filepath": None,
+                },
+            ),
+        ]
+
+    # method parameters are unpacked from `gen_kwargs` as given in `_split_generators`
+    def _generate_examples(self, filepath, split):
+        # The `key` is for legacy reasons (tfds) and is not important in itself, but must be unique for each example.
+        for key, row in enumerate(split):
+            # Yields examples as (key, example) tuples
+            yield key, row
