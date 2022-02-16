10e08b791de7aab9d4fda9b37c14adf7e4383ec9
mariosasko <mariosasko777@gmail.com>
2022-02-16 18:19:33 +0100

    Use xopen

--- src/datasets/utils/streaming_download_manager.py
+++ src/datasets/utils/streaming_download_manager.py
@@ -237,7 +237,8 @@ def xgetsize(path, use_auth_token: Optional[Union[str, bool]] = None) -> int:
         fs, *_ = fsspec.get_fs_token_paths(path, storage_options=storage_options)
         size = fs.size(main_hop)
         if size is None:
-            with fs.open(main_hop, "rb") as f:
+            # use xopen instead of fs.open to make data fetching more robust
+            with xopen(path, use_auth_token=use_auth_token) as f:
                 size = len(f.read())
         return size
 
