82f8ab3638a1c5111ccd1b9891e9212e71e03130
mariosasko <mariosasko777@gmail.com>
2022-03-02 14:41:36 +0100

    Update docs

--- src/datasets/utils/file_utils.py
+++ src/datasets/utils/file_utils.py
@@ -153,7 +153,7 @@ def head_hf_s3(
 
 def hf_github_url(path: str, name: str, dataset=True, revision: Optional[str] = None) -> str:
     from .. import SCRIPTS_VERSION
-    
+
     revision = revision or os.getenv("HF_SCRIPTS_VERSION", SCRIPTS_VERSION)
     if dataset:
         return config.REPO_DATASETS_URL.format(revision=revision, path=path, name=name)
