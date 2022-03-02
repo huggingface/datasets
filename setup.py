d406b6f13506e03b30d4e3fa0a46443d8be78bf4
mariosasko <mariosasko777@gmail.com>
2022-03-02 17:53:22 +0100

    Update setup.py and CI files

--- setup.py
+++ setup.py
@@ -71,9 +71,8 @@ REQUIRED_PKGS = [
     # We use numpy>=1.17 to have np.random.Generator (Dataset shuffling)
     "numpy>=1.17",
     # Backend and serialization.
-    # Minimum 3.0.0 to support mix of struct and list types in parquet, and batch iterators of parquet data
-    # pyarrow 4.0.0 introduced segfault bug, see: https://github.com/huggingface/datasets/pull/2268
-    "pyarrow>=3.0.0,!=4.0.0",
+    # Minimum 5.0.0 to support mix of struct and list types in parquet, and batch iterators of parquet data, masks in StructArray
+    "pyarrow>=5.0.0",
     # For smart caching dataset processing
     "dill",
     # For performance gains with apache arrow
