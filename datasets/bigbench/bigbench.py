4b317d61dc5dd0fbba2bedcff423d1947a12aeca
Anders Johan Andreassen <ajandreassen@google.com>
2022-03-14 00:21:58 -0700

    Draft PR for bigbench datasets

---
+++ datasets/bigbench/bigbench.py
@@ -0,0 +1,207 @@
+# coding=utf-8
+# Copyright 2020 The HuggingFace Datasets Authors and the current dataset script contributor.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""HuggingFace datasets implementation of the json tasks in the BIG-Bench Dataset.
+For the programatic tasks, please use the BIG-Bench API on github.com/google/BIG-bench.
+"""
+
+
+import csv
+import json
+import numpy as np
+import os
+from typing import Optional
+
+import datasets
+
+try:
+  import bigbench.bbseqio.bigbench_bridge as bbb
+  from bigbench.bbseqio import bigbench_json_paths as bb_json_paths
+except ModuleNotFoundError as error:
+  print(f"Failed to import bigbench. Please see https://github.com/google/BIG-bench or install using 'pip install git+https://github.com/google/BIG-bench.git'")
+  raise
+
+_CITATION = """\
+@InProceedings{bigbench,
+title = {Beyond the Imitation Game: Quantifying and extrapolating the
+capabilities of language models},
+author={BIG-Bench Collaboration
+},
+year={2022}
+}
+"""
+
+_DESCRIPTION = """\
+The Beyond the Imitation Game Benchmark (BIG-bench) is a collaborative benchmark intended to 
+probe large language models, and extrapolate their future capabilities.
+"""
+
+_HOMEPAGE = "https://github.com/google/BIG-bench"
+
+_LICENSE = "Apache License 2.0"
+
+def div_or_none(x, y):
+  return x // y if x else x
+
+_GLOBAL_MIN_VALIDATION_EXAMPLES = 16
+
+class BigBenchConfig(datasets.BuilderConfig):
+    def __init__(self,
+                 task_name: str,
+                 
+                 *args, 
+                 subtask_name: Optional[str] = None, 
+                 num_shots: int = 0,
+                 max_examples: Optional[int] = None,
+                 **kwargs):
+        super().__init__(
+            *args,
+            **kwargs,
+        )
+        self.task_name = task_name
+        self.subtask_name = subtask_name
+        self.num_shots = num_shots
+        self.max_examples = max_examples
+
+
+class BigBench(datasets.GeneratorBasedBuilder):
+    """The Beyond the Imitation Game Benchmark (BIG-bench) is a collaborative benchmark
+    intended to probe large language models, and extrapolate their future capabilities."""
+
+    VERSION = datasets.Version("1.0.0")
+
+    BUILDER_CONFIG_CLASS = BigBenchConfig
+
+
+    def _info(self):
+        features = datasets.Features(
+            {
+              "idx": datasets.Value("int32"),
+              "inputs": datasets.Value("string"),
+              "targets": datasets.Sequence(datasets.Value("string")),
+              "multiple_choice_targets": datasets.Sequence(datasets.Value("string")),
+              "multiple_choice_scores": datasets.Sequence(datasets.Value("int32")),
+            })
+        return datasets.DatasetInfo(
+            # This is the description that will appear on the datasets page.
+            description=_DESCRIPTION,
+            # This defines the different columns of the dataset and their types
+            features=features,  # Here we define them above because they are different between the two configurations
+            # If there's a common (input, target) tuple from the features,
+            # specify them here. They'll be used if as_supervised=True in
+            # builder.as_dataset.
+            supervised_keys=None,
+            # Homepage of the dataset for documentation
+            homepage=_HOMEPAGE,
+            # License for the dataset if available
+            license=_LICENSE,
+            # Citation for the dataset
+            citation=_CITATION,
+        )
+
+
+    def _split_generators(self, dl_manager):
+        """Returns SplitGenerators."""
+		
+        return [
+            # TODO(ajandreassen): Is there a way to implement all in HF? 
+          	# datasets.SplitGenerator(
+          	# name=datasets.Split('default'),
+          	# # These kwargs will be passed to _generate_examples
+          	# gen_kwargs={
+          	# "split": "all",
+          	# },
+          	# ),
+          	datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                # These kwargs will be passed to _generate_examples
+                gen_kwargs={
+                    "split": "train",
+                },
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.VALIDATION,
+                # These kwargs will be passed to _generate_examples
+                gen_kwargs={
+                    "split": "validation",
+                },
+            ),
+        ]
+
+    def _generate_examples(
+        self, split # method parameters are unpacked from `gen_kwargs` as given in `_split_generators`
+    ):
+        """ Yields examples as (key, example) tuples. """
+        task_path, json_util = bb_json_paths.get_task_path(self.config.task_name)
+        
+        has_subtasks = bb_json_paths.has_subtasks(self.config.task_name)
+        
+        if not has_subtasks:
+          ds_fn = bbb.get_dataset_fn(task_name = self.config.task_name, 
+                                     task_path = task_path,
+                                     subtask_name = None, 
+                                     num_shots = self.config.num_shots, 
+                                     bigbench_task_type = bbb.BigBenchTaskType.HUGGINGFACE, 
+                                     max_examples = self.config.max_examples, 
+                                     json_util = json_util, 
+                                     min_validation_examples = _GLOBAL_MIN_VALIDATION_EXAMPLES
+                                    )
+          ds = ds_fn(split)
+        elif self.config.subtask_name is not None:
+          ds_fn = bbb.get_dataset_fn(task_name = self.config.task_name, 
+                                     task_path = task_path,
+                                     subtask_name = self.config.subtask_name, 
+                                     num_shots = self.config.num_shots, 
+                                     bigbench_task_type = bbb.BigBenchTaskType.HUGGINGFACE, 
+                                     max_examples = self.config.max_examples, 
+                                     json_util = json_util, 
+                                     min_validation_examples = _GLOBAL_MIN_VALIDATION_EXAMPLES
+                                    )
+          ds = ds_fn(split)
+        else:
+          # Create mixture of all subtasks
+          subtask_names = bb_json_paths.get_subtask_names(self.config.task_name)
+          num_subtasks = len(subtask_names)
+          max_examples_per_subtask = div_or_none(self.config.max_examples, num_subtasks)
+          min_validation_examples_per_subtask = div_or_none(
+              _GLOBAL_MIN_VALIDATION_EXAMPLES, num_subtasks)
+          subtask_datasets = []
+          for subtask_name in subtask_names:
+            subtask_name = subtask_name.split(':')[-1]
+            print(f"Loading subtask {split} split", subtask_name)
+            ds_fn = bbb.get_dataset_fn(task_name = self.config.task_name, 
+                                     task_path = task_path,
+                                     subtask_name = subtask_name, 
+                                     num_shots = self.config.num_shots, 
+                                     bigbench_task_type = bbb.BigBenchTaskType.HUGGINGFACE, 
+                                     max_examples = max_examples_per_subtask, 
+                                     json_util = json_util, 
+                                     min_validation_examples = min_validation_examples_per_subtask
+                                  	)
+            subtask_datasets.append(ds_fn(split))
+          ds = subtask_datasets[0]
+          for sub_ds in subtask_datasets[1:]:
+          	ds = ds.concatenate(sub_ds)
+        
+        unique_key_counter = 0
+        
+        for example in ds:
+          unique_key_counter +=1
+          yield unique_key_counter, {
+            "idx": example["idx"],
+            "inputs": example["inputs"],
+            "targets": example["targets"],
+            "multiple_choice_targets": example["multiple_choice_targets"],
+            "multiple_choice_scores": example["multiple_choice_scores"],
+            }
