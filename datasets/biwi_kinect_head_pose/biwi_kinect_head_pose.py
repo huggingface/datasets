181b0a90f2856890fca8e9f0ee3b5abde6876603
Naveen Reddy D <dnaveen356@gmail.com>
2022-03-20 21:41:15 +0530

    update dataset features and complete generate_examples.

--- datasets/biwi_kinect_head_pose/biwi_kinect_head_pose.py
+++ datasets/biwi_kinect_head_pose/biwi_kinect_head_pose.py
@@ -15,6 +15,7 @@
 """Biwi Kinect Head Pose Database."""
 
 
+import glob
 import os
 
 import datasets
@@ -49,6 +50,33 @@ _URLS = {
     "kinect_head_pose_db": "https://data.vision.ee.ethz.ch/cvl/gfanelli/kinect_head_pose_db.tgz",
 }
 
+_sequence_to_subject_map = {
+    "01": "F01",
+    "02": "F02",
+    "03": "F03",
+    "04": "F04",
+    "05": "F05",
+    "06": "F06",
+    "07": "M01",
+    "08": "M02",
+    "09": "M03",
+    "10": "M04",
+    "11": "M05",
+    "12": "M06",
+    "13": "M07",
+    "14": "M08",
+    "15": "F03",
+    "16": "M09",
+    "17": "M10",
+    "18": "F05",
+    "19": "M11",
+    "20": "M12",
+    "21": "F02",
+    "22": "M01",
+    "23": "M13",
+    "24": "M14",
+}
+
 
 class BiwiKinectHeadPose(datasets.GeneratorBasedBuilder):
 
@@ -59,12 +87,31 @@ class BiwiKinectHeadPose(datasets.GeneratorBasedBuilder):
             description=_DESCRIPTION,
             features=datasets.Features(
                 {
-                    "person_id": datasets.Value("string"),
-                    "frame_number": datasets.Value("string"),
-                    "depth_image": datasets.Value("string"),
-                    "rgb_image": datasets.Image(),
-                    "3D_head_center": datasets.Array2D(shape=(3, 3), dtype="float"),
-                    "3D_head_rotation": datasets.Value("float"),
+                    "sequence_number": datasets.Value("string"),
+                    "subject_id": datasets.Value("string"),
+                    "rgb": datasets.Sequence(datasets.Image()),
+                    "rgb_cal": {
+                        "intrisic_mat": datasets.Array2D(shape=(3, 3), dtype="float64"),
+                        "extrinsic_mat": {
+                            "rotation": datasets.Array2D(shape=(3, 3), dtype="float64"),
+                            "translation": datasets.Sequence(datasets.Value("float64"), length=3),
+                        },
+                    },
+                    "depth": datasets.Sequence(datasets.Value("string")),
+                    "depth_cal": {
+                        "intrisic_mat": datasets.Array2D(shape=(3, 3), dtype="float64"),
+                        "extrinsic_mat": {
+                            "rotation": datasets.Array2D(shape=(3, 3), dtype="float64"),
+                            "translation": datasets.Sequence(datasets.Value("float64"), length=3),
+                        },
+                    },
+                    "head_pose_gt": datasets.Sequence(
+                        {
+                            "center": datasets.Sequence(datasets.Value("float64"), length=3),
+                            "rotation": datasets.Array2D(shape=(3, 3), dtype="float64"),
+                        }
+                    ),
+                    "head_template": datasets.Value("string"),
                 }
             ),
             homepage=_HOMEPAGE,
@@ -84,6 +131,85 @@ class BiwiKinectHeadPose(datasets.GeneratorBasedBuilder):
             ),
         ]
 
+    @staticmethod
+    def _get_calibration_information(cal_file_path):
+        with open(cal_file_path, "r") as f:
+            cal_info = f.read().splitlines()
+
+        intrisic_mat = []
+        extrinsic_mat = []
+
+        for data in cal_info[:3]:
+            row = list(map(float, data.strip().split(" ")))
+            intrisic_mat.append(row)
+
+        for data in cal_info[6:9]:
+            row = list(map(float, data.strip().split(" ")))
+            extrinsic_mat.append(row)
+
+        translation = list(map(float, cal_info[10].strip().split(" ")))
+
+        return {
+            "intrisic_mat": intrisic_mat,
+            "extrinsic_mat": {
+                "rotation": extrinsic_mat,
+                "translation": translation,
+            },
+        }
+
+    @staticmethod
+    def _parse_head_pose_info(head_pose_file):
+        with open(head_pose_file, "r") as f:
+            head_pose_info = f.read().splitlines()
+
+        rotation = []
+        for data in head_pose_info[:3]:
+            row = list(map(float, data.strip().split(" ")))
+            rotation.append(row)
+
+        center = list(map(float, head_pose_info[4].strip().split(" ")))
+
+        return {
+            "center": center,
+            "rotation": rotation,
+        }
+
+    @staticmethod
+    def _get_head_pose_information(path):
+        head_pose_files = sorted(glob.glob(path + "*_pose.txt"))
+
+        head_poses_info = []
+
+        for head_pose_file in head_pose_files:
+            head_pose = BiwiKinectHeadPose._parse_head_pose_info(head_pose_file)
+            head_poses_info.append(head_pose)
+
+        return head_poses_info
+
     def _generate_examples(self, dataset_path):
-        # TO-DO
-        pass
+
+        idx = 0
+        folders = os.listdir(dataset_path)
+        for item in folders:
+            sequence_number = item
+            sequence_base_path = os.path.join(dataset_path, sequence_number, "")
+            if os.path.isdir(sequence_base_path):
+                rgb_files = sorted(glob.glob(sequence_base_path + "*.png"))
+                depth_files = sorted(glob.glob(sequence_base_path + "*.bin"))
+                head_template_path = os.path.join(dataset_path, sequence_number + ".obj")
+                rgb_cal = self._get_calibration_information(sequence_base_path + "rgb.cal")
+                depth_cal = self._get_calibration_information(sequence_base_path + "depth.cal")
+                head_pose_gt = self._get_head_pose_information(sequence_base_path)
+
+                yield idx, {
+                    "sequence_number": sequence_number,
+                    "subject_id": _sequence_to_subject_map[sequence_number],
+                    "rgb": rgb_files,
+                    "rgb_cal": rgb_cal,
+                    "depth": depth_files,
+                    "depth_cal": depth_cal,
+                    "head_pose_gt": head_pose_gt,
+                    "head_template": head_template_path,
+                }
+
+                idx += 1
