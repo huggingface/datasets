a5a735094d499ad142ade446ca505c4287b59bed
polinaeterna <polina@huggingface.co>
2022-03-23 18:33:14 +0400

    change str path to Audio feature in audio column in ASR task

--- src/datasets/tasks/automatic_speech_recognition.py
+++ src/datasets/tasks/automatic_speech_recognition.py
@@ -1,20 +1,18 @@
 from dataclasses import dataclass
 from typing import ClassVar, Dict
 
-from ..features import Features, Value
+from ..features import Features, Value, Audio
 from .base import TaskTemplate
 
 
 @dataclass(frozen=True)
-class AutomaticSpeechRecognition(TaskTemplate):
+class AutomaticSpeechRecognition(TaskTemplate, sampling_rate=None):
     task: str = "automatic-speech-recognition"
-    # TODO(lewtun): Replace input path feature with dedicated `Audio` features
-    # when https://github.com/huggingface/datasets/pull/2324 is implemented
-    input_schema: ClassVar[Features] = Features({"audio_file_path": Value("string")})
-    label_schema: ClassVar[Features] = Features({"transcription": Value("string")})
-    audio_file_path_column: str = "audio_file_path"
+    input_schema: ClassVar[Features] = Features({"audio_file": Audio()})
+    label_schema: ClassVar[Features] = Features({"transcription": Audio()})
+    audio_file_column: str = "audio_file"
     transcription_column: str = "transcription"
 
     @property
     def column_mapping(self) -> Dict[str, str]:
-        return {self.audio_file_path_column: "audio_file_path", self.transcription_column: "transcription"}
+        return {self.audio_file_column: "audio_file", self.transcription_column: "transcription"}
