e7706d610ed147b95243c0f322618e2f7080e3e1
Quentin Lhoest <lhoest.q@gmail.com>
2022-04-06 16:02:05 +0200

    comments and docs

--- src/datasets/utils/_hf_hub_fixes.py
+++ src/datasets/utils/_hf_hub_fixes.py
@@ -8,13 +8,37 @@ from packaging import version
 def create_repo(
     hf_api: HfApi,
     name: str,
+    organization: str,
     token: Optional[str] = None,
-    organization: Optional[str] = None,
     private: Optional[bool] = None,
     repo_type: Optional[str] = None,
     exist_ok: Optional[bool] = False,
     space_sdk: Optional[str] = None,
 ) -> str:
+    """
+    The huggingface_hub.HfApi.create_repo parameters changed in 0.5.0 and some of them were deprecated.
+    This function checks the huggingface_hub version to call the right parameters.
+
+    Args:
+        hf_api (`huggingface_hub.HfApi`): Hub client
+        name (`str`): name of the repository (without the namespace)
+        organization (`str`): namespace for the repository: the username or organization name.
+        token (`str`, *optional*): user or organization token. Defaults to None.
+        private (`bool`, *optional*):
+            Whether the model repo should be private.
+        repo_type (`str`, *optional*):
+            Set to `"dataset"` or `"space"` if uploading to a dataset or
+            space, `None` or `"model"` if uploading to a model. Default is
+            `None`.
+        exist_ok (`bool`, *optional*, defaults to `False`):
+            If `True`, do not raise an error if repo already exists.
+        space_sdk (`str`, *optional*):
+            Choice of SDK to use if repo_type is "space". Can be
+            "streamlit", "gradio", or "static".
+
+    Returns:
+        `str`: URL to the newly created repo.
+    """
     if version.parse(huggingface_hub.__version__) < version.parse("0.5.0"):
         return hf_api.create_repo(
             name=name,
