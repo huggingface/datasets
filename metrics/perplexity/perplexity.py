632ec86b556dc4525329866affe810a93a02efa5
emibaylor <emi.baylor@gmail.com>
2022-04-08 13:54:44 +0000

    fix padding token issue, which fixes output values

--- metrics/perplexity/perplexity.py
+++ metrics/perplexity/perplexity.py
@@ -16,6 +16,7 @@
 import torch
 from torch.nn import CrossEntropyLoss
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import numpy as np
 
 import datasets
 from datasets import logging
@@ -89,8 +90,12 @@ class Perplexity(datasets.Metric):
             reference_urls=["https://huggingface.co/docs/transformers/perplexity"],
         )
 
-    def _compute(
-        self, input_texts, model_id, batch_size: int = 16, add_start_token: bool = True, stride: int = 512, device=None
+    def _compute(self,
+                input_texts,
+                model_id,
+                batch_size: int = 16,
+                add_start_token: bool = True,
+                device=None
     ):
 
         if device is not None:
@@ -100,22 +105,28 @@ class Perplexity(datasets.Metric):
         else:
             device = "cuda" if torch.cuda.is_available() else "cpu"
 
-        print("cuda available?", torch.cuda.is_available())
 
         model = AutoModelForCausalLM.from_pretrained(model_id)
         model = model.to(device)
 
-        tokenizer = AutoTokenizer.from_pretrained(model_id, pad_token="<PAD>")
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+        # if batch_size > 1 (which generally leads to padding being required), and
+        # if there is not an already assigned pad_token, assign an existing
+        # special token to also be the padding token
+        if tokenizer.pad_token is None and batch_size > 1:
+            existing_special_tokens = list(tokenizer.special_tokens_map_extended.values())
+            # check that the model already has at least one special token defined
+            assert len(existing_special_tokens) > 0, "If batch_size > 1, model must have at least one special token to use for padding. Please use a different model or set batch_size=1."
+            # assign one of the special tokens to also be the pad token
+            tokenizer.add_special_tokens({'pad_token':existing_special_tokens[0]})
 
         if add_start_token:
             # leave room for <BOS> token to be added:
-            max_tokenized_len = model.config.n_positions - 1
-            if tokenizer.bos_token is None:
-                tokenizer.add_special_tokens({"bos_token": "<BOS>"})
+            assert tokenizer.bos_token is not None, "Input model must already have a BOS token if using add_start_token=True. Please use a different model, or set add_start_token=False"
+            max_tokenized_len = model.config.max_length - 1
         else:
-            max_tokenized_len = model.config.n_positions
-
-        model.resize_token_embeddings(len(tokenizer))
+            max_tokenized_len = model.config.max_length
 
         encodings = tokenizer(
             input_texts,
@@ -139,12 +150,12 @@ class Perplexity(datasets.Metric):
             ), "When add_start_token=False, each input text must be at least two tokens long. Run with add_start_token=True if inputting strings of only one token, and remove all empty input strings."
 
         ppls = []
-
         loss_fct = CrossEntropyLoss(reduction="none")
 
-        for text_index in logging.tqdm(range(0, len(encoded_texts), batch_size)):
-            encoded_batch = encoded_texts[text_index : min(text_index + batch_size, len(encoded_texts))]
-            attn_mask = attn_masks[text_index : min(text_index + batch_size, len(attn_masks))]
+        for start_index in logging.tqdm(range(0, len(encoded_texts), batch_size)):
+            end_index = min(start_index + batch_size, len(encoded_texts))
+            encoded_batch = encoded_texts[start_index : end_index]
+            attn_mask = attn_masks[start_index : end_index]
 
             if add_start_token:
                 bos_tokens_tensor = torch.tensor([[tokenizer.bos_token_id]] * encoded_batch.size(dim=0)).to(device)
@@ -162,11 +173,11 @@ class Perplexity(datasets.Metric):
             shift_labels = labels[..., 1:].contiguous()
             shift_attention_mask_batch = attn_mask[..., 1:].contiguous()
 
-            perplexity_batch = torch.exp(
+            perplexity_batch = torch.exp2(
                 (loss_fct(shift_logits.transpose(1, 2), shift_labels) * shift_attention_mask_batch).sum(1)
                 / shift_attention_mask_batch.sum(1)
-            ).tolist()
+            )
 
-            ppls += perplexity_batch
+            ppls += perplexity_batch.tolist()
 
-        return {"perplexity": ppls}
+        return {"perplexities": ppls, "mean_perplexity": np.mean(ppls)}
