ef33a84b1daa862e84addeff748fa698a717ab54
Anders Johan Andreassen <ajandreassen@google.com>
2022-04-21 06:20:09 -0700

    trigger test

--- setup.py
+++ setup.py
@@ -143,7 +143,7 @@ TESTS_REQUIRE = [
     "py7zr",
     "tldextract",
     "zstandard",
-    "bigbench @ https://storage.googleapis.com/public_research_data/bigbench/bigbench-0.0.1.tar.gz",
+    "bigbench @ https://storage.googleapis.com/public_research_data/bigbench/bigbench-0.0.1.tar.gz", 
     # metrics dependencies
     "bert_score>=0.3.6",
     "rouge_score",
