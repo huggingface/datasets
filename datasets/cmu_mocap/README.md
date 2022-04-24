35ed03548372f685459ee758552d1cd0d2bcb286
Naveen Reddy D <dnaveen356@gmail.com>
2022-04-24 22:45:37 +0530

    Initial CMU MoCap draft.

---
+++ datasets/cmu_mocap/README.md
@@ -0,0 +1,118 @@
+[Needs More Information]
+
+# Dataset Card for CMU MoCap 
+
+## Table of Contents
+- [Dataset Description](#dataset-description)
+  - [Dataset Summary](#dataset-summary)
+  - [Supported Tasks](#supported-tasks-and-leaderboards)
+  - [Languages](#languages)
+- [Dataset Structure](#dataset-structure)
+  - [Data Instances](#data-instances)
+  - [Data Fields](#data-instances)
+  - [Data Splits](#data-instances)
+- [Dataset Creation](#dataset-creation)
+  - [Curation Rationale](#curation-rationale)
+  - [Source Data](#source-data)
+  - [Annotations](#annotations)
+  - [Personal and Sensitive Information](#personal-and-sensitive-information)
+- [Considerations for Using the Data](#considerations-for-using-the-data)
+  - [Social Impact of Dataset](#social-impact-of-dataset)
+  - [Discussion of Biases](#discussion-of-biases)
+  - [Other Known Limitations](#other-known-limitations)
+- [Additional Information](#additional-information)
+  - [Dataset Curators](#dataset-curators)
+  - [Licensing Information](#licensing-information)
+  - [Citation Information](#citation-information)
+
+## Dataset Description
+
+- **Homepage:** [CMU MoCap homepage](http://mocap.cs.cmu.edu/)
+- **Repository:** [More Information Needed]
+- **Paper:** [More Information Needed]
+- **Leaderboard:** [More Information Needed]
+- **Point of Contact:** [More Information Needed]
+
+### Dataset Summary
+
+The CMU Graphics Lab Motion Capture Database is a dataset containing motion capture recordings of people performing different actions such as walking, jumping, dancing etc. The actions have been performed by over 140 subjects. There are a total of 2605 trials in 6 categories and 23 subcategories.
+
+### Supported Tasks and Leaderboards
+
+[More Information Needed]
+
+### Languages
+
+[More Information Needed]
+
+## Dataset Structure
+
+### Data Instances
+
+[More Information Needed]
+
+### Data Fields
+
+[More Information Needed]
+
+### Data Splits
+
+[More Information Needed]
+
+## Dataset Creation
+
+### Curation Rationale
+
+[More Information Needed]
+
+### Source Data
+
+#### Initial Data Collection and Normalization
+
+[More Information Needed]
+
+#### Who are the source language producers?
+
+[More Information Needed]
+
+### Annotations
+
+#### Annotation process
+
+[More Information Needed]
+
+#### Who are the annotators?
+
+[More Information Needed]
+
+### Personal and Sensitive Information
+
+[More Information Needed]
+
+## Considerations for Using the Data
+
+### Social Impact of Dataset
+
+[More Information Needed]
+
+### Discussion of Biases
+
+[More Information Needed]
+
+### Other Known Limitations
+
+[More Information Needed]
+
+## Additional Information
+
+### Dataset Curators
+
+[Needs More Information]
+
+### Licensing Information
+
+[Needs More Information]
+
+### Citation Information
+
+[Needs More Information]
---
+++ datasets/cmu_mocap/cmu_mocap.py
@@ -0,0 +1,284 @@
+# Copyright 2022 The HuggingFace Datasets Authors and the current dataset script contributor.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+
+import json
+import os
+import glob
+import datasets
+
+
+_CITATION = """\
+
+"""
+
+
+_DESCRIPTION = """\
+The CMU Graphics Lab Motion Capture Database is a dataset containing motion capture recordings of people performing different actions such as walking, jumping, dancing etc. The actions have been performed by over 140 subjects. There are a total of 2605 trials in 6 categories and 23 subcategories.
+"""
+
+_HOMEPAGE = "http://mocap.cs.cmu.edu/"
+
+# TODO: Add the licence for the dataset here if you can find it
+_LICENSE = ""
+
+
+_URLS = {
+    "asf/amc": "http://mocap.cs.cmu.edu/allasfamc.zip",
+    "c3d": [
+        "http://mocap.cs.cmu.edu/allc3d_0.zip",
+        "http://mocap.cs.cmu.edu/allc3d_1a.zip",
+        "http://mocap.cs.cmu.edu/allc3d_1b.zip",
+        "http://mocap.cs.cmu.edu/allc3d_234.zip",
+        "http://mocap.cs.cmu.edu/allc3d_56789.zip",
+    ],
+    "mpg": [
+        "http://mocap.cs.cmu.edu/allmpg/allmpg_0.zip",
+        "http://mocap.cs.cmu.edu/allmpg/allmpg_13.zip",
+        "http://mocap.cs.cmu.edu/allmpg/allmpg_14.zip",
+        "http://mocap.cs.cmu.edu/allmpg/allmpg_15to19.zip",
+        "http://mocap.cs.cmu.edu/allmpg/allmpg_20.zip",
+        "http://mocap.cs.cmu.edu/allmpg/allmpg_30-1.zip",
+        "http://mocap.cs.cmu.edu/allmpg/allmpg_30-2.zip",
+        "http://mocap.cs.cmu.edu/allmpg/allmpg_30-3.zip",
+        "http://mocap.cs.cmu.edu/allmpg/allmpg_40to50-1.zip",
+        "http://mocap.cs.cmu.edu/allmpg/allmpg_40to50-2.zip",
+        "http://mocap.cs.cmu.edu/allmpg/allmpg_40to50-3.zip",
+        "http://mocap.cs.cmu.edu/allmpg/allmpg_60to90.zip",
+    ],
+    "avi": "http://mocap.cs.cmu.edu/allavi.zip",
+}
+
+_METADATA_URL = 'https://huggingface.co/datasets/dnaveenr/cmu_mocap/raw/main/subject_to_details.json'
+
+_CATEGORIES = {
+    "1" : "Human Interaction",
+    "2" : "Interaction with Environment",
+    "3" : "Locomotion",
+    "4" : "Physical Activities & Sports",
+    "5" : "Situations & Scenarios",
+    "6" : "Test Motions",
+    "" : "",
+}
+
+_SUB_CATEGORY_NAMES = {
+    ('1', '1'): 'two subjects', 
+    ('2', '1'): 'playground', 
+    ('2', '2'): 'uneven terrain', 
+    ('2', '3'): 'path with obstacles', 
+    ('3', '1'): 'running', 
+    ('3', '2'): 'walking', 
+    ('3', '3'): 'jumping', 
+    ('3', '4'): 'varied', 
+    ('4', '0'): 'golf', 
+    ('4', '1'): 'frisbee', 
+    ('4', '2'): 'dance', 
+    ('4', '3'): 'gymnastics', 
+    ('4', '4'): 'acrobatics', 
+    ('4', '5'): 'martial arts', 
+    ('4', '6'): 'racquet/paddle sports', 
+    ('4', '7'): 'soccer', 
+    ('4', '8'): 'boxing', 
+    ('4', '9'): 'general exercise and stretching', 
+    ('5', '1'): 'common behaviors and expressions', 
+    ('5', '2'): 'pantomime', 
+    ('5', '3'): 'communication gestures and signals', 
+    ('5', '4'): 'cross-category variety', 
+    ('6', '1'): '', 
+    ('', ''): ''
+}
+
+class CmuMocapConfig(datasets.BuilderConfig):
+    """BuilderConfig for CMU Mocap."""
+
+    def __init__(
+        self,
+        format="asf/amc",
+        **kwargs,
+    ):
+        """BuilderConfig for MetaShift.
+
+        Args:
+            **kwargs: keyword arguments forwarded to super.
+        """
+        super(CmuMocapConfig, self).__init__(**kwargs)
+        self.format = format
+
+
+class CmuMocap(datasets.GeneratorBasedBuilder):
+
+    BUILDER_CONFIGS = [
+        CmuMocapConfig(name="cmu_mocap", version=datasets.Version("1.0.0")),
+    ]
+
+    BUILDER_CONFIG_CLASS = CmuMocapConfig
+
+    def _info(self):
+
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(self._get_feature_types()),
+            homepage=_HOMEPAGE,
+            license=_LICENSE,
+            citation=_CITATION,
+        )
+
+    def _get_feature_types(self):
+        features = {
+            "subject_id": datasets.Value("int32"),
+            "categories": datasets.Sequence(datasets.Value("string")),
+            "subcategories": datasets.Sequence(datasets.Value("string")),
+            "descriptions": datasets.Sequence(datasets.Value("string")),
+        }
+
+        if self.config.format == "asf/amc":
+            features.update(
+                {
+                    "amc_files": datasets.Sequence(datasets.Value("string")),
+                    "asf_file": datasets.Value("string"),
+                }
+            )
+        elif self.config.format == "c3d":
+            features.update(
+                {
+                    "c3d_files": datasets.Sequence(datasets.Value("string")),
+                }
+            )
+        elif self.config.format == "mpg":
+            features.update(
+                {
+                    "mpg_files": datasets.Sequence(datasets.Value("string")),
+                }
+            )
+        elif self.config.format == "avi":
+            features.update(
+                {
+                    "avi_files": datasets.Sequence(datasets.Value("string")),
+                }
+            )
+
+        return features
+
+    def _get_data_path(self, data_dir):
+        if self.config.format == "asf/amc":
+            data_path = os.path.join(data_dir, "all_asfamc", "subjects")
+        else:
+            data_path = os.path.join(data_dir, "subjects")
+
+        return data_path
+
+    def _split_generators(self, dl_manager):
+        urls = _URLS[self.config.format]
+        metadata_path = dl_manager.download(_METADATA_URL)
+        data_dir = dl_manager.download_and_extract(urls)
+        print(data_dir)
+        data_path = self._get_data_path(data_dir)
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                # These kwargs will be passed to _generate_examples
+                gen_kwargs={
+                    "data_path": data_path,
+                    "metadata_path": metadata_path,
+                    "split": "train",
+                },
+            ),
+        ]
+
+    @staticmethod
+    def _get_category_subcategory_info(files, subject_id_to_details):
+        categories = []
+        subcategories = []
+        descriptions = []
+        
+        for file_ in files:
+            trial_number = os.path.basename(file_).split('.')[0]
+            if(trial_number not in subject_id_to_details):
+                category_name = ""
+                sub_category_name = ""
+                description = ""
+            else:
+                category_id = str(subject_id_to_details[trial_number]['main_cat'])
+                category_name = _CATEGORIES[category_id]
+                sub_category_id = str(subject_id_to_details[trial_number]['sub_cat'])
+                sub_category_name = _SUB_CATEGORY_NAMES[(category_id, sub_category_id)]
+                description = subject_id_to_details[trial_number]['description']
+            categories.append(category_name)
+            subcategories.append(sub_category_name)
+            descriptions.append(description)
+        
+        return [categories, subcategories, descriptions]
+        
+        
+
+    def _generate_examples(self, data_path, metadata_path, split):
+        
+        subject_dirs = os.listdir(data_path)
+
+        with open(metadata_path, encoding="utf-8") as f:
+            subject_id_to_details = json.load(f)
+        
+        idx = 0
+        for subject in subject_dirs:
+            subject_id = int(subject)
+            subject_path = os.path.join(data_path, subject, "")
+                       
+            if self.config.format == "asf/amc":
+                asf_file = os.path.join(subject_path, subject + ".asf")
+                files = glob.glob(subject_path + "*.amc") 
+            
+            if self.config.format == "c3d":
+                files = glob.glob(subject_path + "*.c3d")
+            
+            if self.config.format == "mpg":
+                files = glob.glob(subject_path + "*.mpg")
+            
+            if self.config.format == 'avi':
+                files = glob.glob(subject_path + "*.avi")
+            
+            
+            categories, subcategories, descriptions = self._get_category_subcategory_info(files, subject_id_to_details)
+
+            features = {
+                "subject_id": subject_id,
+                "categories": categories,
+                "subcategories": subcategories,
+                "descriptions": descriptions,
+            }
+
+            if self.config.format == "asf/amc":
+                features.update({
+                    "amc_files" : files,
+                    "asf_file" : asf_file,
+                })
+            
+            if self.config.format == "c3d":
+                features.update({
+                    "c3d_files" : files,
+                })
+
+            if self.config.format == "mpg":
+                features.update({
+                    "mpg_files" : files,
+                })
+            
+            if self.config.format == 'avi':
+                features.update({
+                    "avi_files" : files,
+                })
+            
+            yield idx, features
+            idx += 1
