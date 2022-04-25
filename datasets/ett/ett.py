fd5cb6ba70843469dd3006a9a383ca4351f59227
Kashif Rasul <kashif.rasul@gmail.com>
2022-04-25 16:29:16 +0200

    removed unused imports

--- datasets/ett/ett.py
+++ datasets/ett/ett.py
@@ -12,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Electricity Transformer Temperature (ETT) dataset."""
-3
-import csv
-import json
-import os
 from dataclasses import dataclass
 
 import pandas as pd
