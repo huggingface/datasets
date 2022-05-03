3ac31ef57b2771137c69b9ff0e5ffc0da207fc5f
mariosasko <mariosasko777@gmail.com>
2022-05-03 14:11:45 +0200

    Refactor tests for packaged modules Text and Csv

--- tests/packaged_modules/test_text.py
+++ tests/packaged_modules/test_text.py
@@ -1,41 +1,9 @@
-import os
 import textwrap
 
 import pyarrow as pa
 import pytest
 
-from datasets.packaged_modules.csv.csv import Csv
 from datasets.packaged_modules.text.text import Text
-
-
-@pytest.fixture
-def csv_file(tmp_path):
-    filename = tmp_path / "malformed_file.csv"
-    data = textwrap.dedent(
-        """\
-        header1,header2
-        1,2
-        10,20
-        """
-    )
-    with open(filename, "w") as f:
-        f.write(data)
-    return str(filename)
-
-
-@pytest.fixture
-def malformed_csv_file(tmp_path):
-    filename = tmp_path / "malformed_file.csv"
-    data = textwrap.dedent(
-        """\
-        header1,header2
-        1,2
-        10,20,
-        """
-    )
-    with open(filename, "w") as f:
-        f.write(data)
-    return str(filename)
 
 
 @pytest.fixture
@@ -52,20 +20,6 @@ def text_file(tmp_path):
     with open(filename, "w", encoding="utf-8") as f:
         f.write(data)
     return str(filename)
-
-
-def test_csv_generate_tables_raises_error_with_malformed_csv(csv_file, malformed_csv_file, caplog):
-    csv = Csv()
-    generator = csv._generate_tables([csv_file, malformed_csv_file])
-    with pytest.raises(ValueError, match="Error tokenizing data"):
-        for _ in generator:
-            pass
-    assert any(
-        record.levelname == "ERROR"
-        and "Failed to read file" in record.message
-        and os.path.basename(malformed_csv_file) in record.message
-        for record in caplog.records
-    )
 
 
 @pytest.mark.parametrize("keep_linebreaks", [True, False])
