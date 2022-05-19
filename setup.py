1cc4d9c8ee2e886aeaa226e40f1bdfacd9426764
Anders Johan Andreassen <ajandreassen@google.com>
2022-05-19 15:54:18 -0700

    trigger new tests after adding editdistance on bigbench requirements remotely

--- setup.py
+++ setup.py
@@ -144,7 +144,7 @@ TESTS_REQUIRE = [
     "tldextract",
     "zstandard",
     #"bigbench @ https://storage.googleapis.com/public_research_data/bigbench/bigbench-0.0.1.tar.gz",
-	"BIG-bench @ git+https://github.com/andersjohanandreassen/BIG-bench@private_dependency_checker",
+	"BIG-bench @ git+https://github.com/andersjohanandreassen/BIG-bench@private_dependency_checker", 
     "sentencepiece",  # bigbench requires t5 which requires seqio which requires sentencepiece
     # metrics dependencies
     "bert_score>=0.3.6",
