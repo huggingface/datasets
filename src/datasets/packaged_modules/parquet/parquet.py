e1a19d2c7725e7ed33aa5dfd505cbff59acc2d86
mariosasko <mariosasko777@gmail.com>
2022-05-20 14:02:59 +0200

    Add iter_files to missing packaged modules

--- src/datasets/packaged_modules/parquet/parquet.py
+++ src/datasets/packaged_modules/parquet/parquet.py
@@ -34,12 +34,14 @@ class Parquet(datasets.ArrowBasedBuilder):
             files = data_files
             if isinstance(files, str):
                 files = [files]
-            return [datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"files": files})]
+            return [
+                datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"files": dl_manager.iter_files(files)})
+            ]
         splits = []
         for split_name, files in data_files.items():
             if isinstance(files, str):
                 files = [files]
-            splits.append(datasets.SplitGenerator(name=split_name, gen_kwargs={"files": files}))
+            splits.append(datasets.SplitGenerator(name=split_name, gen_kwargs={"files": dl_manager.iter_files(files)}))
         return splits
 
     def _generate_tables(self, files):
