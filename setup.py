7828cd2bcb59d8d23979ac4d95e427c4b2672a9a
Anders Johan Andreassen <ajandreassen@google.com>
2022-05-19 22:12:24 -0700

    changing to bigbench tar file

--- setup.py
+++ setup.py
@@ -143,8 +143,7 @@ TESTS_REQUIRE = [
     "py7zr",
     "tldextract",
     "zstandard",
-    #"bigbench @ https://storage.googleapis.com/public_research_data/bigbench/bigbench-0.0.1.tar.gz",
-	"BIG-bench @ git+https://github.com/andersjohanandreassen/BIG-bench@private_dependency_checker", 
+    "bigbench @ https://storage.googleapis.com/public_research_data/bigbench/bigbench-0.0.1.tar.gz",
     "sentencepiece",  # bigbench requires t5 which requires seqio which requires sentencepiece
     "sacremoses",
     # metrics dependencies
