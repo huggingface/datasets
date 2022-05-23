5c3ab7c2f3930b24fb09be86759ddc43dfd9f8c0
Matt <Rocketknight1@users.noreply.github.com>
2022-05-23 15:11:20 +0100

    Update src/datasets/arrow_dataset.py
    Co-authored-by: Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>

--- src/datasets/arrow_dataset.py
+++ src/datasets/arrow_dataset.py
@@ -325,10 +325,9 @@ class TensorflowDatasetMixin:
         the Dataset, and is suitable for passing to methods like model.fit() or model.predict().
 
         Args:
+            batch_size (:obj:`int`): Size of batches to load from the dataset.
             columns (:obj:`List[str]` or :obj:`str`, optional): Dataset column(s) to load in the tf.data.Dataset. Column
              names that are created by the `collate_fn` and that do not exist in the original dataset can be used.
-             If this argument is not set, we retain all non-numerical columns.
-            batch_size (:obj:`int`, defaults to 8): Size of batches to load from the dataset.
             shuffle(:obj:`bool`, default to `True`): Shuffle the dataset order when loading. Recommended True for training, False for
                 validation/evaluation.
             drop_remainder(:obj:`bool`, default ``None``): Drop the last incomplete batch when loading. If not provided,
