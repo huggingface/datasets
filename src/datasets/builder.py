79c725dff31b264e54cacbd09ec3d5a89f870c3e
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-06-01 15:24:53 +0200

    Fix docstring

--- src/datasets/builder.py
+++ src/datasets/builder.py
@@ -194,9 +194,9 @@ class DatasetBuilder:
     pre-defined set of configurations in :meth:`datasets.DatasetBuilder.builder_configs`.
 
     Args:
-        cache_dir (`str`, *optional*): Directory to read/write cache data. Defaults to '~/.cache/huggingface/datasets'.
+        cache_dir (`str`, *optional*): Directory to cache data. Defaults to ``"~/.cache/huggingface/datasets"``.
         config_name (`str`, *optional*): Name of the dataset configuration.
-            It affects the data generated on disk: different `builder_config`s will have their own subdirectories and
+            It affects the data generated on disk: different configurations will have their own subdirectories and
             versions.
             If not provided, the default configuration is used (if it exists).
         hash (`str`, *optional*): Hash specific to the dataset code. Used to update the caching directory when the
@@ -204,7 +204,7 @@ class DatasetBuilder:
             The typical caching directory (defined in ``self._relative_data_dir``) is: ``name/version/hash/``.
         base_path (`str`, *optional*): Base path for relative paths that are used to download files.
             This can be a remote URL.
-        features ([`Features`], *optional*): Features types tto use with this dataset.
+        features ([`~datasets.Features`], *optional*): Features types to use with this dataset.
             It can be used to change the Features types of a dataset, for example.
         use_auth_token (`str` or `bool`, *optional*): String or boolean to use as Bearer token for remote files on the
             Datasets Hub. If `True`, will get token from ``"~/.huggingface"``.
@@ -215,10 +215,11 @@ class DatasetBuilder:
             For builders like "csv" or "json" that need the user to specify data files. They can be either
             local or remote files. For convenience, you can use a DataFilesDict.
         data_dir (`str`, *optional*): Path to directory containing source data file(s).
-            Use only if `data_files` is not passed, in which case it is equivalent to passing `os.path.join(data_dir, "**")` as `data_files`.
+            Use only if `data_files` is not passed, in which case it is equivalent to passing
+            ``os.path.join(data_dir, "**")`` as `data_files`.
             For builders that require manual download, it must be the path to the local directory containing the
             manually downloaded data.
-        name (`str`): Deprecated. Use 'config_name' instead.
+        name (`str`): Deprecated. Use `config_name` instead.
         **config_kwargs: Keyword arguments to be passed to the [`BuilderConfig`].
     """
 
