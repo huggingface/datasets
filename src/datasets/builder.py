ea1d9ec8badb707b7595f19643dddbe6e582e483
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-06-02 19:35:15 +0200

    Fix builder docstring (#4432)
    * Pass one arg from init to class docstring
    
    * Move args from contructor to class docstring
    
    * Fix args types
    
    * Improve args docstring
    
    * Fix docstring
    
    * Try unpacked kwargs in docstring
    
    * Remove rst syntax from docstring
    
    * Fix **config_kwargs

--- src/datasets/builder.py
+++ src/datasets/builder.py
@@ -182,16 +182,47 @@ class DatasetBuilder:
 
     `DatasetBuilder` has 3 key methods:
 
-        - :meth:`datasets.DatasetBuilder.info`: Documents the dataset, including feature
+        - [`DatasetBuilder.info`]: Documents the dataset, including feature
           names, types, and shapes, version, splits, citation, etc.
-        - :meth:`datasets.DatasetBuilder.download_and_prepare`: Downloads the source data
+        - [`DatasetBuilder.download_and_prepare`]: Downloads the source data
           and writes it to disk.
-        - :meth:`datasets.DatasetBuilder.as_dataset`: Generates a `Dataset`.
+        - [`DatasetBuilder.as_dataset`]: Generates a [`Dataset`].
 
     **Configuration**: Some `DatasetBuilder`s expose multiple variants of the
-    dataset by defining a `datasets.BuilderConfig` subclass and accepting a
+    dataset by defining a [`BuilderConfig`] subclass and accepting a
     config object (or name) on construction. Configurable datasets expose a
-    pre-defined set of configurations in :meth:`datasets.DatasetBuilder.builder_configs`.
+    pre-defined set of configurations in [`DatasetBuilder.builder_configs`].
+
+    Args:
+        cache_dir (`str`, *optional*): Directory to cache data. Defaults to ``"~/.cache/huggingface/datasets"``.
+        config_name (`str`, *optional*): Name of the dataset configuration.
+            It affects the data generated on disk: different configurations will have their own subdirectories and
+            versions.
+            If not provided, the default configuration is used (if it exists).
+        hash (`str`, *optional*): Hash specific to the dataset code. Used to update the caching directory when the
+            dataset loading script code is updated (to avoid reusing old data).
+            The typical caching directory (defined in ``self._relative_data_dir``) is: ``name/version/hash/``.
+        base_path (`str`, *optional*): Base path for relative paths that are used to download files.
+            This can be a remote URL.
+        features ([`Features`], *optional*): Features types to use with this dataset.
+            It can be used to change the Features types of a dataset, for example.
+        use_auth_token (`str` or `bool`, *optional*): String or boolean to use as Bearer token for remote files on the
+            Datasets Hub. If `True`, will get token from ``"~/.huggingface"``.
+        repo_id (`str`, *optional*): ID of the dataset repository.
+            Used to distinguish builders with the same name but not coming from the same namespace, for example "squad"
+            and "lhoestq/squad" repo IDs. In the latter, the builder name would be "lhoestq___squad".
+        data_files (`str` or `Sequence` or `Mapping`, *optional*): Path(s) to source data file(s).
+            For builders like "csv" or "json" that need the user to specify data files. They can be either
+            local or remote files. For convenience, you can use a DataFilesDict.
+        data_dir (`str`, *optional*): Path to directory containing source data file(s).
+            Use only if `data_files` is not passed, in which case it is equivalent to passing
+            ``os.path.join(data_dir, "**")`` as `data_files`.
+            For builders that require manual download, it must be the path to the local directory containing the
+            manually downloaded data.
+        name (`str`): Deprecated. Use `config_name` instead.
+        **config_kwargs (additional keyword arguments): Keyword arguments to be passed to the corresponding builder
+            configuration class, set on the class attribute [`DatasetBuilder.BUILDER_CONFIG_CLASS`]. The builder
+            configuration class is [`BuilderConfig`] or a subclass of it.
     """
 
     # Default version
@@ -224,34 +255,6 @@ class DatasetBuilder:
         name="deprecated",
         **config_kwargs,
     ):
-        """Constructs a DatasetBuilder.
-
-        Callers must pass arguments as keyword arguments.
-
-        Args:
-            cache_dir: `str`, directory to read/write data. Defaults to "~/datasets".
-            config_name: `str` name, optional configuration for the dataset that affects the data generated on disk.
-                Different `builder_config`s will have their own subdirectories and versions.
-                If not provided it uses the default configuration, if it exists.
-            hash: a hash specific to the dataset code. Used to update the caching directory when the dataset loading
-                script code is updated (to avoid reusing old data).
-                The typical caching directory (defined in ``self._relative_data_dir``) is: ``name/version/hash/``
-            base_path: `str`, base path for relative paths that are used to download files. This can be a remote url.
-            features: `Features`, optional features that will be used to read/write the dataset
-                It can be used to changed the :obj:`datasets.Features` description of a dataset for example.
-            use_auth_token (:obj:`str` or :obj:`bool`, optional): Optional string or boolean to use as Bearer token
-                for remote files on the Datasets Hub. If True, will get token from ``"~/.huggingface"``.
-            repo_id: `str`, used to separate builders with the same name but not coming from the same namespace.
-                For example to separate repo_id "squad" from repo_id "lhoestq/squad".
-                In this case, the builder name would be "lhoestq___squad".
-            data_files: for builders like "csv" or "json" that need the user to specify data files. They can be either
-                local or remote files. For convenience you can use a DataFilesDict.
-            data_dir: `str`, for builders that require manual download. It must be the path to the local directory containing
-                the manually downloaded data.
-            config_kwargs: will override the defaults kwargs in config
-            name: Deprecated. Use 'config_name' instead.
-
-        """
         if name != "deprecated":
             warnings.warn(
                 "Parameter 'name' was renamed to 'config_name' in version 2.3.0 and will be removed in 3.0.0.",
