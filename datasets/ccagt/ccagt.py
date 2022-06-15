4c76cbbfb4e6b4a9f2f1408b0256b44684d625e8
João Gustavo Atkinson Amorim <joaogustavoamorim@gmail.com>
2022-06-15 12:01:30 -0300

    Revert "fix categories list"
    This reverts commit 0a2e11886e04c54460ea41711d28af642c4132d3.
    
    i commited the wrong files

--- datasets/ccagt/ccagt.py
+++ datasets/ccagt/ccagt.py
@@ -13,6 +13,7 @@ logger = datasets.logging.get_logger(__name__)
 
 CCAGT_CLASSES = OrderedDict(
     {
+        0: "BACKGROUND",
         1: "NUCLEUS",
         2: "CLUSTER",
         3: "SATELLITE",
@@ -165,7 +166,7 @@ class CCAgT(datasets.GeneratorBasedBuilder):
     DEFAULT_CONFIG_NAME = "semantic_segmentation"
 
     def _info(self):
-        assert len(CCAGT_CLASSES) == 7
+        assert len(CCAGT_CLASSES) == 8
 
         if self.config.name == "semantic_segmentation":
             features = datasets.Features(
