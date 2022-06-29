6c0ede96bc960b5aa40e8852a26141957b3ee9f3
YooSungHyun <shyu0522@hanmail.net>
2022-06-29 14:31:42 +0900

    Update tests/features/test_audio.py
    Co-authored-by: Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>

--- tests/features/test_audio.py
+++ tests/features/test_audio.py
@@ -154,7 +154,7 @@ def test_audio_decode_example_pcm(shared_datadir):
     decoded_example = audio.decode_example(audio.encode_example(audio_input))
     assert decoded_example.keys() == {"path", "array", "sampling_rate"}
     assert decoded_example["path"] is None
-    assert decoded_example["array"].shape == (16208,)
+    assert decoded_example["array"].shape == (16208 * sampling_rate // 16_000,)
     assert decoded_example["sampling_rate"] == 16000
 
 
