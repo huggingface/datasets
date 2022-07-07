b4e5a406fabb26ce2fed489a52acd17e48c6d2c0
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-07-07 11:56:06 +0200

    Fix _resolve_single_pattern_locally for Windows

--- src/datasets/data_files.py
+++ src/datasets/data_files.py
@@ -231,7 +231,7 @@ def _resolve_single_pattern_locally(
     if is_relative_path(pattern):
         pattern = os.path.join(base_path, pattern)
     else:
-        base_path = "/"
+        base_path = os.sep
     fs = LocalFileSystem()
     glob_iter = [PurePath(filepath) for filepath in fs.glob(pattern) if fs.isfile(filepath)]
     matched_paths = [
