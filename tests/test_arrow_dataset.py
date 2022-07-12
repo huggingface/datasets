f3b6697011cb6fc568b8f8b32f53501a8f2e8967
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-07-12 13:18:25 +0200

    Transfer CI to GitHub Actions (#4659)
    * Add CI GH Action test_with_pyarrow_latest
    
    * Trigger CI on push for testing
    
    * Install git OS dependency
    
    * Fix seqeval installation on Python 3.6
    
    * Do not install already present Git
    
    * Add CI GH Action test_with_pyarrow_6
    
    * Skip test_changed_dataset_card for testing
    
    * Add CI GH Action test_on_windows_with_pyarrow_latest
    
    * Skip test_changed_dataset_card for testing
    
    * Set env variables in the GH Action
    
    * Disable audio test
    
    * Add CI GH Action test_on_windows_with_pyarrow_6
    
    * Use strategy for OS
    
    * Fix _resolve_single_pattern_locally for Windows
    
    * Fix
    
    * Use strategy for PyArrow version
    
    * Revert fixing _resolve_single_pattern_locally for Windows
    
    * Revert trigger CI on push
    
    * Add CI GH Action check_code_quality
    
    * Make test dependent on check_code_quality
    
    * Use Python 3.7 on Windows
    
    * Fix _resolve_single_pattern_locally for Windows
    
    * Skip test_dummy_dataset_serialize_s3 on Windows when runned by GitHub Actions
    
    * Rename to GH Action Test to CI
    
    * Skip test_dummy_dataset_serialize_s3 also on DatasetDict
    
    * Clean file
    
    * Skip test_changed_dataset_card on GH Actions
    
    * Force CI re-run
    
    * Set checkout commit at PR head
    
    * Revert "Set checkout commit at PR head"
    
    This reverts commit b41592c3d5bdd7b3c08b4e4e9215392d644790db.
    
    * Checkout all history for all branches
    
    * Revert skip test_changed_dataset_card
    
    * Fix revert skip test_changed_dataset_card
    
    * Replace libsndfile1-dev with libsndfile1
    
    * Add sox as OS dependency on Linux
    
    * Remove fail-fast policy to allow merging in red
    
    * Remove old CircleCI
    
    * Remove old CircleCI deploy
    
    * Clean GH Actions workflows dir

--- tests/test_arrow_dataset.py
+++ tests/test_arrow_dataset.py
@@ -3119,9 +3119,9 @@ def test_pickle_dataset_after_transforming_the_table(in_memory, method_and_param
 
 
 @pytest.mark.skipif(
-    os.name == "nt" and os.getenv("CIRCLECI") == "true",
-    reason='On Windows CircleCI, it raises botocore.exceptions.EndpointConnectionError: Could not connect to the endpoint URL: "http://127.0.0.1:5555/test"',
-)  # TODO(QL): find what's wrong with CircleCI
+    os.name == "nt" and (os.getenv("CIRCLECI") == "true" or os.getenv("GITHUB_ACTIONS") == "true"),
+    reason='On Windows CircleCI or GitHub Actions, it raises botocore.exceptions.EndpointConnectionError: Could not connect to the endpoint URL: "http://127.0.0.1:5555/test"',
+)  # TODO: find what's wrong with CircleCI / GitHub Actions
 @require_s3
 def test_dummy_dataset_serialize_s3(s3, dataset):
     mock_bucket = s3_test_bucket_name
