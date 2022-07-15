db44fb88cd3b4f62a80a9c90ea7161d28992794b
mariosasko <mariosasko777@gmail.com>
2022-07-15 14:21:23 +0200

    Align logging with Transformers

--- src/datasets/utils/logging.py
+++ src/datasets/utils/logging.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # Copyright 2020 Optuna, Hugging Face
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,10 +12,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" Logging utilities. """
+""" Logging utilities."""
 
 import logging
 import os
+import sys
+import threading
 from logging import CRITICAL  # NOQA
 from logging import DEBUG  # NOQA
 from logging import ERROR  # NOQA
@@ -28,6 +31,9 @@ from typing import Optional
 from tqdm import auto as tqdm_lib
 
 
+_lock = threading.Lock()
+_default_handler: Optional[logging.Handler] = None
+
 log_levels = {
     "debug": logging.DEBUG,
     "info": logging.INFO,
@@ -38,11 +44,13 @@ log_levels = {
 
 _default_log_level = logging.WARNING
 
+_tqdm_active = True
+
 
 def _get_default_logging_level():
     """
-    If DATASETS_VERBOSITY env var is set to one of the valid choices return that as the new default level.
-    If it is not - fall back to ``_default_log_level``
+    If DATASETS_VERBOSITY env var is set to one of the valid choices return that as the new default level. If it is
+    not - fall back to `_default_log_level`
     """
     env_level_str = os.getenv("DATASETS_VERBOSITY", None)
     if env_level_str:
@@ -57,118 +65,198 @@ def _get_default_logging_level():
 
 
 def _get_library_name() -> str:
+
     return __name__.split(".")[0]
 
 
 def _get_library_root_logger() -> logging.Logger:
+
     return logging.getLogger(_get_library_name())
 
 
 def _configure_library_root_logger() -> None:
-    # Apply our default configuration to the library root logger.
-    library_root_logger = _get_library_root_logger()
-    library_root_logger.setLevel(_get_default_logging_level())
+
+    global _default_handler
+
+    with _lock:
+        if _default_handler:
+            # This library has already configured the library root logger.
+            return
+        _default_handler = logging.StreamHandler()  # Set sys.stderr as stream.
+        _default_handler.flush = sys.stderr.flush
+
+        # Apply our default configuration to the library root logger.
+        library_root_logger = _get_library_root_logger()
+        library_root_logger.addHandler(_default_handler)
+        library_root_logger.setLevel(_get_default_logging_level())
+        library_root_logger.propagate = False
 
 
 def _reset_library_root_logger() -> None:
-    library_root_logger = _get_library_root_logger()
-    library_root_logger.setLevel(logging.NOTSET)
+
+    global _default_handler
+
+    with _lock:
+        if not _default_handler:
+            return
+
+        library_root_logger = _get_library_root_logger()
+        library_root_logger.removeHandler(_default_handler)
+        library_root_logger.setLevel(logging.NOTSET)
+        _default_handler = None
+
+
+def get_log_levels_dict():
+    return log_levels
 
 
 def get_logger(name: Optional[str] = None) -> logging.Logger:
-    """Return a logger with the specified name.
-    This function can be used in dataset and metrics scripts.
     """
+    Return a logger with the specified name.
+    This function is not supposed to be directly accessed unless you are writing a custom datasets module.
+    """
+
     if name is None:
         name = _get_library_name()
+
+    _configure_library_root_logger()
     return logging.getLogger(name)
 
 
 def get_verbosity() -> int:
-    """Return the current level for the HuggingFace datasets library's root logger.
-    Returns:
-        Logging level, e.g., ``datasets.logging.DEBUG`` and ``datasets.logging.INFO``.
-
-    <Tip>
-
-        HuggingFace datasets library has following logging levels:
-        - `datasets.logging.CRITICAL`, `datasets.logging.FATAL`
-        - `datasets.logging.ERROR`
-        - `datasets.logging.WARNING`, `datasets.logging.WARN`
-        - `datasets.logging.INFO`
-        - `datasets.logging.DEBUG`
-
-    </Tip>
     """
+    Return the current level for the 🤗 Datasets' root logger as an int.
+    Returns:
+        `int`: The logging level.
+    <Tip>
+    🤗 Datasets has following logging levels:
+    - 50: `datasets.logging.CRITICAL` or `datasets.logging.FATAL`
+    - 40: `datasets.logging.ERROR`
+    - 30: `datasets.logging.WARNING` or `datasets.logging.WARN`
+    - 20: `datasets.logging.INFO`
+    - 10: `datasets.logging.DEBUG`
+    </Tip>"""
+
+    _configure_library_root_logger()
     return _get_library_root_logger().getEffectiveLevel()
 
 
 def set_verbosity(verbosity: int) -> None:
-    """Set the level for the HuggingFace datasets library's root logger.
-    Args:
-        verbosity:
-            Logging level, e.g., ``datasets.logging.DEBUG`` and ``datasets.logging.INFO``.
     """
+    Set the verbosity level for the 🤗 Datasets' root logger.
+    Args:
+        verbosity (`int`):
+            Logging level, e.g., one of:
+            - `datasets.logging.CRITICAL` or `datasets.logging.FATAL`
+            - `datasets.logging.ERROR`
+            - `datasets.logging.WARNING` or `datasets.logging.WARN`
+            - `datasets.logging.INFO`
+            - `datasets.logging.DEBUG`
+    """
+
+    _configure_library_root_logger()
     _get_library_root_logger().setLevel(verbosity)
 
 
 def set_verbosity_info():
-    """Set the level for the HuggingFace datasets library's root logger to INFO.
-
-    This will display most of the logging information and tqdm bars.
-
-    Shortcut to ``datasets.logging.set_verbosity(datasets.logging.INFO)``
-    """
+    """Set the verbosity to the `INFO` level."""
     return set_verbosity(INFO)
 
 
 def set_verbosity_warning():
-    """Set the level for the HuggingFace datasets library's root logger to WARNING.
-
-    This will display only the warning and errors logging information and tqdm bars.
-
-    Shortcut to ``datasets.logging.set_verbosity(datasets.logging.WARNING)``
-    """
+    """Set the verbosity to the `WARNING` level."""
     return set_verbosity(WARNING)
 
 
 def set_verbosity_debug():
-    """Set the level for the HuggingFace datasets library's root logger to DEBUG.
-
-    This will display all the logging information and tqdm bars.
-
-    Shortcut to ``datasets.logging.set_verbosity(datasets.logging.DEBUG)``
-    """
+    """Set the verbosity to the `DEBUG` level."""
     return set_verbosity(DEBUG)
 
 
 def set_verbosity_error():
-    """Set the level for the HuggingFace datasets library's root logger to ERROR.
-
-    This will display only the errors logging information and tqdm bars.
-
-    Shortcut to ``datasets.logging.set_verbosity(datasets.logging.ERROR)``
-    """
+    """Set the verbosity to the `ERROR` level."""
     return set_verbosity(ERROR)
 
 
+def disable_default_handler() -> None:
+    """Disable the default handler of the 🤗 Datasets' root logger."""
+
+    _configure_library_root_logger()
+
+    assert _default_handler is not None
+    _get_library_root_logger().removeHandler(_default_handler)
+
+
+def enable_default_handler() -> None:
+    """Enable the default handler of the 🤗 Datasets' root logger."""
+
+    _configure_library_root_logger()
+
+    assert _default_handler is not None
+    _get_library_root_logger().addHandler(_default_handler)
+
+
+def add_handler(handler: logging.Handler) -> None:
+    """adds a handler to the 🤗 Datasets' root logger."""
+
+    _configure_library_root_logger()
+
+    assert handler is not None
+    _get_library_root_logger().addHandler(handler)
+
+
+def remove_handler(handler: logging.Handler) -> None:
+    """removes given handler from the 🤗 Datasets' root logger."""
+
+    _configure_library_root_logger()
+
+    assert handler is not None and handler not in _get_library_root_logger().handlers
+    _get_library_root_logger().removeHandler(handler)
+
+
 def disable_propagation() -> None:
-    """Disable propagation of the library log outputs.
-    Note that log propagation is disabled by default.
     """
+    Disable propagation of the library log outputs. Note that log propagation is disabled by default.
+    """
+
+    _configure_library_root_logger()
     _get_library_root_logger().propagate = False
 
 
 def enable_propagation() -> None:
-    """Enable propagation of the library log outputs.
-    Please disable the HuggingFace datasets library's default handler to prevent double logging if the root logger has
-    been configured.
     """
+    Enable propagation of the library log outputs. Please disable the 🤗 Datasets' default handler to
+    prevent double logging if the root logger has been configured.
+    """
+
+    _configure_library_root_logger()
     _get_library_root_logger().propagate = True
 
 
-# Configure the library root logger at the module level (singleton-like)
-_configure_library_root_logger()
+def enable_explicit_format() -> None:
+    """
+    Enable explicit formatting for every 🤗 Datasets' logger. The explicit formatter is as follows:
+    ```
+        [LEVELNAME|FILENAME|LINE NUMBER] TIME >> MESSAGE
+    ```
+    All handlers currently bound to the root logger are affected by this method.
+    """
+    handlers = _get_library_root_logger().handlers
+
+    for handler in handlers:
+        formatter = logging.Formatter("[%(levelname)s|%(filename)s:%(lineno)s] %(asctime)s >> %(message)s")
+        handler.setFormatter(formatter)
+
+
+def reset_format() -> None:
+    """
+    Resets the formatting for 🤗 Datasets' loggers.
+    All handlers currently bound to the root logger are affected by this method.
+    """
+    handlers = _get_library_root_logger().handlers
+
+    for handler in handlers:
+        handler.setFormatter(None)
 
 
 class EmptyTqdm:
