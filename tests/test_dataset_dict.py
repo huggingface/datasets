4e4222f1b6362c2788aec0dd2cd8cede6dd17b80
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-07-21 16:24:18 +0200

    Refactor conftest fixtures (#4723)
    * Move to directory and rename fixture modules
    
    * Import fixture modules as plugins
    
    * Move file fixtures to dedicated module
    
    * Fix style
    
    * Fix bad import from conftest
    
    * Fix image_file fixture

--- tests/test_dataset_dict.py
+++ tests/test_dataset_dict.py
@@ -12,7 +12,6 @@ from datasets.dataset_dict import DatasetDict
 from datasets.features import ClassLabel, Features, Sequence, Value
 from datasets.splits import NamedSplit
 
-from .conftest import s3_test_bucket_name
 from .utils import (
     assert_arrow_memory_doesnt_increase,
     assert_arrow_memory_increases,
@@ -669,7 +668,7 @@ def test_datasetdict_from_text_split(split, text_path, tmp_path):
     reason='On Windows CircleCI or GitHub Actions, it raises botocore.exceptions.EndpointConnectionError: Could not connect to the endpoint URL: "http://127.0.0.1:5555/test"',
 )  # TODO: find what's wrong with CircleCI / GitHub Actions
 @require_s3
-def test_dummy_dataset_serialize_s3(s3, dataset):
+def test_dummy_dataset_serialize_s3(s3, dataset, s3_test_bucket_name):
     dsets = DatasetDict({"train": dataset, "test": dataset.select(range(2))})
     mock_bucket = s3_test_bucket_name
     dataset_path = f"s3://{mock_bucket}/datasets/dict"
