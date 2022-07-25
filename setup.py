929c228dd5edd610e71b44f63cc511202fdd310e
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-07-25 16:22:52 +0200

    Add rouge_score test requirement needed by bigbench

--- setup.py
+++ setup.py
@@ -146,6 +146,7 @@ TESTS_REQUIRE = [
     "zstandard",
     "bigbench @ https://storage.googleapis.com/public_research_data/bigbench/bigbench-0.0.1.tar.gz",
     "sentencepiece",  # bigbench requires t5 which requires seqio which requires sentencepiece
+    "rouge_score<0.0.7",  # required by bigbench: bigbench.api.util.bb_utils > t5.evaluation.metrics > rouge_score
     "sacremoses",
     # to speed up pip backtracking
     "toml>=0.10.1",
