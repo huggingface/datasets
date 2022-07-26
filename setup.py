feebd9048ac42e756155cf221e7364fe2dee6845
mariosasko <mariosasko777@gmail.com>
2022-07-26 14:11:49 +0200

    Try to fix s3 errors

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ TESTS_REQUIRE = [
     "botocore>=1.22.8",  # to be compatible with aiobotocore and boto3
     "faiss-cpu>=1.6.4",
     "fsspec[s3]",
-    "moto[s3,server]==2.0.4",
+    "moto[s3,server]>=3.0.0",
     "rarfile>=4.0",
     "s3fs>=2021.11.1",  # aligned with fsspec[http]>=2021.11.1
     "tensorflow>=2.3,!=2.6.0,!=2.6.1",
--- tests/test_dataset_dict.py
+++ tests/test_dataset_dict.py
@@ -663,10 +663,10 @@ def test_datasetdict_from_text_split(split, text_path, tmp_path):
     assert all(dataset[split].split == split for split in path.keys())
 
 
-@pytest.mark.skipif(
-    os.name in ["nt", "posix"] and (os.getenv("CIRCLECI") == "true" or os.getenv("GITHUB_ACTIONS") == "true"),
-    reason='On Windows CircleCI or GitHub Actions, it raises botocore.exceptions.EndpointConnectionError: Could not connect to the endpoint URL: "http://127.0.0.1:5555/test"',
-)  # TODO: find what's wrong with CircleCI / GitHub Actions
+# @pytest.mark.skipif(
+#     os.name in ["nt", "posix"] and (os.getenv("CIRCLECI") == "true" or os.getenv("GITHUB_ACTIONS") == "true"),
+#     reason='On Windows CircleCI or GitHub Actions, it raises botocore.exceptions.EndpointConnectionError: Could not connect to the endpoint URL: "http://127.0.0.1:5555/test"',
+# )  # TODO: find what's wrong with CircleCI / GitHub Actions
 @require_s3
 def test_dummy_dataset_serialize_s3(s3, dataset, s3_test_bucket_name):
     dsets = DatasetDict({"train": dataset, "test": dataset.select(range(2))})
