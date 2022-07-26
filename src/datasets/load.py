8ce323eacfbd776f217ca4ca8bfadb56663abfe5
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-07-26 22:57:05 +0200

    Remove repeated comment

--- src/datasets/load.py
+++ src/datasets/load.py
@@ -1,5 +1,4 @@
 # Copyright 2020 The HuggingFace Datasets Authors and the TensorFlow Datasets Authors.
-# Copyright 2020 The HuggingFace Datasets Authors and the TensorFlow Datasets Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
