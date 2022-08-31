740f1800be3ea5b272722580244d1f4257d3c1eb
Paul Lerner <lerner@tuta.io>
2022-08-31 16:38:09 +0200

    feat: improve error message on Keys mismatch. closes #4917

--- src/datasets/features/features.py
+++ src/datasets/features/features.py
@@ -1755,7 +1755,11 @@ class Features(dict):
                 if not isinstance(target, dict):
                     raise ValueError(f"Type mismatch: between {source} and {target}" + stack_position)
                 if sorted(source) != sorted(target):
-                    raise ValueError(f"Keys mismatch: between {source} and {target}" + stack_position)
+                    message = (f"Keys mismatch: between {source} (dataset_info.json) "
+                               f"and {target} (inferred from dataset.arrow).\n"
+                               f"{source.keys()-target.keys()} are missing from dataset.arrow "
+                               f"and {target.keys()-source.keys()} are missing from dataset_info.json"+stack_position)
+                    raise ValueError(message)
                 return {key: recursive_reorder(source[key], target[key], stack + f".{key}") for key in target}
             elif isinstance(source, list):
                 if not isinstance(target, list):
