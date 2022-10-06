af4bd1fa0bc2e8c91f839aeec1c9048af713d9f4
Quentin Lhoest <lhoest.q@gmail.com>
2022-10-06 19:06:12 +0200

    fix torch_iterable_dataset

--- src/datasets/formatting/dataset_wrappers/torch_iterable_dataset.py
+++ src/datasets/formatting/dataset_wrappers/torch_iterable_dataset.py
@@ -1,6 +1,7 @@
 import fsspec.asyn
 import torch
 
+from ...formatting import get_formatter
 from ...iterable_dataset import IterableDataset
 from ...utils.logging import get_logger
 
@@ -44,17 +45,23 @@ class TorchIterableDataset(IterableDataset, torch.utils.data.IterableDataset):
                 logger.debug(
                     f"dataloader worker#{worker_info.id}, ': Starting to iterate over {len(shards_indices)}/{self.n_shards} shards."
                 )
+                format_kwargs = self._format_kwargs if self._format_kwargs is not None else {}
+                features = self._resolve_features().features
+                formatter = get_formatter(self._format_type, features=features, **format_kwargs)
                 for shard_idx in shards_indices:
                     for key, example in self._iter_shard(shard_idx):
-                        if self.features:
-                            # we encode the example for ClassLabel feature types for example
-                            # this also adds the missing columns
-                            example = self.features.encode_example(example)
-                            # Decode example for Audio feature, e.g.
-                            example = self.features.decode_example(example, token_per_repo_id=self.token_per_repo_id)
-                            yield example
-                        else:
-                            yield example
+                        example = features.encode_example(example)
+                        formatted_example = formatter.format_example(
+                            {
+                                column_name: example[column_name]
+                                for column_name in (example if self._format_columns is None else self._format_columns)
+                            }
+                        )
+                        if self._output_all_columns:
+                            for column_name in example:
+                                if column_name not in formatted_example:
+                                    formatted_example[column_name] = example[column_name]
+                        yield formatted_example
                 logger.debug(
                     f"dataloader worker#{worker_info.id}, ': Finished iterating over {len(shards_indices)}/{self.n_shards} shards."
                 )
