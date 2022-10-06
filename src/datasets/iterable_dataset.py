7ca760c7cef526aec63d6d27e2ab029eff002285
Quentin Lhoest <lhoest.q@gmail.com>
2022-10-06 19:07:26 +0200

    fix tests

--- src/datasets/iterable_dataset.py
+++ src/datasets/iterable_dataset.py
@@ -743,7 +743,7 @@ class IterableDataset(DatasetInfoMixin):
         return {
             "type": self._format_type,
             "format_kwargs": self._format_kwargs,
-            "columns": self.column_names if self._format_columns is None else self._format_columns,
+            "columns": self._format_columns,
             "output_all_columns": self._output_all_columns,
         }
 
