99680a7b5946d41873d84e16b30a2587b1f935fa
Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>
2022-10-13 11:56:50 +0200

    Fix task template reload from dict (#5106)
    fix task template reload from dict

--- src/datasets/tasks/summarization.py
+++ src/datasets/tasks/summarization.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import ClassVar, Dict
 
 from ..features import Features, Value
@@ -8,7 +8,7 @@ from .base import TaskTemplate
 @dataclass(frozen=True)
 class Summarization(TaskTemplate):
     # `task` is not a ClassVar since we want it to be part of the `asdict` output for JSON serialization
-    task: str = "summarization"
+    task: str = field(default="summarization", metadata={"include_in_asdict_even_if_is_default": True})
     input_schema: ClassVar[Features] = Features({"text": Value("string")})
     label_schema: ClassVar[Features] = Features({"summary": Value("string")})
     text_column: str = "text"
