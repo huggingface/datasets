4ac0c1b3e9bfb57218eb67be59d5cfc587e3e136
Quentin Lhoest <lhoest.q@gmail.com>
2022-10-14 16:03:43 +0200

    style

--- src/datasets/table.py
+++ src/datasets/table.py
@@ -2152,7 +2152,7 @@ def table_visitor(table: pa.Table, function: Callable[[pa.Array], None]):
 
 def table_iter(pa_table: pa.Table, batch_size: int, drop_last_batch=False):
     """Iterate ober sub-tables of size `batch_size`.
-    
+
     Requires pyarrow>=8.0.0
 
     Args:
