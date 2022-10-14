391dd869b88ec9b0588f3bd85a0ecdfaa3255847
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-10-14 13:48:31 +0200

    Rename test

--- tests/test_arrow_dataset.py
+++ tests/test_arrow_dataset.py
@@ -3081,7 +3081,7 @@ def test_dataset_add_item_introduce_feature_type():
     assert dataset[:] == {"col_1": [None, None, None, "a"]}
 
 
-def test_dataset_filter_indices():
+def test_dataset_filter_batched_indices():
     ds = Dataset.from_dict({"num": [0, 1, 2, 3]})
     ds = ds.filter(lambda num: num % 2 == 0, input_columns="num", batch_size=2)
     assert all(item["num"] % 2 == 0 for item in ds)
