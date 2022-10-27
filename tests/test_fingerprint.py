cdabf8a05ecceacf944ad3f2c3c9e5d824c154d8
gpucce <g.puccetti92@gmail.com>
2022-10-27 16:48:58 +0200

    better test names

--- tests/test_fingerprint.py
+++ tests/test_fingerprint.py
@@ -209,13 +209,13 @@ class RecurseDumpTest(TestCase):
         self.assertNotEqual(hash1, hash2)
         
         co_filename, returned_obj = f"{tempfile}/ipykernel_12345/321456789.py", [0]
-        hash1 = md5(datasets.utils.py_utils.dumps(create_ipython_func(co_filename, returned_obj))).hexdigest()
+        hash4 = md5(datasets.utils.py_utils.dumps(create_ipython_func(co_filename, returned_obj))).hexdigest()
         co_filename, returned_obj = f"{tempfile}/ipykernel_12345/321456789.py", [1]
-        hash2 = md5(datasets.utils.py_utils.dumps(create_ipython_func(co_filename, returned_obj))).hexdigest()
+        hash5 = md5(datasets.utils.py_utils.dumps(create_ipython_func(co_filename, returned_obj))).hexdigest()
         co_filename, returned_obj = f"{tempfile}/ipykernel_12345/654123987.py", [0]
-        hash3 = md5(datasets.utils.py_utils.dumps(create_ipython_func(co_filename, returned_obj))).hexdigest()
-        self.assertEqual(hash1, hash3)
-        self.assertNotEqual(hash1, hash2)
+        hash6 = md5(datasets.utils.py_utils.dumps(create_ipython_func(co_filename, returned_obj))).hexdigest()
+        self.assertEqual(hash4, hash6)
+        self.assertNotEqual(hash4, hash5)
         
         
     def test_dump_jupyter_function(self):
