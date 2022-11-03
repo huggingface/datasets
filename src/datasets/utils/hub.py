b955873646052909db7936efc9b4c6465b478cda
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-11-03 13:05:24 +0100

    Make args positional-or-keyword

--- src/datasets/utils/hub.py
+++ src/datasets/utils/hub.py
@@ -6,6 +6,6 @@ from huggingface_hub import hf_hub_url as _hf_hub_url
 from datasets import config
 
 
-def hf_hub_url(*, repo_id: str, path: str, revision: Optional[str] = None) -> str:
+def hf_hub_url(repo_id: str, path: str, revision: Optional[str] = None) -> str:
     revision = revision or config.HUB_DEFAULT_VERSION
     return _hf_hub_url(repo_id, quote(path), repo_type="dataset", revision=revision)
