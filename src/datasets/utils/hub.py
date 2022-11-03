d19886273eecb25e4526f8fdd820e23cc61d047a
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-11-03 15:18:27 +0100

    Align URLs at the level of the used module

--- src/datasets/utils/hub.py
+++ src/datasets/utils/hub.py
@@ -1,14 +1,14 @@
 from typing import Optional
 from urllib.parse import quote
 
-import huggingface_hub as hfh
+import huggingface_hub.file_download
 
 from datasets import config
 
 
 def hf_hub_url(repo_id: str, path: str, revision: Optional[str] = None) -> str:
     revision = revision or config.HUB_DEFAULT_VERSION
-    hfh.constants.HUGGINGFACE_CO_URL_TEMPLATE = config.HUB_DATASETS_URL.replace(
+    huggingface_hub.file_download.HUGGINGFACE_CO_URL_TEMPLATE = config.HUB_DATASETS_URL.replace(
         "/datasets/{repo_id}/resolve/{revision}/{path}", "/{repo_id}/resolve/{revision}/{filename}"
     )
-    return hfh.hf_hub_url(repo_id, quote(path), repo_type="dataset", revision=revision)
+    return huggingface_hub.file_download.hf_hub_url(repo_id, quote(path), repo_type="dataset", revision=revision)
