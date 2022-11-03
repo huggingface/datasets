196f0dddcd3f53ed7252e9eca8a73107b654e444
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-11-03 11:03:58 +0100

    Use hfh hf_hub_url

--- src/datasets/utils/hub.py
+++ src/datasets/utils/hub.py
@@ -1,9 +1,11 @@
 from typing import Optional
 from urllib.parse import quote
 
+from huggingface_hub import hf_hub_url as _hf_hub_url
+
 from datasets import config
 
 
-def hf_hub_url(repo_id: str, path: str, revision: Optional[str] = None) -> str:
+def hf_hub_url(*, repo_id: str, path: str, revision: Optional[str] = None) -> str:
     revision = revision or config.HUB_DEFAULT_VERSION
-    return config.HUB_DATASETS_URL.format(repo_id=repo_id, path=quote(path), revision=quote(revision))
+    return _hf_hub_url(repo_id, quote(path), repo_type="dataset", revision=revision)
