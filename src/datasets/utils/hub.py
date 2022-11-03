6c0cd6cdad987c05fd938818a98c515f3752b1a5
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-11-03 14:27:45 +0100

    Align hfh and datasets URL for datasets

--- src/datasets/utils/hub.py
+++ src/datasets/utils/hub.py
@@ -1,11 +1,14 @@
 from typing import Optional
 from urllib.parse import quote
 
-from huggingface_hub import hf_hub_url as _hf_hub_url
+import huggingface_hub as hfh
 
 from datasets import config
 
 
 def hf_hub_url(repo_id: str, path: str, revision: Optional[str] = None) -> str:
     revision = revision or config.HUB_DEFAULT_VERSION
-    return _hf_hub_url(repo_id, quote(path), repo_type="dataset", revision=revision)
+    hfh.constants.HUGGINGFACE_CO_URL_TEMPLATE = config.HUB_DATASETS_URL.replace(
+        "/datasets/{repo_id}/resolve/{revision}/{path}", "/{repo_id}/resolve/{revision}/{filename}"
+    )
+    return hfh.hf_hub_url(repo_id, quote(path), repo_type="dataset", revision=revision)
