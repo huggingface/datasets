4aeec04fb0e19dbec97e12fa837cceaaf85a38c6
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-11-03 17:08:05 +0100

    Remove URL alignment in hf_hub_url

--- src/datasets/utils/hub.py
+++ src/datasets/utils/hub.py
@@ -8,7 +8,4 @@ from datasets import config
 
 def hf_hub_url(repo_id: str, path: str, revision: Optional[str] = None) -> str:
     revision = revision or config.HUB_DEFAULT_VERSION
-    huggingface_hub.file_download.HUGGINGFACE_CO_URL_TEMPLATE = config.HUB_DATASETS_URL.replace(
-        "/datasets/{repo_id}/resolve/{revision}/{path}", "/{repo_id}/resolve/{revision}/{filename}"
-    )
     return huggingface_hub.file_download.hf_hub_url(repo_id, quote(path), repo_type="dataset", revision=revision)
