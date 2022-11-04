a3f1bc9eb0216739adcf859c8cc6c1f2449aeb33
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-11-04 16:03:07 +0100

    Make temporary alignment of URLs

--- src/datasets/utils/hub.py
+++ src/datasets/utils/hub.py
@@ -3,9 +3,13 @@ from urllib.parse import quote
 
 import huggingface_hub as hfh
 
-from datasets import config
+from .. import config
+from .py_utils import temporary_assignment
 
 
+@temporary_assignment(
+    hfh.constants, "HUGGINGFACE_CO_URL_TEMPLATE", config.HUB_DATASETS_URL.replace("{path}", "{filename}")
+)
 def hf_hub_url(repo_id: str, path: str, revision: Optional[str] = None) -> str:
     revision = revision or config.HUB_DEFAULT_VERSION
     return hfh.hf_hub_url(repo_id, quote(path), repo_type="dataset", revision=revision)
