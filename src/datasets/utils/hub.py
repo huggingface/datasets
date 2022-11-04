43213c3938cfe542c2a5cf0168db1a2f18d8a648
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-11-04 11:14:00 +0100

    Refactor import of hfh

--- src/datasets/utils/hub.py
+++ src/datasets/utils/hub.py
@@ -1,11 +1,11 @@
 from typing import Optional
 from urllib.parse import quote
 
-import huggingface_hub.file_download
+import huggingface_hub as hfh
 
 from datasets import config
 
 
 def hf_hub_url(repo_id: str, path: str, revision: Optional[str] = None) -> str:
     revision = revision or config.HUB_DEFAULT_VERSION
-    return huggingface_hub.file_download.hf_hub_url(repo_id, quote(path), repo_type="dataset", revision=revision)
+    return hfh.hf_hub_url(repo_id, quote(path), repo_type="dataset", revision=revision)
