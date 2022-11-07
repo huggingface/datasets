8cf4d933e3c0138b4e5f6ca0d9137d8b5be4dd96
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-11-07 09:50:01 +0100

    Move temporary assignment at call time

--- src/datasets/utils/hub.py
+++ src/datasets/utils/hub.py
@@ -7,12 +7,12 @@ from .. import config
 from .py_utils import temporary_assignment
 
 
-@temporary_assignment(
-    huggingface_hub.file_download,
-    "HUGGINGFACE_CO_URL_TEMPLATE",
-    config.HUB_DATASETS_URL.replace("{path}", "{filename}"),
-)
 def hf_hub_url(repo_id: str, path: str, revision: Optional[str] = None) -> str:
     revision = revision or config.HUB_DEFAULT_VERSION
-    # As config.HUB_DATASETS_URL contains "/datasets/", no need to pass repo_type="dataset"
-    return huggingface_hub.file_download.hf_hub_url(repo_id, quote(path), revision=revision)
+    with temporary_assignment(
+        huggingface_hub.file_download,
+        "HUGGINGFACE_CO_URL_TEMPLATE",
+        config.HUB_DATASETS_URL.replace("{path}", "{filename}"),
+    ):
+        # As config.HUB_DATASETS_URL contains "/datasets/", no need to pass repo_type="dataset"
+        return huggingface_hub.file_download.hf_hub_url(repo_id, quote(path), revision=revision)
