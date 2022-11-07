625fb60c58f994de9756802ff3f45ab82becd782
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-11-07 10:23:12 +0100

    Refactor CI fixtures to use monkeypatch instead of patch

--- tests/fixtures/hub.py
+++ tests/fixtures/hub.py
@@ -97,10 +97,8 @@ def hf_private_dataset_repo_txt_data_(hf_api: HfApi, hf_token, text_file):
 
 
 @pytest.fixture()
-def hf_private_dataset_repo_txt_data(hf_private_dataset_repo_txt_data_):
-    with patch("datasets.config.HF_ENDPOINT", CI_HUB_ENDPOINT):
-        with patch("datasets.config.HUB_DATASETS_URL", CI_HUB_DATASETS_URL):
-            yield hf_private_dataset_repo_txt_data_
+def hf_private_dataset_repo_txt_data(hf_private_dataset_repo_txt_data_, ci_hub_config):
+    return hf_private_dataset_repo_txt_data_
 
 
 @pytest.fixture(scope="session")
@@ -123,10 +121,8 @@ def hf_private_dataset_repo_zipped_txt_data_(hf_api: HfApi, hf_token, zip_csv_wi
 
 
 @pytest.fixture()
-def hf_private_dataset_repo_zipped_txt_data(hf_private_dataset_repo_zipped_txt_data_):
-    with patch("datasets.config.HF_ENDPOINT", CI_HUB_ENDPOINT):
-        with patch("datasets.config.HUB_DATASETS_URL", CI_HUB_DATASETS_URL):
-            yield hf_private_dataset_repo_zipped_txt_data_
+def hf_private_dataset_repo_zipped_txt_data(hf_private_dataset_repo_zipped_txt_data_, ci_hub_config):
+    return hf_private_dataset_repo_zipped_txt_data_
 
 
 @pytest.fixture(scope="session")
@@ -149,7 +145,5 @@ def hf_private_dataset_repo_zipped_img_data_(hf_api: HfApi, hf_token, zip_image_
 
 
 @pytest.fixture()
-def hf_private_dataset_repo_zipped_img_data(hf_private_dataset_repo_zipped_img_data_):
-    with patch("datasets.config.HF_ENDPOINT", CI_HUB_ENDPOINT):
-        with patch("datasets.config.HUB_DATASETS_URL", CI_HUB_DATASETS_URL):
-            yield hf_private_dataset_repo_zipped_img_data_
+def hf_private_dataset_repo_zipped_img_data(hf_private_dataset_repo_zipped_img_data_, ci_hub_config):
+    return hf_private_dataset_repo_zipped_img_data_
