5a7e33657374fab9b24ce34b38b42e864868dc5e
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2022-11-08 17:05:32 +0100

    Revert using datasets config URL

--- src/datasets/utils/hub.py
+++ src/datasets/utils/hub.py
@@ -1,18 +1,8 @@
 from typing import Optional
 from urllib.parse import quote
 
-import huggingface_hub.file_download
-
-from .. import config
-from .py_utils import temporary_assignment
+import huggingface_hub as hfh
 
 
 def hf_hub_url(repo_id: str, path: str, revision: Optional[str] = None) -> str:
-    revision = revision or config.HUB_DEFAULT_VERSION
-    with temporary_assignment(
-        huggingface_hub.file_download,
-        "HUGGINGFACE_CO_URL_TEMPLATE",
-        config.HUB_DATASETS_URL.replace("{path}", "{filename}"),
-    ):
-        # As config.HUB_DATASETS_URL contains "/datasets/", no need to pass repo_type="dataset"
-        return huggingface_hub.file_download.hf_hub_url(repo_id, quote(path), revision=revision)
+    return hfh.hf_hub_url(repo_id, quote(path), repo_type="dataset", revision=revision)
