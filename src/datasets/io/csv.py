0463d3c1808a1a66208370610a7f4c4269755433
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2023-01-05 10:03:33 +0100

    Fix deprecation of use_auth_token

--- src/datasets/io/csv.py
+++ src/datasets/io/csv.py
@@ -49,7 +49,6 @@ class CsvDatasetReader(AbstractDatasetReader):
             download_config = None
             download_mode = None
             ignore_verifications = False
-            use_auth_token = None
             base_path = None
 
             self.builder.download_and_prepare(
@@ -58,7 +57,6 @@ class CsvDatasetReader(AbstractDatasetReader):
                 ignore_verifications=ignore_verifications,
                 # try_from_hf_gcs=try_from_hf_gcs,
                 base_path=base_path,
-                use_auth_token=use_auth_token,
                 num_proc=self.num_proc,
             )
             dataset = self.builder.as_dataset(
--- src/datasets/io/generator.py
+++ src/datasets/io/generator.py
@@ -42,7 +42,6 @@ class GeneratorDatasetInputStream(AbstractDatasetInputStream):
             download_config = None
             download_mode = None
             ignore_verifications = False
-            use_auth_token = None
             base_path = None
 
             self.builder.download_and_prepare(
@@ -51,7 +50,6 @@ class GeneratorDatasetInputStream(AbstractDatasetInputStream):
                 ignore_verifications=ignore_verifications,
                 # try_from_hf_gcs=try_from_hf_gcs,
                 base_path=base_path,
-                use_auth_token=use_auth_token,
                 num_proc=self.num_proc,
             )
             dataset = self.builder.as_dataset(
