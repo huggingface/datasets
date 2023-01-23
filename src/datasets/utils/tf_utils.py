e2f8c09655169b2f2cffa2e8ef288c490ccae099
Matt <rocketknight1@gmail.com>
2023-01-23 18:09:26 +0000

    Add docstring for dataset_to_tf and remove debug prints when generating shm arrays

--- src/datasets/utils/tf_utils.py
+++ src/datasets/utils/tf_utils.py
@@ -130,6 +130,30 @@ def dataset_to_tf(
     batch_size,
     drop_remainder,
 ):
+    """Create a tf.data.Dataset from the underlying Dataset. This is a single-process method - the multiprocess
+    equivalent is multiprocess_dataset_to_tf.
+
+            Args:
+                dataset (:obj:`ArrowDataset`): Dataset to wrap with tf.data.Dataset.
+                cols_to_retain (:obj:`List[str]`): Dataset column(s) to load in the
+                    tf.data.Dataset. It is acceptable to include column names that are created by the `collate_fn` and
+                    that do not exist in the original dataset.
+                collate_fn(:obj:`Callable`): A function or callable object (such as a `DataCollator`) that will collate
+                    lists of samples into a batch.
+                collate_fn_args (:obj:`Dict`): A  `dict` of keyword arguments to be passed to the
+                    `collate_fn`. Can be empty.
+                columns_to_np_types (:obj:`Dict[str, np.dtype]`): A `dict` mapping column names to numpy dtypes.
+                output_signature (:obj:`Dict[str, tf.TensorSpec]`): A `dict` mapping column names to
+                    `tf.TensorSpec` objects.
+                shuffle(:obj:`bool`): Shuffle the dataset order when loading. Recommended True for training, False for
+                    validation/evaluation.
+                batch_size (:obj:`int`): Size of batches to load from the dataset.
+                drop_remainder(:obj:`bool`, default ``None``): Drop the last incomplete batch when loading. If not provided,
+                    defaults to the same setting as shuffle.
+
+            Returns:
+                :class:`tf.data.Dataset`
+            """
     if config.TF_AVAILABLE:
         import tensorflow as tf
     else:
@@ -186,12 +210,6 @@ class SharedMemoryContext:
         return shm
 
     def get_array(self, name, shape, dtype, create):
-        if create:
-            print(f"Making array {name} with shape {shape} and dtype {dtype} and itemsize {np.dtype(dtype).itemsize}!")
-        else:
-            print(
-                f"Opening array {name} with shape {shape} and dtype {dtype} and itemsize {np.dtype(dtype).itemsize}!"
-            )
         shm = self.get_shm(name=name, size=np.prod(shape) * np.dtype(dtype).itemsize, create=create)
         return np.ndarray(shape, dtype=dtype, buffer=shm.buf)
 
