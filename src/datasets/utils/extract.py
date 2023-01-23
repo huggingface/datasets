6681c36bbaae9b8b1daa3dbbd4a96b35aaae271b
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2023-01-23 09:31:17 +0100

    Swap log messages for symbolic/hard links in tar extractor (#5452)

--- src/datasets/utils/extract.py
+++ src/datasets/utils/extract.py
@@ -113,9 +113,9 @@ class TarExtractor(BaseExtractor):
             if badpath(finfo.name, base):
                 logger.error(f"Extraction of {finfo.name} is blocked (illegal path)")
             elif finfo.issym() and badlink(finfo, base):
-                logger.error(f"Extraction of {finfo.name} is blocked: Hard link to {finfo.linkname}")
-            elif finfo.islnk() and badlink(finfo, base):
                 logger.error(f"Extraction of {finfo.name} is blocked: Symlink to {finfo.linkname}")
+            elif finfo.islnk() and badlink(finfo, base):
+                logger.error(f"Extraction of {finfo.name} is blocked: Hard link to {finfo.linkname}")
             else:
                 yield finfo
 
