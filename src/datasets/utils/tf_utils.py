29fa15df972353f51fc434cf8eceb574b60a415f
Matt <rocketknight1@gmail.com>
2023-01-24 14:53:25 +0000

    Add docstring for multiprocess_dataset_to_tf

--- src/datasets/utils/tf_utils.py
+++ src/datasets/utils/tf_utils.py
@@ -476,6 +476,31 @@ def multiprocess_dataset_to_tf(
     drop_remainder,
     num_workers,
 ):
+    """Create a tf.data.Dataset from the underlying Dataset. This is a multi-process method - the single-process
+    equivalent is dataset_to_tf.
+
+            Args:
+                dataset (`Dataset`): Dataset to wrap with tf.data.Dataset.
+                cols_to_retain (`List[str]`): Dataset column(s) to load in the
+                    tf.data.Dataset. It is acceptable to include column names that are created by the `collate_fn` and
+                    that do not exist in the original dataset.
+                collate_fn(`Callable`): A function or callable object (such as a `DataCollator`) that will collate
+                    lists of samples into a batch.
+                collate_fn_args (`Dict`): A  `dict` of keyword arguments to be passed to the
+                    `collate_fn`. Can be empty.
+                columns_to_np_types (`Dict[str, np.dtype]`): A `dict` mapping column names to numpy dtypes.
+                output_signature (`Dict[str, tf.TensorSpec]`): A `dict` mapping column names to
+                    `tf.TensorSpec` objects.
+                shuffle(`bool`): Shuffle the dataset order when loading. Recommended True for training, False for
+                    validation/evaluation.
+                batch_size (`int`): Size of batches to load from the dataset.
+                drop_remainder(`bool`, default `None`): Drop the last incomplete batch when loading. If not provided,
+                    defaults to the same setting as shuffle.
+                num_workers (`int`): Number of workers to use for loading the dataset. Should be >= 1.
+
+            Returns:
+                `tf.data.Dataset`
+    """
     if config.TF_AVAILABLE:
         import tensorflow as tf
     else:
