da7f09ed65411c5941de45c372a8aa8d5e55b431
amyeroberts <22614925+amyeroberts@users.noreply.github.com>
2023-01-27 18:08:35 +0000

    Add num_test_batches option (#5471)
    * Add num_test_batches option
    
    * Docstring formatting
    
    * Update src/datasets/arrow_dataset.py
    
    Co-authored-by: Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>
    
    * Change default 200 -> 20
    
    ---------
    
    Co-authored-by: Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>

--- src/datasets/arrow_dataset.py
+++ src/datasets/arrow_dataset.py
@@ -313,6 +313,7 @@ class TensorflowDatasetMixin:
         label_cols: Optional[Union[str, List[str]]] = None,
         prefetch: bool = True,
         num_workers: int = 0,
+        num_test_batches: int = 20,
     ):
         """Create a `tf.data.Dataset` from the underlying Dataset. This `tf.data.Dataset` will load and collate batches from
         the Dataset, and is suitable for passing to methods like `model.fit()` or `model.predict()`. The dataset will yield
@@ -347,6 +348,10 @@ class TensorflowDatasetMixin:
                 background while the model is training.
             num_workers (`int`, defaults to `0`):
                 Number of workers to use for loading the dataset. Only supported on Python versions >= 3.8.
+            num_test_batches (`int`, defaults to `20`):
+                Number of batches to use to infer the output signature of the dataset.
+                The higher this number, the more accurate the signature will be, but the longer it will take to
+                create the dataset.
 
         Returns:
             `tf.data.Dataset`
@@ -414,6 +419,7 @@ class TensorflowDatasetMixin:
             collate_fn_args=collate_fn_args,
             cols_to_retain=cols_to_retain,
             batch_size=batch_size if drop_remainder else None,
+            num_test_batches=num_test_batches,
         )
 
         if "labels" in output_signature:
