a01df372a9af117981215c669a73b8cc1b51d7de
Alvaro Bartolome <alvarobartt@yahoo.com>
2023-02-10 20:27:55 +0100

    Remove `pyyaml` dependency from `QUALITY_REQUIRE`
    Not required as already installed as part of `datasets`, see `REQUIRED_PKGS`

--- setup.py
+++ setup.py
@@ -210,7 +210,7 @@ METRICS_TESTS_REQUIRE = [
 TESTS_REQUIRE.extend(VISION_REQUIRE)
 TESTS_REQUIRE.extend(AUDIO_REQUIRE)
 
-QUALITY_REQUIRE = ["black~=22.0", "flake8>=3.8.3", "isort>=5.0.0", "pyyaml>=5.3.1"]
+QUALITY_REQUIRE = ["black~=22.0", "flake8>=3.8.3", "isort>=5.0.0"]
 
 DOCS_REQUIRE = [
     # Might need to add doc-builder and some specific deps in the future
