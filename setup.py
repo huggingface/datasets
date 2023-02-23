80a47c6975c40b2b62bc0b4c382fac389ad1142a
polinaeterna <polina@huggingface.co>
2023-02-23 20:18:54 +0100

    remove torch and torchaudio from library's requirements

--- setup.py
+++ setup.py
@@ -174,8 +174,6 @@ TESTS_REQUIRE = [
     "tensorflow>=2.3,!=2.6.0,!=2.6.1; sys_platform != 'darwin' or platform_machine != 'arm64'",
     "tensorflow-macos; sys_platform == 'darwin' and platform_machine == 'arm64'",
     "tiktoken;python_version>='3.8'",
-    "torch",
-    "torchaudio<0.12.0",
     "soundfile",
     "transformers",
     "zstandard",
