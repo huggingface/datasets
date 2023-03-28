80bf278a8e6f2a23c1cde4caf52afccb59622f25
Amy Roberts <22614925+amyeroberts@users.noreply.github.com>
2023-03-28 13:59:05 +0100

    Add in ultimate test

--- tests/test_arrow_dataset.py
+++ tests/test_arrow_dataset.py
@@ -2794,18 +2794,18 @@ class BaseDatasetTest(TestCase):
                 self.assertEqual(batch[1]["col_2"].shape.as_list(), [2])
             # del tf_dataset
 
-            # with self._create_dummy_dataset(in_memory, tmp_dir.name, multiple_columns=True) as dset:
-            #     # Check that when we use a transform that creates a new column from existing column values
-            #     # but don't load the old columns that the new column depends on in the final dataset,
-            #     # that they're still kept around long enough to be used in the transform
-            #     transform_dset = dset.with_transform(
-            #         lambda x: {"new_col": [val * 2 for val in x["col_1"]], "col_1": x["col_1"]}
-            #     )
-            #     tf_dataset = transform_dset.to_tf_dataset(columns="new_col", batch_size=2, num_workers=num_workers)
-            #     batch = next(iter(tf_dataset))
-            #     self.assertEqual(batch.shape.as_list(), [2])
-            #     self.assertEqual(batch.dtype.name, "int64")
-            #     del transform_dset
+            with self._create_dummy_dataset(in_memory, tmp_dir.name, multiple_columns=True) as dset:
+                # Check that when we use a transform that creates a new column from existing column values
+                # but don't load the old columns that the new column depends on in the final dataset,
+                # that they're still kept around long enough to be used in the transform
+                transform_dset = dset.with_transform(
+                    lambda x: {"new_col": [val * 2 for val in x["col_1"]], "col_1": x["col_1"]}
+                )
+                tf_dataset = transform_dset.to_tf_dataset(columns="new_col", batch_size=2, num_workers=num_workers)
+                batch = next(iter(tf_dataset))
+                self.assertEqual(batch.shape.as_list(), [2])
+                self.assertEqual(batch.dtype.name, "int64")
+                del transform_dset
         del tf_dataset  # For correct cleanup
 
     # @require_tf
