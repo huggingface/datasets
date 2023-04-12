37ac29bba5622f44ac48414fc0f516aae82cce2a
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2023-04-12 10:45:00 +0200

    Create _register_custom_filesystems function

--- src/datasets/filesystems/__init__.py
+++ src/datasets/filesystems/__init__.py
@@ -22,9 +22,13 @@ COMPRESSION_FILESYSTEMS: List[compression.BaseCompressedFileFileSystem] = [
     compression.ZstdFileSystem,
 ]
 
-# Register custom filesystems
-for fs_class in COMPRESSION_FILESYSTEMS + [HfFileSystem]:
-    fsspec.register_implementation(fs_class.protocol, fs_class)
+
+def _register_custom_filesystems():
+    for fs_class in COMPRESSION_FILESYSTEMS + [HfFileSystem]:
+        fsspec.register_implementation(fs_class.protocol, fs_class)
+
+
+_register_custom_filesystems()
 
 
 def extract_path_from_uri(dataset_path: str) -> str:
