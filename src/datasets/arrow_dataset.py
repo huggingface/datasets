35846fd54fa16aa72ff344d15c98b5e08c5effe0
Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>
2023-04-26 14:11:15 +0200

    Call fs.makedirs in save_to_disk (#5779)
    call fs.makedirs in save_to_disk

--- src/datasets/arrow_dataset.py
+++ src/datasets/arrow_dataset.py
@@ -1392,6 +1392,8 @@ class Dataset(DatasetInfoMixin, IndexableMixin, TensorflowDatasetMixin):
                 raise PermissionError(
                     f"Tried to overwrite {Path(dataset_path).resolve()} but a dataset can't overwrite itself."
                 )
+        else:
+            fs.makedirs(dataset_path, exist_ok=True)
 
         # Get json serializable state
         state = {
