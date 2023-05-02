2e86de798e1b52bfa5d67c70fc2154658ec6610c
Maddie Dawson <maddie.dawson@databricks.com>
2023-05-02 16:37:40 -0700

    Add crashes

--- tests/test_repro_crash.py
+++ tests/test_repro_crash.py
@@ -1,7 +1,9 @@
 import pyspark
 
+from datasets.arrow_writer import ArrowWriter
 
-def test_from_spark():
+
+def test_this_is_fine():
     spark = pyspark.sql.SparkSession.builder.master("local[*]").appName("pyspark").getOrCreate()
     data = [
         ("0", 0, 0.0),
@@ -16,3 +18,68 @@ def test_from_spark():
             yield batch
 
     df.mapInArrow(f, df.schema).collect()
+
+
+def test_crash_from_map_in_arrow_group_by():
+    spark = pyspark.sql.SparkSession.builder.master("local[*]").appName("pyspark").getOrCreate()
+    data = [
+        ("0", 0, 0.0),
+        ("1", 1, 1.0),
+        ("2", 2, 2.0),
+        ("3", 3, 3.0),
+    ]
+    df = spark.createDataFrame(data, "col_1: string, col_2: int, col_3: float")
+
+    def f(it):
+        for batch in it:
+            yield batch
+
+    df.mapInArrow(f, df.schema).orderBy("col_1").collect()
+
+
+def test_crash_from_map_in_arrow_order_by():
+    spark = pyspark.sql.SparkSession.builder.master("local[*]").appName("pyspark").getOrCreate()
+    data = [
+        ("0", 0, 0.0),
+        ("1", 1, 1.0),
+        ("2", 2, 2.0),
+        ("3", 3, 3.0),
+    ]
+    df = spark.createDataFrame(data, "col_1: string, col_2: int, col_3: float")
+
+    def f(it):
+        for batch in it:
+            yield batch
+
+    df.mapInArrow(f, df.schema).groupBy("col_1").count().collect()
+
+
+def test_crash_from_map_in_arrow_arrow_writer():
+    spark = pyspark.sql.SparkSession.builder.master("local[*]").appName("pyspark").getOrCreate()
+    data = [
+        ("0", 0, 0.0),
+        ("1", 1, 1.0),
+        ("2", 2, 2.0),
+        ("3", 3, 3.0),
+    ]
+    df = spark.createDataFrame(data, "col_1: string, col_2: int, col_3: float")
+
+    def f(it):
+        for batch in it:
+            ArrowWriter(path="dummy.txt")
+            yield batch
+
+    df.mapInArrow(f, df.schema).collect()
+
+
+def test_crash_from_order_by_partition():
+    spark = pyspark.sql.SparkSession.builder.master("local[*]").appName("pyspark").getOrCreate()
+    data = [
+        ("0", 0, 0.0),
+        ("1", 1, 1.0),
+        ("2", 2, 2.0),
+        ("3", 3, 3.0),
+    ]
+    df = spark.createDataFrame(data, "col_1: string, col_2: int, col_3: float")
+
+    df.orderBy(pyspark.sql.functions.spark_partition_id()).collect()
