9b533bbdea134b544a81594a06a9733727c70f58
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2023-05-12 17:40:43 +0200

    Use complete extensions

--- src/datasets/load.py
+++ src/datasets/load.py
@@ -86,7 +86,7 @@ from .utils.version import Version
 
 logger = get_logger(__name__)
 
-ALL_ALLOWED_EXTENSIONS = list(_EXTENSION_TO_MODULE.keys()) + ["zip"]
+ALL_ALLOWED_EXTENSIONS = list(_EXTENSION_TO_MODULE.keys()) + [".zip"]
 
 
 def init_dynamic_modules(
@@ -358,7 +358,7 @@ def infer_module_for_data_files(
             - builder kwargs
     """
     extensions_counter = Counter(
-        suffix[1:].lower()
+        suffix.lower()
         for filepath in data_files_list[: config.DATA_FILES_MAX_NUMBER_FOR_MODULE_INFERENCE]
         for suffix in Path(filepath).suffixes
     )
@@ -366,7 +366,7 @@ def infer_module_for_data_files(
         for ext, _ in extensions_counter.most_common():
             if ext in _EXTENSION_TO_MODULE:
                 return _EXTENSION_TO_MODULE[ext]
-            elif ext == "zip":
+            elif ext == ".zip":
                 return infer_module_for_data_files_in_archives(data_files_list, use_auth_token=use_auth_token)
     return None, {}
 
@@ -400,9 +400,7 @@ def infer_module_for_data_files_in_archives(
                     : config.ARCHIVED_DATA_FILES_MAX_NUMBER_FOR_MODULE_INFERENCE
                 ]
             ]
-    extensions_counter = Counter(
-        suffix[1:].lower() for filepath in archived_files for suffix in Path(filepath).suffixes
-    )
+    extensions_counter = Counter(suffix.lower() for filepath in archived_files for suffix in Path(filepath).suffixes)
     if extensions_counter:
         most_common = extensions_counter.most_common(1)[0][0]
         if most_common in _EXTENSION_TO_MODULE:
