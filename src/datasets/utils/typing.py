996c9c95d13fe2ca4c2a4a627c73cb59e7955b58
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2023-05-25 13:36:24 +0200

    Define ListLike typing

--- src/datasets/utils/typing.py
+++ src/datasets/utils/typing.py
@@ -1,8 +1,18 @@
 import os
-from typing import Dict, List, TypeVar, Union
+from typing import TYPE_CHECKING, Dict, List, TypeVar, Union
+
+
+if TYPE_CHECKING:
+    import numpy as np
+    import pandas as pd
 
 
 T = TypeVar("T")
 
 NestedDataStructureLike = Union[T, List[T], Dict[str, T]]
 PathLike = Union[str, bytes, os.PathLike]
+
+
+ArrayLike = Union["pandas.core.arrays.base.ExtensionArray", "np.ndarray"]
+AnyArrayLike = Union[ArrayLike, "pd.core.indexes.base.Index", "pd.core.series.Series"]
+ListLike = Union[AnyArrayLike, List, range]
