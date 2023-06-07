172cfbc5ff8e423ba3270fe47dadd8e797bd0482
Ying Chen <ying.chen@databricks.com>
2023-06-07 14:57:08 -0700

    comments

--- src/datasets/parallel/parallel.py
+++ src/datasets/parallel/parallel.py
@@ -19,6 +19,17 @@ def parallel_map(function, iterable, num_proc, types, disable_tqdm, desc, single
     """
     **Experimental.** Apply a function to iterable elements in parallel, where the implementation uses either
     multiprocessing.Pool or joblib for parallelization.
+
+    Args:
+        function (`Callable[[Any], Any]`): Function to be applied to `iterable`.
+        iterable (`list`, `tuple` or `np.ndarray`): Iterable elements to apply function to.
+        num_proc (`int`): Number of processes (if no backend specified) or jobs (using joblib).
+        types (`tuple`): Additional types (besides `dict` values) to apply `function` recursively to their elements.
+        disable_tqdm (`bool`): Whether to disable the tqdm progressbar.
+        desc (`str`): Prefix for the tqdm progressbar.
+        single_map_nested_func (`Callable`): Map function that applies `function` to an element from `iterable`.
+            Takes a tuple of function, data_struct, types, rank, disable_tqdm, desc as input, where data_struct is an
+            element of `iterable`, and `rank` is used for progress bar.
     """
     if ParallelBackendConfig.backend_name is None:
         return _map_with_multiprocessing_pool(
@@ -76,6 +87,9 @@ def parallel_backend(backend_name: str):
     """
     **Experimental.**  Configures the parallel backend for parallelized dataset loading, which uses the parallelization
     implemented by joblib.
+
+    Args:
+        backend_name (str): Name of backend for parallelization implementation, has to be supported by joblib.
 
      Example usage:
      ```py
