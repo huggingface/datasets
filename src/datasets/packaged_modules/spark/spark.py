961608bb8d5bb3acc2b48f51f575719b553ad0df
Mathew Jacob <mathew.jacob@databricks.com>
2023-06-28 11:12:39 -0700

    Renamed the rename_dataframe_by_partition to _rename_dataframe_by_partition in order to make it private

--- src/datasets/packaged_modules/spark/spark.py
+++ src/datasets/packaged_modules/spark/spark.py
@@ -6,7 +6,6 @@ from typing import TYPE_CHECKING, Iterable, List, Optional, Tuple, Union
 
 import numpy as np
 import pyarrow as pa
-import pyspark
 
 import datasets
 from datasets.arrow_writer import ArrowWriter, ParquetWriter
@@ -32,7 +31,7 @@ class SparkConfig(datasets.BuilderConfig):
     features: Optional[datasets.Features] = None
 
 
-def reorder_dataframe_by_partition(df: "pyspark.sql.DataFrame", new_partition_order: List[int]):
+def _reorder_dataframe_by_partition(df: "pyspark.sql.DataFrame", new_partition_order: List[int]):
     # get the first partition and place it in new dataframe
     df_combined = df.select("*").where(f"part_id = {new_partition_order[0]}")
     for partition_id in new_partition_order[1:]:
@@ -46,10 +45,11 @@ def _generate_iterable_examples(
     df: "pyspark.sql.DataFrame",
     partition_order: List[int],
 ):
+    import pyspark
     def generate_fn():
         # gets all of the columns with an extra column called "part_id", but DOES NOT HIT NETWORK YET
         df_with_partition_id = df.select("*", pyspark.sql.functions.spark_partition_id().alias("part_id"))
-        partition_df = reorder_dataframe_by_partition(df_with_partition_id, partition_order)
+        partition_df = _reorder_dataframe_by_partition(df_with_partition_id, partition_order)
         row_id = 0
         # get iterator to reordered df, prefetching the next df for better performance
         rows = partition_df.toLocalIterator(prefetchPartitions=True)
