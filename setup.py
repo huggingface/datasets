8f57aae06bd325d76cb70cb774450f3a66f169cf
mariosasko <mariosasko777@gmail.com>
2023-07-04 18:16:22 +0200

    Fix 3.10

--- setup.py
+++ setup.py
@@ -164,7 +164,7 @@ TESTS_REQUIRE = [
     "pytest-datadir",
     "pytest-xdist",
     # optional dependencies
-    "apache-beam>=2.26.0,<2.44.0",  # doesn't support recent dill versions for recent python versions
+    "apache-beam>=2.26.0,<2.44.0;python_version<'3.10'",  # doesn't support recent dill versions for recent python versions
     "elasticsearch<8.0.0",  # 8.0 asks users to provide hosts or cloud_id when instantiating ElasticSearch()
     "faiss-cpu>=1.6.4",
     "lz4",
