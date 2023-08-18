f34dba9faf46a9e939d440794f5c57a30c8d3003
Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>
2023-08-18 15:35:12 +0200

    Raise FileNotFoundError when passing data_files that don't exist (#6155)
    * raise filenotfound
    
    * only raise if no magic char in pattern
    
    * add test
    
    * use has_magic

--- src/datasets/data_files.py
+++ src/datasets/data_files.py
@@ -1,6 +1,7 @@
 import os
 import re
 from functools import partial
+from glob import has_magic
 from pathlib import Path, PurePath
 from typing import Callable, Dict, List, Optional, Set, Tuple, Union
 
@@ -583,7 +584,8 @@ class DataFilesList(List[str]):
                     )
                 )
             except FileNotFoundError:
-                pass
+                if not has_magic(pattern):
+                    raise
         origin_metadata = _get_origin_metadata(data_files, download_config=download_config)
         return cls(data_files, origin_metadata)
 
