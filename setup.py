029227a116c14720afca71b9b22e78eb2a1c09a6
Mario Šaško <mariosasko777@gmail.com>
2023-08-30 15:51:31 +0200

    Set minimal fsspec version requirement to 2023.1.0 (#6192)
    * Set minimal fsspec version requirement to 2023.1.0
    
    * Oops :)

--- setup.py
+++ setup.py
@@ -125,8 +125,8 @@ REQUIRED_PKGS = [
     # for better multiprocessing
     "multiprocess",
     # to save datasets locally or on any filesystem
-    # minimum 2022.3.0 so that TqdmCallback is available: see https://github.com/fsspec/filesystem_spec/pull/931
-    "fsspec[http]>=2022.3.0",
+    # minimum 2023.1.0 to support protocol=kwargs in fsspec's `open`, `get_fs_token_paths`, etc.: see https://github.com/fsspec/filesystem_spec/pull/1143
+    "fsspec[http]>=2023.1.0",
     # for data streaming via http
     "aiohttp",
     # To get datasets from the Datasets Hub on huggingface.co
