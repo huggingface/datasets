cc66766c4862db62cc71a1d06f4a389d77f37123
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2023-09-04 11:13:32 +0200

    Do not filter out .zip extensions from no-script datasets (#6208)
    * Rename zip_csv_path fixture dirname and filename
    
    * Test load no-script dataset with ZIP file
    
    * Fix style
    
    * Avoid filtering out .zip extension

--- tests/test_load.py
+++ tests/test_load.py
@@ -1069,3 +1069,12 @@ def test_load_dataset_distributed(tmp_path, csv_path):
         assert all(len(dataset) == len(datasets[0]) > 0 for dataset in datasets)
         assert len(datasets[0].cache_files) > 0
         assert all(dataset.cache_files == datasets[0].cache_files for dataset in datasets)
+
+
+def test_load_dataset_without_script_with_zip(zip_csv_path):
+    path = str(zip_csv_path.parent)
+    ds = load_dataset(path)
+    assert list(ds.keys()) == ["train"]
+    assert ds["train"].column_names == ["col_1", "col_2", "col_3"]
+    assert ds["train"].num_rows == 8
+    assert ds["train"][0] == {"col_1": 0, "col_2": 0, "col_3": 0.0}
