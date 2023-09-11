57dc08fc34ea0ed4fe166510277dbc9602dbe694
polinaeterna <polina@huggingface.co>
2023-09-11 16:25:01 +0200

    overwrite legacy default config name (with '--') with 'default'

--- src/datasets/dataset_dict.py
+++ src/datasets/dataset_dict.py
@@ -1728,6 +1728,9 @@ class DatasetDict(dict):
             )
             with open(dataset_infos_path, encoding="utf-8") as f:
                 dataset_infos: dict = json.load(f)
+            legacy_config_name = "--".join(repo_id.split("/"))
+            if config_name == "default" and legacy_config_name in dataset_infos:
+                dataset_infos.pop(legacy_config_name)
             dataset_infos[config_name] = asdict(info_to_dump)
             buffer = BytesIO()
             buffer.write(json.dumps(dataset_infos, indent=4).encode("utf-8"))
