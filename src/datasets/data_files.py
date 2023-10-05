56fa9645fd24e083adee3cfd0f7d972fce391f0e
Quentin Lhoest <lhoest.q@gmail.com>
2023-10-05 16:45:39 +0200

    add to DataFilesDict.from_patterns too

--- src/datasets/data_files.py
+++ src/datasets/data_files.py
@@ -577,7 +577,7 @@ class DataFilesList(List[str]):
         base_path: Optional[str] = None,
         allowed_extensions: Optional[List[str]] = None,
         download_config: Optional[DownloadConfig] = None,
-        drop_duplicates=True,
+        drop_duplicates: bool = True,
     ) -> "DataFilesList":
         base_path = base_path if base_path is not None else Path().resolve().as_posix()
         data_files = []
@@ -677,6 +677,7 @@ class DataFilesDict(Dict[str, DataFilesList]):
         base_path: Optional[str] = None,
         allowed_extensions: Optional[List[str]] = None,
         download_config: Optional[DownloadConfig] = None,
+        drop_duplicates: bool = True,
     ) -> "DataFilesDict":
         out = cls()
         for key, patterns_for_key in patterns.items():
@@ -686,6 +687,7 @@ class DataFilesDict(Dict[str, DataFilesList]):
                     base_path=base_path,
                     allowed_extensions=allowed_extensions,
                     download_config=download_config,
+                    drop_duplicates=drop_duplicates,
                 )
                 if not isinstance(patterns_for_key, DataFilesList)
                 else patterns_for_key
