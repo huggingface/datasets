04a3f006a1a88c894ea10610d66dfddd73ad1490
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2023-11-13 11:00:08 +0100

    Pin minimum pyarrow version to 14.0.1

--- setup.py
+++ setup.py
@@ -111,8 +111,8 @@ REQUIRED_PKGS = [
     # We use numpy>=1.17 to have np.random.Generator (Dataset shuffling)
     "numpy>=1.17",
     # Backend and serialization.
-    # Minimum 8.0.0 to be able to use .to_reader()
-    "pyarrow>=8.0.0",
+    # Minimum 14.0.1 to fix vulnerability CVE-2023-47248
+    "pyarrow>=14.0.1",
     # For smart caching dataset processing
     "dill>=0.3.0,<0.3.8",  # tmp pin until dill has official support for determinism see https://github.com/uqfoundation/dill/issues/19
     # For performance gains with apache arrow
