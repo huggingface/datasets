523fcbed28171798dabd2d0b365070b01b4f65c6
mariosasko <mariosasko777@gmail.com>
2023-11-29 19:23:29 +0100

    Nit

--- src/datasets/utils/hub.py
+++ src/datasets/utils/hub.py
@@ -8,6 +8,7 @@ from .. import config
 from .file_utils import _retry
 
 
+# `huggingface_hub` in version < 0.20.0 does not retry a failed LFS upload on the "500 (Internal Server Error)" and "504 (Gateway Timeout)" HTTP errors
 if config.HF_HUB_VERSION < version.parse("0.20.0"):
 
     def preupload_lfs_files(hf_api: HfApi, **kwargs):
@@ -15,7 +16,7 @@ if config.HF_HUB_VERSION < version.parse("0.20.0"):
             hf_api.preupload_lfs_files,
             func_kwargs=kwargs,
             exceptions=HTTPError,
-            status_codes=[500, 503, 504],
+            status_codes=[500, 504],
             max_retries=5,
             base_wait_time=1,
             max_wait_time=8,
