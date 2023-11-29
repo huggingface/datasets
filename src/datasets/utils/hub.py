273e075e7b35c8673eddc98340b8ff898f6d65af
mariosasko <mariosasko777@gmail.com>
2023-11-29 19:19:00 +0100

    Retry on 500 server error in `push_to_hub`

--- src/datasets/utils/hub.py
+++ src/datasets/utils/hub.py
@@ -1,12 +1,30 @@
-from typing import Optional
-from urllib.parse import quote
+from functools import partial
 
-import huggingface_hub as hfh
+from huggingface_hub import HfApi, hf_hub_url
 from packaging import version
+from requests import HTTPError
+
+from .. import config
+from .file_utils import _retry
 
 
-def hf_hub_url(repo_id: str, path: str, revision: Optional[str] = None) -> str:
-    if version.parse(hfh.__version__).release < version.parse("0.11.0").release:
-        # old versions of hfh don't url-encode the file path
-        path = quote(path)
-    return hfh.hf_hub_url(repo_id, path, repo_type="dataset", revision=revision)
+if config.HF_HUB_VERSION < version.parse("0.20.0"):
+
+    def preupload_lfs_files(hf_api: HfApi, **kwargs):
+        _retry(
+            hf_api.preupload_lfs_files,
+            func_kwargs=kwargs,
+            exceptions=HTTPError,
+            status_codes=[500, 503, 504],
+            max_retries=5,
+            base_wait_time=1,
+            max_wait_time=8,
+        )
+else:
+
+    def preupload_lfs_files(hf_api: HfApi, **kwargs):
+        hf_api.preupload_lfs_files(**kwargs)
+
+
+# bakckward compatibility
+hf_hub_url = partial(hf_hub_url, repo_type="dataset")
