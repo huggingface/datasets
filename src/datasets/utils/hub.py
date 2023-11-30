06466bfa94013a28a1eb88d58daa5027ac3f0eb5
mariosasko <mariosasko777@gmail.com>
2023-11-30 15:43:48 +0100

    Fix

--- src/datasets/utils/hub.py
+++ src/datasets/utils/hub.py
@@ -1,3 +1,4 @@
+import time
 from functools import partial
 
 from huggingface_hub import HfApi, hf_hub_url
@@ -5,22 +6,37 @@ from packaging import version
 from requests import HTTPError
 
 from .. import config
-from .file_utils import _retry
+from . import logging
 
 
-# `huggingface_hub` in version < 0.20.0 does not retry a failed LFS upload on the "500 (Internal Server Error)" and "504 (Gateway Timeout)" HTTP errors
+logger = logging.get_logger(__name__)
+
+# Retry `preupload_lfs_files` in `huggingface_hub<0.20.0` on the "500 (Internal Server Error)" and "504 (Gateway Timeout)" HTTP errors
 if config.HF_HUB_VERSION < version.parse("0.20.0"):
 
     def preupload_lfs_files(hf_api: HfApi, **kwargs):
-        _retry(
-            hf_api.preupload_lfs_files,
-            func_kwargs=kwargs,
-            exceptions=HTTPError,
-            status_codes=[500, 504],
-            max_retries=5,
-            base_wait_time=1,
-            max_wait_time=8,
-        )
+        max_retries = 5
+        base_wait_time = 1
+        max_wait_time = 8
+        status_codes = [500, 503, 504]
+        retry = 0
+        while True:
+            try:
+                hf_api.preupload_lfs_files(**kwargs)
+            except (RuntimeError, HTTPError) as err:
+                if isinstance(err, RuntimeError) and isinstance(err.__cause__, HTTPError):
+                    err = err.__cause__
+                if retry >= max_retries or err.response.status_code not in status_codes:
+                    raise err
+                else:
+                    sleep_time = min(max_wait_time, base_wait_time * 2**retry)  # Exponential backoff
+                    logger.info(
+                        f"{hf_api.preupload_lfs_files} timed out, retrying in {sleep_time}s... [{retry/max_retries}]"
+                    )
+                    time.sleep(sleep_time)
+                    retry += 1
+            else:
+                break
 else:
 
     def preupload_lfs_files(hf_api: HfApi, **kwargs):
