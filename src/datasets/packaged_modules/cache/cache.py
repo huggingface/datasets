a15ea9db34f3497d1c9234a4e5b3a9b10083a826
Quentin Lhoest <lhoest.q@gmail.com>
2023-12-06 17:55:35 +0100

    allow load from cache in streaming mode

--- src/datasets/packaged_modules/cache/cache.py
+++ src/datasets/packaged_modules/cache/cache.py
@@ -1,9 +1,58 @@
-from datasets import DatasetInfo, GeneratorBasedBuilder, SplitGenerator
+import os
+import shutil
+from typing import List, Optional
+
+import pyarrow as pa
+
+import datasets
+from datasets.naming import filenames_for_dataset_split
 
 
-class Cache(GeneratorBasedBuilder):
-    def _info(self) -> DatasetInfo:
-        return DatasetInfo()
+logger = datasets.utils.logging.get_logger(__name__)
+
+
+class Cache(datasets.ArrowBasedBuilder):
+    def _info(self) -> datasets.DatasetInfo:
+        return datasets.DatasetInfo()
+
+    def download_and_prepare(self, output_dir: Optional[str] = None, *args, **kwargs):
+        if not os.path.exists(self.cache_dir):
+            raise ValueError(f"Cache directory for {self.dataset_name} doesn't exist at {self.cache_dir}")
+        if output_dir is not None:
+            shutil.copytree(self.cache_dir, output_dir)
 
     def _split_generators(self, dl_manager):
-        return [SplitGenerator(name=name, gen_kwargs={}) for name in self.info.splits]
+        if isinstance(self.info.splits, datasets.SplitDict):
+            split_infos: List[datasets.SplitInfo] = list(self.info.splits.values())
+        else:
+            raise ValueError(f"Missing splits info for {self.dataset_name} in cache directory {self.cache_dir}")
+        return [
+            datasets.SplitGenerator(
+                name=split_info.name,
+                gen_kwargs={
+                    "files": filenames_for_dataset_split(
+                        self.cache_dir,
+                        dataset_name=self.dataset_name,
+                        split=split_info.name,
+                        filetype_suffix="arrow",
+                        shard_lengths=split_info.shard_lengths,
+                    )
+                },
+            )
+            for split_info in split_infos
+        ]
+
+    def _generate_tables(self, files):
+        # used to stream from cache
+        for file_idx, file in enumerate(files):
+            with open(file, "rb") as f:
+                try:
+                    for batch_idx, record_batch in enumerate(pa.ipc.open_stream(f)):
+                        pa_table = pa.Table.from_batches([record_batch])
+                        # Uncomment for debugging (will print the Arrow table size and elements)
+                        # logger.warning(f"pa_table: {pa_table} num rows: {pa_table.num_rows}")
+                        # logger.warning('\n'.join(str(pa_table.slice(i, 1).to_pydict()) for i in range(pa_table.num_rows)))
+                        yield f"{file_idx}_{batch_idx}", pa_table
+                except ValueError as e:
+                    logger.error(f"Failed to read file '{file}' with error {type(e)}: {e}")
+                    raise
