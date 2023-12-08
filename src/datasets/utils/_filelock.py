0bea9324420168d30ca6588b9bb046279c3d9dfd
Quentin Lhoest <lhoest.q@gmail.com>
2023-12-08 14:39:14 +0100

    fix max lock length on unix

--- src/datasets/utils/_filelock.py
+++ src/datasets/utils/_filelock.py
@@ -17,7 +17,7 @@
 
 import os
 
-from filelock import FileLock as FileLock_
+from filelock import FileLock as FileLock_, UnixFileLock
 
 
 class FileLock(FileLock_):
@@ -34,11 +34,14 @@ class FileLock(FileLock_):
     @classmethod
     def hash_filename_if_too_long(cls, path: str) -> str:
         filename = os.path.basename(path)
-        if len(filename) > cls.MAX_FILENAME_LENGTH:
+        max_filename_length = cls.MAX_FILENAME_LENGTH
+        if issubclass(cls, UnixFileLock):
+            max_filename_length = min(max_filename_length, os.statvfs(os.path.dirname(path)).f_namemax)
+        if len(filename) > max_filename_length:
             dirname = os.path.dirname(path)
             hashed_filename = str(hash(filename))
             new_filename = (
-                filename[: cls.MAX_FILENAME_LENGTH - len(hashed_filename) - 8] + "..." + hashed_filename + ".lock"
+                filename[: max_filename_length - len(hashed_filename) - 8] + "..." + hashed_filename + ".lock"
             )
             return os.path.join(dirname, new_filename)
         else:
