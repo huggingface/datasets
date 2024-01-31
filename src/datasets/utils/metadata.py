a068fb9d74e36bf746f0193c2d0169908e7b6d55
Quentin Lhoest <lhoest.q@gmail.com>
2024-01-31 18:52:44 +0100

    set default config when single config parquet export

--- src/datasets/utils/metadata.py
+++ src/datasets/utils/metadata.py
@@ -252,6 +252,8 @@ class MetadataConfigs(Dict[str, Dict[str, Any]]):
 
     def get_default_config_name(self) -> Optional[str]:
         default_config_name = None
+        if len(self) == 1:
+            return next(iter(self))
         for config_name, metadata_config in self.items():
             if config_name == "default" or metadata_config.get("default"):
                 if default_config_name is None:
