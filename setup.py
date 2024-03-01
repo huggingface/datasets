ca8409a8bec4508255b9c3e808d0751eb1005260
Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>
2024-03-01 21:56:50 +0100

    Release: 2.18.0 (#6708)
    release: 2.18.0

--- setup.py
+++ setup.py
@@ -253,7 +253,7 @@ EXTRAS_REQUIRE = {
 
 setup(
     name="datasets",
-    version="2.17.2.dev0",  # expected format is one of x.y.z.dev0, or x.y.z.rc1 or x.y.z (no to dashes, yes to dots)
+    version="2.18.0",  # expected format is one of x.y.z.dev0, or x.y.z.rc1 or x.y.z (no to dashes, yes to dots)
     description="HuggingFace community-driven open-source library of datasets",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
