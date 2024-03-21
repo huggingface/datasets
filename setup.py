5ded1b20ce7495cac7b6be2a250d15a5df42f935
Wauplin <lucainp@gmail.com>
2024-03-21 15:44:34 +0100

    Test hfh v0.22.0.rc0

--- setup.py
+++ setup.py
@@ -135,7 +135,7 @@ REQUIRED_PKGS = [
     # for data streaming via http
     "aiohttp",
     # To get datasets from the Datasets Hub on huggingface.co
-    "huggingface-hub>=0.21.2",
+    "huggingface-hub==v0.22.0.rc0",
     # Utilities from PyPA to e.g., compare versions
     "packaging",
     # To parse YAML metadata from dataset cards
