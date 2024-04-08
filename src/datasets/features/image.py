0551685a98d3c6ba8ab74a29054b574462b47dc9
Mike Boss <mboss@ethz.ch>
2024-04-08 11:18:38 +0200

    pa bool as py bool and revert accidental change

--- src/datasets/features/image.py
+++ src/datasets/features/image.py
@@ -225,55 +225,43 @@ class Image:
             `pa.StructArray`: Array in the Image arrow storage type, that is
                 `pa.struct({"bytes": pa.binary(), "path": pa.string()})`.
         """
-        if hasattr(storage, "type"):
-            if pa.types.is_string(storage.type):
+        if pa.types.is_string(storage.type):
+            bytes_array = pa.array([None] * len(storage), type=pa.binary())
+            storage = pa.StructArray.from_arrays([bytes_array, storage], ["bytes", "path"], mask=storage.is_null())
+        elif pa.types.is_binary(storage.type):
+            path_array = pa.array([None] * len(storage), type=pa.string())
+            storage = pa.StructArray.from_arrays([storage, path_array], ["bytes", "path"], mask=storage.is_null())
+        elif pa.types.is_struct(storage.type):
+            if storage.type.get_field_index("bytes") >= 0:
+                bytes_array = storage.field("bytes")
+            else:
                 bytes_array = pa.array([None] * len(storage), type=pa.binary())
-                storage = pa.StructArray.from_arrays([bytes_array, storage], ["bytes", "path"], mask=storage.is_null())
-            elif pa.types.is_binary(storage.type):
+            if storage.type.get_field_index("path") >= 0:
+                path_array = storage.field("path")
+            else:
                 path_array = pa.array([None] * len(storage), type=pa.string())
-                storage = pa.StructArray.from_arrays([storage, path_array], ["bytes", "path"], mask=storage.is_null())
-            elif pa.types.is_struct(storage.type):
-                if storage.type.get_field_index("bytes") >= 0:
-                    bytes_array = storage.field("bytes")
-                else:
-                    bytes_array = pa.array([None] * len(storage), type=pa.binary())
-                if storage.type.get_field_index("path") >= 0:
-                    path_array = storage.field("path")
-                else:
-                    path_array = pa.array([None] * len(storage), type=pa.string())
-                storage = pa.StructArray.from_arrays(
-                    [bytes_array, path_array], ["bytes", "path"], mask=storage.is_null()
-                )
-            elif pa.types.is_list(storage.type):
-                from .features import Array2DExtensionType, Array3DExtensionType
+            storage = pa.StructArray.from_arrays([bytes_array, path_array], ["bytes", "path"], mask=storage.is_null())
+        elif pa.types.is_list(storage.type):
+            from .features import Array2DExtensionType, Array3DExtensionType
 
-                arrays = []
-                for i, is_null in enumerate(storage.is_null()):
-                    if is_null:
-                        arrays.append(None)
+            arrays = []
+            for i, is_null in enumerate(storage.is_null()):
+                if not is_null.as_py():
+                    storage_part = storage.take([i])
+                    shape = get_shapes_from_listarray(storage_part)
+                    dtype = get_dtypes_from_listarray(storage_part)
+
+                    if len(shape) == 2:
+                        extension_type = Array2DExtensionType(shape=shape, dtype=str(dtype))
                     else:
-                        storage_part = storage.take([i])
-                        shape = get_shapes_from_listarray(storage_part)
-                        dtype = get_dtypes_from_listarray(storage_part)
+                        extension_type = Array3DExtensionType(shape=shape, dtype=str(dtype))
+                    array = pa.ExtensionArray.from_storage(extension_type, storage_part)
+                    arrays.append(array.to_numpy().squeeze(0))
+                else:
+                    arrays.append(None)
 
-                        if len(shape) == 2:
-                            extension_type = Array2DExtensionType(shape=shape, dtype=str(dtype))
-                        else:
-                            extension_type = Array3DExtensionType(shape=shape, dtype=str(dtype))
-                        array = pa.ExtensionArray.from_storage(extension_type, storage_part)
-                        arrays.append(array.to_numpy().squeeze(0))
-
-                bytes_array = pa.array(
-                    [encode_np_array(arr)["bytes"] if arr is not None else None for arr in arrays],
-                    type=pa.binary(),
-                )
-                path_array = pa.array([None] * len(storage), type=pa.string())
-                storage = pa.StructArray.from_arrays(
-                    [bytes_array, path_array], ["bytes", "path"], mask=bytes_array.is_null()
-                )
-        else:
             bytes_array = pa.array(
-                [encode_np_array(arr)["bytes"] if arr is not None else None for arr in storage],
+                [encode_np_array(arr)["bytes"] if arr is not None else None for arr in arrays],
                 type=pa.binary(),
             )
             path_array = pa.array([None] * len(storage), type=pa.string())
