cd5e65ee325416f2791a7209924901e9481e9ba8
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-05-02 10:25:59 +0200

    Fix test_delete_from_hub by passing encoding

--- tests/test_hub.py
+++ tests/test_hub.py
@@ -75,7 +75,9 @@ def test_delete_from_hub(
         CommitOperationDelete(path_in_repo="dogs/train/0000.csv", is_folder=False),
         CommitOperationAdd(
             path_in_repo="README.md",
-            path_or_fileobj=b"---\nconfigs:\n- config_name: cats\n  data_files:\n  - split: train\n    path: cats/train/*\n---\n",
+            path_or_fileobj="---\nconfigs:\n- config_name: cats\n  data_files:\n  - split: train\n    path: cats/train/*\n---\n".encode(
+                encoding="utf-8"
+            ),
         ),
     ]
     assert mock_method.call_args.kwargs.get("operations") == expected_operations
