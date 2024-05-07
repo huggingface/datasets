fa8dc419311e18487469732b4d4c5f6c0cde807b
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-05-07 12:24:23 +0200

    Create Python function delete_from_hub

--- src/datasets/hub.py
+++ src/datasets/hub.py
@@ -1,3 +1,4 @@
+import time
 from itertools import chain
 from typing import Optional, Union
 
@@ -9,12 +10,92 @@ from huggingface_hub import (
     DatasetCardData,
     HfApi,
     HfFileSystem,
+    create_branch,
 )
 
 from datasets import config
 from datasets.info import DatasetInfosDict
-from datasets.load import load_dataset_builder
+from datasets.inspect import get_dataset_config_names, get_dataset_default_config_name
+from datasets.load import load_dataset, load_dataset_builder
 from datasets.utils.metadata import MetadataConfigs
+
+
+def convert_to_parquet(
+    repo_id: str,
+    revision: Optional[str] = None,
+    token: Optional[Union[bool, str]] = None,
+    trust_remote_code: Optional[bool] = None,
+) -> CommitInfo:
+    """Convert Hub [script-based dataset](dataset_script) to Parquet [data-only dataset](repository_structure), so that
+    the dataset viewer will be supported.
+
+    This function:
+    - makes a copy of the script on the "main" branch into a dedicated branch called "script" (if it does not already exist)
+    - creates a pull request to the Hub dataset to convert it to Parquet files (and deletes the script from the main branch)
+
+    If in the future you need to recreate the Parquet files from the "script" branch, pass the `revision="script"` argument.
+
+    Note that you should pass the `trust_remote_code=True` argument only if you trust the remote code to be executed locally on your machine.
+
+    Args:
+        repo_id (`str`): ID of the source Hub dataset repository, in the following format: `<user>/<dataset_name>` or
+            `<org>/<dataset_name>`.
+        revision (`str`, *optional*): Branch of the source Hub dataset repository. Defaults to the `"main"` branch.
+        token (`bool` or `str`, *optional*): Authentication token for the Hugging Face Hub.
+        trust_remote_code (`bool`, defaults to `True`): Whether you trust the remote code of the Hub script-based
+            dataset to be executed locally on your machine. This option should only be set to `True` for repositories
+            where you have read the code and which you trust.
+
+            <Tip warning={true}>
+
+            `trust_remote_code` will default to False in the next major release.
+
+            </Tip>
+
+    Returns:
+        `huggingface_hub.CommitInfo`
+    """
+    print(f"{repo_id}")
+    configs = get_dataset_config_names(repo_id, token=token, revision=revision, trust_remote_code=trust_remote_code)
+    print(f"{configs = }")
+    default_config = get_dataset_default_config_name(
+        repo_id, token=token, revision=revision, trust_remote_code=trust_remote_code
+    )
+    print(f"{default_config = }")
+    if default_config:
+        config = default_config
+        configs.remove(default_config)
+    else:
+        config = configs.pop(0)
+    print(f"{config = }")
+    dataset = load_dataset(repo_id, config, revision=revision, trust_remote_code=trust_remote_code)
+    commit_info = dataset.push_to_hub(
+        repo_id,
+        config_name=config,
+        commit_message="Convert dataset to Parquet",
+        commit_description="Convert dataset to Parquet.",
+        create_pr=True,
+        token=token,
+        set_default=default_config is not None,
+    )
+    time.sleep(5)
+    pr_revision, pr_url = commit_info.pr_revision, commit_info.pr_url
+    for config in configs:
+        print(f"{config = }")
+        dataset = load_dataset(repo_id, config, revision=revision, trust_remote_code=trust_remote_code)
+        dataset.push_to_hub(
+            repo_id,
+            config_name=config,
+            commit_message=f"Add {config} data files",
+            revision=pr_revision,
+            token=token,
+        )
+        time.sleep(5)
+    _delete_files(repo_id, revision=pr_revision, token=token)
+    if not revision:
+        create_branch(repo_id, branch="script", repo_type="dataset", token=token, exist_ok=True)
+    print(f"You can find your PR to convert the dataset to Parquet at: {pr_url}")
+    return commit_info
 
 
 def delete_from_hub(
@@ -33,7 +114,7 @@ def delete_from_hub(
         token (`bool` or `str`, *optional*): Authentication token for the Hugging Face Hub.
 
     Returns:
-        huggingface_hub.CommitInfo
+        `huggingface_hub.CommitInfo`
     """
     operations = []
     # data_files
@@ -85,3 +166,59 @@ def delete_from_hub(
     )
     print(f"You can find your PR to delete the dataset config at: {commit_info.pr_url}")
     return commit_info
+
+
+def _delete_files(dataset_id, revision=None, token=None):
+    dataset_name = dataset_id.split("/")[-1]
+    hf_api = HfApi(token=token)
+    repo_files = hf_api.list_repo_files(
+        dataset_id,
+        repo_type="dataset",
+    )
+    if repo_files:
+        legacy_json_file = []
+        python_files = []
+        data_files = []
+        for filename in repo_files:
+            if filename in {".gitattributes", "README.md"}:
+                continue
+            elif filename == f"{dataset_name}.py":
+                hf_api.delete_file(
+                    filename,
+                    dataset_id,
+                    repo_type="dataset",
+                    revision=revision,
+                    commit_message="Delete loading script",
+                )
+            elif filename == "dataset_infos.json":
+                legacy_json_file.append(filename)
+            elif filename.endswith(".py"):
+                python_files.append(filename)
+            else:
+                data_files.append(filename)
+        if legacy_json_file:
+            hf_api.delete_file(
+                "dataset_infos.json",
+                dataset_id,
+                repo_type="dataset",
+                revision=revision,
+                commit_message="Delete legacy dataset_infos.json",
+            )
+        if python_files:
+            for filename in python_files:
+                hf_api.delete_file(
+                    filename,
+                    dataset_id,
+                    repo_type="dataset",
+                    revision=revision,
+                    commit_message="Delete loading script auxiliary file",
+                )
+        if data_files:
+            for filename in data_files:
+                hf_api.delete_file(
+                    filename,
+                    dataset_id,
+                    repo_type="dataset",
+                    revision=revision,
+                    commit_message="Delete data file",
+                )
