4d92856bbfda0d48d07e82bb520d9434d20fae4b
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-05-16 16:38:22 +0200

    Create function to convert to parquet (#6878)
    * Create Python function delete_from_hub
    
    * Fix typo in docs
    
    * Replace config with datasets.config to avoid confusion
    
    * Pass datasets.config.HF_ENDPOINT to HfApi
    
    * Replace create_branch with api.create_branch
    
    * Update commit message for adding config data files
    
    * Test convert_to_parquet
    
    * Fix style for Python 3.8
    
    * Do not raise exception if no rights to create script branch

--- src/datasets/commands/convert_to_parquet.py
+++ src/datasets/commands/convert_to_parquet.py
@@ -1,12 +1,8 @@
-import time
 from argparse import ArgumentParser
 from typing import Optional
 
-from huggingface_hub import HfApi, create_branch, get_repo_discussions
-from huggingface_hub.utils import HfHubHTTPError
-
-from datasets import get_dataset_config_names, get_dataset_default_config_name, load_dataset
 from datasets.commands import BaseDatasetsCLICommand
+from datasets.hub import convert_to_parquet
 
 
 def _command_factory(args):
@@ -45,118 +41,6 @@ class ConvertToParquetCommand(BaseDatasetsCLICommand):
         self._trust_remote_code = trust_remote_code
 
     def run(self) -> None:
-        dataset_id = self._dataset_id
-        token = self._token
-        revision = self._revision
-        trust_remote_code = self._trust_remote_code
-        print(f"{dataset_id}")
-        configs = get_dataset_config_names(
-            dataset_id, token=token, revision=revision, trust_remote_code=trust_remote_code
+        _ = convert_to_parquet(
+            self._dataset_id, revision=self._revision, token=self._token, trust_remote_code=self._trust_remote_code
         )
-        print(f"{configs = }")
-        default_config = get_dataset_default_config_name(
-            dataset_id, token=token, revision=revision, trust_remote_code=trust_remote_code
-        )
-        print(f"{default_config = }")
-        if default_config:
-            config = default_config
-            configs.remove(default_config)
-        else:
-            config = configs.pop(0)
-        print(f"{config = }")
-        dataset = load_dataset(dataset_id, config, revision=revision, trust_remote_code=trust_remote_code)
-        commit_info = dataset.push_to_hub(
-            dataset_id,
-            config_name=config,
-            commit_message="Convert dataset to Parquet",
-            commit_description="Convert dataset to Parquet.",
-            create_pr=True,
-            token=token,
-            set_default=default_config is not None,
-        )
-        time.sleep(5)
-        if commit_info:
-            pr_revision, pr_url = commit_info.pr_revision, commit_info.pr_url
-        else:
-            pr_revision, pr_url = infer_pr(dataset_id, token=token)
-        for config in configs:
-            print(f"{config = }")
-            dataset = load_dataset(dataset_id, config, revision=revision, trust_remote_code=trust_remote_code)
-            dataset.push_to_hub(
-                dataset_id,
-                config_name=config,
-                commit_message=f"Add {config} data files",
-                revision=pr_revision,
-                token=token,
-            )
-            time.sleep(5)
-        delete_files(dataset_id, revision=pr_revision, token=token)
-        if not revision:
-            try:
-                create_branch(dataset_id, branch="script", repo_type="dataset", token=token, exist_ok=True)
-            except HfHubHTTPError:
-                pass
-        print(f"You can find your PR to convert the dataset to Parquet at: {pr_url}")
-
-
-def infer_pr(dataset_id, token=None):
-    discussions = get_repo_discussions(dataset_id, repo_type="dataset", token=token)
-    prs = [discussion for discussion in discussions if discussion.is_pull_request and discussion.status == "open"]
-    pr = sorted(prs, key=lambda pr: pr.num)[-1]
-    return pr.git_reference, pr.url
-
-
-def delete_files(dataset_id, revision=None, token=None):
-    dataset_name = dataset_id.split("/")[-1]
-    hf_api = HfApi(token=token)
-    repo_files = hf_api.list_repo_files(
-        dataset_id,
-        repo_type="dataset",
-    )
-    if repo_files:
-        legacy_json_file = []
-        python_files = []
-        data_files = []
-        for filename in repo_files:
-            if filename in {".gitattributes", "README.md"}:
-                continue
-            elif filename == f"{dataset_name}.py":
-                hf_api.delete_file(
-                    filename,
-                    dataset_id,
-                    repo_type="dataset",
-                    revision=revision,
-                    commit_message="Delete loading script",
-                )
-            elif filename == "dataset_infos.json":
-                legacy_json_file.append(filename)
-            elif filename.endswith(".py"):
-                python_files.append(filename)
-            else:
-                data_files.append(filename)
-        if legacy_json_file:
-            hf_api.delete_file(
-                "dataset_infos.json",
-                dataset_id,
-                repo_type="dataset",
-                revision=revision,
-                commit_message="Delete legacy dataset_infos.json",
-            )
-        if python_files:
-            for filename in python_files:
-                hf_api.delete_file(
-                    filename,
-                    dataset_id,
-                    repo_type="dataset",
-                    revision=revision,
-                    commit_message="Delete loading script auxiliary file",
-                )
-        if data_files:
-            for filename in data_files:
-                hf_api.delete_file(
-                    filename,
-                    dataset_id,
-                    repo_type="dataset",
-                    revision=revision,
-                    commit_message="Delete data file",
-                )
