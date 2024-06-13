574791e0a0cf57ba761f679a054b9e89e4a3ee22
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-06-13 16:59:56 +0200

    Set dev version (#6970)

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "2.20.0"
+__version__ = "2.20.1.dev0"
 
 from .arrow_dataset import Dataset
 from .arrow_reader import ReadInstruction
