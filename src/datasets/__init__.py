98fdc9e78e6d057ca66e58a37f49d6618aab8130
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-06-13 16:55:53 +0200

    Release: 2.20.0 (#6969)

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "2.19.3.dev0"
+__version__ = "2.20.0"
 
 from .arrow_dataset import Dataset
 from .arrow_reader import ReadInstruction
