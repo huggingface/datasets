5c532a94e6e205c8a9516f5aa309686233e05a00
Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>
2024-06-19 13:05:33 +0200

    make style

--- src/datasets/formatting/formatting.py
+++ src/datasets/formatting/formatting.py
@@ -194,10 +194,10 @@ class NumpyArrowExtractor(BaseArrowExtractor[dict, np.ndarray, dict]):
                 or (isinstance(x, float) and np.isnan(x))
                 for x in array
             ):
-                if np.lib.NumpyVersion(np.__version__) >= '2.0.0b1':
+                if np.lib.NumpyVersion(np.__version__) >= "2.0.0b1":
                     return np.asarray(array, dtype=object)
                 return np.array(array, copy=False, dtype=object)
-        if np.lib.NumpyVersion(np.__version__) >= '2.0.0b1':
+        if np.lib.NumpyVersion(np.__version__) >= "2.0.0b1":
             return np.asarray(array)
         else:
             return np.array(array, copy=False)
