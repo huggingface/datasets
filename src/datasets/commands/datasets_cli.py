5d4687e4ee1dba2b14e59117492d7f75ea66800a
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-06-25 08:43:20 +0200

    Remove deprecated DummyDataCommand

--- src/datasets/commands/datasets_cli.py
+++ src/datasets/commands/datasets_cli.py
@@ -4,7 +4,6 @@ from argparse import ArgumentParser
 from datasets.commands.convert import ConvertCommand
 from datasets.commands.convert_to_parquet import ConvertToParquetCommand
 from datasets.commands.delete_from_hub import DeleteFromHubCommand
-from datasets.commands.dummy_data import DummyDataCommand
 from datasets.commands.env import EnvironmentCommand
 from datasets.commands.run_beam import RunBeamCommand
 from datasets.commands.test import TestCommand
@@ -27,7 +26,6 @@ def main():
     EnvironmentCommand.register_subcommand(commands_parser)
     TestCommand.register_subcommand(commands_parser)
     RunBeamCommand.register_subcommand(commands_parser)
-    DummyDataCommand.register_subcommand(commands_parser)
     ConvertToParquetCommand.register_subcommand(commands_parser)
     DeleteFromHubCommand.register_subcommand(commands_parser)
 
