3dd82cef7ae092ca9e15747fd499176ef379d52f
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-06-25 08:53:20 +0200

    Remove deprecated extract_path_from_uri

--- src/datasets/filesystems/__init__.py
+++ src/datasets/filesystems/__init__.py
@@ -26,22 +26,6 @@ for fs_class in COMPRESSION_FILESYSTEMS:
     fsspec.register_implementation(fs_class.protocol, fs_class, clobber=True)
 
 
-@deprecated(
-    "This function is deprecated and will be removed in a future version. Please use `fsspec.core.strip_protocol` instead."
-)
-def extract_path_from_uri(dataset_path: str) -> str:
-    """
-    Preprocesses `dataset_path` and removes remote filesystem (e.g. removing `s3://`).
-
-    Args:
-        dataset_path (`str`):
-            Path (e.g. `dataset/train`) or remote uri (e.g. `s3://my-bucket/dataset/train`) of the dataset directory.
-    """
-    if "://" in dataset_path:
-        dataset_path = dataset_path.split("://")[1]
-    return dataset_path
-
-
 def is_remote_filesystem(fs: fsspec.AbstractFileSystem) -> bool:
     """
     Checks if `fs` is a remote filesystem.
