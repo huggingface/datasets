7a6e4ac3de40a52279d25e7e0e6739df2cbc023e
coyotte508 <coyotte508@gmail.com>
2024-07-26 15:44:29 +0200

    🧪 Do not mock create_commit

--- tests/test_hub.py
+++ tests/test_hub.py
@@ -57,81 +57,14 @@ def test_convert_to_parquet(temporary_repo, hf_api, hf_token, ci_hub_config, ci_
             repo_id=repo_id,
             repo_type="dataset",
         )
-        commit_info = SimpleNamespace(
-            pr_revision="refs/pr/1",  # "main", #
-            pr_url="https:///hub-ci.huggingface.co/datasets/__DUMMY_USER__/__DUMMY_DATASET__/refs%2Fpr%2F1",
-        )
-        with patch.object(datasets.hub.HfApi, "create_commit", return_value=commit_info) as mock_create_commit:
-            with patch.object(datasets.hub.HfApi, "create_branch") as mock_create_branch:
-                with patch.object(datasets.hub.HfApi, "list_repo_tree", return_value=[]):  # not needed
-                    _ = convert_to_parquet(repo_id, token=hf_token, trust_remote_code=True)
+        with patch.object(datasets.hub.HfApi, "create_branch") as mock_create_branch:
+            with patch.object(datasets.hub.HfApi, "list_repo_tree", return_value=[]):  # not needed
+                _ = convert_to_parquet(repo_id, token=hf_token, trust_remote_code=True)
     # mock_create_branch
     assert mock_create_branch.called
     assert mock_create_branch.call_count == 2
     for call_args, expected_branch in zip(mock_create_branch.call_args_list, ["refs/pr/1", "script"]):
         assert call_args.kwargs.get("branch") == expected_branch
-    # mock_create_commit
-    assert mock_create_commit.called
-    assert mock_create_commit.call_count == 2
-    expected_readmes = [
-        dedent(f"""\
-        ---
-        dataset_info:
-          config_name: first
-          features:
-          - name: text
-            dtype: string
-          splits:
-          - name: train
-            num_bytes: 55
-            num_examples: 5
-          download_size: 790
-          dataset_size: 55
-        {METADATA_CONFIGS_FIELD}:
-        - config_name: first
-          data_files:
-          - split: train
-            path: first/train-*
-          default: true
-        ---
-        """),
-        dedent(f"""\
-        ---
-        dataset_info:
-          config_name: second
-          features:
-          - name: text
-            dtype: string
-          splits:
-          - name: train
-            num_bytes: 60
-            num_examples: 5
-          download_size: 798
-          dataset_size: 60
-        {METADATA_CONFIGS_FIELD}:
-        - config_name: second
-          data_files:
-          - split: train
-            path: second/train-*
-        ---
-        """),
-    ]
-    for call_args, expected_commit_message, expected_create_pr, expected_readme, expected_parquet_path_in_repo in zip(
-        mock_create_commit.call_args_list,
-        ["Convert dataset to Parquet", "Add 'second' config data files"],
-        [True, False],
-        expected_readmes,
-        ["first/train-00000-of-00001.parquet", "second/train-00000-of-00001.parquet"],
-    ):
-        assert call_args.kwargs.get("commit_message") == expected_commit_message
-        assert call_args.kwargs.get("create_pr") is expected_create_pr
-        operations = call_args.kwargs.get("operations")
-        assert len(operations) == 2
-        for operation in operations:
-            if operation.path_in_repo == "README.md":
-                assert operation.path_or_fileobj.decode() == expected_readme
-            else:
-                assert operation.path_in_repo == expected_parquet_path_in_repo
 
 
 def test_delete_from_hub(temporary_repo, hf_api, hf_token, csv_path, ci_hub_config, ci_hfh_hf_hub_url) -> None:
