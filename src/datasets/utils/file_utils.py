baea190799dfa22493621fe06584b006b57f16ce
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-07-29 08:56:15 +0200

    Fix prepare_single_hop_path_and_storage_options (#7068)
    * Transform all HF HTTP URLs to HF protocol
    
    * Fix test URL
    
    * Remove HF headers for non-HF HTTP URLs
    
    * Fix for HTTP storage_options without 'headers'
    
    * Remove unused cookies
    
    * Refactor
    
    * Refactor list to set to check membership
    
    * Refactor to add protocol key to storage_options only at the end
    
    * Fix overwriting storage_options nested values
    
    * Add tests
    
    * Revert "Transform all HF HTTP URLs to HF protocol"
    
    This reverts commit a337212b7ab137950c82a12e6e8d52c7b065fe53.
    
    * Test that DownloadConfig.storage_options are not modified
    
    * Fix so DownloadConfig.storage_options are not modified
    
    * Refactor fix
    
    * Test also GitHub URL
    
    * Fix DownloadConfig.storage_options for GitHub URL

--- src/datasets/utils/file_utils.py
+++ src/datasets/utils/file_utils.py
@@ -1150,7 +1150,7 @@ def _prepare_single_hop_path_and_storage_options(
         urlpath = "hf://" + urlpath[len(config.HF_ENDPOINT) + 1 :].replace("/resolve/", "@", 1)
     protocol = urlpath.split("://")[0] if "://" in urlpath else "file"
     if download_config is not None and protocol in download_config.storage_options:
-        storage_options = download_config.storage_options[protocol]
+        storage_options = download_config.storage_options[protocol].copy()
     elif download_config is not None and protocol not in download_config.storage_options:
         storage_options = {
             option_name: option_value
@@ -1159,40 +1159,34 @@ def _prepare_single_hop_path_and_storage_options(
         }
     else:
         storage_options = {}
-    if storage_options:
-        storage_options = {protocol: storage_options}
-    if protocol in ["http", "https"]:
-        storage_options[protocol] = {
-            "headers": {
-                **get_authentication_headers_for_url(urlpath, token=token),
-                "user-agent": get_datasets_user_agent(),
-            },
-            "client_kwargs": {"trust_env": True},  # Enable reading proxy env variables.
-            **(storage_options.get(protocol, {})),
-        }
+    if protocol in {"http", "https"}:
+        client_kwargs = storage_options.pop("client_kwargs", {})
+        storage_options["client_kwargs"] = {"trust_env": True, **client_kwargs}  # Enable reading proxy env variables
         if "drive.google.com" in urlpath:
             response = http_head(urlpath)
-            cookies = None
             for k, v in response.cookies.items():
                 if k.startswith("download_warning"):
                     urlpath += "&confirm=" + v
                     cookies = response.cookies
-                    storage_options[protocol] = {"cookies": cookies, **storage_options.get(protocol, {})}
-        # Fix Google Drive URL to avoid Virus scan warning
-        if "drive.google.com" in urlpath and "confirm=" not in urlpath:
-            urlpath += "&confirm=t"
+                    storage_options = {"cookies": cookies, **storage_options}
+            # Fix Google Drive URL to avoid Virus scan warning
+            if "confirm=" not in urlpath:
+                urlpath += "&confirm=t"
         if urlpath.startswith("https://raw.githubusercontent.com/"):
             # Workaround for served data with gzip content-encoding: https://github.com/fsspec/filesystem_spec/issues/389
-            storage_options[protocol]["headers"]["Accept-Encoding"] = "identity"
+            headers = storage_options.pop("headers", {})
+            storage_options["headers"] = {"Accept-Encoding": "identity", **headers}
     elif protocol == "hf":
-        storage_options[protocol] = {
+        storage_options = {
             "token": token,
             "endpoint": config.HF_ENDPOINT,
-            **storage_options.get(protocol, {}),
+            **storage_options,
         }
         # streaming with block_size=0 is only implemented in 0.21 (see https://github.com/huggingface/huggingface_hub/pull/1967)
         if config.HF_HUB_VERSION < version.parse("0.21.0"):
-            storage_options[protocol]["block_size"] = "default"
+            storage_options["block_size"] = "default"
+    if storage_options:
+        storage_options = {protocol: storage_options}
     return urlpath, storage_options
 
 
