1c9870dfff2a5b22ebdae960182e5b07f28c7ad5
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-07-30 12:51:01 +0200

    Fix push_to_hub by not calling create_branch if branch exists (#7069)
    * Fix push_to_hub by not calling create_branch if branch exists
    
    * Fix push_to_hub by not calling create_branch if branch exists
    
    * Reword comment
    
    * Fix push_to_hub by not calling create_branch if PR ref
    
    * Update test

--- src/datasets/dataset_dict.py
+++ src/datasets/dataset_dict.py
@@ -1708,7 +1708,8 @@ class DatasetDict(dict):
         )
         repo_id = repo_url.repo_id
 
-        if revision is not None:
+        if revision is not None and not revision.startswith("refs/pr/"):
+            # We do not call create_branch for a PR reference: 400 Bad Request
             api.create_branch(repo_id, branch=revision, token=token, repo_type="dataset", exist_ok=True)
 
         if not data_dir:
