97e4f6a6c6d6cc738eac6090f564d59a6f3d5330
Patrick Smyth <psmyth1994@gmail.com>
2024-08-15 12:05:22 -0500

    add polars as an updatable type

--- src/datasets/arrow_dataset.py
+++ src/datasets/arrow_dataset.py
@@ -3439,7 +3439,12 @@ class Dataset(DatasetInfoMixin, IndexableMixin, TensorflowDatasetMixin):
                 returned_lazy_dict = False
             if update_data is None:
                 # Check if the function returns updated examples
-                update_data = isinstance(processed_inputs, (Mapping, pa.Table, pd.DataFrame))
+                updatable_types = (Mapping, pa.Table, pd.DataFrame)
+                if config.POLARS_AVAILABLE and "polars" in sys.modules:
+                    import polars as pl
+
+                    updatable_types += (pl.DataFrame,)
+                update_data = isinstance(processed_inputs, updatable_types)
                 validate_function_output(processed_inputs, indices)
             if not update_data:
                 return None  # Nothing to update, let's move on
