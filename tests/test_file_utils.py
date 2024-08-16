35b67a227d518f6f92f1e921edb3f2ccaccf496a
Quentin Lhoest <lhoest.q@gmail.com>
2024-08-16 17:09:09 +0200

    style

--- tests/test_file_utils.py
+++ tests/test_file_utils.py
@@ -1,8 +1,7 @@
 import os
 import re
-from dataclasses import dataclass, field
 from pathlib import Path
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 
 import pytest
 import zstandard as zstd
