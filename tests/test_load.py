04e471f48dc0b7db079aa230420ec2c077f7b338
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-08-26 07:15:19 +0200

    Fix wrong SHA in test_HubDatasetModuleFactoryWithParquetExport_errors_on_wrong_sha

--- tests/test_load.py
+++ tests/test_load.py
@@ -802,7 +802,7 @@ class ModuleFactoryTest(TestCase):
         factory = HubDatasetModuleFactoryWithParquetExport(
             SAMPLE_DATASET_IDENTIFIER,
             download_config=self.download_config,
-            revision="1a21ac5846fc3f36ad5f128740c58932d3d7806f",
+            revision="0e1cee81e718feadf49560b287c4eb669c2efb1a",
         )
         factory.get_module()
         factory = HubDatasetModuleFactoryWithParquetExport(
