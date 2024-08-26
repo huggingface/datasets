bafa80b51b1135e0b9355bdc62a326bde5024115
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-08-26 07:08:47 +0200

    Fix wrong SHA in test_HubDatasetModuleFactoryWithParquetExport

--- tests/test_load.py
+++ tests/test_load.py
@@ -790,10 +790,10 @@ class ModuleFactoryTest(TestCase):
         )
         assert module_factory_result.builder_configs_parameters.builder_configs[0].data_files == {
             "train": [
-                "hf://datasets/hf-internal-testing/dataset_with_script@0c97cd1168f31e683059ddcf0703e3f45d9007c4/default/train/0000.parquet"
+                "hf://datasets/hf-internal-testing/dataset_with_script@3d8a0e457ddb581301fb13f60d38906c9cdcaf1c/default/train/0000.parquet"
             ],
             "validation": [
-                "hf://datasets/hf-internal-testing/dataset_with_script@0c97cd1168f31e683059ddcf0703e3f45d9007c4/default/validation/0000.parquet"
+                "hf://datasets/hf-internal-testing/dataset_with_script@3d8a0e457ddb581301fb13f60d38906c9cdcaf1c/default/validation/0000.parquet"
             ],
         }
 
