d0a289b1282260b553ebc2217f023aa768ab05f0
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-09-23 10:10:14 +0200

    Support cast_array_to_features with struct with missing fields

--- src/datasets/table.py
+++ src/datasets/table.py
@@ -2000,10 +2000,14 @@ def cast_array_to_feature(
             sequence_kwargs = vars(feature).copy()
             feature = sequence_kwargs.pop("feature")
             feature = {name: Sequence(subfeature, **sequence_kwargs) for name, subfeature in feature.items()}
-        if isinstance(feature, dict) and {field.name for field in array.type} == set(feature):
+        if isinstance(feature, dict) and (array_fields := {field.name for field in array.type}) <= set(feature):
             if array.type.num_fields == 0:
                 return array
-            arrays = [_c(array.field(name), subfeature) for name, subfeature in feature.items()]
+            null_array = pa.array([None] * len(array))
+            arrays = [
+                _c(array.field(name) if name in array_fields else null_array, subfeature)
+                for name, subfeature in feature.items()
+            ]
             return pa.StructArray.from_arrays(arrays, names=list(feature), mask=array.is_null())
     elif pa.types.is_list(array.type) or pa.types.is_large_list(array.type):
         # feature must be either [subfeature] or LargeList(subfeature) or Sequence(subfeature)
