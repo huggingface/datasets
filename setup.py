33af4cfe07c7c6fe0e5633a780a4b7407b0bff27
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-09-27 10:54:52 +0200

    Support and test Python 3.11

--- setup.py
+++ setup.py
@@ -264,6 +264,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     keywords="datasets machine learning datasets",
