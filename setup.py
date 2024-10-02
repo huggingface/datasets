6cdfbfe09752be83a72bc003f82c62f08ed63eec
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-10-02 07:38:24 +0200

    Pin multiprocess<0.70.1 to align with dill<0.3.9

--- setup.py
+++ setup.py
@@ -126,7 +126,7 @@ REQUIRED_PKGS = [
     # for fast hashing
     "xxhash",
     # for better multiprocessing
-    "multiprocess",
+    "multiprocess<0.70.17",  # to align with dill<0.3.9 (see above)
     # to save datasets locally or on any filesystem
     # minimum 2023.1.0 to support protocol=kwargs in fsspec's `open`, `get_fs_token_paths`, etc.: see https://github.com/fsspec/filesystem_spec/pull/1143
     "fsspec[http]>=2023.1.0,<=2024.6.1",
