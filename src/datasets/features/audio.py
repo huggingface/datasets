efdc2115926d0e6ac79dc503b35a8f9b73644625
Fawaz Ahmed <fawazahmed0@hotmail.com>
2024-11-04 23:19:05 +0530

    make sure path is not none

--- src/datasets/features/audio.py
+++ src/datasets/features/audio.py
@@ -166,7 +166,7 @@ class Audio:
                 "Decoding 'mp3' files requires system library 'libsndfile'>=1.1.0, "
                 'You can try to update `soundfile` python library: `pip install "soundfile>=0.12.1"`. '
             )
-        if os.path.exists(path):
+        if path is not None and os.path.exists(path):
             array, sampling_rate = sf.read(path)
         elif file is None:
             token_per_repo_id = token_per_repo_id or {}
