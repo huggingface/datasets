5915d016afef5de53ac688a1f7c2e02918eeeb98
Ferdinand Schlatt <ferdinand.schlatt@uni-jena.de>
2024-12-21 13:32:45 +0100

    use relpath to resolve symlink

--- src/datasets/data_files.py
+++ src/datasets/data_files.py
@@ -385,8 +385,9 @@ def resolve_pattern(
         glob_kwargs["expand_info"] = False
     matched_paths = [
         filepath if filepath.startswith(protocol_prefix) else protocol_prefix + filepath
-        for filepath in fs.glob(pattern, **glob_kwargs).items()
-        if (xbasename(filepath) not in files_to_ignore)
+        for filepath, info in fs.glob(pattern, detail=True, **glob_kwargs).items()
+        if (info["type"] == "file" or (info.get("islink") and os.path.isfile(os.path.realpath(filepath))))
+        and (xbasename(filepath) not in files_to_ignore)
         and not _is_inside_unrequested_special_dir(filepath, fs_pattern)
         and not _is_unrequested_hidden_file_or_is_inside_unrequested_hidden_dir(filepath, fs_pattern)
     ]  # ignore .ipynb and __pycache__, but keep /../
