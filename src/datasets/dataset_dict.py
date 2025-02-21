b6b1642f51fa8cee00a477e4b6be3f8fb4558506
jp1924 <jp42maru@gmail.com>
2025-02-21 16:44:17 +0900

    rm breakpoint

--- src/datasets/dataset_dict.py
+++ src/datasets/dataset_dict.py
@@ -907,7 +907,6 @@ class DatasetDict(dict):
 
             class bind(partial):
                 def __call__(self, *fn_args, **fn_kwargs):
-                    breakpoint()
                     return self.func(*fn_args, *self.args, **fn_kwargs)
 
             function = bind(function, with_split)
