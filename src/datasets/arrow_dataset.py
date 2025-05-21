a1e48e022b0b3ffc69fca2a6003a0a70e2da5d71
Christopher Akiki <christopher.akiki@gmail.com>
2025-05-21 15:21:38 +0200

    [MINOR:TYPO] Update arrow_dataset.py
    r/hub/filesystem in save_to_disk

--- src/datasets/arrow_dataset.py
+++ src/datasets/arrow_dataset.py
@@ -1443,7 +1443,7 @@ class Dataset(DatasetInfoMixin, IndexableMixin, TensorflowDatasetMixin):
                 Path (e.g. `dataset/train`) or remote URI (e.g. `s3://my-bucket/dataset/train`)
                 of the dataset directory where the dataset will be saved to.
             max_shard_size (`int` or `str`, *optional*, defaults to `"500MB"`):
-                The maximum size of the dataset shards to be uploaded to the hub. If expressed as a string, needs to be digits followed by a unit
+                The maximum size of the dataset shards to be saved to the filesystem. If expressed as a string, needs to be digits followed by a unit
                 (like `"50MB"`).
             num_shards (`int`, *optional*):
                 Number of shards to write. By default the number of shards depends on `max_shard_size` and `num_proc`.
