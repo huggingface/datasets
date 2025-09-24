9efbb0297cf986ef88b60e3be0a242362c8cf447
Wauplin <lucainp@gmail.com>
2025-09-24 14:54:08 +0200

    offline

--- tests/test_offline_util.py
+++ tests/test_offline_util.py
@@ -2,6 +2,7 @@ from tempfile import NamedTemporaryFile
 
 import httpx
 import pytest
+import requests
 from huggingface_hub.errors import OfflineModeIsEnabled
 
 from datasets.utils.file_utils import fsspec_get, fsspec_head
@@ -14,10 +15,16 @@ from .utils import OfflineSimulationMode, RequestWouldHangIndefinitelyError, off
 def test_offline_with_timeout():
     with offline(OfflineSimulationMode.CONNECTION_TIMES_OUT):
         with pytest.raises(RequestWouldHangIndefinitelyError):
+            requests.request("GET", "https://huggingface.co")
+        with pytest.raises(RequestWouldHangIndefinitelyError):
             httpx.request("GET", "https://huggingface.co")
+
+        with pytest.raises(requests.exceptions.Timeout):
+            requests.request("GET", "https://huggingface.co", timeout=1.0)
         with pytest.raises(httpx.ConnectTimeout):
             httpx.request("GET", "https://huggingface.co", timeout=1.0)
-        with pytest.raises(httpx.ConnectTimeout), NamedTemporaryFile() as temp_file:
+
+        with pytest.raises((requests.exceptions.Timeout, httpx.ConnectTimeout)), NamedTemporaryFile() as temp_file:
             fsspec_get("hf://dummy", temp_file=temp_file)
 
 
@@ -25,9 +32,12 @@ def test_offline_with_timeout():
 @require_not_windows  # fsspec get keeps a file handle on windows that raises PermissionError
 def test_offline_with_connection_error():
     with offline(OfflineSimulationMode.CONNECTION_FAILS):
+        with pytest.raises(requests.exceptions.Timeout):
+            requests.request("GET", "https://huggingface.co")
         with pytest.raises(httpx.ConnectError):
             httpx.request("GET", "https://huggingface.co")
-        with pytest.raises(httpx.ConnectError), NamedTemporaryFile() as temp_file:
+
+        with pytest.raises((requests.exceptions.Timeout, httpx.ConnectError)), NamedTemporaryFile() as temp_file:
             fsspec_get("hf://dummy", temp_file=temp_file)
 
 
