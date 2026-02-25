67d93a96b7efc656d183636175f29d7bc211d1b5
Quentin Lhoest <lhoest.q@gmail.com>
2026-02-25 13:18:03 +0100

    set dev version

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "4.6.0"
+__version__ = "4.6.1.dev0"
 
 from .arrow_dataset import Column, Dataset
 from .arrow_reader import ReadInstruction
