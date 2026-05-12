a821ddcd68ae5147d3cd78f51b6232967aab9bb3
Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>
2026-05-12 11:44:25 +0200

    fix parquet reshard (#8193)
    * fix parquet reshard
    
    * add test

--- tests/packaged_modules/test_parquet.py
+++ tests/packaged_modules/test_parquet.py
@@ -1,5 +1,6 @@
 import pytest
 
+from datasets import load_dataset
 from datasets.builder import InvalidConfigName
 from datasets.data_files import DataFilesList
 from datasets.packaged_modules.parquet.parquet import ParquetConfig
@@ -14,3 +15,12 @@ def test_config_raises_when_invalid_name() -> None:
 def test_config_raises_when_invalid_data_files(data_files) -> None:
     with pytest.raises(ValueError, match="Expected a DataFilesDict"):
         _ = ParquetConfig(name="name", data_files=data_files)
+
+
+def test_parquet_reshard(multi_row_groups_parquet_path):
+    ds = load_dataset("parquet", data_files=multi_row_groups_parquet_path, split="train", streaming=True)
+    assert ds.num_shards == 1
+    expected = list(ds)
+    resharded_ds = ds.reshard()
+    assert resharded_ds.num_shards == 4
+    assert list(resharded_ds) == expected
