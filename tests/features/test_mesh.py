688514ae09004d259b5b2717d432242871068f2f
Vinay-Umrethe <vinayumrethe99@gmail.com>
2026-05-27 22:00:19 +0530

    style: Match other test_features

--- tests/features/test_mesh.py
+++ tests/features/test_mesh.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 import pyarrow as pa
@@ -39,40 +38,43 @@ def test_mesh_feature_type_to_arrow():
         lambda mesh_path: {"bytes": open(mesh_path, "rb").read()},
     ],
 )
-def test_mesh_feature_encode_example(mesh_file, build_example):
+def test_mesh_feature_encode_example(shared_datadir, build_example):
+    mesh_path = str(shared_datadir / "test_mesh_glb.glb")
     mesh = Mesh()
-    encoded_example = mesh.encode_example(build_example(mesh_file))
+    encoded_example = mesh.encode_example(build_example(mesh_path))
     assert isinstance(encoded_example, dict)
     assert encoded_example.keys() == {"bytes", "path"}
     assert encoded_example["bytes"] is not None or encoded_example["path"] is not None
 
 
 @require_trimesh
-def test_mesh_decode_example(mesh_file):
+def test_mesh_decode_example(shared_datadir):
     import trimesh
 
+    mesh_path = str(shared_datadir / "test_mesh_glb.glb")
     mesh = Mesh()
-    with open(mesh_file, "rb") as f:
+    with open(mesh_path, "rb") as f:
         mesh_bytes = f.read()
 
-    decoded_example = mesh.decode_example({"path": mesh_file, "bytes": None})
+    decoded_example = mesh.decode_example({"path": mesh_path, "bytes": None})
     assert isinstance(decoded_example, (trimesh.Trimesh, trimesh.Scene))
 
-    decoded_example = mesh.decode_example({"path": mesh_file, "bytes": mesh_bytes})
+    decoded_example = mesh.decode_example({"path": mesh_path, "bytes": mesh_bytes})
     assert isinstance(decoded_example, (trimesh.Trimesh, trimesh.Scene))
 
     with pytest.raises(ValueError, match="requires a 'path' value"):
         mesh.decode_example({"path": None, "bytes": mesh_bytes})
 
     with pytest.raises(RuntimeError):
-        Mesh(decode=False).decode_example({"path": mesh_file, "bytes": None})
+        Mesh(decode=False).decode_example({"path": mesh_path, "bytes": None})
 
 
 @require_trimesh
-def test_dataset_with_mesh_feature(mesh_file):
+def test_dataset_with_mesh_feature(shared_datadir):
     import trimesh
 
-    data = {"mesh": [mesh_file]}
+    mesh_path = str(shared_datadir / "test_mesh_glb.glb")
+    data = {"mesh": [mesh_path]}
     features = Features({"mesh": Mesh()})
     dset = Dataset.from_dict(data, features=features)
     item = dset[0]
@@ -91,31 +93,34 @@ def test_dataset_with_mesh_feature(mesh_file):
     assert isinstance(column[0], (trimesh.Trimesh, trimesh.Scene))
 
 
-def test_dataset_with_mesh_feature_decode_false(mesh_file):
-    data = {"mesh": [mesh_file]}
+def test_dataset_with_mesh_feature_decode_false(shared_datadir):
+    mesh_path = str(shared_datadir / "test_mesh_glb.glb")
+    data = {"mesh": [mesh_path]}
     features = Features({"mesh": Mesh(decode=False)})
     dset = Dataset.from_dict(data, features=features)
     item = dset[0]
     assert item.keys() == {"mesh"}
     assert isinstance(item["mesh"], dict)
-    assert item["mesh"]["path"] == mesh_file
+    assert item["mesh"]["path"] == mesh_path
 
 
 @require_trimesh
-def test_dataset_cast_to_mesh_features(mesh_file):
+def test_dataset_cast_to_mesh_features(shared_datadir):
     import trimesh
 
-    data = {"mesh": [mesh_file]}
+    mesh_path = str(shared_datadir / "test_mesh_glb.glb")
+    data = {"mesh": [mesh_path]}
     dset = Dataset.from_dict(data)
     dset = dset.cast(Features({"mesh": Mesh()}))
     item = dset[0]
     assert isinstance(item["mesh"], (trimesh.Trimesh, trimesh.Scene))
 
 
-def test_dataset_concatenate_mesh_features(mesh_file):
-    data1 = {"mesh": [mesh_file]}
+def test_dataset_concatenate_mesh_features(shared_datadir):
+    mesh_path = str(shared_datadir / "test_mesh_glb.glb")
+    data1 = {"mesh": [mesh_path]}
     dset1 = Dataset.from_dict(data1, features=Features({"mesh": Mesh(decode=False)}))
-    with open(mesh_file, "rb") as f:
+    with open(mesh_path, "rb") as f:
         data2 = {"mesh": [{"bytes": f.read()}]}
     dset2 = Dataset.from_dict(data2, features=Features({"mesh": Mesh(decode=False)}))
     concatenated_dataset = concatenate_datasets([dset1, dset2])
@@ -141,15 +146,14 @@ def test_require_decoding():
     assert require_decoding(Mesh())
 
 
-def test_mesh_embed_storage(mesh_file):
-    features = Features({"mesh": Mesh()})
+def test_mesh_embed_storage(shared_datadir):
+    mesh_path = str(shared_datadir / "test_mesh_glb.glb")
+    example = {"bytes": None, "path": mesh_path}
+    storage = pa.array([example], type=pa.struct({"bytes": pa.binary(), "path": pa.string()}))
+    embedded_storage = Mesh().embed_storage(storage)
+    embedded_example = embedded_storage.to_pylist()[0]
+    assert embedded_example == {"bytes": open(mesh_path, "rb").read(), "path": "test_mesh_glb.glb"}
 
-    with open(mesh_file, "rb") as f:
-        content = f.read()
-
-    # Test bytes are embedded
-    storage = features["mesh"].embed_storage(pa.array([{"path": mesh_file, "bytes": None}]))
-    assert storage.to_pylist() == [{"path": os.path.basename(mesh_file), "bytes": content}]
-
-    storage = features["mesh"].embed_storage(pa.array([{"path": mesh_file, "bytes": None}]), local_files=False)
-    assert storage.to_pylist() == [{"path": mesh_file, "bytes": None}]
+    non_embedded_storage = Mesh().embed_storage(storage, local_files=False)
+    non_embedded_example = non_embedded_storage.to_pylist()[0]
+    assert non_embedded_example == {"bytes": None, "path": mesh_path}
